12c2bf716cc91e7cd4d04eaf9739d269b96b5299
richard.eckart@gmail.com <richard.eckart@gmail.com@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2011-01-07 17:17:37 +0000

    Added missing @Override annotations.

--- de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.io.xml/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/xml/XmlReader.java
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.io.xml/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/xml/XmlReader.java
@@ -2,13 +2,13 @@
  * Copyright 2010
  * Ubiquitous Knowledge Processing (UKP) Lab
  * Technische Universität Darmstadt
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -178,6 +178,7 @@ public class XmlReader extends CasCollectionReader_ImplBase {
 		}
 	}
 
+	@Override
 	public void getNext(CAS aCAS)
 		throws IOException, CollectionException
 	{
@@ -220,12 +221,14 @@ public class XmlReader extends CasCollectionReader_ImplBase {
 
 	}
 
+	@Override
 	public Progress[] getProgress()
 	{
 		return new Progress[] { new ProgressImpl(currentParsedFile, xmlFiles
 				.size(), Progress.ENTITIES) };
 	}
 
+	@Override
 	public boolean hasNext()
 		throws IOException, CollectionException
 	{
