718fd11914358a5197baaea8d73bb799523d89d5
Richard Eckart de Castilho <richard.eckart@gmail.com>
2011-01-20 18:45:55 +0000

    Updated JavaDoc.

---
+++ de.tudarmstadt.ukp.dkpro.core.stanfordnlp/src/main/java/de/tudarmstadt/ukp/dkpro/core/stanfordnlp/package-info.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Copyright 2010
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ ******************************************************************************/
+
+/**
+ * Integration of NLP components from the <a href="http://nlp.stanford.edu/software/corenlp.shtml">
+ * Stanford CoreNLP suite</a>.
+ *
+ * @since 1.1.0
+ */
+package de.tudarmstadt.ukp.dkpro.core.stanfordnlp;
