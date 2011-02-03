552fd98e5de2540325fe464d6ce0d87f7a4066b2
Oliver Ferschke <oliver.ferschke@gmail.com>
2011-02-03 19:47:42 +0000

    Added type to untyped helper method. Added javadoc.

--- de.tudarmstadt.ukp.dkpro.core.io.jwpl/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/jwpl/ExtendedWikipediaReader.java
+++ de.tudarmstadt.ukp.dkpro.core.io.jwpl/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/jwpl/ExtendedWikipediaReader.java
@@ -2,13 +2,13 @@
  * Copyright 2010
  * Ubiquitous Knowledge Processing (UKP) Lab
  * Technische Universität Darmstadt
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -513,10 +513,16 @@ public class ExtendedWikipediaReader
 				.getNumberOfPages()).intValue(), Progress.ENTITIES) };
 	}
 
-	private static String toOneLine(Collection coll)
+	/**
+	 * Returns a one-line String representation of the languages in the collection
+	 *
+	 * @param a collection of Languages objects
+	 * @return String represtations of all Language-Objects in one line
+	 */
+	private static String toOneLine(Collection<Language> coll)
 	{
 		StringBuffer buf = new StringBuffer();
-		Iterator it = coll.iterator();
+		Iterator<Language> it = coll.iterator();
 		buf.append(" | ");
 		while (it.hasNext()) {
 			Object o = it.next();
