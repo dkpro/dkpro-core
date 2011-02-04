59c680ea5745e575c8389c237685f47276f2c4d4
richard.eckart@gmail.com <richard.eckart@gmail.com@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2011-02-04 21:44:27 +0000

    Added parameter to lower-case tokens before passing them to the stemmer.

--- de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.snowball/src/test/java/de/tudarmstadt/ukp/dkpro/core/snowball/SnowballStemmerTest.java
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.snowball/src/test/java/de/tudarmstadt/ukp/dkpro/core/snowball/SnowballStemmerTest.java
@@ -2,13 +2,13 @@
  * Copyright 2010
  * Ubiquitous Knowledge Processing (UKP) Lab
  * Technische Universität Darmstadt
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,7 +19,6 @@ package de.tudarmstadt.ukp.dkpro.core.snowball;
 
 import static org.junit.Assert.assertTrue;
 import static org.uimafit.factory.AnalysisEngineFactory.createPrimitive;
-import static org.uimafit.factory.TypeSystemDescriptionFactory.createTypeSystemDescription;
 import static org.uimafit.util.JCasUtil.iterate;
 
 import org.apache.uima.analysis_engine.AnalysisEngine;
@@ -27,7 +26,6 @@ import org.apache.uima.jcas.JCas;
 import org.junit.Test;
 import org.uimafit.factory.JCasBuilder;
 
-import de.tudarmstadt.ukp.dkpro.core.api.featurepath.FeaturePathAnnotatorBase;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Stem;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 
@@ -37,9 +35,7 @@ public class SnowballStemmerTest
 	public void testGerman()
 		throws Exception
 	{
-		AnalysisEngine engine = createPrimitive(SnowballStemmer.class,
-				createTypeSystemDescription(),
-				FeaturePathAnnotatorBase.PARAM_PATHS, new String[] { Token.class.getName() } );
+		AnalysisEngine engine = createPrimitive(SnowballStemmer.class);
 
 		JCas jcas = engine.newJCas();
 		jcas.setDocumentLanguage("de");
@@ -63,9 +59,7 @@ public class SnowballStemmerTest
 	public void testEnglish()
 		throws Exception
 	{
-		AnalysisEngine engine = createPrimitive(SnowballStemmer.class,
-				createTypeSystemDescription(),
-				FeaturePathAnnotatorBase.PARAM_PATHS, new String[] { Token.class.getName() } );
+		AnalysisEngine engine = createPrimitive(SnowballStemmer.class);
 
 		JCas jcas = engine.newJCas();
 		jcas.setDocumentLanguage("en");
@@ -92,6 +86,64 @@ public class SnowballStemmerTest
 			assertTrue(i != 0 || "comput".equals(t.getStem().getValue()));
 			assertTrue(i != 1 || "Comput".equals(t.getStem().getValue()));
 			assertTrue(i != 2 || "deliber".equals(t.getStem().getValue()));
+			i ++;
+		}
+	}
+
+	@Test
+	public void testEnglishCaseInsensitive()
+		throws Exception
+	{
+		AnalysisEngine engine = createPrimitive(SnowballStemmer.class,
+				SnowballStemmer.PARAM_LOWER_CASE, true);
+
+		JCas jcas = engine.newJCas();
+		jcas.setDocumentLanguage("en");
+		JCasBuilder cb = new JCasBuilder(jcas);
+		cb.add("EDUCATIONAL", Token.class);
+		cb.add(" ");
+		cb.add("Educational", Token.class);
+		cb.add(" ");
+		cb.add("educational", Token.class);
+		cb.close();
+
+		engine.process(jcas);
+
+		int i = 0;
+		for (Token t : iterate(jcas, Token.class)) {
+			String stem = t.getStem().getValue();
+			assertTrue(stem, i != 0 || "educ".equals(stem));
+			assertTrue(stem, i != 1 || "educ".equals(stem));
+			assertTrue(stem, i != 2 || "educ".equals(stem));
+			i ++;
+		}
+	}
+
+	@Test
+	public void testEnglishCaseSensitive()
+		throws Exception
+	{
+		AnalysisEngine engine = createPrimitive(SnowballStemmer.class,
+				SnowballStemmer.PARAM_LOWER_CASE, false);
+
+		JCas jcas = engine.newJCas();
+		jcas.setDocumentLanguage("en");
+		JCasBuilder cb = new JCasBuilder(jcas);
+		cb.add("EDUCATIONAL", Token.class);
+		cb.add(" ");
+		cb.add("Educational", Token.class);
+		cb.add(" ");
+		cb.add("educational", Token.class);
+		cb.close();
+
+		engine.process(jcas);
+
+		int i = 0;
+		for (Token t : iterate(jcas, Token.class)) {
+			String stem = t.getStem().getValue();
+			assertTrue(stem, i != 0 || "EDUCATIONAL".equals(stem));
+			assertTrue(stem, i != 1 || "Educat".equals(stem));
+			assertTrue(stem, i != 2 || "educ".equals(stem));
 			i ++;
 		}
 	}
