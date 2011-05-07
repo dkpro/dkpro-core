c007ed94c26f1ffc599cd1c948c89ddc992694fa
Richard Eckart de Castilho <richard.eckart@gmail.com>
2011-05-07 22:36:13 +0000

    Improved test case.

--- de.tudarmstadt.ukp.dkpro.core.io.text/src/test/java/de/tudarmstadt/ukp/dkpro/core/io/text/TextReaderTest.java
+++ de.tudarmstadt.ukp.dkpro.core.io.text/src/test/java/de/tudarmstadt/ukp/dkpro/core/io/text/TextReaderTest.java
@@ -2,13 +2,13 @@
  * Copyright 2010
  * Ubiquitous Knowledge Processing (UKP) Lab
  * Technische Universität Darmstadt
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -47,12 +47,14 @@ public class TextReaderTest
 			System.out.println(md.getDocumentUri());
 
 			assertTrue(
-					!"file:src/test/resources/texts/test1.txt".equals(md.getDocumentUri())
-					|| "This is a test.".equals(jcas.getDocumentText()));
+					!"file:src/test/resources/texts/test1.txt".equals(md.getDocumentUri()) || (
+							"This is a test.".equals(jcas.getDocumentText()) &&
+							15 == md.getEnd()));
 
 			assertTrue(
 					!"file:src/test/resources/texts/test2.txt".equals(md.getDocumentUri())
 					|| "This is a second test.".equals(jcas.getDocumentText()));
+
 		}
 	}
 	
