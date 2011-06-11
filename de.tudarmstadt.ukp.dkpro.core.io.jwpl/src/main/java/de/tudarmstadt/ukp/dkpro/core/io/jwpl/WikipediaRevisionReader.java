1be5b740c76993ed4bf51d9c2108c4bf2ea2fdb3
Richard Eckart de Castilho <richard.eckart@gmail.com>
2011-06-11 18:44:50 +0000

    Added missing license header.

--- de.tudarmstadt.ukp.dkpro.core.io.jwpl/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/jwpl/WikipediaRevisionReader.java
+++ de.tudarmstadt.ukp.dkpro.core.io.jwpl/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/jwpl/WikipediaRevisionReader.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * Copyright 2010
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package de.tudarmstadt.ukp.dkpro.core.io.jwpl;
 
 import java.io.IOException;
@@ -13,7 +30,7 @@ import de.tudarmstadt.ukp.wikipedia.revisionmachine.api.Revision;
 
 /**
  * Reads all revisions of all articles.
- * 
+ *
  * @author zesch
  *
  */
@@ -26,7 +43,7 @@ public class WikipediaRevisionReader extends WikipediaRevisionReaderBase
     {
         // if hasNext() is true, a next timestamp should be available
         Timestamp timestamp = timestampIter.next();
-        
+
         try {
             Revision revision = this.revisionEncoder.getRevision(currentArticle.getPageId(), timestamp);
 
@@ -40,7 +57,7 @@ public class WikipediaRevisionReader extends WikipediaRevisionReaderBase
                 text = revision.getRevisionText();
             }
             jcas.setDocumentText(text);
-            
+
             addDocumentMetaData(jcas, currentArticle.getPageId(), revision.getRevisionID());
             addRevisionAnnotation(jcas, revision);
         }
