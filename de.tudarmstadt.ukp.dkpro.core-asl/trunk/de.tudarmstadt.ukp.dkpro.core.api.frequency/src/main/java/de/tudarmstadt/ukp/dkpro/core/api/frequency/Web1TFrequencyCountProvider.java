a8b14b113cd859ed3b2d342252888f2174eae72a
richard.eckart@gmail.com <richard.eckart@gmail.com@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2011-06-16 18:20:00 +0000

    - Added RAT plugin - Added license headers - Formatted code - Added xalan and xerces to dependency management in master pom

--- de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.api.frequency/src/main/java/de/tudarmstadt/ukp/dkpro/core/api/frequency/Web1TFrequencyCountProvider.java
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.api.frequency/src/main/java/de/tudarmstadt/ukp/dkpro/core/api/frequency/Web1TFrequencyCountProvider.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * Copyright 2011
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package de.tudarmstadt.ukp.dkpro.core.api.frequency;
 
 import java.util.Map;
@@ -8,35 +25,36 @@ import org.uimafit.component.Resource_ImplBase;
 
 import de.tudarmstadt.ukp.dkpro.teaching.frequency.FrequencyCountProvider;
 
-public final class Web1TFrequencyCountProvider extends Resource_ImplBase implements FrequencyCountProvider
+public final class Web1TFrequencyCountProvider
+	extends Resource_ImplBase
+	implements FrequencyCountProvider
 {
+	private FrequencyCountProvider provider;
 
-    private FrequencyCountProvider provider;
-    
-    @Override
-    public boolean initialize(ResourceSpecifier aSpecifier, Map aAdditionalParams)
-        throws ResourceInitializationException
-    {
-        if (!super.initialize(aSpecifier, aAdditionalParams)) {
-            return false;
-        }
+	@Override
+	public boolean initialize(ResourceSpecifier aSpecifier, Map aAdditionalParams)
+		throws ResourceInitializationException
+	{
+		if (!super.initialize(aSpecifier, aAdditionalParams)) {
+			return false;
+		}
 
-        provider = new Web1TFrequencyCountProvider();
+		provider = new Web1TFrequencyCountProvider();
 
-        return true;
-    }
-    
-    @Override
-    public long getFrequency(String phrase)
-        throws Exception
-    {
-        return provider.getFrequency(phrase);
-    }
+		return true;
+	}
 
-    @Override
-    public double getNormalizedFrequency(String phrase)
-        throws Exception
-    {
-        return provider.getNormalizedFrequency(phrase);
-    }
+	@Override
+	public long getFrequency(String phrase)
+		throws Exception
+	{
+		return provider.getFrequency(phrase);
+	}
+
+	@Override
+	public double getNormalizedFrequency(String phrase)
+		throws Exception
+	{
+		return provider.getNormalizedFrequency(phrase);
+	}
 }
