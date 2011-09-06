18d822887d69cdb5911c9d5ad1979fab62dfd595
richard.eckart@gmail.com <richard.eckart@gmail.com@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2011-09-06 17:18:50 +0000

    Using modern uimaFIT API.

--- de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.ngrams/src/main/java/de/tudarmstadt/ukp/dkpro/core/ngrams/NGramAnnotator.java
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.ngrams/src/main/java/de/tudarmstadt/ukp/dkpro/core/ngrams/NGramAnnotator.java
@@ -2,13 +2,13 @@
  * Copyright 2010
  * Ubiquitous Knowledge Processing (UKP) Lab
  * Technische Universität Darmstadt
- *   
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *   
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- *   
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,7 +17,7 @@
  ******************************************************************************/
 package de.tudarmstadt.ukp.dkpro.core.ngrams;
 
-import static org.uimafit.util.JCasUtil.iterate;
+import static org.uimafit.util.JCasUtil.selectCovered;
 import static org.uimafit.util.JCasUtil.select;
 
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
@@ -41,7 +41,7 @@ public class NGramAnnotator
 		throws AnalysisEngineProcessException
 	{
 		for (Sentence s : select(aJCas, Sentence.class)) {
-			for (NGram ngram : NGramIterable.create(iterate(Token.class, s), n)) {
+			for (NGram ngram : NGramIterable.create(selectCovered(Token.class, s), n)) {
 				ngram.addToIndexes();
 			}
 		}
