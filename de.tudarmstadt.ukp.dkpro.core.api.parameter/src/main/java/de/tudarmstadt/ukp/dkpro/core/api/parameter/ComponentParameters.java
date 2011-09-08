e7592c79a653556e7b8650ec64b8bc40e4a206f9
Richard Eckart de Castilho <richard.eckart@gmail.com>
2011-09-08 12:56:26 +0000

    Added api.parameter to harmonize names of parameters across all DKPro components.

---
+++ de.tudarmstadt.ukp.dkpro.core.api.parameter/src/main/java/de/tudarmstadt/ukp/dkpro/core/api/parameter/ComponentParameters.java
@@ -0,0 +1,18 @@
+package de.tudarmstadt.ukp.dkpro.core.api.parameter;
+
+public interface ComponentParameters
+{
+	public static final String PARAM_LANGUAGE = "Language";
+
+	public static final String PARAM_MODEL_LOCATION = "ModelLocation";
+
+	public static final String PARAM_MODEL_ENCODING = "ModelEncoding";
+
+	public static final String PARAM_SOURCE_LOCATION = "SourceLocation";
+
+	public static final String PARAM_SOURCE_ENCODING = "SourceEncoding";
+
+	public static final String PARAM_TARGET_LOCATION = "TargetLocation";
+
+	public static final String PARAM_TARGET_ENCODING = "TargetEncoding";
+}
