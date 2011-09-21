914d3cbb0e0844aa144fe335df5f039937fccf5f
Richard Eckart de Castilho <richard.eckart@gmail.com>
2011-09-21 22:41:14 +0000

    Added very rudimentary integration of the Stanford lemmatizer. This does not do a few things that the CoreNLP version does. Mind this only works for English.

---
+++ de.tudarmstadt.ukp.dkpro.core.stanfordnlp/src/test/java/de/tudarmstadt/ukp/dkpro/core/stanfordnlp/StanfordLemmatizerTest.java
@@ -0,0 +1,88 @@
+/*******************************************************************************
+ * Copyright 2010
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ ******************************************************************************/
+package de.tudarmstadt.ukp.dkpro.core.stanfordnlp;
+
+import static org.junit.Assert.assertEquals;
+import static org.uimafit.factory.AnalysisEngineFactory.createPrimitive;
+import static org.uimafit.util.JCasUtil.select;
+
+import java.net.URL;
+import java.util.Collection;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.jcas.JCas;
+import org.junit.Assume;
+import org.junit.Test;
+import org.uimafit.testing.factory.TokenBuilder;
+
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+
+public class StanfordLemmatizerTest
+{
+	@Test
+	public void testEnglish() throws Exception
+	{
+        runTest("en", "bidirectional-distsim-wsj-0-18", "This is a test .",
+        		new String[] { "DT", "VBZ", "DT", "NN",  "."    },
+        		new String[] { "this",  "be",  "a", "test", "." });
+
+	}
+
+	private JCas runTest(String aLanguage, String aVariant, String testDocument, String[] tags, String[] lemmas)
+		throws Exception
+	{
+		checkModelsAndBinary(aLanguage, aVariant);
+
+		AnalysisEngine posTagger = createPrimitive(StanfordPosTagger.class,
+				StanfordPosTagger.PARAM_MODEL_PATH,
+				"classpath:/de/tudarmstadt/ukp/dkpro/core/stanfordnlp/lib/postagger-" + aLanguage
+						+ "-" + aVariant + ".tagger");
+
+
+		AnalysisEngine lemmatizer = createPrimitive(StanfordLemmatizer.class);
+
+        JCas aJCas = posTagger.newJCas();
+        aJCas.setDocumentLanguage(aLanguage);
+
+        TokenBuilder<Token, Sentence> tb = new TokenBuilder<Token, Sentence>(Token.class, Sentence.class);
+        tb.buildTokens(aJCas, testDocument);
+
+        posTagger.process(aJCas);
+        lemmatizer.process(aJCas);
+
+        // test POS annotations
+        if (tags != null && lemmas != null) {
+        	checkTags(tags, lemmas, select(aJCas, Token.class));
+        }
+
+        return aJCas;
+    }
+
+	private void checkTags(String[] tags, String[] lemmas, Collection<Token> actual)
+	{
+		assertEquals("Number of tags " + actual.size(), lemmas.length,
+				actual.size());
+		int i = 0;
+        for (Token token : actual) {
+            assertEquals("In position "+i, tags[i], token.getPos().getPosValue());
+            assertEquals("In position "+i, lemmas[i], token.getLemma().getValue());
+            i++;
+        }
+	}
+
+	private void checkModelsAndBinary(String aLanguage, String aVariant)
+	{
+		String loc = "/de/tudarmstadt/ukp/dkpro/core/stanfordnlp/lib/postagger-" + aLanguage + "-"
+				+ aVariant + ".tagger";
+		URL url = getClass().getResource(loc);
+		Assume.assumeTrue(url != null);
+	}
+}
