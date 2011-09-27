f78a8916459348c1a95887791d3bfcd9a0bf9ca8
richard.eckart@gmail.com <richard.eckart@gmail.com@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2011-09-27 10:35:58 +0000

    Added missing license headers.

--- de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.frequency-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/frequency/Web1TFrequencyProviderTest.java
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.frequency-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/frequency/Web1TFrequencyProviderTest.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * Copyright 2011
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package de.tudarmstadt.ukp.dkpro.core.frequency;
 
 import static org.junit.Assert.assertEquals;
@@ -17,7 +34,7 @@ public class Web1TFrequencyProviderTest
                 "src/test/resources/web1t/index-1gms",
                 "src/test/resources/web1t/index-2gms"
         );
-        
+
         assertEquals(2147436244l, web1t.getFrequency("!"));
         assertEquals(528,         web1t.getFrequency("Nilmeier"));
         assertEquals(106,         web1t.getFrequency("influx takes"));
@@ -32,7 +49,7 @@ public class Web1TFrequencyProviderTest
                 1,
                 2
         );
-        
+
         assertEquals(2147436244l, web1t.getFrequency("!"));
         assertEquals(528,         web1t.getFrequency("Nilmeier"));
         assertEquals(106,         web1t.getFrequency("influx takes"));
