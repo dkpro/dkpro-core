c7ff7f144524e25b6de489c691b770b511d201a2
Richard Eckart de Castilho <richard.eckart@gmail.com>
2011-11-20 19:05:54 +0000

    Added asserts to StanfordCoreferenceResolverTest.

--- de.tudarmstadt.ukp.dkpro.core.stanfordnlp/src/test/java/de/tudarmstadt/ukp/dkpro/core/stanfordnlp/StanfordCoreferenceResolverTest.java
+++ de.tudarmstadt.ukp.dkpro.core.stanfordnlp/src/test/java/de/tudarmstadt/ukp/dkpro/core/stanfordnlp/StanfordCoreferenceResolverTest.java
@@ -10,11 +10,16 @@
  ******************************************************************************/
 package de.tudarmstadt.ukp.dkpro.core.stanfordnlp;
 
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
 import static org.uimafit.factory.AnalysisEngineFactory.createAggregate;
 import static org.uimafit.factory.AnalysisEngineFactory.createAggregateDescription;
 import static org.uimafit.factory.AnalysisEngineFactory.createPrimitiveDescription;
 import static org.uimafit.util.JCasUtil.select;
 import static org.uimafit.util.JCasUtil.toText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.uima.analysis_engine.AnalysisEngine;
@@ -34,7 +39,11 @@ public class StanfordCoreferenceResolverTest
 	public void test()
 		throws Exception
 	{ 
+		// Make sure the test only runs when the models are present
 		checkModel("/de/tudarmstadt/ukp/dkpro/core/stanfordnlp/lib/lexparser-en-pcfg.ser.gz");
+		checkModel("/de/tudarmstadt/ukp/dkpro/core/stanfordnlp/lib/ner-en-all.3class.distsim.crf.ser.gz");
+		
+		// Coreference resolution requires the parser and the NER to run before
 		AnalysisEngine aggregate = createAggregate(createAggregateDescription(
 				createPrimitiveDescription(StanfordSegmenter.class),
 				createPrimitiveDescription(StanfordParser.class,
@@ -51,15 +60,22 @@ public class StanfordCoreferenceResolverTest
 						"classpath:/de/tudarmstadt/ukp/dkpro/core/stanfordnlp/lib/ner-en-all.3class.distsim.crf.ser.gz"),
 				createPrimitiveDescription(StanfordCoreferenceResolver.class)));
 
+		// Set up a simple example
 		JCas jcas = aggregate.newJCas();
 		jcas.setDocumentLanguage("en");
 		jcas.setDocumentText("John bought a car. He is very happy with it.");
-
 		aggregate.process(jcas);
-
-		for (CoreferenceChain chain : select(jcas, CoreferenceChain.class)) {
+		
+		// Dump results
+		List<CoreferenceChain> chains = new ArrayList<CoreferenceChain>(select(jcas, CoreferenceChain.class));
+		for (CoreferenceChain chain : chains) {
 			System.out.println("Chain: [" + StringUtils.join(toText(chain.links()), "] , [") + "]");
 		}
+	
+		// Checks
+		assertEquals(2, chains.size());
+		assertEquals(asList("John", "He"), toText(chains.get(0).links()));
+		assertEquals(asList("a car", "it"), toText(chains.get(1).links()));
 	}
 	
     void checkModel(String aModel)
