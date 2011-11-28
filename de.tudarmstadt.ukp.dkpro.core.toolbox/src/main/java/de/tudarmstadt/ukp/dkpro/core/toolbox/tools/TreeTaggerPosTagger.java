f45e48c889bb481cba660107b4c9ae076394a486
Torsten Zesch <torsten.zesch@gmail.com>
2011-11-28 20:51:38 +0000

    LanguageIdentificator also in toolbox + some fixes.

--- de.tudarmstadt.ukp.dkpro.core.toolbox/src/main/java/de/tudarmstadt/ukp/dkpro/core/toolbox/tools/TreeTaggerPosTagger.java
+++ de.tudarmstadt.ukp.dkpro.core.toolbox/src/main/java/de/tudarmstadt/ukp/dkpro/core/toolbox/tools/TreeTaggerPosTagger.java
@@ -17,21 +17,21 @@
  ******************************************************************************/
 package de.tudarmstadt.ukp.dkpro.core.toolbox.tools;
 
+import static org.uimafit.factory.AnalysisEngineFactory.createAggregateDescription;
 import static org.uimafit.factory.AnalysisEngineFactory.createPrimitive;
+import static org.uimafit.factory.AnalysisEngineFactory.createPrimitiveDescription;
 import static org.uimafit.util.JCasUtil.select;
 
-import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import org.apache.uima.UIMAException;
 import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.jcas.JCas;
-import org.apache.uima.jcas.tcas.Annotation;
-import org.uimafit.testing.factory.TokenBuilder;
 
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
 import de.tudarmstadt.ukp.dkpro.core.toolbox.core.Tag;
 import de.tudarmstadt.ukp.dkpro.core.toolbox.core.TaggedToken;
 import de.tudarmstadt.ukp.dkpro.core.treetagger.TreeTaggerPosLemmaTT4J;
@@ -39,38 +39,35 @@ import de.tudarmstadt.ukp.dkpro.core.treetagger.TreeTaggerPosLemmaTT4J;
 public class TreeTaggerPosTagger
 {
 
-    private final AnalysisEngine tagger;
+    private AnalysisEngineDescription tagger;
 
-    public TreeTaggerPosTagger() throws Exception {
-        tagger = createPrimitive(TreeTaggerPosLemmaTT4J.class);
-
+    public TreeTaggerPosTagger()
+        throws Exception
+   {
+        tagger = createAggregateDescription(
+                createPrimitiveDescription(BreakIteratorSegmenter.class),
+                createPrimitiveDescription(TreeTaggerPosLemmaTT4J.class)
+        );
     }
     
-    public Collection<TaggedToken> tag(String text, String language) {
+    public Collection<TaggedToken> tag(String text, String language)
+        throws Exception
+    {
         List<TaggedToken> taggedTokens = new ArrayList<TaggedToken>();
 
-        try {
-            JCas jcas = tagger.newJCas();
-            jcas.setDocumentLanguage(language);
+        AnalysisEngine engine = createPrimitive(tagger);
+        JCas jcas = engine.newJCas();
+        jcas.setDocumentLanguage(language);
+        jcas.setDocumentText(text);
+        engine.process(jcas);
 
-            TokenBuilder<Token, Annotation> tb = new TokenBuilder<Token, Annotation>(Token.class, Annotation.class);
-            tb.buildTokens(jcas, text);
-            tagger.process(jcas);
-
-            for (Token t : select(jcas, Token.class)) {
-                String token = t.getCoveredText();
-                Tag tag = new Tag(
-                        t.getPos().getPosValue(),
-                        language
-                );
-                taggedTokens.add(new TaggedToken(token, tag));
-            }
-        }
-        catch (UIMAException e) {
-            e.printStackTrace();
-        }
-        catch (MalformedURLException e) {
-            e.printStackTrace();
+        for (Token t : select(jcas, Token.class)) {
+            String token = t.getCoveredText();
+            Tag tag = new Tag(
+                    t.getPos().getPosValue(),
+                    language
+            );
+            taggedTokens.add(new TaggedToken(token, tag));
         }
 
         return taggedTokens;
