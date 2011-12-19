cf22f498232ad95419595274e20c3a07d50fe5c4
s.yang@ishuo.de <s.yang@ishuo.de@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2011-12-19 21:30:36 +0000

    now using AutoModelResolver

--- de.tudarmstadt.ukp.dkpro.core.treetagger/src/test/java/de/tudarmstadt/ukp/dkpro/core/treetagger/SegmenterCompatibilityTest.java
+++ de.tudarmstadt.ukp.dkpro.core.treetagger/src/test/java/de/tudarmstadt/ukp/dkpro/core/treetagger/SegmenterCompatibilityTest.java
@@ -31,12 +31,13 @@ import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.jcas.JCas;
 import org.junit.Before;
 import org.junit.Test;
+import org.uimafit.factory.ExternalResourceFactory;
 
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Lemma;
 import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
+import de.tudarmstadt.ukp.experiments.sy.externalresource.AutoResourceResolver;
 
-public
-class SegmenterCompatibilityTest
+public class SegmenterCompatibilityTest
 {
 	@Before
 	public void initTrace()
@@ -45,28 +46,35 @@ class SegmenterCompatibilityTest
 	}
 
 	@Test
-	public void segmenterCompatibilityTest() throws Exception {
-	
-	    AnalysisEngineDescription desc = createAggregateDescription(
-                createPrimitiveDescription(BreakIteratorSegmenter.class),
-                createPrimitiveDescription(TreeTaggerPosLemmaTT4J.class)
-        );
-        AnalysisEngine engine = createPrimitive(desc);
+	public void segmenterCompatibilityTest()
+		throws Exception
+	{
 
-        JCas aJCas = engine.newJCas();
-        aJCas.setDocumentLanguage("en");
-        aJCas.setDocumentText("Two cats sat on two mats.");
-        engine.process(aJCas);
+		AnalysisEngineDescription desc = createAggregateDescription(
+				createPrimitiveDescription(BreakIteratorSegmenter.class),
+				createPrimitiveDescription(TreeTaggerPosLemmaTT4J.class));
+		ExternalResourceFactory.bindResource(desc,
+				TreeTaggerTT4JBase.PARAM_RESOURCE_RESOLVER,
+				AutoResourceResolver.class);
+		AnalysisEngine engine = createPrimitive(desc);
 
-        checkLemma(new String[] { "Two", "cat", "sit", "on", "two", "mat", "." },  select(aJCas, Lemma.class));
-    }
+		JCas aJCas = engine.newJCas();
+		aJCas.setDocumentLanguage("en");
+		aJCas.setDocumentText("Two cats sat on two mats.");
+		engine.process(aJCas);
+
+		checkLemma(
+				new String[] { "Two", "cat", "sit", "on", "two", "mat", "." },
+				select(aJCas, Lemma.class));
+	}
 
 	private void checkLemma(String[] expected, Collection<Lemma> actual)
 	{
-        int i = 0;
-        for (Lemma lemmaAnnotation : actual) {
-            assertEquals("In position "+i, expected[i], lemmaAnnotation.getValue());
-            i++;
-        }
+		int i = 0;
+		for (Lemma lemmaAnnotation : actual) {
+			assertEquals("In position " + i, expected[i],
+					lemmaAnnotation.getValue());
+			i++;
+		}
 	}
 }
--- de.tudarmstadt.ukp.dkpro.core.treetagger/src/test/java/de/tudarmstadt/ukp/dkpro/core/treetagger/TreeTaggerChunkerTT4JTest.java
+++ de.tudarmstadt.ukp.dkpro.core.treetagger/src/test/java/de/tudarmstadt/ukp/dkpro/core/treetagger/TreeTaggerChunkerTT4JTest.java
@@ -17,8 +17,8 @@
  ******************************************************************************/
 package de.tudarmstadt.ukp.dkpro.core.treetagger;
 
+import static de.tudarmstadt.ukp.dkpro.core.treetagger.TreeTaggerPosLemmaTT4JTest.createTreeTaggerWithAutoModelResolver;
 import static org.junit.Assert.assertEquals;
-import static org.uimafit.factory.AnalysisEngineFactory.createPrimitive;
 import static org.uimafit.factory.TypeSystemDescriptionFactory.createTypeSystemDescription;
 import static org.uimafit.util.JCasUtil.select;
 
@@ -33,102 +33,92 @@ import org.uimafit.testing.factory.TokenBuilder;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import de.tudarmstadt.ukp.dkpro.core.api.syntax.type.chunk.Chunk;
 
-public
-class TreeTaggerChunkerTT4JTest
+public class TreeTaggerChunkerTT4JTest
 {
-    @Test
-    public
-    void treeTaggerAnnotatorEnglishTest()
-    throws Exception
-    {
-        runEnglishTest();
-        runEnglishTest2();
-    }
+	@Test
+	public void treeTaggerAnnotatorEnglishTest()
+		throws Exception
+	{
+		runEnglishTest();
+		runEnglishTest2();
+	}
 
-    @Test
-    public
-    void treeTaggerAnnotatorGermanTest()
-    throws Exception
-    {
-        runGermanTest();
-    }
+	@Test
+	public void treeTaggerAnnotatorGermanTest()
+		throws Exception
+	{
+		runGermanTest();
+	}
 
-    private
-    void runEnglishTest()
-    throws Exception
-    {
-        runTest(
-        		"This is a test .",
-        		new String[] { "this", "be",  "a",   "test", "."    },
-        		new String[] { "NC",   "VC",  "NC",          "O" },
-        		new String[] { "NC",   "VC",  "NC",          "O" },
-        		"en");
-    }
+	private void runEnglishTest()
+		throws Exception
+	{
+		runTest("This is a test .", new String[] { "this", "be", "a", "test",
+				"." }, new String[] { "NC", "VC", "NC", "O" }, new String[] {
+				"NC", "VC", "NC", "O" }, "en");
+	}
 
-    private
-    void runEnglishTest2()
-    throws Exception
-    {
-        runTest(
-        		"A neural-net .",
-        		new String[] { "a",   "neural", "-", "net", "." },
-        		new String[] { "NC",                        "O" },
-        		new String[] { "NC",                        "O" },
-        		"en");
-    }
+	private void runEnglishTest2()
+		throws Exception
+	{
+		runTest("A neural-net .",
+				new String[] { "a", "neural", "-", "net", "." }, new String[] {
+						"NC", "O" }, new String[] { "NC", "O" }, "en");
+	}
 
-    private
-    void runGermanTest()
-    throws Exception
-    {
-        runTest(
-        		"Das ist ein Test .",
-        		new String[] { "d",   "sein",  "ein", "Test", "." },
-        		new String[] { "NC",  "VC",    "NC",          "0" },
-        		new String[] { "NC",  "VC",    "NC",          "O" },
-        		"de");
-    }
+	private void runGermanTest()
+		throws Exception
+	{
+		runTest("Das ist ein Test .", new String[] { "d", "sein", "ein",
+				"Test", "." }, new String[] { "NC", "VC", "NC", "0" },
+				new String[] { "NC", "VC", "NC", "O" }, "de");
+	}
 
-    private
-    void checkModelsAndBinary(String lang)
-    {
+	private void checkModelsAndBinary(String lang)
+	{
 		Assume.assumeTrue(getClass().getResource(
-				"/de/tudarmstadt/ukp/dkpro/core/treetagger/lib/" + lang + "-tagger-little-endian.par") != null);
+				"/de/tudarmstadt/ukp/dkpro/core/treetagger/lib/" + lang
+						+ "-tagger-little-endian.par") != null);
 
 		Assume.assumeTrue(getClass().getResource(
 				"/de/tudarmstadt/ukp/dkpro/core/treetagger/bin/LICENSE.txt") != null);
-    }
+	}
 
-	private JCas runTest(String testDocument, String[] lemmas, String[] tags, String[] tagClasses,
-			String language)
+	private JCas runTest(String testDocument, String[] lemmas, String[] tags,
+			String[] tagClasses, String language)
 		throws Exception
 	{
-    	checkModelsAndBinary(language);
+		checkModelsAndBinary(language);
 
-		AnalysisEngine tagger = createPrimitive(TreeTaggerPosLemmaTT4J.class);
-        AnalysisEngine chunker = createPrimitive(TreeTaggerChunkerTT4J.class);
+		AnalysisEngine tagger = createTreeTaggerWithAutoModelResolver(TreeTaggerPosLemmaTT4J.class);
+		AnalysisEngine chunker = createTreeTaggerWithAutoModelResolver(TreeTaggerChunkerTT4J.class);
 
-        JCas aJCas = CasCreationUtils.createCas(createTypeSystemDescription(), null, null).getJCas();
-        aJCas.setDocumentLanguage(language);
+		JCas aJCas = CasCreationUtils.createCas(createTypeSystemDescription(),
+				null, null).getJCas();
+		aJCas.setDocumentLanguage(language);
 
-        TokenBuilder<Token, Annotation> tb = TokenBuilder.create(Token.class, Annotation.class);
-        tb.buildTokens(aJCas, testDocument);
+		TokenBuilder<Token, Annotation> tb = TokenBuilder.create(Token.class,
+				Annotation.class);
+		tb.buildTokens(aJCas, testDocument);
 
-        tagger.process(aJCas);
-        chunker.process(aJCas);
+		tagger.process(aJCas);
+		chunker.process(aJCas);
 
-        // test Chunk annotations
-        if (tagClasses != null && tags != null) {
-	        int i = 0;
-	        for (Chunk posAnnotation : select(aJCas, Chunk.class)) {
-	        	System.out.println(posAnnotation.getChunkValue()+": ["+posAnnotation.getCoveredText()+"]");
-	            assertEquals("In position "+i, tagClasses[i], posAnnotation.getType().getShortName());
-	            assertEquals("In position "+i, tags[i], posAnnotation.getChunkValue());
-	            i++;
-	        }
-	        assertEquals(tags.length, i);
-        }
+		// test Chunk annotations
+		if (tagClasses != null && tags != null) {
+			int i = 0;
+			for (Chunk posAnnotation : select(aJCas, Chunk.class)) {
+				System.out.println(posAnnotation.getChunkValue() + ": ["
+						+ posAnnotation.getCoveredText() + "]");
+				assertEquals("In position " + i, tagClasses[i], posAnnotation
+						.getType().getShortName());
+				assertEquals("In position " + i, tags[i],
+						posAnnotation.getChunkValue());
+				i++;
+			}
+			assertEquals(tags.length, i);
+		}
 
-        return aJCas;
-    }
+		return aJCas;
+	}
 }
