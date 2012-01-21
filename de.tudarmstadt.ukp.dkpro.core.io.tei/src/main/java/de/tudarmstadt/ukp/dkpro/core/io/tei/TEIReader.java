09307a9035ef2d4df0e90e537923c1a00fd3f8b1
Richard Eckart de Castilho <richard.eckart@gmail.com>
2012-01-21 19:47:41 +0000

    - Simplified code.

--- de.tudarmstadt.ukp.dkpro.core.io.tei/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/tei/TEIReader.java
+++ de.tudarmstadt.ukp.dkpro.core.io.tei/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/tei/TEIReader.java
@@ -23,11 +23,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.uima.UimaContext;
-import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.Type;
-import org.apache.uima.cas.TypeSystem;
 import org.apache.uima.cas.text.AnnotationFS;
 import org.apache.uima.collection.CollectionException;
+import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
@@ -39,169 +38,139 @@ import org.jaxen.XPath;
 import org.jaxen.dom4j.Dom4jXPath;
 import org.uimafit.descriptor.ConfigurationParameter;
 
-import de.tudarmstadt.ukp.dkpro.core.api.io.ResourceCollectionReaderBase;
+import de.tudarmstadt.ukp.dkpro.core.api.io.JCasResourceCollectionReader_ImplBase;
 import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.TagsetMappingFactory;
+import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 
 /**
- * Reads the Brown corpus.
- * Writes token, POS, and sentence annotations if configured accordingly.
+ * Reads the Brown corpus. Writes token, POS, and sentence annotations if configured accordingly.
  * 
  * @author zesch
- *
  */
 public class TEIReader
-    extends ResourceCollectionReaderBase
+	extends JCasResourceCollectionReader_ImplBase
 {
-    
-    private static final String LANG_CODE = "en";
+	private static final String LANG_CODE = "en";
 
-    public static final String PARAM_WRITE_TOKENS = "WriteTokens";
-    @ConfigurationParameter(name = PARAM_WRITE_TOKENS, mandatory = true, defaultValue = "true")
-    private boolean writeTokens;
-    
-    public static final String PARAM_WRITE_POS = "WritePOS";
-    @ConfigurationParameter(name = PARAM_WRITE_POS, mandatory = true, defaultValue = "true")
-    private boolean writePOS;
-    
-    public static final String PARAM_WRITE_SENTENCES = "WriteSentences";
-    @ConfigurationParameter(name = PARAM_WRITE_SENTENCES, mandatory = true, defaultValue = "true")
-    private boolean writeSentences;
+	public static final String PARAM_WRITE_TOKENS = "WriteTokens";
+	@ConfigurationParameter(name = PARAM_WRITE_TOKENS, mandatory = true, defaultValue = "true")
+	private boolean writeTokens;
 
-    private Type tokenType;
-    private Type sentenceType;
+	public static final String PARAM_WRITE_POS = "WritePOS";
+	@ConfigurationParameter(name = PARAM_WRITE_POS, mandatory = true, defaultValue = "true")
+	private boolean writePOS;
 
-    @Override
-    public void initialize(UimaContext aContext)
-        throws ResourceInitializationException
-    {
-        super.initialize(aContext);
-        
-        if (writePOS && ! writeTokens) {
-            throw new ResourceInitializationException(
-                    new IllegalArgumentException("Setting WritePOS to 'true' requires WriteToken to be 'true' too.")
-            );
-        }
-    }
-    
-    @Override
-    public void getNext(CAS aCAS)
-        throws IOException, CollectionException
-    {
-        
-        Resource res = nextFile();
-        initCas(aCAS, res);
+	public static final String PARAM_WRITE_SENTENCES = "WriteSentences";
+	@ConfigurationParameter(name = PARAM_WRITE_SENTENCES, mandatory = true, defaultValue = "true")
+	private boolean writeSentences;
 
-        TypeSystem typeSystem = aCAS.getTypeSystem();
-        
-        StringBuilder sb = new StringBuilder();
-        int offset = 0;
-        
-        List<AnnotationFS> tokenAnnotations    = new ArrayList<AnnotationFS>();
-        List<AnnotationFS> posAnnotations      = new ArrayList<AnnotationFS>();
-        List<AnnotationFS> sentenceAnnotations = new ArrayList<AnnotationFS>();
+	@Override
+	public void initialize(UimaContext aContext)
+		throws ResourceInitializationException
+	{
+		super.initialize(aContext);
 
-        tokenType = typeSystem.getType(Token.class.getName());
-        sentenceType = typeSystem.getType(Sentence.class.getName());
+		if (writePOS && !writeTokens) {
+			throw new ResourceInitializationException(new IllegalArgumentException(
+					"Setting WritePOS to 'true' requires WriteToken to be 'true' too."));
+		}
+	}
 
-        try {
-            SAXReader reader = new SAXReader();
-            Document document = reader.read(new BufferedInputStream(res.getInputStream()));
-            Element root = document.getRootElement();
-        
-            SimpleNamespaceContext nsContext = new SimpleNamespaceContext();
-            nsContext.addNamespace("tei", "http://www.tei-c.org/ns/1.0");
-        
-            String sentenceXPath = "//tei:s";
-            String tokenXPath = "./tei:w|tei:c";
-        
-            final XPath sentenceXP = new Dom4jXPath(sentenceXPath);
-            final XPath tokenXP = new Dom4jXPath(tokenXPath);
-            sentenceXP.setNamespaceContext(nsContext);
-            tokenXP.setNamespaceContext(nsContext);
-        
-            for (Object sentenceElement : sentenceXP.selectNodes( root )) {
+	@Override
+	public void getNext(JCas aJCas)
+		throws IOException, CollectionException
+	{
+		Resource res = nextFile();
+		initCas(aJCas, res);
+		aJCas.setDocumentLanguage(LANG_CODE);
 
-                if (sentenceElement instanceof Element) {
-                    int savedOffset = offset;
-                    for (Object tokenElement : tokenXP.selectNodes( sentenceElement )) {
-                
-                        if (tokenElement instanceof Element) {
-                            Element node = (Element) tokenElement;
-                            String tokenText = node.getText();
-                            int len = tokenText.length();
-                            String posString = node.attributeValue("type");
-                            
-                            sb.append(tokenText);
-                            sb.append(" ");
-                            
-                            // Add the Part of Speech
-                            if (writePOS) {
-                                Type posType = TagsetMappingFactory.getTagType(
-                                        TagsetMappingFactory.getMapping("tagger", LANG_CODE, null),
-                                        posString,
-                                        typeSystem);
-                                AnnotationFS posAnno = aCAS.createAnnotation(
-                                            posType,
-                                            offset,
-                                            offset + len
-                                );
-                                posAnno.setStringValue(posType.getFeatureByBaseName("PosValue"), posString);
-                                posAnnotations.add(posAnno);
-                            }
+		StringBuilder sb = new StringBuilder();
 
-                            if (writeTokens) {
-                                AnnotationFS tokenAnno = aCAS.createAnnotation(
-                                        tokenType,
-                                        offset,
-                                        offset + len
-                                );
-                                if (writePOS) {
-                                    tokenAnno.setFeatureValue(
-                                            tokenType.getFeatureByBaseName("pos"),
-                                            posAnnotations.get(posAnnotations.size()-1)
-                                    );
-                                }
-                                tokenAnnotations.add(tokenAnno);
-                            }
-                            
-                            // increase offset by size of token + 1 for the space
-                            offset += len + 1;
-                        }
-                    }
-                    
-                    if (writeSentences) {
-                        AnnotationFS sentenceAnno = aCAS.createAnnotation(
-                                sentenceType,
-                                savedOffset,
-                                offset
-                        );
-                        sentenceAnnotations.add(sentenceAnno);
-                    }
-                }
-            }
-        }
-        catch (IOException e) {
-            throw new CollectionException(e);
-        } catch (JaxenException e) {
-            throw new CollectionException(e);
-        } catch (DocumentException e) {
-            throw new CollectionException(e);
-        }
-        
-        aCAS.setDocumentText(sb.toString());
-        aCAS.setDocumentLanguage(LANG_CODE);
-     
-        // finally add the annotations to the CAS
-        for (AnnotationFS t : tokenAnnotations) {
-            aCAS.addFsToIndexes(t);
-        }
-        for (AnnotationFS p : posAnnotations) {
-            aCAS.addFsToIndexes(p);
-        }
-        for (AnnotationFS s : sentenceAnnotations) {
-            aCAS.addFsToIndexes(s);
-        }
-    }
+		List<AnnotationFS> tokenAnnotations = new ArrayList<AnnotationFS>();
+		List<AnnotationFS> posAnnotations = new ArrayList<AnnotationFS>();
+		List<AnnotationFS> sentenceAnnotations = new ArrayList<AnnotationFS>();
+
+		try {
+			SAXReader reader = new SAXReader();
+			Document document = reader.read(new BufferedInputStream(res.getInputStream()));
+			Element root = document.getRootElement();
+
+			SimpleNamespaceContext nsContext = new SimpleNamespaceContext();
+			nsContext.addNamespace("tei", "http://www.tei-c.org/ns/1.0");
+
+			String sentenceXPath = "//tei:s";
+			String tokenXPath = "./tei:w|tei:c";
+
+			final XPath sentenceXP = new Dom4jXPath(sentenceXPath);
+			final XPath tokenXP = new Dom4jXPath(tokenXPath);
+			sentenceXP.setNamespaceContext(nsContext);
+			tokenXP.setNamespaceContext(nsContext);
+
+			for (Object sentenceElement : sentenceXP.selectNodes(root)) {
+				if (sentenceElement instanceof Element) {
+					int sentenceBegin = sb.length();
+					for (Object tokenElement : tokenXP.selectNodes(sentenceElement)) {
+						if (tokenElement instanceof Element) {
+							Element node = (Element) tokenElement;
+							
+							// Add token text
+							int tokenBegin = sb.length();
+							sb.append(node.getText());
+
+							// Add the Part of Speech
+							POS posAnno = null;
+							if (writePOS) {
+								String posString = node.attributeValue("type");
+								Type posType = TagsetMappingFactory.getTagType(
+										TagsetMappingFactory.getMapping("tagger", LANG_CODE, null),
+										posString, aJCas.getTypeSystem());
+								posAnno = (POS) aJCas.getCas().createAnnotation(posType,
+										tokenBegin, sb.length());
+								posAnno.setPosValue(posString);
+								posAnnotations.add(posAnno);
+							}
+
+							// Add token annotation
+							if (writeTokens) {
+								Token tokenAnno = new Token(aJCas, tokenBegin, sb.length());
+								tokenAnno.setPos(posAnno);
+								tokenAnnotations.add(tokenAnno);
+							}
+
+							sb.append(" ");
+						}
+					}
+
+					if (writeSentences) {
+						Sentence sentenceAnno = new Sentence(aJCas, sentenceBegin, sb.length());
+						sentenceAnnotations.add(sentenceAnno);
+					}
+				}
+			}
+		}
+		catch (IOException e) {
+			throw new CollectionException(e);
+		}
+		catch (JaxenException e) {
+			throw new CollectionException(e);
+		}
+		catch (DocumentException e) {
+			throw new CollectionException(e);
+		}
+
+		aJCas.setDocumentText(sb.toString());
+
+		// finally add the annotations to the CAS
+		for (AnnotationFS t : tokenAnnotations) {
+			aJCas.addFsToIndexes(t);
+		}
+		for (AnnotationFS p : posAnnotations) {
+			aJCas.addFsToIndexes(p);
+		}
+		for (AnnotationFS s : sentenceAnnotations) {
+			aJCas.addFsToIndexes(s);
+		}
+	}
 }
