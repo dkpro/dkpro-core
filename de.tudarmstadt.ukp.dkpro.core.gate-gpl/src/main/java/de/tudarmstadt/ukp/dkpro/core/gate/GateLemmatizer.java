bf5471580c56c84199167ae3e4e9a1e4c35ed812
Torsten Zesch <torsten.zesch@gmail.com>
2012-03-13 19:39:12 +0000

    added gate lemmatizer

---
+++ de.tudarmstadt.ukp.dkpro.core.gate-gpl/src/main/java/de/tudarmstadt/ukp/dkpro/core/gate/GateLemmatizer.java
@@ -0,0 +1,114 @@
+package de.tudarmstadt.ukp.dkpro.core.gate;
+
+import gate.creole.ResourceInstantiationException;
+import gate.creole.morph.Interpret;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.apache.uima.UimaContext;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.uimafit.component.JCasAnnotator_ImplBase;
+import org.uimafit.descriptor.ConfigurationParameter;
+import org.uimafit.util.JCasUtil;
+
+import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.N;
+import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS;
+import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.PR;
+import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.V;
+import de.tudarmstadt.ukp.dkpro.core.api.resources.ResourceUtils;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Lemma;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+
+
+/**
+ * Wrapper for the GATE rule based lemmatizer.
+ * 
+ * Based on code by Asher Stern from the BIUTEE textual entailment tool.
+ * 
+ * @author zesch
+ * @since 1.4.0
+ *
+ */
+public class GateLemmatizer extends JCasAnnotator_ImplBase {
+
+    public static final String PARAM_RULE_FILE = "RuleFile";
+    @ConfigurationParameter(name = PARAM_RULE_FILE, mandatory = false)
+    private String ruleFile;
+	
+	// constants
+	public static final String GATE_LEMMATIZER_VERB_CATEGORY_STRING = "VB";
+	public static final String GATE_LEMMATIZER_NOUN_CATEGORY_STRING = "NN";
+	public static final String GATE_LEMMATIZER_ALL_CATEGORIES_STRING = "*";
+
+	private Interpret gateLemmatizerInterpretObject = null;
+	
+	@Override
+	public void initialize(UimaContext context)
+			throws ResourceInitializationException
+	{
+		super.initialize(context);
+		
+		URL ruleFileURL = null;
+		try {
+			if (ruleFile != null) {
+				ruleFileURL = ResourceUtils.resolveLocation(ruleFile, this, context);
+			}
+			else {
+				ruleFileURL = ResourceUtils.resolveLocation("src/main/resources/rules/en.rul", this, context);
+			}
+						
+			gateLemmatizerInterpretObject = new Interpret();
+			gateLemmatizerInterpretObject.init(ruleFileURL);
+		} catch (IOException e) {
+			throw new ResourceInitializationException(e);
+		} catch (ResourceInstantiationException e) {
+			throw new ResourceInitializationException(e);
+		}		
+	}
+
+	@Override
+	public void process(JCas jcas)
+		throws AnalysisEngineProcessException
+	{
+		String category = null;
+		for (Token token : JCasUtil.select(jcas, Token.class)) {
+			POS pos = token.getPos();
+			
+			if (pos != null) {
+				if (pos.getClass().equals(V.class)) {
+					category = GATE_LEMMATIZER_VERB_CATEGORY_STRING;
+				}
+				else if (pos.getClass().equals(N.class)) {
+					category = GATE_LEMMATIZER_NOUN_CATEGORY_STRING;
+				}
+				else if (pos.getClass().equals(PR.class)) {
+					category = GATE_LEMMATIZER_NOUN_CATEGORY_STRING;
+				}
+				else
+					category = GATE_LEMMATIZER_ALL_CATEGORIES_STRING;
+			}
+			else
+			{
+				category = GATE_LEMMATIZER_ALL_CATEGORIES_STRING;
+			}
+			
+			String tokenString = token.getCoveredText();
+			String lemmaString = gateLemmatizerInterpretObject.runMorpher(tokenString, category);
+			
+			Lemma lemma = new Lemma(jcas, token.getBegin(), token.getEnd());
+			lemma.setValue(lemmaString);
+			lemma.addToIndexes();
+			
+			// remove (a potentially existing) old lemma before adding a new one
+			if (token.getLemma() != null) {
+				Lemma oldLemma = token.getLemma();
+				oldLemma.removeFromIndexes();
+			}
+			
+			token.setLemma(lemma);
+		}
+	}
+}
---
+++ de.tudarmstadt.ukp.dkpro.core.gate-gpl/src/test/java/de/tudarmstadt/ukp/dkpro/core/gate/GateLemmatizerTest.java
@@ -0,0 +1,73 @@
+package de.tudarmstadt.ukp.dkpro.core.gate;
+
+import static org.junit.Assert.assertEquals;
+import static org.uimafit.factory.AnalysisEngineFactory.createPrimitive;
+import static org.uimafit.factory.AnalysisEngineFactory.createPrimitiveDescription;
+
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.jcas.JCas;
+import org.junit.Test;
+import org.uimafit.factory.JCasBuilder;
+import org.uimafit.util.JCasUtil;
+
+import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.CARD;
+import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.N;
+import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS;
+import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.PP;
+import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.PUNC;
+import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.V;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Lemma;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+
+public class GateLemmatizerTest {
+
+	@Test
+	public void lemmatizerTest()
+		throws Exception
+	{
+		AnalysisEngineDescription lemmatizer = createPrimitiveDescription(
+                GateLemmatizer.class
+        );
+		
+        AnalysisEngine engine = createPrimitive(lemmatizer);
+        JCas jcas = engine.newJCas();
+        jcas.setDocumentLanguage("en");
+        
+        JCasBuilder builder = new JCasBuilder(jcas);
+        Token t1 = builder.add("Two", Token.class);
+        POS p1 = builder.add(t1.getBegin(), CARD.class);
+        t1.setPos(p1);
+        
+        Token t2 = builder.add("cars", Token.class);
+        POS p2 = builder.add(t2.getBegin(), N.class);
+        t2.setPos(p2);
+        
+        Token t3 = builder.add("went", Token.class);
+        POS p3 = builder.add(t3.getBegin(), V.class);
+        t3.setPos(p3);
+        
+        Token t4 = builder.add("around", Token.class);
+        POS p4 = builder.add(t4.getBegin(), PP.class);
+        t4.setPos(p4);
+        
+        Token t5 = builder.add("corners", Token.class);
+        POS p5 = builder.add(t5.getBegin(), N.class);
+        t5.setPos(p5);
+        
+        Token t6 = builder.add(".", Token.class);
+        POS p6 = builder.add(t6.getBegin(), PUNC.class);
+        t6.setPos(p6);
+        
+        builder.close();
+        engine.process(jcas);
+        
+        String[] expectedLemmas = new String[] {"Two", "car", "go", "around", "corner", "."};
+        
+        int i=0;
+        for (Lemma l : JCasUtil.select(jcas, Lemma.class)) {
+        	assertEquals(expectedLemmas[i], l);
+        	i++;
+        }
+	}
+}
