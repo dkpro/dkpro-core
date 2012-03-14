fb78958d2875c1f4da77f0d44ea3d4a67916dbbd
Torsten Zesch <torsten.zesch@gmail.com>
2012-03-14 11:44:21 +0000

    Added license header.

--- de.tudarmstadt.ukp.dkpro.core.gate-gpl/src/main/java/de/tudarmstadt/ukp/dkpro/core/gate/GateLemmatizer.java
+++ de.tudarmstadt.ukp.dkpro.core.gate-gpl/src/main/java/de/tudarmstadt/ukp/dkpro/core/gate/GateLemmatizer.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright 2010
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ ******************************************************************************/
 package de.tudarmstadt.ukp.dkpro.core.gate;
 
 import gate.creole.ResourceInstantiationException;
