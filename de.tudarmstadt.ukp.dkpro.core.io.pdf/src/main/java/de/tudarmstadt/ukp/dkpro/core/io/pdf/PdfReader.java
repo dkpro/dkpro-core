1c5d24c3971a7b280f1a75a11178859dcafa3104
s.yang@ishuo.de <s.yang@ishuo.de@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2012-03-26 14:43:22 +0000

    [PdfReader] updated code style

--- de.tudarmstadt.ukp.dkpro.core.io.pdf/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/pdf/PdfReader.java
+++ de.tudarmstadt.ukp.dkpro.core.io.pdf/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/pdf/PdfReader.java
@@ -39,27 +39,27 @@ import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Paragraph;
 
 /**
  * Collection reader for PDF files.
- *
+ * 
  * @author Richard Eckart de Castilho
  */
 public class PdfReader
-	extends FileSetCollectionReaderBase
+    extends FileSetCollectionReaderBase
 {
-	public static final String BUILT_IN = "<built-in>";
-	private static final String NOT_RESTRICTED = "-1";
+    public static final String BUILT_IN = "<built-in>";
+    private static final String NOT_RESTRICTED = "-1";
 
-	public static final String PARAM_SUBSTITUTION_TABLE = "SubstitutionTable";
-	@ConfigurationParameter(name=PARAM_SUBSTITUTION_TABLE, mandatory=false, defaultValue=BUILT_IN)
-	private String aSubstitutionTableLocation;
+    public static final String PARAM_SUBSTITUTION_TABLE = "SubstitutionTable";
+    @ConfigurationParameter(name = PARAM_SUBSTITUTION_TABLE, mandatory = false, defaultValue = BUILT_IN)
+    private String aSubstitutionTableLocation;
 
-	public static final String PARAM_HEADING_TYPE = "HeadingType";
-	@ConfigurationParameter(name=PARAM_HEADING_TYPE, mandatory=false, defaultValue=BUILT_IN)
-	private String headingType;
+    public static final String PARAM_HEADING_TYPE = "HeadingType";
+    @ConfigurationParameter(name = PARAM_HEADING_TYPE, mandatory = false, defaultValue = BUILT_IN)
+    private String headingType;
 
-	public static final String PARAM_PARAGRAPH_TYPE = "ParagraphType";
-	@ConfigurationParameter(name=PARAM_PARAGRAPH_TYPE, mandatory=false, defaultValue=BUILT_IN)
-	private String paragraphType;
-	
+    public static final String PARAM_PARAGRAPH_TYPE = "ParagraphType";
+    @ConfigurationParameter(name = PARAM_PARAGRAPH_TYPE, mandatory = false, defaultValue = BUILT_IN)
+    private String paragraphType;
+
     public static final String PARAM_START_PAGE = "StartPage";
     @ConfigurationParameter(name = PARAM_START_PAGE, mandatory = false, defaultValue = NOT_RESTRICTED)
     private int startPage;
@@ -68,70 +68,69 @@ public class PdfReader
     @ConfigurationParameter(name = PARAM_END_PAGE, mandatory = false, defaultValue = NOT_RESTRICTED)
     private int endPage;
 
-	private Trie<String> substitutionTable;
+    private Trie<String> substitutionTable;
 
-	@Override
-	public void initialize(UimaContext aContext)
-		throws ResourceInitializationException
-	{
-		super.initialize(aContext);
+    @Override
+    public void initialize(UimaContext aContext)
+        throws ResourceInitializationException
+    {
+        super.initialize(aContext);
 
-		if (BUILT_IN.equals(headingType)) {
-			headingType = Heading.class.getName();
-		}
+        if (BUILT_IN.equals(headingType)) {
+            headingType = Heading.class.getName();
+        }
 
-		if (BUILT_IN.equals(paragraphType)) {
-			paragraphType = Paragraph.class.getName();
-		}
+        if (BUILT_IN.equals(paragraphType)) {
+            paragraphType = Paragraph.class.getName();
+        }
 
-		if (aSubstitutionTableLocation != null) {
-			if (BUILT_IN.equals(aSubstitutionTableLocation)) {
-				aSubstitutionTableLocation =
-					"classpath:/de/tudarmstadt/ukp/dkpro/core/io/pdf/substitutionTable.xml";
-			}
+        if (aSubstitutionTableLocation != null) {
+            if (BUILT_IN.equals(aSubstitutionTableLocation)) {
+                aSubstitutionTableLocation = "classpath:/de/tudarmstadt/ukp/dkpro/core/io/pdf/substitutionTable.xml";
+            }
 
-			InputStream is = null;
-			try {
-				URL url = ResourceUtils.resolveLocation(aSubstitutionTableLocation, this, aContext);
-				is = url.openStream();
-				substitutionTable = SubstitutionTrieParser.parse(is);
-			}
-			catch (IOException e) {
-				throw new ResourceInitializationException(e);
-			}
-			finally {
-				closeQuietly(is);
-			}
-		}
-		else {
-			substitutionTable = null;
-		}
-	}
+            InputStream is = null;
+            try {
+                URL url = ResourceUtils.resolveLocation(aSubstitutionTableLocation, this, aContext);
+                is = url.openStream();
+                substitutionTable = SubstitutionTrieParser.parse(is);
+            }
+            catch (IOException e) {
+                throw new ResourceInitializationException(e);
+            }
+            finally {
+                closeQuietly(is);
+            }
+        }
+        else {
+            substitutionTable = null;
+        }
+    }
 
-	@Override
-	public void getNext(CAS aCAS)
-		throws IOException, CollectionException
-	{
-		FileResource resource = nextFile();
-		initCas(aCAS, resource);
+    @Override
+    public void getNext(CAS aCAS)
+        throws IOException, CollectionException
+    {
+        FileResource resource = nextFile();
+        initCas(aCAS, resource);
 
-		InputStream is = null;
-		try {
-			is = resource.getInputStream();
-			final Pdf2CasConverter converter = new Pdf2CasConverter();
-			converter.setSubstitutionTable(substitutionTable);
-			converter.setHeadingType(headingType);
-			converter.setParagraphType(paragraphType);
+        InputStream is = null;
+        try {
+            is = resource.getInputStream();
+            final Pdf2CasConverter converter = new Pdf2CasConverter();
+            converter.setSubstitutionTable(substitutionTable);
+            converter.setHeadingType(headingType);
+            converter.setParagraphType(paragraphType);
             if (startPage != Integer.parseInt(NOT_RESTRICTED)) {
                 converter.setStartPage(startPage);
             }
             if (endPage != Integer.parseInt(NOT_RESTRICTED)) {
                 converter.setEndPage(endPage);
             }
-			converter.writeText(aCAS, is);
-		}
-		finally {
-			closeQuietly(is);
-		}
-	}
+            converter.writeText(aCAS, is);
+        }
+        finally {
+            closeQuietly(is);
+        }
+    }
 }
