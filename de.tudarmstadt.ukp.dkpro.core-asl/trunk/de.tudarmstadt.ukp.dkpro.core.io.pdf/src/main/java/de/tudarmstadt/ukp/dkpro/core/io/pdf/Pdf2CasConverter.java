c22ae770d55eb27c08bdbdd5049e412b51b95256
s.yang@ishuo.de <s.yang@ishuo.de@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2012-03-26 14:25:44 +0000

    [PdfReader] updated code style

--- de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.io.pdf/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/pdf/Pdf2CasConverter.java
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.io.pdf/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/pdf/Pdf2CasConverter.java
@@ -32,307 +32,310 @@ import org.apache.uima.cas.text.AnnotationFS;
 
 /**
  * Converts a PDF to a CAS. Uses a substitution table.
- *
+ * 
  * @author Richard Eckart de Castilho
  */
 public class Pdf2CasConverter
-	extends PDFLayoutEventStripper
+    extends PDFLayoutEventStripper
 {
-	private final Log log = LogFactory.getLog(getClass());
+    private final Log log = LogFactory.getLog(getClass());
 
-	private Trie<String> substitutionTable;
-	private CAS cas;
-	private StringBuilder text;
-	private Style regionStyle;
-	private StringBuilder regionText;
-	private String paragraphType;
-	private String headingType;
+    private Trie<String> substitutionTable;
+    private CAS cas;
+    private StringBuilder text;
+    private Style regionStyle;
+    private StringBuilder regionText;
+    private String paragraphType;
+    private String headingType;
 
-	public Pdf2CasConverter()
-		throws IOException
-	{
-		super();
-	}
+    public Pdf2CasConverter()
+        throws IOException
+    {
+        super();
+    }
 
-	public void writeText(final CAS aCas, final InputStream aIs)
-		throws IOException
-	{
-		final PDDocument doc = PDDocument.load(aIs);
+    public void writeText(final CAS aCas, final InputStream aIs)
+        throws IOException
+    {
+        final PDDocument doc = PDDocument.load(aIs);
 
-		try {
-			if (doc.isEncrypted()) {
-				throw new IOException("Encrypted documents currently not supported");
-			}
+        try {
+            if (doc.isEncrypted()) {
+                throw new IOException("Encrypted documents currently not supported");
+            }
 
-			cas = aCas;
-			text = new StringBuilder();
+            cas = aCas;
+            text = new StringBuilder();
 
-			writeText(doc);
-		}
-		finally {
-			doc.close();
-		}
-	}
+            writeText(doc);
+        }
+        finally {
+            doc.close();
+        }
+    }
 
-	@Override
-	protected void startDocument(final PDDocument aPdf)
-		throws IOException
-	{
-		if (log.isTraceEnabled()) {
-			log.trace("<document>");
-		}
-	}
+    @Override
+    protected void startDocument(final PDDocument aPdf)
+        throws IOException
+    {
+        if (log.isTraceEnabled()) {
+            log.trace("<document>");
+        }
+    }
 
-	@Override
-	protected void endDocument(final PDDocument aPdf)
-		throws IOException
-	{
-		cas.setDocumentText(text.toString());
+    @Override
+    protected void endDocument(final PDDocument aPdf)
+        throws IOException
+    {
+        cas.setDocumentText(text.toString());
 
-		if (log.isTraceEnabled()) {
-			log.trace("</document>");
-		}
-	}
+        if (log.isTraceEnabled()) {
+            log.trace("</document>");
+        }
+    }
 
-	@Override
-	protected void processLineSeparator()
-		throws IOException
-	{
-		if (log.isTraceEnabled()) {
-			log.trace("<br/>");
-		}
+    @Override
+    protected void processLineSeparator()
+        throws IOException
+    {
+        if (log.isTraceEnabled()) {
+            log.trace("<br/>");
+        }
 
-		if (regionText == null) {
-			throw new IllegalStateException("No region started");
-		}
+        if (regionText == null) {
+            throw new IllegalStateException("No region started");
+        }
 
-		regionText.append("\n");
-	}
+        regionText.append("\n");
+    }
 
-	@Override
-	protected void processWordSeparator()
-		throws IOException
-	{
-		if (log.isTraceEnabled()) {
-			log.trace("< >");
-		}
+    @Override
+    protected void processWordSeparator()
+        throws IOException
+    {
+        if (log.isTraceEnabled()) {
+            log.trace("< >");
+        }
 
-		if (regionText == null) {
-			throw new IllegalStateException("No region started");
-		}
+        if (regionText == null) {
+            throw new IllegalStateException("No region started");
+        }
 
-		regionText.append(" ");
-	}
+        regionText.append(" ");
+    }
 
-	@Override
-	protected void startPage(final int aFirstPage, final int aLastPage, final int aCurrentPage,
-			final PDPage page)
-		throws IOException
-	{
-		if (log.isTraceEnabled()) {
-			log.trace("<page>");
-		}
+    @Override
+    protected void startPage(final int aFirstPage, final int aLastPage, final int aCurrentPage,
+            final PDPage page)
+        throws IOException
+    {
+        if (log.isTraceEnabled()) {
+            log.trace("<page>");
+        }
 
-		if (log.isDebugEnabled()) {
-			log.debug("Decoding page " + aCurrentPage + " of " + (aLastPage - aFirstPage + 1));
-		}
-	}
+        if (log.isDebugEnabled()) {
+            log.debug("Decoding page " + aCurrentPage + " of " + (aLastPage - aFirstPage + 1));
+        }
+    }
 
-	@Override
-	protected void endPage(final int aStartPage, final int aEndPage, final int aCurrentPage,
-			final PDPage page)
-		throws IOException
-	{
-		if (log.isTraceEnabled()) {
-			log.trace("</page>");
-		}
-	}
+    @Override
+    protected void endPage(final int aStartPage, final int aEndPage, final int aCurrentPage,
+            final PDPage page)
+        throws IOException
+    {
+        if (log.isTraceEnabled()) {
+            log.trace("</page>");
+        }
+    }
 
-	@Override
-	protected void startRegion(final Style aStyle)
-		throws IOException
-	{
-		if (log.isTraceEnabled()) {
-			log.trace("<" + aStyle + ">");
-		}
+    @Override
+    protected void startRegion(final Style aStyle)
+        throws IOException
+    {
+        if (log.isTraceEnabled()) {
+            log.trace("<" + aStyle + ">");
+        }
 
-		regionStyle = aStyle;
-		regionText = new StringBuilder();
-	}
+        regionStyle = aStyle;
+        regionText = new StringBuilder();
+    }
 
-	@Override
-	protected void endRegion(final Style aStyle)
-		throws IOException
-	{
-		if (log.isTraceEnabled()) {
-			log.trace("</" + aStyle + ">");
-		}
+    @Override
+    protected void endRegion(final Style aStyle)
+        throws IOException
+    {
+        if (log.isTraceEnabled()) {
+            log.trace("</" + aStyle + ">");
+        }
 
-		if (regionText == null) {
-			throw new IllegalStateException("No region started");
-		}
+        if (regionText == null) {
+            throw new IllegalStateException("No region started");
+        }
 
-		if (regionStyle != aStyle) {
-			throw new IllegalStateException("Current region has style " + regionStyle
-					+ ", but closing region has style " + aStyle);
-		}
+        if (regionStyle != aStyle) {
+            throw new IllegalStateException("Current region has style " + regionStyle
+                    + ", but closing region has style " + aStyle);
+        }
 
-		// Append text
-		int begin = text.length();
-		sanitize(regionText);
-		text.append(regionText.toString());
-		int end = text.length();
-		text.append('\n');
+        // Append text
+        int begin = text.length();
+        sanitize(regionText);
+        text.append(regionText.toString());
+        int end = text.length();
+        text.append('\n');
 
-		// Add annotation
-		switch (aStyle) {
-		case HEADING:
-			if (headingType != null) {
-				Type t = cas.getTypeSystem().getType(headingType);
-				AnnotationFS a = cas.createAnnotation(t, begin, end);
-				cas.addFsToIndexes(a);
-			}
-			break;
-		case PARAGRAPH:
-			if (paragraphType != null) {
-				Type t = cas.getTypeSystem().getType(paragraphType);
-				AnnotationFS a = cas.createAnnotation(t, begin, end);
-				cas.addFsToIndexes(a);
-			}
-			break;
-		default:
-			throw new IllegalStateException("Unknown region style: "+aStyle);
-		}
+        // Add annotation
+        switch (aStyle) {
+        case HEADING:
+            if (headingType != null) {
+                Type t = cas.getTypeSystem().getType(headingType);
+                AnnotationFS a = cas.createAnnotation(t, begin, end);
+                cas.addFsToIndexes(a);
+            }
+            break;
+        case PARAGRAPH:
+            if (paragraphType != null) {
+                Type t = cas.getTypeSystem().getType(paragraphType);
+                AnnotationFS a = cas.createAnnotation(t, begin, end);
+                cas.addFsToIndexes(a);
+            }
+            break;
+        default:
+            throw new IllegalStateException("Unknown region style: " + aStyle);
+        }
 
-		regionStyle = null;
-		regionText = null;
-	}
+        regionStyle = null;
+        regionText = null;
+    }
 
-	@Override
-	protected void writeCharacters(final TextPosition aText)
-		throws IOException
-	{
-		if (log.isTraceEnabled()) {
-			log.trace("[" + aText.getCharacter() + "]");
-		}
+    @Override
+    protected void writeCharacters(final TextPosition aText)
+        throws IOException
+    {
+        if (log.isTraceEnabled()) {
+            log.trace("[" + aText.getCharacter() + "]");
+        }
 
-		if (regionText == null) {
-			throw new IllegalStateException("No region started");
-		}
+        if (regionText == null) {
+            throw new IllegalStateException("No region started");
+        }
 
-		regionText.append(aText.getCharacter());
-	}
+        regionText.append(aText.getCharacter());
+    }
 
-	private static boolean isValidXMLChar(final int aCodePoint)
-	{
-		return (aCodePoint == 0x0009) || (aCodePoint == 0x000A) || (aCodePoint == 0x000D)
-				|| ((0x0020 <= aCodePoint) && (aCodePoint <= 0xD7FF))
-				|| ((0xE000 <= aCodePoint) && (aCodePoint <= 0xFFFD));
-	}
+    private static boolean isValidXMLChar(final int aCodePoint)
+    {
+        return (aCodePoint == 0x0009) || (aCodePoint == 0x000A) || (aCodePoint == 0x000D)
+                || ((0x0020 <= aCodePoint) && (aCodePoint <= 0xD7FF))
+                || ((0xE000 <= aCodePoint) && (aCodePoint <= 0xFFFD));
+    }
 
-	private StringBuilder sanitize(final StringBuilder aContent)
-	{
-		int i = 0;
-		int lastBreak = 0;
-		while (i < aContent.length()) {
-			// Check valid unicode char
-			if (!isValidXMLChar(aContent.codePointAt(i))) {
-				aContent.setCharAt(i, ' ');
-				i++;
-				continue;
-			}
+    private StringBuilder sanitize(final StringBuilder aContent)
+    {
+        int i = 0;
+        int lastBreak = 0;
+        while (i < aContent.length()) {
+            // Check valid unicode char
+            if (!isValidXMLChar(aContent.codePointAt(i))) {
+                aContent.setCharAt(i, ' ');
+                i++;
+                continue;
+            }
 
-			// Set up how many characters we want to skip
-			int seek = i + 1;
+            // Set up how many characters we want to skip
+            int seek = i + 1;
 
-			// Do we maybe have an entity?
-			if (aContent.charAt(i) == '&') {
-				// REC 2006-10-21 Some PDFs seem to have entities and others don't
-				// so we may encounter &'s that do not introduce an entity and
-				// just ignore them.
-				final int end = aContent.indexOf(";", i);
-				if (end != -1) {
-					final String cand = aContent.substring(i, end + 1);
-					String r = null;
-					try {
-						if (cand.startsWith("&#x")) {
-							final int cp = Integer.parseInt(cand.substring(2, cand.length() - 1),
-									16);
-							r = isValidXMLChar(cp) ? String.valueOf(Character.toChars(cp)) : " ";
-						}
-						else if (cand.startsWith("&#")) {
-							final int cp = Integer.parseInt(cand.substring(2, cand.length() - 1));
-							r = isValidXMLChar(cp) ? String.valueOf(Character.toChars(cp)) : " ";
-						}
-						else {
-							// RE 2006-10-22 The chance that there is a & and a ;
-							// together in a string is quite big. Let's be tolerant.
-						}
-					}
-					catch (final NumberFormatException e) {
-						log.warn("Invalid numeric entity in fragment [" + cand + "] - Dropping it.");
-					}
+            // Do we maybe have an entity?
+            if (aContent.charAt(i) == '&') {
+                // REC 2006-10-21 Some PDFs seem to have entities and others
+                // don't
+                // so we may encounter &'s that do not introduce an entity and
+                // just ignore them.
+                final int end = aContent.indexOf(";", i);
+                if (end != -1) {
+                    final String cand = aContent.substring(i, end + 1);
+                    String r = null;
+                    try {
+                        if (cand.startsWith("&#x")) {
+                            final int cp = Integer.parseInt(cand.substring(2, cand.length() - 1),
+                                    16);
+                            r = isValidXMLChar(cp) ? String.valueOf(Character.toChars(cp)) : " ";
+                        }
+                        else if (cand.startsWith("&#")) {
+                            final int cp = Integer.parseInt(cand.substring(2, cand.length() - 1));
+                            r = isValidXMLChar(cp) ? String.valueOf(Character.toChars(cp)) : " ";
+                        }
+                        else {
+                            // RE 2006-10-22 The chance that there is a & and a
+                            // ;
+                            // together in a string is quite big. Let's be
+                            // tolerant.
+                        }
+                    }
+                    catch (final NumberFormatException e) {
+                        log.warn("Invalid numeric entity in fragment [" + cand + "] - Dropping it.");
+                    }
 
-					// Expand the entity and set proper skip (if found)
-					if (r != null) {
-						aContent.replace(i, i + cand.length(), r);
-						seek = i + r.length();
-					}
-				}
-			}
+                    // Expand the entity and set proper skip (if found)
+                    if (r != null) {
+                        aContent.replace(i, i + cand.length(), r);
+                        seek = i + r.length();
+                    }
+                }
+            }
 
-			// Match against the Trie after numeric entity expansion is over
-			if (substitutionTable != null) {
-				final Trie<String>.Node match = substitutionTable.getNode(aContent, i);
-				if (match != null) {
-					aContent.replace(i, i + match.level, match.value);
-					seek = i + match.value.length();
-				}
-			}
+            // Match against the Trie after numeric entity expansion is over
+            if (substitutionTable != null) {
+                final Trie<String>.Node match = substitutionTable.getNode(aContent, i);
+                if (match != null) {
+                    aContent.replace(i, i + match.level, match.value);
+                    seek = i + match.value.length();
+                }
+            }
 
-			// Check line breaks
-			while (i < seek) {
-				if (aContent.charAt(i) == '\n') {
-					lastBreak = i;
-				}
-				else if (Character.isWhitespace(aContent.codePointAt(i)) && (i > (lastBreak + 79))) {
-					lastBreak = i;
-					aContent.replace(i, i + 1, "\n");
-				}
-				i++;
-			}
-		}
+            // Check line breaks
+            while (i < seek) {
+                if (aContent.charAt(i) == '\n') {
+                    lastBreak = i;
+                }
+                else if (Character.isWhitespace(aContent.codePointAt(i)) && (i > (lastBreak + 79))) {
+                    lastBreak = i;
+                    aContent.replace(i, i + 1, "\n");
+                }
+                i++;
+            }
+        }
 
-		return aContent;
-	}
+        return aContent;
+    }
 
-	public void setSubstitutionTable(Trie<String> aSubstitutionTable)
-	{
-		substitutionTable = aSubstitutionTable;
-	}
+    public void setSubstitutionTable(Trie<String> aSubstitutionTable)
+    {
+        substitutionTable = aSubstitutionTable;
+    }
 
-	public Trie<String> getSubstitutionTable()
-	{
-		return substitutionTable;
-	}
+    public Trie<String> getSubstitutionTable()
+    {
+        return substitutionTable;
+    }
 
-	public String getParagraphType()
-	{
-		return paragraphType;
-	}
+    public String getParagraphType()
+    {
+        return paragraphType;
+    }
 
-	public void setParagraphType(String aParagraphType)
-	{
-		paragraphType = aParagraphType;
-	}
+    public void setParagraphType(String aParagraphType)
+    {
+        paragraphType = aParagraphType;
+    }
 
-	public String getHeadingType()
-	{
-		return headingType;
-	}
+    public String getHeadingType()
+    {
+        return headingType;
+    }
 
-	public void setHeadingType(String aHeadingType)
-	{
-		headingType = aHeadingType;
-	}
+    public void setHeadingType(String aHeadingType)
+    {
+        headingType = aHeadingType;
+    }
 }
