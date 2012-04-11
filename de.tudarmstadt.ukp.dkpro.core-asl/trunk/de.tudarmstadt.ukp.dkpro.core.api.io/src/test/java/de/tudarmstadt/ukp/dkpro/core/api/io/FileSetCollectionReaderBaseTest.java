66e44d197007aaadb4c54763293bb6007390f713
s.yang@ishuo.de <s.yang@ishuo.de@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2012-04-11 21:22:14 +0000

    Added license header

--- de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.api.io/src/test/java/de/tudarmstadt/ukp/dkpro/core/api/io/FileSetCollectionReaderBaseTest.java
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.api.io/src/test/java/de/tudarmstadt/ukp/dkpro/core/api/io/FileSetCollectionReaderBaseTest.java
@@ -1,7 +1,23 @@
+/*******************************************************************************
+ * Copyright 2012
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package de.tudarmstadt.ukp.dkpro.core.api.io;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 import static org.uimafit.factory.CollectionReaderFactory.createCollectionReader;
 import static org.uimafit.factory.TypeSystemDescriptionFactory.createTypeSystemDescription;
 
@@ -11,7 +27,6 @@ import org.apache.tools.ant.types.resources.FileResource;
 import org.apache.uima.cas.CAS;
 import org.apache.uima.collection.CollectionException;
 import org.apache.uima.collection.CollectionReader;
-import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.util.CasCreationUtils;
 import org.junit.Test;
 
