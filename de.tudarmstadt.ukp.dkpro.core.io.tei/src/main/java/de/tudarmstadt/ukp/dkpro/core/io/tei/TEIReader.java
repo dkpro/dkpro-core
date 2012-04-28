3d570f8ee7540307cfcec6c036ddb1c4cb0a3a3f
Richard Eckart de Castilho <richard.eckart@gmail.com>
2012-04-28 13:16:26 +0000

    Task 55: [TEI] Files from the Digitale Bibliothek of TextGrid are not supported http://code.google.com/p/dkpro-core-asl/issues/detail?id=55 - Added support for TEI documents with multiple texts inside. - Added corresponding unit test.

--- de.tudarmstadt.ukp.dkpro.core.io.tei/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/tei/TEIReader.java
+++ de.tudarmstadt.ukp.dkpro.core.io.tei/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/tei/TEIReader.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2011
+ * Copyright 2012
  * Ubiquitous Knowledge Processing (UKP) Lab
  * Technische Universität Darmstadt
  *
@@ -17,53 +17,132 @@
  ******************************************************************************/
 package de.tudarmstadt.ukp.dkpro.core.io.tei;
 
-import static de.tudarmstadt.ukp.dkpro.core.api.lexmorph.TagsetMappingFactory.getTagType;
+import static java.util.Arrays.asList;
+import static org.apache.commons.io.IOUtils.closeQuietly;
+import static org.apache.commons.lang.StringUtils.isNotBlank;
 
-import java.io.BufferedInputStream;
 import java.io.IOException;
-import java.util.Map;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
 
+import org.apache.commons.io.FilenameUtils;
 import org.apache.uima.UimaContext;
-import org.apache.uima.cas.Type;
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.CASException;
 import org.apache.uima.collection.CollectionException;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
 import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.util.Logger;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
+import org.dom4j.io.SAXWriter;
 import org.jaxen.JaxenException;
-import org.jaxen.SimpleNamespaceContext;
 import org.jaxen.XPath;
 import org.jaxen.dom4j.Dom4jXPath;
 import org.uimafit.descriptor.ConfigurationParameter;
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
-import de.tudarmstadt.ukp.dkpro.core.api.io.JCasResourceCollectionReader_ImplBase;
-import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.TagsetMappingFactory;
+import de.tudarmstadt.ukp.dkpro.core.api.io.ResourceCollectionReaderBase;
 import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS;
+import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Lemma;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 
 /**
- * Reads corpora in TEI format (e.g. the Brown TEI corpus). Writes token, POS, and sentence annotations if configured accordingly.
+ * Reader for the TEI XML.
  * 
- * @author zesch
+ * @author Richard Eckart de Castilho
  */
 public class TEIReader
-	extends JCasResourceCollectionReader_ImplBase
+	extends ResourceCollectionReaderBase
 {
-	public static final String PARAM_WRITE_TOKENS = "WriteTokens";
+	public static final String PARAM_WRITE_TOKENS = "writeTokens";
 	@ConfigurationParameter(name = PARAM_WRITE_TOKENS, mandatory = true, defaultValue = "true")
 	private boolean writeTokens;
 
-	public static final String PARAM_WRITE_POS = "WritePOS";
+	public static final String PARAM_WRITE_POS = "writePOS";
 	@ConfigurationParameter(name = PARAM_WRITE_POS, mandatory = true, defaultValue = "true")
 	private boolean writePOS;
 
-	public static final String PARAM_WRITE_SENTENCES = "WriteSentences";
+	public static final String PARAM_WRITE_LEMMA = "writeLemma";
+	@ConfigurationParameter(name = PARAM_WRITE_LEMMA, mandatory = true, defaultValue = "true")
+	private boolean writeLemma;
+
+	public static final String PARAM_WRITE_SENTENCES = "writeSentences";
 	@ConfigurationParameter(name = PARAM_WRITE_SENTENCES, mandatory = true, defaultValue = "true")
 	private boolean writeSentences;
+	
+	/**
+	 * Use the xml:id attribute on the TEI elements as document ID. Mind that many TEI files
+	 * may not have this attribute on all TEI elements and you may end up with no document ID
+	 * at all. Also mind that the IDs should be unique.
+	 */
+	public static final String PARAM_USE_XML_ID = "useXmlId";
+	@ConfigurationParameter(name = PARAM_USE_XML_ID, mandatory = true, defaultValue = "false")
+	private boolean useXmlId;
 
+	/**
+	 * When not using the XML ID, use only the filename instead of the whole URL as ID. Mind that
+	 * the filenames should be unique in this case.
+	 */
+	public static final String PARAM_USE_FILENAME_ID = "useFilenameId";
+	@ConfigurationParameter(name = PARAM_USE_FILENAME_ID, mandatory = true, defaultValue = "false")
+	private boolean useFilenameId;
+
+	// REC: This does not seem to work. Maybe because SAXWriter does not generate this event?
+	public static final String PARAM_OMIT_IGNORABLE_WHITESPACE = "omitIgnorableWhitespace";
+	@ConfigurationParameter(name = PARAM_OMIT_IGNORABLE_WHITESPACE, mandatory = true, defaultValue = "false")
+	private boolean omitIgnorableWhitespace;
+
+	/**
+	 * (character) contains a significant punctuation mark as identified by the CLAWS tagger.
+	 */
+	private static final String TAG_CHARACTER = "c";
+	
+	/**
+	 * (word) represents a grammatical (not necessarily orthographic) word.
+	 */
+	private static final String TAG_WORD = "w";
+	
+	/**
+	 * (s-unit) contains a sentence-like division of a text.
+	 */
+	private static final String TAG_SUNIT = "s";
+
+	/**
+	 * contains a single text of any kind, whether unitary or composite, for example a poem or
+	 * drama, a collection of essays, a novel, a dictionary, or a corpus sample.
+	 */
+	private static final String TAG_TEXT = "text";
+	
+	/**
+	 * contains the full title of a work of any kind.
+	 */
+	private static final String TAG_TITLE = "title";
+	
+	/**
+	 * (TEI document) contains a single TEI-conformant document, comprising a TEI header and a text,
+	 * either in isolation or as part of a teiCorpus element.
+	 */
+	private static final String TAG_TEI_DOC = "TEI";
+
+	private static final String ATTR_TYPE = "type";
+
+	private static final String ATTR_LEMMA = "lemma";
+
+	private Iterator<Element> teiElementIterator;
+	private Element currentTeiElement;
+	private Resource currentResource;
+	private int currentTeiElementNumber;
+	
 	@Override
 	public void initialize(UimaContext aContext)
 		throws ResourceInitializationException
@@ -72,89 +151,300 @@ public class TEIReader
 
 		if (writePOS && !writeTokens) {
 			throw new ResourceInitializationException(new IllegalArgumentException(
-					"Setting WritePOS to 'true' requires WriteToken to be 'true' too."));
+					"Setting writePOS to 'true' requires writeToken to be 'true' too."));
 		}
-	}
-
-	@Override
-	public void getNext(JCas aJCas)
-		throws IOException, CollectionException
-	{
-		Resource res = nextFile();
-		initCas(aJCas, res);
-
-		Element root;
+		
 		try {
-			SAXReader reader = new SAXReader();
-			Document document = reader.read(new BufferedInputStream(res.getInputStream()));
-			root = document.getRootElement();
+			// Init with an empty iterator
+			teiElementIterator = asList(new Element[0]).iterator();
+			
+			// Make sure we know about the first element;
+			nextTeiElement();
 		}
-		catch (DocumentException e) {
-			throw new CollectionException(e);
+		catch (CollectionException e) {
+			new ResourceInitializationException(e);
 		}
 		catch (IOException e) {
-			throw new CollectionException(e);
+			new ResourceInitializationException(e);
 		}
+	}
+	
+	private void nextTeiElement() throws CollectionException, IOException
+	{
+		if (teiElementIterator == null) {
+			currentTeiElement = null;
+			return;
+		}
+		
+		while (!teiElementIterator.hasNext() && super.hasNext()) {
+			currentResource = nextFile();
 
-		StringBuilder sb = new StringBuilder();
-		try {
-			SimpleNamespaceContext nsContext = new SimpleNamespaceContext();
-			nsContext.addNamespace("tei", "http://www.tei-c.org/ns/1.0");
+			InputStream is = null;
 
-			String sentenceXPath = "//tei:s";
-			String tokenXPath = "./tei:w|tei:c";
+			try {
+				is = currentResource.getInputStream();
+				InputSource source = new InputSource(is);
+				source.setPublicId(currentResource.getLocation());
+				source.setSystemId(currentResource.getLocation());
+				
+				SAXReader reader = new SAXReader();
+				Document xml = reader.read(source);
+				
+				final XPath teiPath = new Dom4jXPath("//tei:TEI");
+				teiPath.addNamespace("tei", "http://www.tei-c.org/ns/1.0");
 
-			final XPath sentenceXP = new Dom4jXPath(sentenceXPath);
-			final XPath tokenXP = new Dom4jXPath(tokenXPath);
-			sentenceXP.setNamespaceContext(nsContext);
-			tokenXP.setNamespaceContext(nsContext);
-
-			Map<String, String> mapping = TagsetMappingFactory.getMapping(
-					TagsetMappingFactory.TAGGER, aJCas.getDocumentLanguage(), null);
-			
-			for (Object sentenceElement : sentenceXP.selectNodes(root)) {
-				if (sentenceElement instanceof Element) {
-					int sentenceBegin = sb.length();
-					for (Object tokenElement : tokenXP.selectNodes(sentenceElement)) {
-						if (tokenElement instanceof Element) {
-							Element node = (Element) tokenElement;
-							
-							// Add token text
-							int tokenBegin = sb.length();
-							sb.append(node.getText());
-
-							// Add the Part of Speech
-							POS posAnno = null;
-							if (writePOS) {
-								String posString = node.attributeValue("type");
-								Type posType = getTagType(mapping, posString, aJCas.getTypeSystem());
-								posAnno = (POS) aJCas.getCas().createAnnotation(posType,
-										tokenBegin, sb.length());
-								posAnno.setPosValue(posString);
-								posAnno.addToIndexes();
-							}
-
-							// Add token annotation
-							if (writeTokens) {
-								Token tokenAnno = new Token(aJCas, tokenBegin, sb.length());
-								tokenAnno.setPos(posAnno);
-								tokenAnno.addToIndexes();
-							}
-
-							sb.append(" ");
-						}
-					}
-
-					if (writeSentences) {
-						new Sentence(aJCas, sentenceBegin, sb.length()).addToIndexes();
-					}
-				}
+				List<Element> teiElements = teiPath.selectNodes(xml);
+				
+//				System.out.printf("Found %d TEI elements in %s.%n", teiElements.size(),
+//						currentResource.getLocation());
+				
+				teiElementIterator = teiElements.iterator();
+				currentTeiElementNumber = 0;
+			}
+			catch (DocumentException e) {
+				throw new IOException(e);
+			}
+			catch (JaxenException e) {
+				throw new IOException(e);
+			}
+			finally {
+				closeQuietly(is);
 			}
 		}
-		catch (JaxenException e) {
-			throw new CollectionException(e);
+		
+		currentTeiElement = teiElementIterator.hasNext() ? teiElementIterator.next() : null;
+		currentTeiElementNumber++;
+		
+		if (!super.hasNext() && !teiElementIterator.hasNext()) {
+			// Mark end of processing.
+			teiElementIterator = null;
+		}
+	}
+	
+	@Override
+	public boolean hasNext()
+		throws IOException, CollectionException
+	{
+		return teiElementIterator != null || currentTeiElement != null;	
+	}
+	
+	@Override
+	public void getNext(CAS aCAS)
+		throws IOException, CollectionException
+	{
+		initCas(aCAS, currentResource);
+
+		// Set up language
+		if (getConfigParameterValue(PARAM_LANGUAGE) != null) {
+			aCAS.setDocumentLanguage((String) getConfigParameterValue(PARAM_LANGUAGE));
 		}
 
-		aJCas.setDocumentText(sb.toString());
+		InputStream is = null;
+
+		try {
+			JCas jcas = aCAS.getJCas();
+
+			// Create handler
+			Handler handler = newSaxHandler();
+			handler.setJCas(jcas);
+			handler.setLogger(getLogger());
+
+			// Parse TEI text
+			SAXWriter writer = new SAXWriter(handler);
+			writer.write(currentTeiElement);
+			handler.endDocument();
+		}
+		catch (CASException e) {
+			throw new CollectionException(e);
+		}
+		catch (SAXException e) {
+			throw new IOException(e);
+		}
+		finally {
+			closeQuietly(is);
+		}
+		
+		// Move currentTeiElement to the next text
+		nextTeiElement();
+	}
+	
+	protected Handler newSaxHandler()
+	{
+		return new TeiHandler();
+	}
+	
+	/**
+	 * @author Richard Eckart de Castilho
+	 */
+	protected abstract static class Handler
+		extends DefaultHandler
+	{
+		private JCas jcas;
+		private Logger logger;
+
+		public void setJCas(final JCas aJCas)
+		{
+			jcas = aJCas;
+		}
+
+		protected JCas getJCas()
+		{
+			return jcas;
+		}
+
+		public void setLogger(Logger aLogger)
+		{
+			logger = aLogger;
+		}
+
+		public Logger getLogger()
+		{
+			return logger;
+		}
+	}
+	
+	public class TeiHandler
+		extends Handler
+	{
+		private String documentId = null;
+		private boolean inTextElement = false;
+		private boolean captureText = false;
+		private int sentenceStart = -1;
+		private int tokenStart = -1;
+		private String posTag = null;
+		private String lemma = null;
+
+		private final StringBuilder buffer = new StringBuilder();
+
+		@Override
+		public void endDocument()
+			throws SAXException
+		{
+			getJCas().setDocumentText(buffer.toString());
+		}
+
+		protected StringBuilder getBuffer()
+		{
+			return buffer;
+		}
+		
+		@Override
+		public void startElement(String aUri, String aLocalName, String aName,
+				Attributes aAttributes)
+			throws SAXException
+		{
+//			System.out.printf("%b START %s %n", captureText, aLocalName);
+			if (!inTextElement && TAG_TEI_DOC.equals(aName)) {
+				if (useXmlId) {
+					documentId = aAttributes.getValue("xml:id");
+				}
+				else if (useFilenameId) {
+					documentId = FilenameUtils.getName(currentResource.getPath()) + "#"
+							+ currentTeiElementNumber;
+				} 
+				else {
+					documentId = currentResource.getPath()+"#"+currentTeiElementNumber;
+				}
+			}
+			else if (!inTextElement && TAG_TITLE.equals(aName)) {
+				captureText = true;
+			}
+			else if (TAG_TEXT.equals(aName)) {
+				captureText = true;
+				inTextElement = true;
+			}
+			else if (TAG_SUNIT.equals(aName)) {
+				sentenceStart = getBuffer().length();
+			}
+			else if (TAG_WORD.equals(aName) || TAG_CHARACTER.equals(aName)) {
+				tokenStart = getBuffer().length();
+				posTag = aAttributes.getValue(ATTR_TYPE);
+				lemma = aAttributes.getValue(ATTR_LEMMA);
+			}
+		}
+		
+		@Override
+		public void endElement(String aUri, String aLocalName, String aName)
+			throws SAXException
+		{
+//			System.out.printf("%b END %s %n", captureText, aLocalName);
+			if (!inTextElement && TAG_TITLE.equals(aName)) {
+				DocumentMetaData.get(getJCas()).setDocumentTitle(getBuffer().toString().trim());
+				DocumentMetaData.get(getJCas()).setDocumentId(documentId);
+				getBuffer().setLength(0);
+				captureText = false;
+			}
+			else if (TAG_TEXT.equals(aName)) {
+				captureText = false;
+				inTextElement = false;
+			}
+			else if (TAG_SUNIT.equals(aName)) {
+				if (writeSentences) {
+					new Sentence(getJCas(), sentenceStart, getBuffer().length()).addToIndexes();
+				}
+				sentenceStart = -1;
+			}
+			else if (TAG_WORD.equals(aName) || TAG_CHARACTER.equals(aName)) {
+				if (isNotBlank(getBuffer().substring(tokenStart, getBuffer().length()))) {
+					Token token = new Token(getJCas(), tokenStart, getBuffer().length());
+					trim(token);
+					
+					if (posTag != null && writePOS) {
+						POS pos = new POS(getJCas(), token.getBegin(), token.getEnd());
+						pos.setPosValue(posTag);
+						pos.addToIndexes();
+						token.setPos(pos);
+					}
+					
+					if (lemma != null && writeLemma) {
+						Lemma l = new Lemma(getJCas(), token.getBegin(), token.getEnd());
+						l.setValue(lemma);
+						l.addToIndexes();
+						token.setLemma(l);
+					}
+					
+					// FIXME: if writeTokens is disabled, the JCas wrapper should not be generated
+					// at all!
+					if (writeTokens) {
+						token.addToIndexes();
+					}
+				}
+				
+				tokenStart = -1;
+			}
+		}
+		
+		@Override
+		public void characters(char[] aCh, int aStart, int aLength)
+			throws SAXException
+		{
+			if (captureText) {
+				buffer.append(aCh, aStart, aLength);
+			}
+		}
+		
+		@Override
+		public void ignorableWhitespace(char[] aCh, int aStart, int aLength)
+			throws SAXException
+		{
+			if (captureText && !omitIgnorableWhitespace) {
+				buffer.append(aCh, aStart, aLength);
+			}
+		}
+		
+		private void trim(Annotation aAnnotation)
+		{
+			StringBuilder buffer = getBuffer();
+			int s = aAnnotation.getBegin();
+			int e = aAnnotation.getEnd();
+			while (Character.isWhitespace(buffer.charAt(s))) {
+				s++;
+			}
+			while ((e > s+1) && Character.isWhitespace(buffer.charAt(e-1))) {
+				e--;
+			}
+			aAnnotation.setBegin(s);
+			aAnnotation.setEnd(e);
+		}
 	}
 }
