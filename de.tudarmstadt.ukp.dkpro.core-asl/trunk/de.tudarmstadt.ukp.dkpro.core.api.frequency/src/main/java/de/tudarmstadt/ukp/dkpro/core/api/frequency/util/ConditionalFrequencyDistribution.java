01ef9300a9494f38f6eca0e3dbd52e929eb768eb
torsten.zesch <torsten.zesch@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2012-06-04 13:56:12 +0000

    Better documentation and some code duplication removed.
    thanks to Fabian Hirschmann for providing the patch.

--- de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.api.frequency/src/main/java/de/tudarmstadt/ukp/dkpro/core/api/frequency/util/ConditionalFrequencyDistribution.java
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.api.frequency/src/main/java/de/tudarmstadt/ukp/dkpro/core/api/frequency/util/ConditionalFrequencyDistribution.java
@@ -24,79 +24,194 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * Inspired by nltk.probability.ConditionalFreqDist
+ * Inspired by nltk.probability.ConditionalFreqDist Maps a condition to a
+ * {@link FrequencyDistribution}.
+ * 
+ * <p>
+ * This class could be used to learn how frequently a word collocates with another word. Suppose we
+ * want to know how frequently the word "of" appears after the word "because", then
+ * {@link ConditionalFrequencyDistribution} can be used as follows:
+ * 
+ * <p>
+ * <blockquote>
+ * 
+ * <pre>
+ * {
+ *     &#064;code
+ *     ConditionalFrequencyDistribution&lt;String, String&gt; cfd = new ConditionalFrequencyDistribution&lt;String, String&gt;();
+ * 
+ *     cfd.addSample(&quot;because&quot;, &quot;in&quot;);
+ *     cfd.addSample(&quot;because&quot;, &quot;of&quot;);
+ *     cfd.addSample(&quot;despite&quot;, &quot;in&quot;);
+ *     cfd.addSample(&quot;because&quot;, &quot;of&quot;);
+ * 
+ *     System.out.println(cfd.getCount(&quot;despite&quot;, &quot;of&quot;));
+ * }
+ * </pre>
+ * 
+ * </blockquote> The last call to {@link ConditionalFrequencyDistribution#getCount} will yield 2,
+ * because given the <code>condition</code> that the first word in a two-word sequence is "because",
+ * the word "of" has appeared twice.
+ * 
+ * <p>
+ * This class was inspired by NLTK's <a href=
+ * "http://nltk.googlecode.com/svn/trunk/doc/api/nltk.probability.ConditionalFreqDist-class.html">
+ * FreqDist</a>.
+ * 
+ * @param <C>
+ *            the type of the conditions
+ * @param <V>
+ *            the type of the samples
+ * @see FrequencyDistribution
  * 
  * @author zesch
  */
-public class ConditionalFrequencyDistribution<C,V> {
-    
-	private Map<C,FrequencyDistribution<V>> cfd;
-	
-	private long n;
-	
-	public ConditionalFrequencyDistribution() {
-		cfd = new HashMap<C,FrequencyDistribution<V>>();
-		n = 0;
-	}
-	
-	public ConditionalFrequencyDistribution(Map<C,Iterable<V>> samples) {
-		this();
-		for (Map.Entry<C, Iterable<V>> entry : samples.entrySet()) {
-		    addSamples(entry.getKey(), entry.getValue());
-		}
-	}
-	
-	/**
-	 * @return The total number of sample outcomes that have been recorded by this ConditionalFreqDist.
-	 */
-	public long getN() {
-		return n;
-	}
+public class ConditionalFrequencyDistribution<C, V>
+{
 
-	public long getCount(C t, V u) {
-		if (cfd.containsKey(t)) {
-			return cfd.get(t).getCount(u);
-		}
-		else {
-			return 0;
-		}
-	}
+    private Map<C, FrequencyDistribution<V>> cfd;
 
-	public FrequencyDistribution<V> getFrequencyDistribution(C c) {
-	    return cfd.get(c);
-	}
-	
-	public Set<C> getConditions() {
-	    return this.cfd.keySet();
-	}
-	
-	public boolean hasCondition(C t) {
-	    return this.cfd.containsKey(t);
-	}
-	
-    public void addSample(C t, V sample) {
-        List<V> samples = new ArrayList<V>();
-        samples.add(sample);
-        
-        addSamples(t, samples);
+    /** The total number of samples of all FrequencyDistributions. */
+    private long n;
+
+    /**
+     * Creates a new empty {@link ConditionalFrequencyDistribution}.
+     */
+    public ConditionalFrequencyDistribution()
+    {
+        cfd = new HashMap<C, FrequencyDistribution<V>>();
+        n = 0;
     }
 
-    public void addSamples(C t, Iterable<V> samples) {
-	    FrequencyDistribution<V> freqDist = null;
+    /**
+     * Creates a new {@link ConditionalFrequencyDistribution} and fills it with samples from a map.
+     * 
+     * @param samples
+     *            the {@link Iterable} to fill from
+     */
+    public ConditionalFrequencyDistribution(Map<C, Iterable<V>> samples)
+    {
+        this();
+        for (Map.Entry<C, Iterable<V>> entry : samples.entrySet()) {
+            addSamples(entry.getKey(), entry.getValue());
+        }
+    }
 
-	    if (cfd.containsKey(t)) {
-	        freqDist = cfd.get(t);
-	    }
-	    else {
-	        freqDist = new FrequencyDistribution<V>();
-	        cfd.put(t, freqDist);
-	    }
+    /**
+     * @return The total number of sample outcomes that have been recorded by this
+     *         ConditionalFreqDist.
+     */
+    public long getN()
+    {
+        return n;
+    }
 
-	    long countBefore = freqDist.getN();
-	    freqDist.incAll(samples);
-	    this.n = n + (freqDist.getN() - countBefore); 
-	}
-	
+    /**
+     * Returns the total number of samples which equal a given <code>sample</code> under a given
+     * <code>condition</code>.
+     * 
+     * <p>
+     * If there are no samples for the condition in question on record, <code>0</code> will be
+     * returned.
+     * 
+     * @param condition
+     *            the condition
+     * @param sample
+     *            the sample under a given condition
+     * @return the number of all samples which equal <code>sample</code>
+     */
+    public long getCount(C condition, V sample)
+    {
+        if (cfd.containsKey(condition)) {
+            return cfd.get(condition).getCount(sample);
+        }
+        else {
+            return 0;
+        }
+    }
+
+    /**
+     * Returns the {@link FrequencyDistribution} under a given <code>condition</code>, or
+     * <code>null</code> if this distribution contains no such {@link FrequencyDistribution} for
+     * this <code>condition</code>.
+     * 
+     * @param condition
+     *            the condition
+     * @return the distribution the condition maps to
+     */
+    public FrequencyDistribution<V> getFrequencyDistribution(C condition)
+    {
+        return cfd.get(condition);
+    }
+
+    /**
+     * Returns all conditions for which samples have been recorded.
+     * 
+     * @return a {@link Set} of all recorded conditions
+     */
+    public Set<C> getConditions()
+    {
+        return this.cfd.keySet();
+    }
+
+    /**
+     * Indicates whether samples have been recorded under a given <code>condition</code>.
+     * 
+     * @param condition
+     *            the condition in question
+     * @return true if samples for <code>condition</code> exist
+     */
+    public boolean hasCondition(C condition)
+    {
+        return this.cfd.containsKey(condition);
+    }
+
+    /**
+     * Adds a sample under a given <code>condition</code>.
+     * 
+     * @param condition
+     *            the condition for this sample
+     * @param sample
+     *            the sample to add
+     */
+    public void addSample(C condition, V sample)
+    {
+        List<V> samples = new ArrayList<V>();
+        samples.add(sample);
+
+        addSamples(condition, samples);
+    }
+
+    /**
+     * Adds multiple samples under a given <code>condition</code>.
+     * 
+     * <p>
+     * If there is no {@link FrequencyDistribution} present for the given <code>condition</code>, a
+     * new empty one will be created and populated from the given <code>samples</code>.
+     * 
+     * @param condition
+     *            the condition for the samples
+     * @param samples
+     *            the samples to add
+     */
+
+    public void addSamples(C condition, Iterable<V> samples)
+    {
+        FrequencyDistribution<V> freqDist = null;
+
+        if (cfd.containsKey(condition)) {
+            freqDist = cfd.get(condition);
+        }
+        else {
+            freqDist = new FrequencyDistribution<V>();
+            cfd.put(condition, freqDist);
+        }
+
+        long countBefore = freqDist.getN();
+        freqDist.incAll(samples);
+        this.n = n + (freqDist.getN() - countBefore);
+    }
+
     @Override
     public String toString()
     {
--- de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.api.frequency/src/main/java/de/tudarmstadt/ukp/dkpro/core/api/frequency/util/FrequencyDistribution.java
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.api.frequency/src/main/java/de/tudarmstadt/ukp/dkpro/core/api/frequency/util/FrequencyDistribution.java
@@ -24,87 +24,181 @@ import bak.pcj.map.ObjectKeyLongOpenHashMap;
 
 // TODO already store sorted!
 /**
- * Inspired by nltk.probability.FreqDist
+ * It is basically a mapping from samples (keys) to long values (counts).
  * 
+ * <p>
+ * Suppose we want to record the number of occurrences of each word in a sentence, then this class
+ * can be used as follows:
+ * <p>
+ * <blockquote>
+ * 
+ * <pre>
+ * {
+ *     &#064;code
+ *     FrequencyDistribution&lt;String&gt; fd = new FrequencyDistribution&lt;String&gt;();
+ *     for (String word : &quot;foo bar baz foo&quot;.split(&quot; &quot;)) {
+ *         fd.inc(word);
+ *     }
+ *     System.out.println(fd.getCount(&quot;foo&quot;));
+ * }
+ * </pre>
+ * 
+ * </blockquote> The last call to {@link FrequencyDistribution#getCount} will yield 2, because the
+ * word "foo" has appeared twice in the given sequence of words.
+ * 
+ * <p>
+ * This class was inspired by NLTK's <a
+ * href="http://nltk.googlecode.com/svn/trunk/doc/api/nltk.probability.FreqDist-class.html">
+ * FreqDist</a>.
+ * 
+ * @param <T>
+ *            the type of the samples
+ * @see ConditionalFrequencyDistribution
  * @author zesch
  */
-public class FrequencyDistribution<T> {
-    
-	private ObjectKeyLongMap freqDist;
-	
-	private long n;
-	
-	public FrequencyDistribution() {
-		freqDist = new ObjectKeyLongOpenHashMap();
-		n = 0;
-	}
-	
-	public FrequencyDistribution(Iterable<T> iterable) {
-		this();
-		for (T o : iterable) {
-			addSample(o, 1);
-		}
-	}
-	
-	public boolean contains(T o) {
-	    return this.freqDist.containsKey(o);
-	}
-	
-	public void inc(T o) {
-		addSample(o, 1);
-	}
-	
-	public void incAll(Iterable<T> iterable ) {
-		for (T o : iterable) {
-			addSample(o, 1);
-		}	
-	}
-	
-	/**
-	 * @return The total number of sample outcomes that have been recorded by this FreqDist.
-	 */
-	public long getN() {
-		return n;
-	}
+public class FrequencyDistribution<T>
+{
 
-	/**
-	 * @return The total number of sample values (or bins) that have counts greater than zero.
-	 */
-	public long getB() {
-	    return this.freqDist.size();
-	}
-	
-	public long getCount(T o) {
-		if (freqDist.containsKey(o)) {
-			return freqDist.get(o);
-		}
-		else {
-			return 0;
-		}
-	}
+    private ObjectKeyLongMap freqDist;
 
-	@SuppressWarnings("unchecked")
-    public Set<T> getKeys() {
-//	    this.freqDist = sortByValue(this.freqDist);
-	    return this.freqDist.keySet();
-	}
-	
-	public void addSample(T o, long number) {
-		this.n = this.n + number;
-		if (freqDist.containsKey(o)) {
-			freqDist.put(o, freqDist.get(o) + number);
-		}
-		else {
-			freqDist.put(o, number);
-		}
-	}
-	
+    /** The total number of samples (accumulated count). */
+    private long n;
+
+    /**
+     * Creates a new empty {@link FrequencyDistribution}.
+     */
+    public FrequencyDistribution()
+    {
+        freqDist = new ObjectKeyLongOpenHashMap();
+        n = 0;
+    }
+
+    /**
+     * Creates a new {@link FrequencyDistribution} prefilled with samples from an {@link Iterable}.
+     * The count for each sample in the iterable is cumulatively increased by 1.
+     * 
+     * @param iterable
+     *            the {@link Iterable} used to fill the {@link FrequencyDistribution}
+     */
+    public FrequencyDistribution(Iterable<T> iterable)
+    {
+        this();
+        incAll(iterable);
+    }
+
+    /**
+     * Indicates whether this distribution contains outcomes for a given <code>sample</code>.
+     * 
+     * @param sample
+     *            the sample to look up
+     * @return true if samples exist
+     */
+    public boolean contains(T sample)
+    {
+        return this.freqDist.containsKey(sample);
+    }
+
+    /**
+     * Increments the count for a given <code>sample</code>.
+     * 
+     * @param sample
+     *            the sample to increment the count for
+     */
+    public void inc(T sample)
+    {
+        addSample(sample, 1);
+    }
+
+    /**
+     * Increments the count for each sample in a given {@link Iterable}.
+     * 
+     * @param iterable
+     *            the samples used to increment the counts
+     */
+
+    public void incAll(Iterable<T> iterable)
+    {
+        for (T o : iterable) {
+            addSample(o, 1);
+        }
+    }
+
+    /**
+     * Returns the total number of sample outcomes that have been recorded by this frequency
+     * distribution. This is equal to the accumulated count of all samples (duplicates included).
+     * 
+     * @return the total number of sample outcomes
+     */
+    public long getN()
+    {
+        return n;
+    }
+
+    /**
+     * Returns the total number of sample values (or bins) that have counts greater than zero. This
+     * is equal to the accumulated counts of all distinct samples (duplicates excluded).
+     * 
+     * @return the total number of bins
+     */
+    public long getB()
+    {
+        return this.freqDist.size();
+    }
+
+    /**
+     * Returns the count for a given <code>sample</code>. If no such samples have been recorded yet,
+     * <code>0</code> will be returned.
+     * 
+     * @param sample
+     *            the sample to get the count for
+     * @return the count for a given sample
+     */
+    public long getCount(T sample)
+    {
+        if (freqDist.containsKey(sample)) {
+            return freqDist.get(sample);
+        }
+        else {
+            return 0;
+        }
+    }
+
+    /**
+     * Returns the {@link Set} of sample values (or bins) for which counts have been recorded.
+     * 
+     * @return the set of bins
+     */
+    @SuppressWarnings("unchecked")
+    public Set<T> getKeys()
+    {
+        return this.freqDist.keySet();
+    }
+
+    /**
+     * Increases the count for a given <code>sample</code>.
+     * 
+     * @param sample
+     *            the sample to increase the count for
+     * @param number
+     *            the number to increase by
+     */
+    public void addSample(T sample, long number)
+    {
+        this.n = this.n + number;
+        if (freqDist.containsKey(sample)) {
+            freqDist.put(sample, freqDist.get(sample) + number);
+        }
+        else {
+            freqDist.put(sample, number);
+        }
+    }
+
     @SuppressWarnings("unchecked")
     @Override
     public String toString()
     {
         StringBuilder sb = new StringBuilder();
-//        this.freqDist = sortByValue(this.freqDist);
+        // this.freqDist = sortByValue(this.freqDist);
         for (Object o : freqDist.keySet()) {
             sb.append((T) o.toString());
             sb.append(" - ");
@@ -114,36 +208,37 @@ public class FrequencyDistribution<T> {
 
         return sb.toString();
     }
-//
-//    /**
-//     * Sorts a Map by its values.
-//     * 
-//     * @return the sorted Map
-//     */
-//    @SuppressWarnings("unchecked")
-//    private ObjectKeyIntOpenHashMap sortByValue(ObjectKeyIntOpenHashMap map)
-//    {
-//        List list = new LinkedList();
-//        ObjectKeyIntMapIterator entryIter = map.entries();
-//        while (entryIter.hasNext()) {
-//            entryIter.next();
-//            list.add();
-//        }
-//        Collections.sort(list, new Comparator()
-//        {
-//            @Override
-//            public int compare(Object arg0, Object arg1)
-//            {
-//                return ((Comparable) ((Map.Entry) (arg1)).getValue()).compareTo(((Map.Entry) (arg0)).getValue());
-//            }
-//
-//        });
-//
-//        Map result = new LinkedHashMap();
-//        for (Iterator iter = list.iterator(); iter.hasNext();) {
-//            Map.Entry entry = (Map.Entry) iter.next();
-//            result.put(entry.getKey(), entry.getValue());
-//        }
-//        return result;
-//    }
+    //
+    // /**
+    // * Sorts a Map by its values.
+    // *
+    // * @return the sorted Map
+    // */
+    // @SuppressWarnings("unchecked")
+    // private ObjectKeyIntOpenHashMap sortByValue(ObjectKeyIntOpenHashMap map)
+    // {
+    // List list = new LinkedList();
+    // ObjectKeyIntMapIterator entryIter = map.entries();
+    // while (entryIter.hasNext()) {
+    // entryIter.next();
+    // list.add();
+    // }
+    // Collections.sort(list, new Comparator()
+    // {
+    // @Override
+    // public int compare(Object arg0, Object arg1)
+    // {
+    // return ((Comparable) ((Map.Entry) (arg1)).getValue()).compareTo(((Map.Entry)
+    // (arg0)).getValue());
+    // }
+    //
+    // });
+    //
+    // Map result = new LinkedHashMap();
+    // for (Iterator iter = list.iterator(); iter.hasNext();) {
+    // Map.Entry entry = (Map.Entry) iter.next();
+    // result.put(entry.getKey(), entry.getValue());
+    // }
+    // return result;
+    // }
 }
