a24f8f6aa0704536ade430763518c56f8135e87a
Richard Eckart de Castilho <richard.eckart@gmail.com>
2012-06-07 10:19:07 +0000

    Task 69: Switch all test cases to OpenNLP POS Tagger http://code.google.com/p/dkpro-core-asl/issues/detail?id=69 - Migrate tests to OpenNlpPosTagger - Simplified tests using testing module

--- de.tudarmstadt.ukp.dkpro.core.posfilter/src/test/java/de/tudarmstadt/ukp/dkpro/core/posfilter/PosFilterTest.java
+++ de.tudarmstadt.ukp.dkpro.core.posfilter/src/test/java/de/tudarmstadt/ukp/dkpro/core/posfilter/PosFilterTest.java
@@ -17,103 +17,79 @@
  ******************************************************************************/
 package de.tudarmstadt.ukp.dkpro.core.posfilter;
 
-import static org.junit.Assert.assertEquals;
+import static java.util.Arrays.asList;
 import static org.uimafit.factory.AnalysisEngineFactory.createAggregateDescription;
 import static org.uimafit.factory.AnalysisEngineFactory.createPrimitiveDescription;
-import static org.uimafit.util.JCasUtil.*;
+import static org.uimafit.util.JCasUtil.select;
 
-import org.apache.uima.analysis_engine.AnalysisEngine;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.jcas.JCas;
-import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-import org.uimafit.factory.AnalysisEngineFactory;
-
-import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS;
+import org.junit.rules.TestName;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
-import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
-import de.tudarmstadt.ukp.dkpro.core.treetagger.TreeTaggerPosLemmaTT4J;
+import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpPosTagger;
+import de.tudarmstadt.ukp.dkpro.core.testing.AssertAnnotations;
+import de.tudarmstadt.ukp.dkpro.core.testing.TestRunner;
 
 public class PosFilterTest
 {
-
 	@Test
-	public void FilterTest()
+	public void testEnglish1()
 		throws Exception
 	{
-		checkModelsAndBinary("en");
+		String testDocument = "This is a not so long test sentence . This is a longer second " +
+				"test sentence . More sentences are necessary for the tests .";
 
-		String testDocument = "This is a not so long test sentence. This is a longer second test " +
-				"sentence. More sentences are necessary for the tests.";
+		String[] tokens = new String[] { "long", "test", "sentence", "second", "test", "sentence",
+				"More", "sentences", "necessary", "tests" };
 
-		AnalysisEngineDescription aggregate = createAggregateDescription(
-				createPrimitiveDescription(BreakIteratorSegmenter.class),
-				createPrimitiveDescription(TreeTaggerPosLemmaTT4J.class),
-				createPrimitiveDescription(PosFilter.class,
-						PosFilter.PARAM__TYPE_TO_REMOVE, Token.class.getName(),
-						PosFilter.PARAM_ADJ, true,
-						PosFilter.PARAM_N, true));
-
-		AnalysisEngine engine = AnalysisEngineFactory.createAggregate(aggregate);
-		JCas aJCas = engine.newJCas();
-		aJCas.setDocumentLanguage("en");
-		aJCas.setDocumentText(testDocument);
-
-		engine.process(aJCas);
-
-		int i = 0;
-		for (Token token : select(aJCas, Token.class)) {
-			System.out.println(token.getCoveredText());
-			i++;
-		}
-		assertEquals(10, i);
+		runTest("en", testDocument, tokens, PosFilter.PARAM_ADJ, true, PosFilter.PARAM_N, true);
 	}
 
 	@Test
-	public void FilterTest2()
+	public void testEnglish2()
 		throws Exception
 	{
-		checkModelsAndBinary("en");
+		String testDocument = "This is a not so long test sentence . This is a longer second " +
+				"test sentence . More sentences are necessary for the tests .";
 
-		String testDocument = "This is a not so long test sentence. This is a longer second test " +
-				"sentence. More sentences are necessary for the tests.";
+		String[] tokens = new String[] { "is", "long", "test", "sentence", "is", "second", "test",
+				"sentence", "More", "sentences", "are", "necessary", "tests" };
 
+		runTest("en", testDocument, tokens, PosFilter.PARAM_ADJ, true, PosFilter.PARAM_N, true,
+				PosFilter.PARAM_V, true);
+	}
+	
+	private void runTest(String language, String testDocument,
+			String[] aTokens, Object... aExtraParams)
+		throws Exception
+	{
+		List<Object> posFilterParams = new ArrayList<Object>();
+		posFilterParams.addAll(asList(PosFilter.PARAM__TYPE_TO_REMOVE, Token.class.getName()));
+		posFilterParams.addAll(asList(aExtraParams));
+		
 		AnalysisEngineDescription aggregate = createAggregateDescription(
-				createPrimitiveDescription(BreakIteratorSegmenter.class),
-				createPrimitiveDescription(TreeTaggerPosLemmaTT4J.class),
+				createPrimitiveDescription(OpenNlpPosTagger.class),
 				createPrimitiveDescription(PosFilter.class,
-						PosFilter.PARAM__TYPE_TO_REMOVE, Token.class.getName(),
-						PosFilter.PARAM_ADJ, true,
-						PosFilter.PARAM_N, true,
-						PosFilter.PARAM_V, true));
+						posFilterParams.toArray(new Object[posFilterParams.size()])));
 
-		AnalysisEngine engine = AnalysisEngineFactory.createAggregate(aggregate);
-		JCas aJCas = engine.newJCas();
-		aJCas.setDocumentText(testDocument);
-		aJCas.setDocumentLanguage("en");
-
-		engine.process(aJCas);
-
-		for (POS p : select(aJCas, POS.class)) {
-			System.out.println(p);
-		}
-
-		int i = 0;
-		for (Token token : select(aJCas, Token.class)) {
-			System.out.println(token.getCoveredText());
-			i++;
-		}
-		assertEquals(13, i);
+		JCas jcas = TestRunner.runTest(aggregate, language, testDocument);
+		
+		AssertAnnotations.assertToken(aTokens, select(jcas, Token.class));
 	}
 
-	private void checkModelsAndBinary(String lang)
-	{
-		Assume.assumeTrue(getClass().getResource(
-				"/de/tudarmstadt/ukp/dkpro/core/treetagger/lib/" + lang
-						+ "-tagger-little-endian.par") != null);
+	@Rule
+	public TestName name = new TestName();
 
-		Assume.assumeTrue(getClass().getResource(
-				"/de/tudarmstadt/ukp/dkpro/core/treetagger/bin/LICENSE.txt") != null);
+	@Before
+	public void printSeparator()
+	{
+		System.out.println("\n=== " + name.getMethodName() + " =====================");
 	}
 
 }
