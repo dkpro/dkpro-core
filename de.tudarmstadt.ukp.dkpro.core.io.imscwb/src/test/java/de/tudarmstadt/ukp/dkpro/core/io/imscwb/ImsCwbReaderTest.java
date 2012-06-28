f0e240285db7d69aaf157a7d87c50bf5b3c55bd9
Richard Eckart de Castilho <richard.eckart@gmail.com>
2012-06-28 21:14:01 +0000

    Task 83: [io.imscwb] Use mapping provide in ImsCwbReader http://code.google.com/p/dkpro-core-asl/issues/detail?id=83 - Renamed PARAM_TAGGER_TAGSET to PARAM_POS_MAPPING_LOCATION - Use mapping provider

--- de.tudarmstadt.ukp.dkpro.core.io.imscwb/src/test/java/de/tudarmstadt/ukp/dkpro/core/io/imscwb/ImsCwbReaderTest.java
+++ de.tudarmstadt.ukp.dkpro.core.io.imscwb/src/test/java/de/tudarmstadt/ukp/dkpro/core/io/imscwb/ImsCwbReaderTest.java
@@ -36,92 +36,84 @@ import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 
 public class ImsCwbReaderTest
 {
-    @Test
-    public void wackyTest()
-        throws Exception
-    {
-        
-        CollectionReader reader = createCollectionReader(
-                ImsCwbReader.class,
-                ImsCwbReader.PARAM_PATH, "src/test/resources/wacky/",
-                ImsCwbReader.PARAM_TAGGER_TAGSET, "de",
-                ImsCwbReader.PARAM_ENCODING, "ISO-8859-15",
-                ResourceCollectionReaderBase.PARAM_PATTERNS, new String[] {
-                    ResourceCollectionReaderBase.INCLUDE_PREFIX + "*.txt" }
-        );
+	@Test
+	public void wackyTest()
+		throws Exception
+	{
 
-        String firstSentence = "Nikita ( La Femme Nikita ) Dieser Episodenf\u00FChrer wurde von September 1998 bis Mai 1999 von Konstantin C.W. Volkmann geschrieben und im Mai 2000 von Stefan B\u00F6rzel \u00FCbernommen . ";
-        
-        int i = 0;
-        for (JCas jcas : new JCasIterable(reader)) {
-            // System.out.println(jcas.getDocumentText());
-            if (i == 0) {
-                assertEquals(11406, JCasUtil.select(jcas, Token.class).size());
-                assertEquals(11406, JCasUtil.select(jcas, Lemma.class).size());
-                assertEquals(11406, JCasUtil.select(jcas, POS.class).size());
-                assertEquals(717, JCasUtil.select(jcas, Sentence.class).size());
+		CollectionReader reader = createCollectionReader(ImsCwbReader.class,
+				ImsCwbReader.PARAM_PATH, "src/test/resources/wacky/",
+				ImsCwbReader.PARAM_LANGUAGE, "de",
+				ImsCwbReader.PARAM_ENCODING, "ISO-8859-15",
+				ResourceCollectionReaderBase.PARAM_PATTERNS, new String[] { "[+]*.txt" });
 
-                assertEquals(firstSentence, JCasUtil.select(jcas, Sentence.class).iterator().next().getCoveredText());
-                
-                assertEquals("\"http://www.epguides.de/nikita.htm\"", DocumentMetaData.get(jcas).getDocumentTitle());
-            }
-            i++;
-        }
-        
-        assertEquals(4, i);
+		String firstSentence = "Nikita ( La Femme Nikita ) Dieser Episodenf\u00FChrer wurde von " +
+				"September 1998 bis Mai 1999 von Konstantin C.W. Volkmann geschrieben und im Mai " +
+				"2000 von Stefan B\u00F6rzel \u00FCbernommen . ";
 
-    }
-    
-    @Test
-    public void wackyTest_noAnnotations()
-        throws Exception
-    {
-        
-        CollectionReader reader = createCollectionReader(
-                ImsCwbReader.class,
-                ImsCwbReader.PARAM_PATH, "src/test/resources/wacky/",
-                ImsCwbReader.PARAM_TAGGER_TAGSET, "src/test/resources/stts.map",
-                ImsCwbReader.PARAM_ENCODING, "ISO-8859-15",
-                ResourceCollectionReaderBase.PARAM_PATTERNS, new String[] {
-                    ResourceCollectionReaderBase.INCLUDE_PREFIX + "*.txt" },
-                ImsCwbReader.PARAM_WRITE_TOKENS, false,
-                ImsCwbReader.PARAM_WRITE_LEMMAS, false,
-                ImsCwbReader.PARAM_WRITE_POS, false,
-                ImsCwbReader.PARAM_WRITE_SENTENCES, false
-        );
+		int i = 0;
+		for (JCas jcas : new JCasIterable(reader)) {
+			// System.out.println(jcas.getDocumentText());
+			if (i == 0) {
+				assertEquals(11406, JCasUtil.select(jcas, Token.class).size());
+				assertEquals(11406, JCasUtil.select(jcas, Lemma.class).size());
+				assertEquals(11406, JCasUtil.select(jcas, POS.class).size());
+				assertEquals(717, JCasUtil.select(jcas, Sentence.class).size());
 
-        int i = 0;
-        for (JCas jcas : new JCasIterable(reader)) {
-            if (i == 0) {
-                assertEquals(0, JCasUtil.select(jcas, Token.class).size());
-                assertEquals(0, JCasUtil.select(jcas, POS.class).size());
-                assertEquals(0, JCasUtil.select(jcas, Sentence.class).size());
-            }
-            i++;
-        }
-        
-        assertEquals(4, i);
-    }
+				assertEquals(firstSentence, JCasUtil.select(jcas, Sentence.class).iterator().next()
+						.getCoveredText());
 
-    @Test(expected=ResourceInitializationException.class)
-    public void wackyTest__expectedException()
-        throws Exception
-    {
-        
-        CollectionReader reader = createCollectionReader(
-                ImsCwbReader.class,
-                ImsCwbReader.PARAM_PATH, "src/test/resources/wacky",
-                ImsCwbReader.PARAM_TAGGER_TAGSET, "src/test/resources/stts.map",
-                ImsCwbReader.PARAM_ENCODING, "ISO-8859-15",
-                ImsCwbReader.PARAM_WRITE_TOKENS, false,
-                ImsCwbReader.PARAM_WRITE_LEMMAS, true,
-                ImsCwbReader.PARAM_WRITE_POS, false,
-                ImsCwbReader.PARAM_WRITE_SENTENCES, false
-        );
+				assertEquals("\"http://www.epguides.de/nikita.htm\"", DocumentMetaData.get(jcas)
+						.getDocumentTitle());
+			}
+			i++;
+		}
 
-        for (JCas jcas : new JCasIterable(reader)) {
-            // should never get here
-            System.out.println(jcas.getDocumentText());
-        }
-    }
+		assertEquals(4, i);
+
+	}
+
+	@Test
+	public void wackyTest_noAnnotations()
+		throws Exception
+	{
+		CollectionReader reader = createCollectionReader(ImsCwbReader.class,
+				ImsCwbReader.PARAM_PATH, "src/test/resources/wacky/",
+				ImsCwbReader.PARAM_LANGUAGE, "de",
+				ImsCwbReader.PARAM_ENCODING, "ISO-8859-15",
+				ResourceCollectionReaderBase.PARAM_PATTERNS, new String[] { "[+]*.txt" },
+				ImsCwbReader.PARAM_WRITE_TOKENS, false, 
+				ImsCwbReader.PARAM_WRITE_LEMMAS, false,
+				ImsCwbReader.PARAM_WRITE_POS, false, 
+				ImsCwbReader.PARAM_WRITE_SENTENCES, false);
+
+		int i = 0;
+		for (JCas jcas : new JCasIterable(reader)) {
+			if (i == 0) {
+				assertEquals(0, JCasUtil.select(jcas, Token.class).size());
+				assertEquals(0, JCasUtil.select(jcas, POS.class).size());
+				assertEquals(0, JCasUtil.select(jcas, Sentence.class).size());
+			}
+			i++;
+		}
+
+		assertEquals(4, i);
+	}
+
+	@Test(expected = ResourceInitializationException.class)
+	public void wackyTest__expectedException()
+		throws Exception
+	{
+		CollectionReader reader = createCollectionReader(ImsCwbReader.class,
+				ImsCwbReader.PARAM_PATH, "src/test/resources/wacky",
+				ImsCwbReader.PARAM_LANGUAGE, "de",
+				ImsCwbReader.PARAM_ENCODING, "ISO-8859-15", ImsCwbReader.PARAM_WRITE_TOKENS, false,
+				ImsCwbReader.PARAM_WRITE_LEMMAS, true, ImsCwbReader.PARAM_WRITE_POS, false,
+				ImsCwbReader.PARAM_WRITE_SENTENCES, false);
+
+		for (JCas jcas : new JCasIterable(reader)) {
+			// should never get here
+			System.out.println(jcas.getDocumentText());
+		}
+	}
 }
