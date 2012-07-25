3ea8609a168fe4381ee3f4064c53bae0455c1725
Torsten Zesch <torsten.zesch@gmail.com>
2012-07-25 12:20:44 +0000

    Populate performance measuring module.

---
+++ de.tudarmstadt.ukp.dkpro.core.performance/src/main/java/de/tudarmstadt/ukp/dkpro/core/performance/ThroughputTest.java
@@ -0,0 +1,85 @@
+/*******************************************************************************
+ * Copyright 2012
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.tudarmstadt.ukp.dkpro.core.performance;
+
+import static de.tudarmstadt.ukp.dkpro.core.api.io.ResourceCollectionReaderBase.INCLUDE_PREFIX;
+import static org.uimafit.factory.AnalysisEngineFactory.createAggregateDescription;
+import static org.uimafit.factory.AnalysisEngineFactory.createPrimitiveDescription;
+import static org.uimafit.factory.CollectionReaderFactory.createCollectionReader;
+
+import java.io.IOException;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.collection.CollectionReader;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.uimafit.pipeline.SimplePipeline;
+
+import de.tudarmstadt.ukp.dkpro.core.io.tei.TEIReader;
+
+/**
+ * @author zesch
+ *
+ */
+public class ThroughputTest {
+
+    public void testAggregate(CollectionReader reader, AnalysisEngineDescription aggr)
+        throws IOException, UIMAException
+    {
+ 
+        AnalysisEngineDescription startTimerDesc = getTimerAED(false);
+        AnalysisEngineDescription endTimerDesc   = getTimerAED(true);
+        
+        AnalysisEngineDescription timerAggregate = createAggregateDescription(
+                startTimerDesc,
+                aggr,
+                endTimerDesc
+        );
+        
+        SimplePipeline.runPipeline(
+                reader,
+                timerAggregate
+        );
+    }
+    
+    public static CollectionReader getStandardReader(String languageCode) throws IOException, ResourceInitializationException {
+        if (languageCode.equals("en")) {
+//            String brownPath = "src/test/resources/brown_tei_gzip";
+            String brownPath = "/home/zesch/tmp/brown_tei";
+            
+            return createCollectionReader(
+                    TEIReader.class,
+                    TEIReader.PARAM_LANGUAGE, "en",
+                    TEIReader.PARAM_PATH, brownPath,
+                    TEIReader.PARAM_PATTERNS, new String[] {INCLUDE_PREFIX + "*.xml.gz"}
+            );
+        }
+        else {
+            throw new IllegalArgumentException("No standard reader available for language code: " + languageCode);
+        }
+    }
+    
+    private AnalysisEngineDescription getTimerAED(boolean isFinalTimer)
+        throws ResourceInitializationException
+    {
+        return createPrimitiveDescription(
+                ThroughputTestAE.class,
+                ThroughputTestAE.PARAM_IS_FINAL_TIME, isFinalTimer
+        );
+    }
+}
