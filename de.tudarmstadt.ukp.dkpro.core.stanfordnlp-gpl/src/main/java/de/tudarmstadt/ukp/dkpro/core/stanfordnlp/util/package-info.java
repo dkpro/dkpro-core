f5934f18244f53ed1779282e636736b2c9915b3f
Richard Eckart de Castilho <richard.eckart@gmail.com>
2012-10-13 20:17:10 +0000

    - Added several package-level javadocs.

---
+++ de.tudarmstadt.ukp.dkpro.core.stanfordnlp-gpl/src/main/java/de/tudarmstadt/ukp/dkpro/core/stanfordnlp/util/package-info.java
@@ -0,0 +1,15 @@
+/*******************************************************************************
+ * Copyright 2010
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ ******************************************************************************/
+
+/**
+ * Utility classes shared between different components.
+ */
+package de.tudarmstadt.ukp.dkpro.core.stanfordnlp.util;
