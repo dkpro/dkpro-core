881a313f192f9dbdb49310096e06271ab5a0d5cf
richard.eckart@gmail.com <richard.eckart@gmail.com@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2012-11-05 20:35:21 +0000

    - Add memory checks and GC calls, so that unit tests should run with 1GB - Updated expected results for some parser models

--- de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.clearnlp-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/clearnlp/ClearNlpDependencyParserTest.java
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.clearnlp-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/clearnlp/ClearNlpDependencyParserTest.java
@@ -23,6 +23,7 @@ import static org.uimafit.util.JCasUtil.select;
 
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.jcas.JCas;
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -43,6 +44,8 @@ public class ClearNlpDependencyParserTest
 	public void testEnglishDependencies()
 		throws Exception
 	{
+		Assume.assumeTrue(Runtime.getRuntime().maxMemory() > 3000000000l);
+
 		JCas jcas = runTest("en", null, documentEnglish);
 		
 		String[] dependencies = new String[] { "ADVMOD 15,26,10,14", "AMOD 35,43,15,26",
@@ -58,12 +61,14 @@ public class ClearNlpDependencyParserTest
 	public void testEnglishMedical()
 		throws Exception
 	{
+		Assume.assumeTrue(Runtime.getRuntime().maxMemory() > 1200000000l);
+		
 		JCas jcas = runTest("en", "medical", documentEnglish);
 		
-		String[] dependencies = new String[] { "ADVMOD 15,26,10,14", "AMOD 35,43,15,26",
-				"AMOD 69,81,64,68", "AMOD 99,101,102,110", "CC 69,81,82,85", "CONJ 69,81,86,98",
-				"DET 35,43,8,9", "DOBJ 3,7,35,43", "DOBJ 52,60,69,81", "NN 35,43,27,34",
-				"NSUBJ 3,7,0,2", "NSUBJ 52,60,46,51", "PREP 52,60,61,63", "PREP 69,81,99,101",
+		String[] dependencies = new String[] { "ADVCL 52,60,102,110", "ADVMOD 15,26,10,14",
+				"AMOD 35,43,15,26", "AMOD 69,81,64,68", "CC 69,81,82,85", "CONJ 69,81,86,98",
+				"DET 35,43,8,9", "DOBJ 3,7,35,43", "MARK 102,110,99,101", "NN 35,43,27,34",
+				"NSUBJ 3,7,0,2", "NSUBJ 52,60,46,51", "POBJ 61,63,69,81", "PREP 52,60,61,63",
 				"PUNCT 3,7,111,112", "PUNCT 35,43,44,45", "RCMOD 35,43,52,60" };
 
 		AssertAnnotations.assertDependencies(dependencies, select(jcas, Dependency.class));
@@ -73,12 +78,14 @@ public class ClearNlpDependencyParserTest
 	public void testEnglishCraft()
 		throws Exception
 	{
+		Assume.assumeTrue(Runtime.getRuntime().maxMemory() > 1000000000l);
+		
 		JCas jcas = runTest("en", "craft", documentEnglish);
 		
-		String[] dependencies = new String[] { "ADVMOD 15,26,10,14", "AMOD 35,43,15,26",
-				"AMOD 69,81,64,68", "AMOD 99,101,102,110", "CC 69,81,82,85", "CONJ 69,81,86,98",
-				"DET 35,43,8,9", "DOBJ 3,7,35,43", "DOBJ 52,60,69,81", "NN 35,43,27,34",
-				"NSUBJ 3,7,0,2", "NSUBJ 52,60,46,51", "PREP 52,60,61,63", "PREP 69,81,99,101",
+		String[] dependencies = new String[] { "ACOMP 52,60,102,110", "ADVMOD 102,110,61,63",
+				"ADVMOD 15,26,10,14", "AMOD 35,43,15,26", "AMOD 69,81,64,68", "CC 69,81,82,85",
+				"CONJ 69,81,86,98", "DEP 52,60,46,51", "DET 35,43,8,9", "DOBJ 3,7,35,43",
+				"MARK 102,110,99,101", "NN 35,43,27,34", "NSUBJ 3,7,0,2", "POBJ 61,63,69,81",
 				"PUNCT 3,7,111,112", "PUNCT 35,43,44,45", "RCMOD 35,43,52,60" };
 
 		AssertAnnotations.assertDependencies(dependencies, select(jcas, Dependency.class));
@@ -102,6 +109,10 @@ public class ClearNlpDependencyParserTest
 	@Before
 	public void printSeparator()
 	{
+		Runtime.getRuntime().gc();
+		Runtime.getRuntime().gc();
+		Runtime.getRuntime().gc();
+		
 		System.out.println("\n=== "+testName.getMethodName()+" =====================");
 	}
 }
