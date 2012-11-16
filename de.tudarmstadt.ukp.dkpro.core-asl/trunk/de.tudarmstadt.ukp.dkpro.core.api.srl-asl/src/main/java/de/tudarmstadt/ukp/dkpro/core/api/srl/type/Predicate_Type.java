5c27c786f1bae78814c0894ad74b072d7af747db
richard.eckart@gmail.com <richard.eckart@gmail.com@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2012-11-16 09:06:22 +0000

    - Added api.arl module

---
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.api.srl-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/api/srl/type/Predicate_Type.java
@@ -0,0 +1,123 @@
+
+/* First created by JCasGen Mon Nov 12 23:24:12 CET 2012 */
+package de.tudarmstadt.ukp.dkpro.core.api.srl.type;
+
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.cas.impl.CASImpl;
+import org.apache.uima.cas.impl.FSGenerator;
+import org.apache.uima.cas.FeatureStructure;
+import org.apache.uima.cas.impl.TypeImpl;
+import org.apache.uima.cas.Type;
+import org.apache.uima.cas.impl.FeatureImpl;
+import org.apache.uima.cas.Feature;
+import org.apache.uima.jcas.tcas.Annotation_Type;
+
+/** The predicate or main verb of a sentence.
+ * Updated by JCasGen Mon Nov 12 23:24:12 CET 2012
+ * @generated */
+public class Predicate_Type extends Annotation_Type {
+  /** @generated */
+  @Override
+  protected FSGenerator getFSGenerator() {return fsGenerator;}
+  /** @generated */
+  private final FSGenerator fsGenerator = 
+    new FSGenerator() {
+      public FeatureStructure createFS(int addr, CASImpl cas) {
+  			 if (Predicate_Type.this.useExistingInstance) {
+  			   // Return eq fs instance if already created
+  		     FeatureStructure fs = Predicate_Type.this.jcas.getJfsFromCaddr(addr);
+  		     if (null == fs) {
+  		       fs = new Predicate(addr, Predicate_Type.this);
+  			   Predicate_Type.this.jcas.putJfsFromCaddr(addr, fs);
+  			   return fs;
+  		     }
+  		     return fs;
+        } else return new Predicate(addr, Predicate_Type.this);
+  	  }
+    };
+  /** @generated */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = Predicate.typeIndexID;
+  /** @generated 
+     @modifiable */
+  @SuppressWarnings ("hiding")
+  public final static boolean featOkTst = JCasRegistry.getFeatOkTst("de.tudarmstadt.ukp.dkpro.core.api.srl.type.Predicate");
+ 
+  /** @generated */
+  final Feature casFeat_category;
+  /** @generated */
+  final int     casFeatCode_category;
+  /** @generated */ 
+  public String getCategory(int addr) {
+        if (featOkTst && casFeat_category == null)
+      jcas.throwFeatMissing("category", "de.tudarmstadt.ukp.dkpro.core.api.srl.type.Predicate");
+    return ll_cas.ll_getStringValue(addr, casFeatCode_category);
+  }
+  /** @generated */    
+  public void setCategory(int addr, String v) {
+        if (featOkTst && casFeat_category == null)
+      jcas.throwFeatMissing("category", "de.tudarmstadt.ukp.dkpro.core.api.srl.type.Predicate");
+    ll_cas.ll_setStringValue(addr, casFeatCode_category, v);}
+    
+  
+ 
+  /** @generated */
+  final Feature casFeat_arguments;
+  /** @generated */
+  final int     casFeatCode_arguments;
+  /** @generated */ 
+  public int getArguments(int addr) {
+        if (featOkTst && casFeat_arguments == null)
+      jcas.throwFeatMissing("arguments", "de.tudarmstadt.ukp.dkpro.core.api.srl.type.Predicate");
+    return ll_cas.ll_getRefValue(addr, casFeatCode_arguments);
+  }
+  /** @generated */    
+  public void setArguments(int addr, int v) {
+        if (featOkTst && casFeat_arguments == null)
+      jcas.throwFeatMissing("arguments", "de.tudarmstadt.ukp.dkpro.core.api.srl.type.Predicate");
+    ll_cas.ll_setRefValue(addr, casFeatCode_arguments, v);}
+    
+   /** @generated */
+  public int getArguments(int addr, int i) {
+        if (featOkTst && casFeat_arguments == null)
+      jcas.throwFeatMissing("arguments", "de.tudarmstadt.ukp.dkpro.core.api.srl.type.Predicate");
+    if (lowLevelTypeChecks)
+      return ll_cas.ll_getRefArrayValue(ll_cas.ll_getRefValue(addr, casFeatCode_arguments), i, true);
+    jcas.checkArrayBounds(ll_cas.ll_getRefValue(addr, casFeatCode_arguments), i);
+	return ll_cas.ll_getRefArrayValue(ll_cas.ll_getRefValue(addr, casFeatCode_arguments), i);
+  }
+   
+  /** @generated */ 
+  public void setArguments(int addr, int i, int v) {
+        if (featOkTst && casFeat_arguments == null)
+      jcas.throwFeatMissing("arguments", "de.tudarmstadt.ukp.dkpro.core.api.srl.type.Predicate");
+    if (lowLevelTypeChecks)
+      ll_cas.ll_setRefArrayValue(ll_cas.ll_getRefValue(addr, casFeatCode_arguments), i, v, true);
+    jcas.checkArrayBounds(ll_cas.ll_getRefValue(addr, casFeatCode_arguments), i);
+    ll_cas.ll_setRefArrayValue(ll_cas.ll_getRefValue(addr, casFeatCode_arguments), i, v);
+  }
+ 
+
+
+
+  /** initialize variables to correspond with Cas Type and Features
+	* @generated */
+  public Predicate_Type(JCas jcas, Type casType) {
+    super(jcas, casType);
+    casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl)this.casType, getFSGenerator());
+
+ 
+    casFeat_category = jcas.getRequiredFeatureDE(casType, "category", "uima.cas.String", featOkTst);
+    casFeatCode_category  = (null == casFeat_category) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_category).getCode();
+
+ 
+    casFeat_arguments = jcas.getRequiredFeatureDE(casType, "arguments", "uima.cas.FSArray", featOkTst);
+    casFeatCode_arguments  = (null == casFeat_arguments) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_arguments).getCode();
+
+  }
+}
+
+
+
+    
