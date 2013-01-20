76cc7086bb64819800074bd2125332287c916b2b
Torsten Zesch <torsten.zesch@gmail.com>
2013-01-20 14:29:35 +0000

    Added license header.

--- de.tudarmstadt.ukp.dkpro.core.io.jwpl-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/jwpl/WikipediaLinkReader.java
+++ de.tudarmstadt.ukp.dkpro.core.io.jwpl-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/jwpl/WikipediaLinkReader.java
@@ -1,17 +1,31 @@
+/*******************************************************************************
+ * Copyright 2012
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package de.tudarmstadt.ukp.dkpro.core.io.jwpl;
 
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
-import org.apache.commons.collections.ListUtils;
 import org.apache.uima.collection.CollectionException;
 import org.apache.uima.jcas.JCas;
 import org.uimafit.descriptor.ConfigurationParameter;
 
 import de.tudarmstadt.ukp.dkpro.core.io.jwpl.type.WikipediaLink;
-import de.tudarmstadt.ukp.dkpro.core.io.jwpl.util.WikiUtils;
 import de.tudarmstadt.ukp.wikipedia.api.Page;
 import de.tudarmstadt.ukp.wikipedia.api.exception.WikiTitleParsingException;
 import de.tudarmstadt.ukp.wikipedia.parser.Link;
