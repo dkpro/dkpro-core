bac48a1357c0dd704aa1f391701824fa3f2878ed
richard.eckart@gmail.com <richard.eckart@gmail.com@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2013-02-11 11:56:32 +0000

    Task 109: [io.bnc] Add test case http://code.google.com/p/dkpro-core-asl/issues/detail?id=109 - Added test case and test file FX8.xml from BNC with kind permission. See NOTICE.txt for details

---
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.io.bnc-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/io/bnc/BncReaderTest.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright 2011
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universit√§t Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.tudarmstadt.ukp.dkpro.core.io.bnc;
+
+import static org.junit.Assert.assertEquals;
+import static org.uimafit.factory.AnalysisEngineFactory.createPrimitiveDescription;
+import static org.uimafit.factory.CollectionReaderFactory.createDescription;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.collection.CollectionReaderDescription;
+import org.junit.Test;
+import org.uimafit.component.xwriter.CASDumpWriter;
+import org.uimafit.pipeline.SimplePipeline;
+
+public class BncReaderTest
+{
+	@Test
+	public void test() throws Exception
+	{
+		CollectionReaderDescription reader = createDescription(BncReader.class, 
+				BncReader.PARAM_PATH, "src/test/resources",
+				BncReader.PARAM_PATTERNS, new String[] { "[+]FX8.xml" },
+				BncReader.PARAM_LANGUAGE, "en");
+		
+		AnalysisEngineDescription casDumper = createPrimitiveDescription(CASDumpWriter.class, 
+				CASDumpWriter.PARAM_OUTPUT_FILE, "target/test-output/FX8.dump");
+		
+		SimplePipeline.runPipeline(reader, casDumper);
+		
+		String reference = FileUtils.readFileToString(
+				new File("src/test/resources/reference/FX8.dump"), "UTF-8");
+		String actual = FileUtils.readFileToString(
+				new File("target/test-output/FX8.dump"), "UTF-8");
+		assertEquals(reference, actual);
+	}
+}
