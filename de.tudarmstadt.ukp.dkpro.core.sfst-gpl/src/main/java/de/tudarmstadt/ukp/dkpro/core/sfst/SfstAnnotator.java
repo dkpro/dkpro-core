5b357346d774c726d2f6a80617eeedccb538c713
Richard Eckart de Castilho <richard.eckart@gmail.com>
2013-03-16 15:18:55 +0000

    Task 121: Add missing JavaDoc for parameters and unify parameter names http://code.google.com/p/dkpro-core-asl/issues/detail?id=121

--- de.tudarmstadt.ukp.dkpro.core.sfst-gpl/src/main/java/de/tudarmstadt/ukp/dkpro/core/sfst/SfstAnnotator.java
+++ de.tudarmstadt.ukp.dkpro.core.sfst-gpl/src/main/java/de/tudarmstadt/ukp/dkpro/core/sfst/SfstAnnotator.java
@@ -60,18 +60,26 @@ import de.tudarmstadt.ukp.dkpro.core.sfst.parser.TurkishAnalysisParser;
 public class SfstAnnotator
     extends JCasAnnotator_ImplBase
 {
+	/**
+	 * Use this language instead of the language set in the CAS to locate the model.
+	 */
+	public static final String PARAM_LANGUAGE = ComponentParameters.PARAM_LANGUAGE;
+	@ConfigurationParameter(name = PARAM_LANGUAGE, mandatory = false)
+	protected String language;
 
-    public static final String PARAM_LANGUAGE = ComponentParameters.PARAM_LANGUAGE;
-    @ConfigurationParameter(name = PARAM_LANGUAGE, mandatory = false)
-    protected String language;
+	/**
+	 * Override the default variant used to locate the model.
+	 */
+	public static final String PARAM_VARIANT = ComponentParameters.PARAM_VARIANT;
+	@ConfigurationParameter(name = PARAM_VARIANT, mandatory = false)
+	protected String variant;
 
-    public static final String PARAM_VARIANT = ComponentParameters.PARAM_VARIANT;
-    @ConfigurationParameter(name = PARAM_VARIANT, mandatory = false)
-    protected String variant;
-
-    public static final String PARAM_MODEL_LOCATION = ComponentParameters.PARAM_MODEL_LOCATION;
-    @ConfigurationParameter(name = PARAM_MODEL_LOCATION, mandatory = false)
-    protected String modelLocation;
+	/**
+	 * Load the model from this location instead of locating the model automatically.
+	 */
+	public static final String PARAM_MODEL_LOCATION = ComponentParameters.PARAM_MODEL_LOCATION;
+	@ConfigurationParameter(name = PARAM_MODEL_LOCATION, mandatory = false)
+	protected String modelLocation;
 
     private CasConfigurableProviderBase<File> modelProvider;
     private CasConfigurableProviderBase<AnalysisParser> parserProvider;
