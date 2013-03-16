9eb0773e08c2e784812b352d1d0d5ada2791043d
richard.eckart@gmail.com <richard.eckart@gmail.com@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2013-03-16 15:21:33 +0000

    Task 121: Add missing JavaDoc for parameters and unify parameter names http://code.google.com/p/dkpro-core-asl/issues/detail?id=121

--- de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.clearnlp-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/clearnlp/ClearNlpPosTagger.java
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.clearnlp-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/clearnlp/ClearNlpPosTagger.java
@@ -73,26 +73,46 @@ import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 public class ClearNlpPosTagger
 	extends JCasAnnotator_ImplBase
 {
+	/**
+	 * Use this language instead of the language set in the CAS to locate the model.
+	 */
 	public static final String PARAM_LANGUAGE = ComponentParameters.PARAM_LANGUAGE;
 	@ConfigurationParameter(name = PARAM_LANGUAGE, mandatory = false)
 	protected String language;
 
+	/**
+	 * Override the default variant used to locate the model.
+	 */
 	public static final String PARAM_VARIANT = ComponentParameters.PARAM_VARIANT;
 	@ConfigurationParameter(name = PARAM_VARIANT, mandatory = false)
 	protected String variant;
 
+	/**
+	 * Load the model from this location instead of locating the model automatically.
+	 */
 	public static final String PARAM_MODEL_LOCATION = ComponentParameters.PARAM_MODEL_LOCATION;
 	@ConfigurationParameter(name = PARAM_MODEL_LOCATION, mandatory = false)
 	protected String modelLocation;
 
+	/**
+	 * Load the part-of-speech tag to UIMA type mapping from this location instead of locating
+	 * the mapping automatically.
+	 */
 	public static final String PARAM_TAGGER_MAPPING_LOCATION = ComponentParameters.PARAM_TAGGER_MAPPING_LOCATION;
 	@ConfigurationParameter(name = PARAM_TAGGER_MAPPING_LOCATION, mandatory = false)
 	protected String mappingLocation;
 
+	/**
+	 * Use the {@link String#intern()} method on tags. This is usually a good idea to avoid
+	 * spaming the heap with thousands of strings representing only a few different tags.
+	 */
 	public static final String PARAM_INTERN_TAGS = ComponentParameters.PARAM_INTERN_TAGS;
 	@ConfigurationParameter(name = PARAM_INTERN_TAGS, mandatory = false, defaultValue = "true")
 	private boolean internTags;
 
+	/**
+	 * Log the tag set(s) when a model is loaded.
+	 */
 	public static final String PARAM_PRINT_TAGSET = ComponentParameters.PARAM_PRINT_TAGSET;
 	@ConfigurationParameter(name = PARAM_PRINT_TAGSET, mandatory = true, defaultValue="false")
 	protected boolean printTagSet;
