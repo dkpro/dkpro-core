35793b988ff2e806f4d429729d3f9ad7dd53bab3
Richard Eckart de Castilho <richard.eckart@gmail.com>
2013-04-09 17:07:13 +0000

    Task 132: Add SRL component from ClearNLP http://code.google.com/p/dkpro-core-asl/issues/detail?id=132 - Added SRL component and tests

---
+++ de.tudarmstadt.ukp.dkpro.core.clearnlp-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/clearnlp/ClearNlpSemanticRoleLabelerTest.java
@@ -0,0 +1,99 @@
+/*******************************************************************************
+ * Copyright 2013
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.tudarmstadt.ukp.dkpro.core.clearnlp;
+
+import static org.uimafit.factory.AnalysisEngineFactory.createAggregateDescription;
+import static org.uimafit.factory.AnalysisEngineFactory.createPrimitiveDescription;
+import static org.uimafit.util.JCasUtil.select;
+
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.jcas.JCas;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+import de.tudarmstadt.ukp.dkpro.core.api.srl.type.SemanticPredicate;
+import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpPosTagger;
+import de.tudarmstadt.ukp.dkpro.core.testing.AssertAnnotations;
+import de.tudarmstadt.ukp.dkpro.core.testing.TestRunner;
+
+public class ClearNlpSemanticRoleLabelerTest
+{
+	static final String documentEnglish = "We need a very complicated example sentence , which "
+			+ "contains as many constituents and dependencies as possible .";
+
+	@Test
+	public void testEnglish()
+		throws Exception
+	{
+		// Assume.assumeTrue(Runtime.getRuntime().maxMemory() > 3000000000l);
+
+		JCas jcas = runTest("en", null, documentEnglish);
+
+		String[] predicates = new String[] {
+				"contains (contain.01): [(A0:sentence)(R-A0:which)(A1:as)]",
+				"need (need.01): [(A0:We)(A1:sentence)]" };
+
+		AssertAnnotations.assertSemanticPredicates(predicates,
+				select(jcas, SemanticPredicate.class));
+	}
+
+	@Test
+	public void testEnglishMayo()
+		throws Exception
+	{
+		// Assume.assumeTrue(Runtime.getRuntime().maxMemory() > 3000000000l);
+
+		JCas jcas = runTest("en", "mayo", documentEnglish);
+
+		String[] predicates = new String[] {
+				"contains (contain.01): [(A0:sentence)(R-A0:which)(A1:as)]",
+				"need (need.01): [(A0:We)(A1:sentence)]" };
+
+		AssertAnnotations.assertSemanticPredicates(predicates,
+				select(jcas, SemanticPredicate.class));
+	}
+
+	private JCas runTest(String aLanguage, String aVariant, String aText)
+		throws Exception
+	{
+		AnalysisEngineDescription engine = createAggregateDescription(
+				createPrimitiveDescription(OpenNlpPosTagger.class),
+				createPrimitiveDescription(ClearNlpLemmatizer.class),
+				createPrimitiveDescription(ClearNlpDependencyParser.class),
+				createPrimitiveDescription(ClearNlpSemanticRoleLabeler.class,
+						ClearNlpDependencyParser.PARAM_VARIANT, aVariant,
+						ClearNlpDependencyParser.PARAM_PRINT_TAGSET, true));
+
+		return TestRunner.runTest(engine, aLanguage, aText);
+	}
+
+	@Rule
+	public TestName testName = new TestName();
+
+	@Before
+	public void printSeparator()
+	{
+		Runtime.getRuntime().gc();
+		Runtime.getRuntime().gc();
+		Runtime.getRuntime().gc();
+
+		System.out.println("\n=== " + testName.getMethodName() + " =====================");
+	}
+}
