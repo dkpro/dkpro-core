abea2b03896b4fbc1021b82264c7d4a5cf8ce149
Pedro Santos <pedrobssantos@gmail.com>
2013-04-25 12:11:59 +0000

    Issue 105: Adding type capabilities to NormalizationDataReader.

--- de.tudarmstadt.ukp.dkpro.core.umlautnormalizer-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/umlautnormalizer/io/NormalizationDataReader.java
+++ de.tudarmstadt.ukp.dkpro.core.umlautnormalizer-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/umlautnormalizer/io/NormalizationDataReader.java
@@ -27,14 +27,19 @@ import org.apache.commons.io.IOUtils;
 import org.apache.uima.cas.CAS;
 import org.apache.uima.collection.CollectionException;
 import org.uimafit.descriptor.ConfigurationParameter;
+import org.uimafit.descriptor.TypeCapability;
 
 import de.tudarmstadt.ukp.dkpro.core.api.io.ResourceCollectionReaderBase;
 import de.tudarmstadt.ukp.dkpro.core.api.parameter.ComponentParameters;
 
+@TypeCapability(
+        outputs={
+                "de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData"})
+
 public class NormalizationDataReader
     extends ResourceCollectionReaderBase
 {
-    
+
     /**
      * Name of configuration parameter that contains the character encoding used by the input files.
      */
