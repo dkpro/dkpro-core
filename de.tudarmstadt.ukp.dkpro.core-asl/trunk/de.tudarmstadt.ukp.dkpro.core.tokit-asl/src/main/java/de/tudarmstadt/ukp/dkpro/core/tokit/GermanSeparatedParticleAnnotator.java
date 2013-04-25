b8fd5198ac62285acce914934a6cdc1c615f2e97
pedrobssantos <pedrobssantos@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2013-04-25 15:01:09 +0000

    Issue 105: Adding type capabilities to GermanSeparetedParticleAnnotator.

--- de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.tokit-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/tokit/GermanSeparatedParticleAnnotator.java
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.tokit-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/tokit/GermanSeparatedParticleAnnotator.java
@@ -2,13 +2,13 @@
  * Copyright 2012
  * Ubiquitous Knowledge Processing (UKP) Lab
  * Technische Universität Darmstadt
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,6 +22,7 @@ import java.util.List;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.jcas.JCas;
 import org.uimafit.component.JCasAnnotator_ImplBase;
+import org.uimafit.descriptor.TypeCapability;
 import org.uimafit.util.JCasUtil;
 
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Lemma;
@@ -29,19 +30,28 @@ import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 
 /**
- * Annotator to be used for post-processing of German corpora that have been lemmatized and POS-tagged with the 
+ * Annotator to be used for post-processing of German corpora that have been lemmatized and POS-tagged with the
  * TreeTagger, based on the STTS tagset.
- * 
+ *
  * This Annotator deals with German particle verbs. Particle verbs consist of a particle and a stem, e.g. anfangen = an+fangen
  * There are many usages of German particle verbs where the stem and the particle are separated, e.g., Wir fangen gleich an.
- * The TreeTagger lemmatizes the verb stem as "fangen" and the separated particle as "an", 
+ * The TreeTagger lemmatizes the verb stem as "fangen" and the separated particle as "an",
  * the proper verblemma "anfangen" is thus not available as an annotation.
  * The GermanSeparatedParticleAnnotator replaces the lemma of the stem of particle-verbs (e.g., fangen) by the proper verb lemma
  * (e.g. anfangen) and leaves the lemma of the separated particle unchanged.
- * 
+ *
  * @author Judith Eckle-Kohler
  *
  */
+@TypeCapability(
+        inputs={
+                "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence",
+                "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token",
+                "de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS",
+                "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Lemma"},
+        outputs={
+                "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Lemma"})
+
 public class GermanSeparatedParticleAnnotator extends JCasAnnotator_ImplBase {
 
 	@Override
@@ -56,7 +66,7 @@ public class GermanSeparatedParticleAnnotator extends JCasAnnotator_ImplBase {
 					// go back and find the next finite verb
 						String particle = token.getCoveredText();
 						String verblemma = "";
-						
+
 						int j = i-1;
 						while (j >= 0){
 							Token t = tokens.get(j);
@@ -71,10 +81,10 @@ public class GermanSeparatedParticleAnnotator extends JCasAnnotator_ImplBase {
 							}
 							j--;
 						}
-						
-					} 					
+
+					}
 				}
-			
+
 			} // for all tokens in the sentence
 
 		} // for all sentences
