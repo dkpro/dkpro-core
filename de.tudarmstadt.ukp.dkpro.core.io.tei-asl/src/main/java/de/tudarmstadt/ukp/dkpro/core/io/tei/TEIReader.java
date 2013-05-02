852afb916d21b118906e3c3e237f8bef73c0255e
Pedro Santos <pedrobssantos@gmail.com>
2013-05-02 15:20:05 +0000

    Issue 133: Changing TEIReader to use pos mapping files.

--- de.tudarmstadt.ukp.dkpro.core.io.tei-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/tei/TEIReader.java
+++ de.tudarmstadt.ukp.dkpro.core.io.tei-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/tei/TEIReader.java
@@ -62,11 +62,11 @@ import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 
 /**
  * Reader for the TEI XML.
- * 
+ *
  * @author Richard Eckart de Castilho
  */
 @TypeCapability(
-		outputs = { 
+		outputs = {
 			"de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData",
 		    "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence",
 		    "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token",
@@ -90,7 +90,7 @@ public class TEIReader
 	public static final String PARAM_WRITE_SENTENCES = "writeSentences";
 	@ConfigurationParameter(name = PARAM_WRITE_SENTENCES, mandatory = true, defaultValue = "true")
 	private boolean writeSentences;
-	
+
 	/**
 	 * Use the xml:id attribute on the TEI elements as document ID. Mind that many TEI files
 	 * may not have this attribute on all TEI elements and you may end up with no document ID
@@ -125,12 +125,12 @@ public class TEIReader
 	 * (character) contains a significant punctuation mark as identified by the CLAWS tagger.
 	 */
 	private static final String TAG_CHARACTER = "c";
-	
+
 	/**
 	 * (word) represents a grammatical (not necessarily orthographic) word.
 	 */
 	private static final String TAG_WORD = "w";
-	
+
 	/**
 	 * (s-unit) contains a sentence-like division of a text.
 	 */
@@ -141,12 +141,12 @@ public class TEIReader
 	 * drama, a collection of essays, a novel, a dictionary, or a corpus sample.
 	 */
 	private static final String TAG_TEXT = "text";
-	
+
 	/**
 	 * contains the full title of a work of any kind.
 	 */
 	private static final String TAG_TITLE = "title";
-	
+
 	/**
 	 * (TEI document) contains a single TEI-conformant document, comprising a TEI header and a text,
 	 * either in isolation or as part of a teiCorpus element.
@@ -174,11 +174,11 @@ public class TEIReader
 			throw new ResourceInitializationException(new IllegalArgumentException(
 					"Setting writePOS to 'true' requires writeToken to be 'true' too."));
 		}
-		
+
 		try {
 			// Init with an empty iterator
 			teiElementIterator = asList(new Element[0]).iterator();
-			
+
 			// Make sure we know about the first element;
 			nextTeiElement();
 		}
@@ -191,21 +191,21 @@ public class TEIReader
 
 		posMappingProvider = new MappingProvider();
 		posMappingProvider.setDefault(MappingProvider.LOCATION, "classpath:/de/tudarmstadt/ukp/dkpro/" +
-				"core/api/lexmorph/tagset/${language}-${tagger.tagset}-tagger.map");
+				"core/api/lexmorph/tagset/${language}-${tagger.tagset}-pos.map");
 		posMappingProvider.setDefault(MappingProvider.BASE_TYPE, POS.class.getName());
 		posMappingProvider.setDefault("tagger.tagset", "default");
 		posMappingProvider.setOverride(MappingProvider.LOCATION, mappingPosLocation);
 		posMappingProvider.setOverride(MappingProvider.LANGUAGE, getLanguage());
 		posMappingProvider.setOverride("tagger.tagset", posTagset);
 	}
-	
+
 	private void nextTeiElement() throws CollectionException, IOException
 	{
 		if (teiElementIterator == null) {
 			currentTeiElement = null;
 			return;
 		}
-		
+
 		while (!teiElementIterator.hasNext() && super.hasNext()) {
 			currentResource = nextFile();
 
@@ -213,26 +213,26 @@ public class TEIReader
 
 			try {
 				is = currentResource.getInputStream();
-				
+
 				if (currentResource.getPath().endsWith(".gz")) {
 				    is = new GZIPInputStream(is);
 				}
-				
+
 				InputSource source = new InputSource(is);
 				source.setPublicId(currentResource.getLocation());
 				source.setSystemId(currentResource.getLocation());
-				
+
 				SAXReader reader = new SAXReader();
 				Document xml = reader.read(source);
-				
+
 				final XPath teiPath = new Dom4jXPath("//tei:TEI");
 				teiPath.addNamespace("tei", "http://www.tei-c.org/ns/1.0");
 
 				List<Element> teiElements = teiPath.selectNodes(xml);
-				
+
 //				System.out.printf("Found %d TEI elements in %s.%n", teiElements.size(),
 //						currentResource.getLocation());
-				
+
 				teiElementIterator = teiElements.iterator();
 				currentTeiElementNumber = 0;
 			}
@@ -246,23 +246,23 @@ public class TEIReader
 				closeQuietly(is);
 			}
 		}
-		
+
 		currentTeiElement = teiElementIterator.hasNext() ? teiElementIterator.next() : null;
 		currentTeiElementNumber++;
-		
+
 		if (!super.hasNext() && !teiElementIterator.hasNext()) {
 			// Mark end of processing.
 			teiElementIterator = null;
 		}
 	}
-	
+
 	@Override
 	public boolean hasNext()
 		throws IOException, CollectionException
 	{
-		return teiElementIterator != null || currentTeiElement != null;	
+		return teiElementIterator != null || currentTeiElement != null;
 	}
-	
+
 	@Override
 	public void getNext(CAS aCAS)
 		throws IOException, CollectionException
@@ -301,16 +301,16 @@ public class TEIReader
 		finally {
 			closeQuietly(is);
 		}
-		
+
 		// Move currentTeiElement to the next text
 		nextTeiElement();
 	}
-	
+
 	protected Handler newSaxHandler()
 	{
 		return new TeiHandler();
 	}
-	
+
 	/**
 	 * @author Richard Eckart de Castilho
 	 */
@@ -340,7 +340,7 @@ public class TEIReader
 			return logger;
 		}
 	}
-	
+
 	public class TeiHandler
 		extends Handler
 	{
@@ -365,7 +365,7 @@ public class TEIReader
 		{
 			return buffer;
 		}
-		
+
 		@Override
 		public void startElement(String aUri, String aLocalName, String aName,
 				Attributes aAttributes)
@@ -379,7 +379,7 @@ public class TEIReader
 				else if (useFilenameId) {
 					documentId = FilenameUtils.getName(currentResource.getPath()) + "#"
 							+ currentTeiElementNumber;
-				} 
+				}
 				else {
 					documentId = currentResource.getPath()+"#"+currentTeiElementNumber;
 				}
@@ -400,7 +400,7 @@ public class TEIReader
 				lemma = aAttributes.getValue(ATTR_LEMMA);
 			}
 		}
-		
+
 		@Override
 		public void endElement(String aUri, String aLocalName, String aName)
 			throws SAXException
@@ -426,34 +426,34 @@ public class TEIReader
 				if (isNotBlank(getBuffer().substring(tokenStart, getBuffer().length()))) {
 					Token token = new Token(getJCas(), tokenStart, getBuffer().length());
 					trim(token);
-					
+
 					if (posTag != null && writePOS) {
 						Type posTagType = posMappingProvider.getTagType(posTag);
-						POS pos = (POS) getJCas().getCas().createAnnotation(posTagType, 
+						POS pos = (POS) getJCas().getCas().createAnnotation(posTagType,
 								token.getBegin(), token.getEnd());
 						pos.setPosValue(posTag);
 						pos.addToIndexes();
 						token.setPos(pos);
 					}
-					
+
 					if (lemma != null && writeLemma) {
 						Lemma l = new Lemma(getJCas(), token.getBegin(), token.getEnd());
 						l.setValue(lemma);
 						l.addToIndexes();
 						token.setLemma(l);
 					}
-					
+
 					// FIXME: if writeTokens is disabled, the JCas wrapper should not be generated
 					// at all!
 					if (writeTokens) {
 						token.addToIndexes();
 					}
 				}
-				
+
 				tokenStart = -1;
 			}
 		}
-		
+
 		@Override
 		public void characters(char[] aCh, int aStart, int aLength)
 			throws SAXException
@@ -462,7 +462,7 @@ public class TEIReader
 				buffer.append(aCh, aStart, aLength);
 			}
 		}
-		
+
 		@Override
 		public void ignorableWhitespace(char[] aCh, int aStart, int aLength)
 			throws SAXException
@@ -471,7 +471,7 @@ public class TEIReader
 				buffer.append(aCh, aStart, aLength);
 			}
 		}
-		
+
 		private void trim(Annotation aAnnotation)
 		{
 			StringBuilder buffer = getBuffer();
