49385bb6455dcfd873fb35868f1efe7f2a6ac75e
Pedro Santos <pedrobssantos@gmail.com>
2013-05-14 07:33:13 +0000

    Issue 75: Renaming Morpheme to CompoundPart since Morpheme is a type which already exists.

--- de.tudarmstadt.ukp.dkpro.core.api.segmentation-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/api/segmentation/type/CompoundPart_Type.java
+++ de.tudarmstadt.ukp.dkpro.core.api.segmentation-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/api/segmentation/type/CompoundPart_Type.java
@@ -1,5 +1,5 @@
 
-/* First created by JCasGen Mon May 13 18:03:33 CEST 2013 */
+/* First created by JCasGen Tue May 14 09:31:43 CEST 2013 */
 package de.tudarmstadt.ukp.dkpro.core.api.segmentation.type;
 
 import org.apache.uima.jcas.JCas;
@@ -10,10 +10,10 @@ import org.apache.uima.cas.FeatureStructure;
 import org.apache.uima.cas.impl.TypeImpl;
 import org.apache.uima.cas.Type;
 
-/** A part of a decompounding word which is a Morpheme.
- * Updated by JCasGen Mon May 13 18:03:33 CEST 2013
+/** A part of a decompounding word.
+ * Updated by JCasGen Tue May 14 09:31:43 CEST 2013
  * @generated */
-public class Morpheme_Type extends Split_Type {
+public class CompoundPart_Type extends Split_Type {
   /** @generated */
   @Override
   protected FSGenerator getFSGenerator() {return fsGenerator;}
@@ -21,31 +21,31 @@ public class Morpheme_Type extends Split_Type {
   private final FSGenerator fsGenerator = 
     new FSGenerator() {
       public FeatureStructure createFS(int addr, CASImpl cas) {
-  			 if (Morpheme_Type.this.useExistingInstance) {
+  			 if (CompoundPart_Type.this.useExistingInstance) {
   			   // Return eq fs instance if already created
-  		     FeatureStructure fs = Morpheme_Type.this.jcas.getJfsFromCaddr(addr);
+  		     FeatureStructure fs = CompoundPart_Type.this.jcas.getJfsFromCaddr(addr);
   		     if (null == fs) {
-  		       fs = new Morpheme(addr, Morpheme_Type.this);
-  			   Morpheme_Type.this.jcas.putJfsFromCaddr(addr, fs);
+  		       fs = new CompoundPart(addr, CompoundPart_Type.this);
+  			   CompoundPart_Type.this.jcas.putJfsFromCaddr(addr, fs);
   			   return fs;
   		     }
   		     return fs;
-        } else return new Morpheme(addr, Morpheme_Type.this);
+        } else return new CompoundPart(addr, CompoundPart_Type.this);
   	  }
     };
   /** @generated */
   @SuppressWarnings ("hiding")
-  public final static int typeIndexID = Morpheme.typeIndexID;
+  public final static int typeIndexID = CompoundPart.typeIndexID;
   /** @generated 
      @modifiable */
   @SuppressWarnings ("hiding")
-  public final static boolean featOkTst = JCasRegistry.getFeatOkTst("de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Morpheme");
+  public final static boolean featOkTst = JCasRegistry.getFeatOkTst("de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.CompoundPart");
 
 
 
   /** initialize variables to correspond with Cas Type and Features
 	* @generated */
-  public Morpheme_Type(JCas jcas, Type casType) {
+  public CompoundPart_Type(JCas jcas, Type casType) {
     super(jcas, casType);
     casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl)this.casType, getFSGenerator());
 
