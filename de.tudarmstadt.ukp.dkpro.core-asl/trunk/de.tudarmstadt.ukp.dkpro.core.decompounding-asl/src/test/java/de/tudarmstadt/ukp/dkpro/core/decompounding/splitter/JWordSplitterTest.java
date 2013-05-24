78940cc85b946d4f1064f45b1a2484d7ffd82a4a
pedrobssantos <pedrobssantos@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2013-05-24 10:33:41 +0000

    Issue 140: Adding unit tests for splitter package.

---
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.decompounding-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/decompounding/splitter/JWordSplitterTest.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright 2010
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package de.tudarmstadt.ukp.dkpro.core.decompounding.splitter;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import de.tudarmstadt.ukp.dkpro.core.decompounding.dictionary.Dictionary;
+import de.tudarmstadt.ukp.dkpro.core.decompounding.dictionary.SimpleDictionary;
+
+public class JWordSplitterTest
+{
+
+    @Test
+    public void testSplitter()
+        throws IOException
+    {
+        JWordSplitterAlgorithm splitter = new JWordSplitterAlgorithm();
+        List<DecompoundedWord> result = splitter.split("Aktionsplan").getAllSplits();
+        Assert.assertEquals(2, result.size());
+        Assert.assertEquals("Aktionsplan", result.get(0).toString());
+        Assert.assertEquals("Aktion(s)+plan", result.get(1).toString());
+    }
+
+    @Test
+	public void testSplliter2() throws IOException {
+	    JWordSplitterAlgorithm splitter = new JWordSplitterAlgorithm();
+	    URL dictURL = getClass().getResource("/dic/de_DE.dic");
+
+	    Dictionary dict = new SimpleDictionary(dictURL.openStream());
+	    splitter.setDictionary(dict);
+	    List<DecompoundedWord> result = splitter.split("geräteelektronik").getAllSplits();
+	    assertThat(result.size(),is(1));
+	}
+}
---
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.decompounding-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/decompounding/splitter/LeftToRightSplitAlgorithmTest.java
@@ -0,0 +1,118 @@
+/*******************************************************************************
+ * Copyright 2010
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package de.tudarmstadt.ukp.dkpro.core.decompounding.splitter;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import de.tudarmstadt.ukp.dkpro.core.decompounding.dictionary.Dictionary;
+import de.tudarmstadt.ukp.dkpro.core.decompounding.dictionary.LinkingMorphemes;
+import de.tudarmstadt.ukp.dkpro.core.decompounding.dictionary.SimpleDictionary;
+
+public class LeftToRightSplitAlgorithmTest
+{
+
+	@Test
+	public void testSplit1()
+	{
+		Dictionary dict = new SimpleDictionary("Akt", "ion", "plan", "Aktion",
+				"Aktionsplan");
+		LinkingMorphemes morphemes = new LinkingMorphemes("s");
+		LeftToRightSplitterAlgorithm algo = new LeftToRightSplitterAlgorithm(dict,
+				morphemes);
+
+		List<DecompoundedWord> result = algo.split("Aktionsplan").getAllSplits();
+		Assert.assertEquals(6, result.size());
+		Assert.assertEquals("aktionsplan", result.get(0).toString());
+		Assert.assertEquals("akt+ionsplan", result.get(1).toString());
+		Assert.assertEquals("akt+ion+splan", result.get(2).toString());
+		Assert.assertEquals("akt+ion(s)+plan", result.get(3).toString());
+		Assert.assertEquals("aktion+splan", result.get(4).toString());
+		Assert.assertEquals("aktion(s)+plan", result.get(5).toString());
+	}
+
+	@Test
+	public void testSplit2()
+	{
+		Dictionary dict = new SimpleDictionary("Donau", "dampf", "schiff",
+				"fahrt", "dampfschiff", "schifffahrt");
+		LinkingMorphemes morphemes = new LinkingMorphemes("s");
+		LeftToRightSplitterAlgorithm algo = new LeftToRightSplitterAlgorithm(dict,
+				morphemes);
+
+		List<DecompoundedWord> result = algo.split("Donaudampfschifffahrt").getAllSplits();
+		Assert.assertEquals(6, result.size());
+	}
+
+	@Test
+	public void testSplit3()
+	{
+		Dictionary dict = new SimpleDictionary("Super", "mann", "anzug",
+				"Supermann", "anzug");
+		LinkingMorphemes morphemes = new LinkingMorphemes("s");
+		LeftToRightSplitterAlgorithm algo = new LeftToRightSplitterAlgorithm(dict,
+				morphemes);
+
+		List<DecompoundedWord> result = algo.split("Supermannanzug").getAllSplits();
+		// Super+mann+anzug, Supermann+anzug
+		Assert.assertEquals(4, result.size());
+	}
+
+	@Test
+	public void testMorphemes1()
+	{
+		Dictionary dict = new SimpleDictionary("alarm", "reaktion");
+		LinkingMorphemes morphemes = new LinkingMorphemes("en");
+		LeftToRightSplitterAlgorithm algo = new LeftToRightSplitterAlgorithm(dict,
+				morphemes);
+
+		List<DecompoundedWord> result = algo.split("alarmreaktionen").getAllSplits();
+		// Super+mann+anzug, Supermann+anzug
+		Assert.assertEquals(3, result.size());
+		Assert.assertEquals("alarmreaktionen", result.get(0).toString());
+		Assert.assertEquals("alarm+reaktionen", result.get(1).toString());
+		Assert.assertEquals("alarm+reaktion(en)", result.get(2).toString());
+	}
+
+	@Test
+	public void testSplit4() throws IOException{
+
+	    URL dictURL = getClass().getResource("/dic/de_DE.dic");
+	    URL morphURL =  getClass().getResource("/dic/de_DE.linking");
+
+	    Dictionary dict = new SimpleDictionary(dictURL.openStream());
+	    LinkingMorphemes morphemes = new LinkingMorphemes(morphURL.openStream());
+
+	    LeftToRightSplitterAlgorithm splitter = new LeftToRightSplitterAlgorithm(dict,morphemes);
+
+	    List<DecompoundedWord> result = splitter.split("geräteelektronik").getAllSplits();
+
+	    assertThat(result.size(),is(1));
+
+	}
+
+}
