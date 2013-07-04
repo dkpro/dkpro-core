48a72595c1f3429e90e52a5ab3d03826615e22e5
pedrobssantos <pedrobssantos@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2013-07-04 19:51:25 +0000

    Issue 148: 	Upgrade to uimaFIT 2.0.0
    Removing uimafit-legacy-support and org.uimafit.uimafit dependencies.

--- de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.api.io-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/api/io/FileSetCollectionReaderBase.java
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.api.io-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/api/io/FileSetCollectionReaderBase.java
@@ -30,11 +30,11 @@ import org.apache.uima.UimaContext;
 import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.CASException;
 import org.apache.uima.collection.CollectionException;
+import org.apache.uima.fit.component.CasCollectionReader_ImplBase;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.util.Progress;
 import org.apache.uima.util.ProgressImpl;
-import org.uimafit.component.CasCollectionReader_ImplBase;
-import org.uimafit.descriptor.ConfigurationParameter;
 
 import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
 import de.tudarmstadt.ukp.dkpro.core.api.parameter.ComponentParameters;
@@ -114,12 +114,12 @@ public abstract class FileSetCollectionReaderBase
 		// Parse the patterns and inject them into the FileSet
 		List<String> includes = new ArrayList<String>();
 		List<String> excludes = new ArrayList<String>();
-		for (int i = 0; i < patterns.length; i++) {
-			if (patterns[i].startsWith(INCLUDE_PREFIX)) {
-				includes.add(patterns[i].substring(INCLUDE_PREFIX.length()));
+		for (String pattern : patterns) {
+			if (pattern.startsWith(INCLUDE_PREFIX)) {
+				includes.add(pattern.substring(INCLUDE_PREFIX.length()));
 			}
-			else if (patterns[i].startsWith(EXCLUDE_PREFIX)) {
-				excludes.add(patterns[i].substring(EXCLUDE_PREFIX.length()));
+			else if (pattern.startsWith(EXCLUDE_PREFIX)) {
+				excludes.add(pattern.substring(EXCLUDE_PREFIX.length()));
 			}
 			else {
 				throw new ResourceInitializationException(new IllegalArgumentException(
