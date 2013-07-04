161923ca8135e9a5662d5d46a48af0910ef67c42
pedrobssantos <pedrobssantos@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2013-07-04 20:42:40 +0000

    Issue 148: 	Upgrade to uimaFIT 2.0.0
    Removing uimafit-legacy-support and org.uimafit.uimafit dependencies.

--- de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.posfilter-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/posfilter/PosMapper.java
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.posfilter-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/posfilter/PosMapper.java
@@ -28,12 +28,12 @@ import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.Type;
+import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.fit.descriptor.TypeCapability;
+import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
-import org.uimafit.component.JCasAnnotator_ImplBase;
-import org.uimafit.descriptor.ConfigurationParameter;
-import org.uimafit.descriptor.TypeCapability;
-import org.uimafit.util.JCasUtil;
 
 import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS;
 import de.tudarmstadt.ukp.dkpro.core.api.resources.MappingProvider;
