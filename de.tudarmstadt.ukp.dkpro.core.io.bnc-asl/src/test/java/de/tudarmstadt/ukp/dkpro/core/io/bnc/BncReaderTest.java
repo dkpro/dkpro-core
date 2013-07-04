4dee7b9ff4a9ba44758c0d54405009df4d7e25c1
Pedro Santos <pedrobssantos@gmail.com>
2013-07-04 20:22:03 +0000

    Issue 148: 	Upgrade to uimaFIT 2.0.0
    Removing uimafit-legacy-support and org.uimafit.uimafit dependencies.

--- de.tudarmstadt.ukp.dkpro.core.io.bnc-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/io/bnc/BncReaderTest.java
+++ de.tudarmstadt.ukp.dkpro.core.io.bnc-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/io/bnc/BncReaderTest.java
@@ -17,34 +17,34 @@
  ******************************************************************************/
 package de.tudarmstadt.ukp.dkpro.core.io.bnc;
 
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createPrimitiveDescription;
+import static org.apache.uima.fit.factory.CollectionReaderFactory.createDescription;
 import static org.junit.Assert.assertEquals;
-import static org.uimafit.factory.AnalysisEngineFactory.createPrimitiveDescription;
-import static org.uimafit.factory.CollectionReaderFactory.createDescription;
 
 import java.io.File;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.component.xwriter.CASDumpWriter;
+import org.apache.uima.fit.pipeline.SimplePipeline;
 import org.junit.Test;
-import org.uimafit.component.xwriter.CASDumpWriter;
-import org.uimafit.pipeline.SimplePipeline;
 
 public class BncReaderTest
 {
 	@Test
 	public void test() throws Exception
 	{
-		CollectionReaderDescription reader = createDescription(BncReader.class, 
+		CollectionReaderDescription reader = createDescription(BncReader.class,
 				BncReader.PARAM_PATH, "src/test/resources",
 				BncReader.PARAM_PATTERNS, new String[] { "[+]FX8.xml" },
 				BncReader.PARAM_LANGUAGE, "en");
-		
-		AnalysisEngineDescription casDumper = createPrimitiveDescription(CASDumpWriter.class, 
+
+		AnalysisEngineDescription casDumper = createPrimitiveDescription(CASDumpWriter.class,
 				CASDumpWriter.PARAM_OUTPUT_FILE, "target/test-output/FX8.dump");
-		
+
 		SimplePipeline.runPipeline(reader, casDumper);
-		
+
 		String reference = FileUtils.readFileToString(
 				new File("src/test/resources/reference/FX8.dump"), "UTF-8");
 		String actual = FileUtils.readFileToString(
