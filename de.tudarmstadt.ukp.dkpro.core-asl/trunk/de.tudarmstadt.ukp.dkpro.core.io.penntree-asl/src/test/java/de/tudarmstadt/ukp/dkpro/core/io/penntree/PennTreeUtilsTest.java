a6c7f3f5123a314ed1542533d9ae5be97a812ac3
pedrobssantos <pedrobssantos@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2013-07-04 20:31:21 +0000

    Issue 148: 	Upgrade to uimaFIT 2.0.0
    Removing uimafit-legacy-support and org.uimafit.uimafit dependencies.

--- de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.io.penntree-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/io/penntree/PennTreeUtilsTest.java
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.io.penntree-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/io/penntree/PennTreeUtilsTest.java
@@ -17,11 +17,11 @@
  ******************************************************************************/
 package de.tudarmstadt.ukp.dkpro.core.io.penntree;
 
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createAggregateDescription;
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createPrimitive;
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createPrimitiveDescription;
+import static org.apache.uima.fit.util.JCasUtil.selectSingle;
 import static org.junit.Assert.assertEquals;
-import static org.uimafit.factory.AnalysisEngineFactory.createAggregateDescription;
-import static org.uimafit.factory.AnalysisEngineFactory.createPrimitive;
-import static org.uimafit.factory.AnalysisEngineFactory.createPrimitiveDescription;
-import static org.uimafit.util.JCasUtil.selectSingle;
 
 import org.apache.uima.analysis_engine.AnalysisEngine;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
@@ -53,7 +53,7 @@ public class PennTreeUtilsTest
 				"(NNS approaches)))))))))))) (. .)))");
 	}
 
-	private static void doTest(String aBracket) 
+	private static void doTest(String aBracket)
 	{
 		String expected = aBracket;
 
@@ -63,7 +63,7 @@ public class PennTreeUtilsTest
 
 		assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	public void testSelectDfs()
 	{
@@ -87,33 +87,33 @@ public class PennTreeUtilsTest
 		System.out.println(PennTreeUtils.selectDfs(n, 11));
 		System.out.println(PennTreeUtils.selectDfs(n, 12));
 	}
-	
+
     @Test
     public void testFromUimaConversion()
         throws Exception
     {
-        String documentEnglish = 
+        String documentEnglish =
                 "It is for this reason that deconstruction remains a ( fundamental ) threat to " +
                 "Marxism , and by implication to other culturalist and contextualizing " +
                 "approaches .";
-        
+
         String pennTree = "(ROOT (S (S (NP (PRP It)) (VP (VBZ is) (PP (IN for) (NP (DT this) " +
         		"(NN reason))) (SBAR (IN that) (S (NP (NN deconstruction)) (VP (VBZ remains) " +
         		"(NP (NP (DT a) (-LRB- -LRB-) (NN fundamental) (-RRB- -RRB-) (NN threat)) (PP " +
         		"(TO to) (NP (NNP Marxism))))))))) (, ,) (CC and) (S (PP (IN by) (NP " +
         		"(NN implication))) (PP (TO to) (NP (NP (JJ other) (NN culturalist)) (CC and) " +
         		"(NP (VBG contextualizing) (NNS approaches))))) (. .)))";
-        
+
         JCas jcas = runTest("en", "chunking", documentEnglish);
-        
+
         ROOT root = selectSingle(jcas, ROOT.class);
         PennTreeNode r = PennTreeUtils.convertPennTree(root);
-        
+
         assertEquals(documentEnglish.trim(), PennTreeUtils.toText(r).trim());
         AssertAnnotations.assertPennTree(pennTree, PennTreeUtils.toPennTree(r));
     }
 
-	
+
 	/**
      * Setup CAS to test parser for the English language (is only called once if
      * an English test is run)
@@ -130,16 +130,16 @@ public class PennTreeUtilsTest
                 OpenNlpParser.PARAM_CREATE_PENN_TREE_STRING, true);
 
         AnalysisEngineDescription aggregate = createAggregateDescription(segmenter, parser);
-        
+
         AnalysisEngine engine = createPrimitive(aggregate);
         JCas jcas = engine.newJCas();
         jcas.setDocumentLanguage(aLanguage);
         jcas.setDocumentText(aText);
         engine.process(jcas);
-        
+
         return jcas;
     }
-    
+
     @Rule
     public TestName name = new TestName();
 
