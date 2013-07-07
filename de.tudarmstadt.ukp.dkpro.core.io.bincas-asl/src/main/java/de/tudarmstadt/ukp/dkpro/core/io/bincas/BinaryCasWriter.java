999bc54fa3c56fd175ab52195548c6db86e491d0
Richard Eckart de Castilho <richard.eckart@gmail.com>
2013-07-07 19:11:01 +0000

    Updates issue 126. Support lenient/compressed binary CAS serialization - Added BinaryCasReader/Writer - Added test cases for formats 0 and 4. 6 doesn't work for some reason

---
+++ de.tudarmstadt.ukp.dkpro.core.io.bincas-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/bincas/BinaryCasWriter.java
@@ -0,0 +1,117 @@
+/*******************************************************************************
+ * Copyright 2013
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.tudarmstadt.ukp.dkpro.core.io.bincas;
+
+import static org.apache.commons.io.IOUtils.closeQuietly;
+import static org.apache.uima.cas.impl.Serialization.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.impl.CASMgrSerializer;
+import org.apache.uima.cas.impl.Serialization;
+import org.apache.uima.cas.impl.TypeSystemImpl;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
+
+import de.tudarmstadt.ukp.dkpro.core.api.io.JCasFileWriter_ImplBase;
+import de.tudarmstadt.ukp.dkpro.core.api.resources.CompressionUtils;
+
+public class BinaryCasWriter
+	extends JCasFileWriter_ImplBase
+{
+	/**
+	 * Location to write the type system to. If this is not set, a file called typesystem.xml will
+	 * be written to the output path. If this is set, it is expected to be a file relative
+	 * to the current work directory or an absolute file.
+	 * <br>
+	 * If this parameter is set, the {@link #PARAM_COMPRESS} parameter has no effect on the
+	 * type system. Instead, if the type system file should be compressed or not is detected from
+	 * the file name extension (e.g. ".gz").
+	 * <br>
+	 * If this parameter is set, the type system and index repository are no longer serialized into
+	 * the same file as the test of the CAS. The {@link SerializedCasReader} can currently not
+	 * read such files. Use this only if you really know what you are doing.
+	 */
+	public static final String PARAM_TYPE_SYSTEM_FILE = "typeSystemFile";
+	@ConfigurationParameter(name=PARAM_TYPE_SYSTEM_FILE, mandatory=false)
+	private File typeSystemFile;
+
+	public static final String PARAM_FORMAT = "format";
+    @ConfigurationParameter(name=PARAM_FORMAT, mandatory=true, defaultValue="0")
+    private int format;
+	
+	
+	private boolean typeSystemWritten;
+	
+	@Override
+	public void process(JCas aJCas)
+		throws AnalysisEngineProcessException
+	{
+		OutputStream docOS = null;
+        try {
+        	docOS = getOutputStream(aJCas, ".bin");
+        	Serialization ser = new Serialization();
+        	switch (format) {
+        	case 0: serializeCAS(aJCas.getCas(), docOS); break;
+        	case 4: ser.serializeWithCompression(aJCas.getCas(), docOS); break;
+            case 6: ser.serializeWithCompression(aJCas.getCas(), docOS, aJCas.getTypeSystem()); break;
+            default: 
+                throw new IllegalArgumentException("Unknown format [" + format
+                        + "]. Must be 0, 4 or 6");
+        	}
+    	    
+    		if (!typeSystemWritten) {
+    			writeTypeSystem(aJCas);
+    			typeSystemWritten = true;
+    		}
+        }
+        catch (Exception e) {
+            throw new AnalysisEngineProcessException(e);
+        }
+        finally {
+            closeQuietly(docOS);
+        }
+	}
+
+	private void writeTypeSystem(JCas aJCas) throws IOException
+	{
+		ObjectOutputStream typeOS = null;
+		
+		File typeOSFile;
+		if (typeSystemFile != null) {
+			typeOSFile = typeSystemFile;
+		}
+		else {
+			typeOSFile = getTargetPath("typesystem", ".ser");
+		}
+		
+		typeOS = new ObjectOutputStream(CompressionUtils.getOutputStream(typeOSFile));
+		
+		try {
+			CASMgrSerializer casMgrSerializer = serializeCASMgr(aJCas.getCasImpl());
+			casMgrSerializer.addTypeSystem((TypeSystemImpl) aJCas.getTypeSystem());
+			typeOS.writeObject(casMgrSerializer);
+		}
+		finally {
+			closeQuietly(typeOS);
+		}
+	}}
