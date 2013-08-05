7837afa4077f1fc7177fd0d7d5d4b455a6bcdf6e
Dr. Judith Eckle-Kohler <jek@eckle-kohler.de>
2013-08-05 19:58:39 +0000

    added package semantictagging with:
    - SemanticTagProvider (Interface)
    - NounSemanticFieldResource
    - NounSemanticFieldAnnotator
    
    and a test class for the annotator:
    - NounSemanticFieldAnnotatorTest along with a test resource nounSemanticFieldMapTest.txt
    
    In the pom, removed scope=test in the api.ner dependency (de.tudarmstadt.ukp.dkpro.core.api.ner-asl),
    because the annotator uses the NamedEntity type for the annotation

---
+++ de.tudarmstadt.ukp.dkpro.core.dictionaryannotator-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/dictionaryannotator/semantictagging/NounSemanticFieldResource.java
@@ -0,0 +1,106 @@
+/*******************************************************************************
+ * Copyright 2013
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.tudarmstadt.ukp.dkpro.core.dictionaryannotator.semantictagging;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.uima.fit.component.Resource_ImplBase;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.resource.ResourceSpecifier;
+
+import de.tudarmstadt.ukp.dkpro.core.api.resources.ResourceUtils;
+
+
+/**
+ * 
+ * This shared resource can be added as ExternalResource in Analysis Engines
+ * that annotate common nouns with semantic field information from WordNet.
+ *     
+ * @author Judith Eckle-Kohler
+ * 
+ */
+
+public class NounSemanticFieldResource 
+	extends Resource_ImplBase
+	implements SemanticTagProvider
+{
+	
+    public final static String PARAM_RESOURCE_PATH = "resourcePath";
+    @ConfigurationParameter(name = PARAM_RESOURCE_PATH, mandatory = true)
+    // TODO add default like: defaultValue = "classpath:de/tudarmstadt/ukp/dkpro/core/decompounding/lib/spelling/de/igerman98/de_DE_igerman98.dic"
+    private String resourcePath;
+
+    private Map<String,String> nounSemanticFieldMap= new HashMap<String,String>();
+    
+    @Override
+    public boolean initialize(ResourceSpecifier aSpecifier, Map aAdditionalParams)
+        throws ResourceInitializationException
+    {
+        if (!super.initialize(aSpecifier, aAdditionalParams)) {
+            return false;
+        }
+
+        try {
+            final URL uri = ResourceUtils.resolveLocation(resourcePath, this, null);
+            readFileToMap(new BufferedReader(new InputStreamReader(uri.openStream())));
+           
+        }
+        catch (IOException e) {
+            throw new ResourceInitializationException(e);
+        }
+
+        return true;
+
+    }
+
+
+	@Override
+	public String getSemanticTag(String nounLemma) throws Exception {
+
+		if (nounSemanticFieldMap.containsKey(nounLemma)) {
+			return nounSemanticFieldMap.get(nounLemma);
+		} else {
+			return "UNKNOWN"; 
+		}
+	}
+	
+	
+	private void readFileToMap(BufferedReader bufferedReader) {		
+		String line;
+		try {
+			while((line = bufferedReader.readLine())!=null){	
+				String temp[] = line.split("\t");
+				String noun = temp[0];
+				String semField = temp[1];
+				System.out.println(line);
+				nounSemanticFieldMap.put(noun, semField);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+
+}
