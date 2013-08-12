df4ce7fd4dec6f8df015e1942ebf544682661569
eckle.kohler@gmail.com <eckle.kohler@gmail.com@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2013-08-12 08:39:22 +0000

    Issue 169. Added UbySemanticFieldResource, UbySemanticFieldAnnotator and UbyResourceUtils
    The test class UbySemanticFieldAnnotatorTest successfully runs a test on a real (MySQL) DB, therefore the test method is ignored.
    A suitable test case for an in-memory UBY DB should be added.
    
    
    In NounSemanticFieldResource: fixed exception handling

---
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.dictionaryannotator-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/dictionaryannotator/semantictagging/UbySemanticFieldResource.java
@@ -0,0 +1,172 @@
+/*******************************************************************************
+ * Copyright 2013
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.tudarmstadt.ukp.dkpro.core.dictionaryannotator.semantictagging;
+
+import static de.tudarmstadt.ukp.dkpro.core.dictionaryannotator.semantictagging.UbyResourceUtils.getMostFrequentSense;
+import static de.tudarmstadt.ukp.dkpro.core.dictionaryannotator.semantictagging.UbyResourceUtils.corePosToUbyPos;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.uima.fit.component.Resource_ImplBase;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.resource.ResourceSpecifier;
+
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+import de.tudarmstadt.ukp.lmf.api.Uby;
+import de.tudarmstadt.ukp.lmf.exceptions.UbyInvalidArgumentException;
+import de.tudarmstadt.ukp.lmf.model.core.LexicalEntry;
+import de.tudarmstadt.ukp.lmf.model.core.Lexicon;
+import de.tudarmstadt.ukp.lmf.model.core.Sense;
+import de.tudarmstadt.ukp.lmf.model.enums.EPartOfSpeech;
+import de.tudarmstadt.ukp.lmf.model.meta.SemanticLabel;
+import de.tudarmstadt.ukp.lmf.transform.DBConfig;
+
+
+/**
+ * 
+ * This shared resource can be added as ExternalResource in Analysis Engines
+ * that annotate common nouns, main verbs and adjectives
+ * with semantic field information from WordNet (for English text) 
+ * or GermaNet (for German text).
+ *     
+ * @author Judith Eckle-Kohler
+ * 
+ */
+public class UbySemanticFieldResource 
+	extends Resource_ImplBase
+	implements SemanticTagProvider
+{
+	
+	public static final String PARAM_URL = "ubyDatabaseUrl";
+	@ConfigurationParameter(name = PARAM_URL, mandatory = true)
+	private String ubyDatabaseUrl;
+
+	public static final String PARAM_DRIVER = "databaseDriver";
+	@ConfigurationParameter(name = PARAM_DRIVER, mandatory = true)
+	private String databaseDriver;
+	
+	//vendor name of the accessed database, e.g. mysql, hsqldb
+	public static final String PARAM_DRIVER_NAME = "databaseDriverName"; //name of database driver (this parameter is called "vendor" in DBConfig), e.g. mysql, h2
+	@ConfigurationParameter(name = PARAM_DRIVER_NAME, mandatory = true)
+	private String databaseDriverName;
+	
+	public static final String PARAM_USERNAME = "ubyUsername";
+	@ConfigurationParameter(name = PARAM_USERNAME, mandatory = true)
+	private String ubyUsername;
+	
+	public static final String PARAM_PASSWORD = "ubyPassword";
+	@ConfigurationParameter(name = PARAM_PASSWORD, mandatory = true)
+	private String ubyPassword;
+	
+	// the documentLanguage is specified as ISO 2-letter code (following the DKPro-Core convention)
+	public static final String PARAM_LANGUAGE = "documentLanguage";
+	@ConfigurationParameter(name = PARAM_LANGUAGE, mandatory = true)
+	private String documentLanguage;
+
+	private Uby uby;
+	private Lexicon lexicon;
+
+	
+    @Override
+    public boolean initialize(ResourceSpecifier aSpecifier, Map aAdditionalParams)
+        throws ResourceInitializationException
+    {
+        if (!super.initialize(aSpecifier, aAdditionalParams)) {
+            return false;
+        }
+
+        try {
+ 			DBConfig dbConfig = new DBConfig(ubyDatabaseUrl,databaseDriver,databaseDriverName,ubyUsername,ubyPassword,false);
+			uby = new Uby(dbConfig);
+			
+			if (documentLanguage.equals("en")) {
+				lexicon = uby.getLexiconByName("WordNet");
+			} else if (documentLanguage.equals("de")) {
+				lexicon = uby.getLexiconByName("GermaNet");
+			}			
+        }
+        catch (UbyInvalidArgumentException e) {       	       	
+            throw new ResourceInitializationException(e);
+        }
+
+        return true;
+
+    }
+
+
+	@Override
+	public String getSemanticTag(Token token) throws Exception {
+				
+		Sense sense = null;
+		String semanticField = "";
+		List<LexicalEntry> lexicalEntries;
+				
+		// does the token have a POS which has relevant information in the lexicon?	
+		if (corePosToUbyPos(token.getPos().getPosValue()).length == 0) {
+			return "UNKNOWN"; 
+		// does the lexicon contain the lemma?
+		} else if (uby.getLexicalEntries(token.getLemma().getValue(),null,lexicon).isEmpty()) { 
+			return "UNKNOWN"; 
+		} else { // the lexicon contains the lemma
+			for (EPartOfSpeech pos : corePosToUbyPos(token.getPos().getPosValue())) {
+				
+				if (!uby.getLexicalEntries(token.getLemma().getValue(),pos,lexicon).isEmpty()) { // the lemma is listed in the lexicon with the given POS
+					lexicalEntries = uby.getLexicalEntries(token.getLemma().getValue(),pos,lexicon);
+					if (lexicon.getName().equals("WordNet")) {
+						
+						// WordNet contains MFS information, since the senses are ordered by decreasing frequency in SemCor: 
+						// in UBY, this is the sense with index = 1
+						sense = getMostFrequentSense(lexicalEntries);
+					} else if (lexicon.getName().equals("GermaNet")) {
+						// GermaNet does not contain MFS information; the first sense is used
+						sense = lexicalEntries.get(0).getSenses().get(0);
+					}		
+				}
+			}
+			semanticField = getSemanticField(sense);		
+			if (semanticField == null) {
+				return "UNKNOWN"; 
+			} else {
+				return semanticField;
+			}
+		}
+				
+	}
+
+
+
+	private String getSemanticField(Sense sense) {	
+		String semanticField = null;
+		
+		if (!(sense == null)) {
+			for (SemanticLabel sl : sense.getSemanticLabels()) {
+				if (sl.getType().toString().matches("semanticField")) {
+					semanticField = sl.getLabel();
+					semanticField = semanticField.replaceAll(".*\\.", "");
+				}			
+			}
+		}
+		return semanticField;
+	}
+
+		
+}
+
+
---
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.dictionaryannotator-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/dictionaryannotator/semantictagging/UbySemanticFieldAnnotatorTest.java
@@ -0,0 +1,148 @@
+/*******************************************************************************
+ * Copyright 2013
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.tudarmstadt.ukp.dkpro.core.dictionaryannotator.semantictagging;
+
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngine;
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+import static org.apache.uima.fit.factory.ExternalResourceFactory.createExternalResourceDescription;
+import static org.apache.uima.fit.util.JCasUtil.select;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.fit.testing.factory.TokenBuilder;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.ADJ;
+import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.NN;
+import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS;
+import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.V;
+import de.tudarmstadt.ukp.dkpro.core.api.ner.type.NamedEntity;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Lemma;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+import de.tudarmstadt.ukp.dkpro.core.testing.AssertAnnotations;
+
+
+/**
+ * @author Judith Eckle-Kohler
+ *
+ */
+public class UbySemanticFieldAnnotatorTest {
+
+	@Ignore	
+	@Test
+	public void testUbySemanticFieldAnnotator()
+		throws Exception
+	{
+		runAnnotatorTestOnMySqlDb("en", "Vanilla in the blue sky prefers braveness over jumpiness .",
+        		new String[] { "vanilla", "in", "the", "blue", "sky", "prefer", "braveness", "over", "jumpiness", "."    },
+        		new String[] { "NN", "NOT_RELEVANT", "NOT_RELEVANT", "ADJ", "NN", "V", "NN", "NOT_RELEVANT", "NN", "$."    },
+        		new String[] { "UNKNOWN '.'", "UNKNOWN 'in'", "UNKNOWN 'over'", "UNKNOWN 'the'", "all 'blue'", "attribute 'braveness'", "emotion 'prefers'", "feeling 'jumpiness'", "object 'sky'", "plant 'Vanilla'"     });
+
+		runAnnotatorTestOnMySqlDb("en", "Vanilla in the distantGalaxyBehindJupiter prefers braveness over jumpiness .",
+        		new String[] { "vanilla", "in", "the", "distantGalaxyBehindJupiter", "prefer", "braveness", "over", "jumpiness", "."    },
+        		new String[] { "NN", "NOT_RELEVANT", "NOT_RELEVANT", "NN", "V", "NN", "NOT_RELEVANT", "NN", "$."    },
+        		new String[] { "UNKNOWN '.'", "UNKNOWN 'distantGalaxyBehindJupiter'", "UNKNOWN 'in'", "UNKNOWN 'over'", "UNKNOWN 'the'", "attribute 'braveness'", "emotion 'prefers'", "feeling 'jumpiness'", "plant 'Vanilla'"     });
+       
+
+
+	}
+
+	/**
+	 * @param language
+	 * @param testDocument
+	 * @param documentLemmas
+	 * @param documentPosTags
+	 * @param documentUbySemanticFields
+	 * @return
+	 * @throws UIMAException 
+	 */
+	private void runAnnotatorTestOnMySqlDb(String language, 
+			String testDocument, 
+			String[] documentLemmas,
+			String[] documentPosTags, 
+			String[] documentUbySemanticFields) throws UIMAException {
+
+               
+		AnalysisEngineDescription processor = createEngineDescription(
+
+				createEngineDescription(UbySemanticFieldAnnotator.class,
+						UbySemanticFieldAnnotator.PARAM_UBY_SEMANTIC_FIELD_RESOURCE, 
+							createExternalResourceDescription(UbySemanticFieldResource.class,
+									UbySemanticFieldResource.PARAM_URL, "localhost/uby_medium_0_2_0",
+									UbySemanticFieldResource.PARAM_DRIVER, "com.mysql.jdbc.Driver",
+									UbySemanticFieldResource.PARAM_DRIVER_NAME, "mysql",
+									UbySemanticFieldResource.PARAM_USERNAME, "root",
+									UbySemanticFieldResource.PARAM_PASSWORD, "pass",
+									UbySemanticFieldResource.PARAM_LANGUAGE, language)
+									)
+		);
+
+		AnalysisEngine engine = createEngine(processor);
+		JCas aJCas = engine.newJCas();
+		aJCas.setDocumentLanguage(language);
+
+		TokenBuilder<Token, Sentence> tb = new TokenBuilder<Token, Sentence>(Token.class,
+				Sentence.class);
+		tb.buildTokens(aJCas, testDocument);
+
+		int offset = 0;
+		for (Token token : JCasUtil.select(aJCas, Token.class)) {
+			
+			if (documentPosTags[offset].matches("NN")) {
+				NN nn = new NN(aJCas, token.getBegin(), token.getEnd());
+				nn.setPosValue(documentPosTags[offset]);
+				nn.addToIndexes();
+				token.setPos(nn);
+			} else if (documentPosTags[offset].matches("V")) {
+				V v = new V(aJCas, token.getBegin(), token.getEnd());
+				v.setPosValue(documentPosTags[offset]);
+				v.addToIndexes();
+				token.setPos(v);
+			} else if (documentPosTags[offset].matches("ADJ")) {
+				ADJ adj = new ADJ(aJCas, token.getBegin(), token.getEnd());
+				adj.setPosValue(documentPosTags[offset]);
+				adj.addToIndexes();
+				token.setPos(adj);				
+			} else {
+				POS pos = new POS(aJCas, token.getBegin(), token.getEnd());
+				pos.setPosValue(documentPosTags[offset]);
+				pos.addToIndexes();
+				token.setPos(pos);
+			}
+			
+			Lemma lemma = new Lemma(aJCas, token.getBegin(), token.getEnd());
+			lemma.setValue(documentLemmas[offset]);
+			lemma.addToIndexes();
+			token.setLemma(lemma);
+
+			offset++;
+		}
+		engine.process(aJCas);
+
+		AssertAnnotations.assertNamedEntity(null,documentUbySemanticFields,
+				select(aJCas, NamedEntity.class));
+	
+	}
+
+
+}
