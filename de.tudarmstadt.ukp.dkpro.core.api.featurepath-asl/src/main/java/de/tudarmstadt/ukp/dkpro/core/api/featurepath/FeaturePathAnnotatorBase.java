d0dbc7b81cc60f969984e340e97f4ec5857a3c58
Richard Eckart de Castilho <richard.eckart@gmail.com>
2013-08-22 20:58:54 +0000

    Update issue 121. Add missing JavaDoc for parameters and unify parameter names - Formatting - Removed warnings

--- de.tudarmstadt.ukp.dkpro.core.api.featurepath-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/api/featurepath/FeaturePathAnnotatorBase.java
+++ de.tudarmstadt.ukp.dkpro.core.api.featurepath-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/api/featurepath/FeaturePathAnnotatorBase.java
@@ -29,169 +29,162 @@ import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
 
 public abstract class FeaturePathAnnotatorBase
-	extends JCasAnnotator_ImplBase
+    extends JCasAnnotator_ImplBase
 {
 
-	/**
-	 * Specify a path that is used for annotation. Format is de.type.name/feature/path. All type
-	 * objects will be annotated with a IndexTermAnnotation. The value of the IndexTerm is specified
-	 * by the featurepath.
-	 */
-	public static final String PARAM_PATHS = "Paths";
-	@ConfigurationParameter(name = PARAM_PATHS, mandatory = false)
-	protected Set<String> paths;
+    /**
+     * Specify a path that is used for annotation. Format is de.type.name/feature/path. All type
+     * objects will be annotated with a IndexTermAnnotation. The value of the IndexTerm is specified
+     * by the feature path.
+     */
+    public static final String PARAM_PATHS = "paths";
+    @ConfigurationParameter(name = PARAM_PATHS, mandatory = false)
+    protected Set<String> paths;
 
-	/**
-	 * Specifies a featurepath that is used in the filter. If this is set, you also have to specify
-	 * <code>PARAM_FILTER_CONDITION_OPERATOR</code> and <code>PARAM_FILTER_CONDITION_VALUE</code>.
-	 */
-	public static final String PARAM_FILTER_FEATUREPATH = "FilterFeaturePath";
-	@ConfigurationParameter(name = PARAM_FILTER_FEATUREPATH, mandatory = false)
-	protected String filterFeaturePath;
+    /**
+     * Specifies a feature path that is used in the filter. If this is set, you also have to specify
+     * <code>PARAM_FILTER_CONDITION_OPERATOR</code> and <code>PARAM_FILTER_CONDITION_VALUE</code>.
+     */
+    public static final String PARAM_FILTER_FEATUREPATH = "filterFeaturePath";
+    @ConfigurationParameter(name = PARAM_FILTER_FEATUREPATH, mandatory = false)
+    protected String filterFeaturePath;
 
-	/**
-	 * Specifies the operator for a filtering condition.
-	 * <p>
-	 * It is only used if <code>PARAM_FILTER_FEATUREPATH</code> is set.
-	 *
-	 * @see de.tudarmstadt.ukp.dkpro.core.util.featurepath.FilterOp
-	 */
+    /**
+     * Specifies the operator for a filtering condition.
+     * <p>
+     * It is only used if <code>PARAM_FILTER_FEATUREPATH</code> is set.
+     * 
+     * @see FilterOp
+     */
 
-	public static final String PARAM_FILTER_CONDITION_OPERATOR = "FilterConditionOperator";
-	@ConfigurationParameter(name = PARAM_FILTER_CONDITION_OPERATOR, mandatory = false)
-	protected String filterConditionOperator;
+    public static final String PARAM_FILTER_CONDITION_OPERATOR = "filterConditionOperator";
+    @ConfigurationParameter(name = PARAM_FILTER_CONDITION_OPERATOR, mandatory = false)
+    protected String filterConditionOperator;
 
-	/**
-	 * Specifies the value for a filtering condition.
-	 * <p>
-	 * It is only used if <code>PARAM_FILTER_FEATUREPATH</code> is set.
-	 *
-	 * @see de.tudarmstadt.ukp.dkpro.core.util.featurepath.Condition
-	 */
-	public static final String PARAM_FILTER_CONDITION_VALUE = "FilterConditionValue";
-	@ConfigurationParameter(name = PARAM_FILTER_CONDITION_VALUE, mandatory = false)
-	protected String filterConditionValue;
+    /**
+     * Specifies the value for a filtering condition.
+     * <p>
+     * It is only used if <code>PARAM_FILTER_FEATUREPATH</code> is set.
+     * 
+     * @see Condition
+     */
+    public static final String PARAM_FILTER_CONDITION_VALUE = "filterConditionValue";
+    @ConfigurationParameter(name = PARAM_FILTER_CONDITION_VALUE, mandatory = false)
+    protected String filterConditionValue;
 
-	protected Condition filterCondition;
-	protected FeaturePathInfo filterFeaturePathInfo = new FeaturePathInfo();
-	protected FeaturePathInfo fp = new FeaturePathInfo();
+    protected Condition filterCondition;
+    protected FeaturePathInfo filterFeaturePathInfo = new FeaturePathInfo();
+    protected FeaturePathInfo fp = new FeaturePathInfo();
 
-	@Override
-	public void initialize(UimaContext aContext)
-		throws ResourceInitializationException
-	{
+    @Override
+    public void initialize(UimaContext aContext)
+        throws ResourceInitializationException
+    {
 
-		super.initialize(aContext);
+        super.initialize(aContext);
 
-		// Check for at least one path entry
-		if (paths == null || paths.size() == 0) {
-			paths = getDefaultPaths();
-		}
+        // Check for at least one path entry
+        if (paths == null || paths.size() == 0) {
+            paths = getDefaultPaths();
+        }
 
-		// Check for at least one path entry
-		if (paths == null || paths.size() == 0) {
-			throw new ResourceInitializationException(new IllegalArgumentException(
-					"PARAM_PATHS must not be empty, please specify at least one path"));
-		}
+        // Check for at least one path entry
+        if (paths == null || paths.size() == 0) {
+            throw new ResourceInitializationException(new IllegalArgumentException(
+                    "PARAM_PATHS must not be empty, please specify at least one path"));
+        }
 
-		// check if filter featurepath exists, if so, try to build the condition
-		if (filterFeaturePath != null) {
-			try {
+        // check if filter feature path exists, if so, try to build the condition
+        if (filterFeaturePath != null) {
+            try {
+                initializeFilter();
+            }
+            catch (FeaturePathException e) {
+                throw new ResourceInitializationException(e);
+            }
+        }
+    }
 
-				initializeFilter();
+    protected Set<String> getDefaultPaths()
+    {
+        return null;
+    }
 
-			}
-			catch (FeaturePathException e) {
-				// TODO Auto-generated catch block
-				// e.printStackTrace();
-				throw new ResourceInitializationException(e);
-			}
-		}
-	}
+    /**
+     * Method that initializes the filter.
+     * 
+     * @throws FeaturePathException
+     *             if an error occurs during the initialization of the {@link FeaturePathInfo}
+     *             object.
+     */
+    private void initializeFilter()
+        throws FeaturePathException
+    {
 
-	protected
-	Set<String> getDefaultPaths()
-	{
-		return null;
-	}
+        this.filterFeaturePathInfo.initialize(this.filterFeaturePath);
 
-	/**
-	 * Method that intializes the filter.
-	 *
-	 * @throws FeaturePathException
-	 *             if an error occurs during the initialization of the FeaturePathInfo object.
-	 */
-	private void initializeFilter()
-		throws FeaturePathException
-	{
+        // check for operator + value if filterconditionfeaturepath is set
+        if (filterFeaturePath != null) {
 
-		this.filterFeaturePathInfo.initialize(this.filterFeaturePath);
+            // operator must not be blank
+            if (StringUtils.isBlank(filterConditionOperator)) {
+                throw new IllegalArgumentException(
+                        "PARAM_FILTER_CONDITION_OPERATOR must not be blank!");
+            }
+            // operator must not be null
+            // TODO Check me, can this happen? Can a
+            if (filterConditionValue == null) {
+                throw new IllegalArgumentException("PARAM_FILTER_CONDITION_VALUE must not be null!");
+            }
+        }
+        // get condition operator
+        FilterOp operator = Condition.getOperator(this.filterConditionOperator);
+        if (operator == null) {
+            throw new IllegalStateException("not a valid operator!");
+        }
 
-		// check for operator + value if filterconditionfeaturepath is set
-		if (filterFeaturePath != null) {
+        // create new Condition object
+        this.filterCondition = new Condition(operator, this.filterConditionValue);
 
-			// operator must not be blank
-			if (StringUtils.isBlank(filterConditionOperator)) {
-				throw new IllegalArgumentException(
-						"PARAM_FILTER_CONDITION_OPERATOR must not be blank!");
-			}
-			// operator must not be null
-			// TODO Check me, can this happen? Can a
-			if (filterConditionValue == null) {
-				throw new IllegalArgumentException("PARAM_FILTER_CONDITION_VALUE must not be null!");
-			}
-		}
-		// get condition operator
-		FilterOp operator = Condition.getOperator(this.filterConditionOperator);
-		if (operator == null) {
-			throw new IllegalStateException("not a valid operator!");
-		}
+    }
 
-		// create new Condition object
-		this.filterCondition = new Condition(operator, this.filterConditionValue);
+    @Override
+    public void process(JCas jcas)
+        throws AnalysisEngineProcessException
+    {
+        try {
+            generateAnnotations(jcas);
+        }
+        catch (FeaturePathException e) {
+            throw new AnalysisEngineProcessException(e);
+        }
+    }
 
-	}
+    /**
+     * Checks for empty feature path and initializes the feature path object.
+     * 
+     * @param aFp
+     *            the feature path that is to be initialized with the featurePathString
+     * @param featurePathString
+     *            the string that is used to initialize feature path
+     * @throws FeaturePathException
+     *             if an error occurs during initialization of the {@link FeaturePathInfo} object
+     */
+    public void initializeFeaturePathInfoFrom(FeaturePathInfo aFp, String[] featurePathString)
+        throws FeaturePathException
+    {
+        if (featurePathString.length > 1) {
+            aFp.initialize(featurePathString[1]);
+        }
+        else {
+            aFp.initialize("");
+        }
+    }
 
-	@Override
-	public void process(JCas jcas)
-		throws AnalysisEngineProcessException
-	{
-		try {
-			generateAnnotations(jcas);
-		}
-		catch (FeaturePathException e) {
-			throw new AnalysisEngineProcessException(e);
-		}
-	}
-
-	/**
-	 * Checks for empty featurepath and initializes the featurepathobject.
-	 *
-	 * @param aFp
-	 *            the featurepath that is to be initialized with the featurePathString
-	 * @param featurePathString
-	 *            the string that is used to initialize fp
-	 * @throws FeaturePathException
-	 *             if an error occurs during initialization of the FeaturePathInfo object
-	 */
-	public void initializeFeaturePathInfoFrom(FeaturePathInfo aFp, String[] featurePathString)
-		throws FeaturePathException
-	{
-		if (featurePathString.length > 1) {
-			aFp.initialize(featurePathString[1]);
-		}
-		else {
-			aFp.initialize("");
-		}
-	}
-
-	/**
-	 * Method to create annotations.
-	 *
-	 * @param jcas
-	 * @throws FeaturePathException
-	 */
-	abstract protected void generateAnnotations(JCas jcas)
-		throws FeaturePathException, UIMARuntimeException, AnalysisEngineProcessException;
+    /**
+     * Method to create annotations.
+     */
+    abstract protected void generateAnnotations(JCas jcas)
+        throws FeaturePathException, UIMARuntimeException, AnalysisEngineProcessException;
 
 }
