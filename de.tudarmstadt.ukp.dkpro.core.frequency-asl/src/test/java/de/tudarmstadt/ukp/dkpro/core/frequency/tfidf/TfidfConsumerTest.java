2a1360dd286a5c421757dd4ef7b50972597a2470
Richard Eckart de Castilho <richard.eckart@gmail.com>
2013-08-30 21:02:11 +0000

    No issue. Fixed warnings.

--- de.tudarmstadt.ukp.dkpro.core.frequency-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/frequency/tfidf/TfidfConsumerTest.java
+++ de.tudarmstadt.ukp.dkpro.core.frequency-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/frequency/tfidf/TfidfConsumerTest.java
@@ -21,18 +21,21 @@ import static de.tudarmstadt.ukp.dkpro.core.api.io.ResourceCollectionReaderBase.
 import static de.tudarmstadt.ukp.dkpro.core.api.io.ResourceCollectionReaderBase.PARAM_SOURCE_LOCATION;
 import static de.tudarmstadt.ukp.dkpro.core.api.io.ResourceCollectionReaderBase.PARAM_PATTERNS;
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
-import static org.apache.uima.fit.factory.CollectionReaderFactory.createReader;
+import static org.apache.uima.fit.factory.CollectionReaderFactory.createReaderDescription;
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
-import org.apache.uima.collection.CollectionReader;
+import org.apache.uima.collection.CollectionReaderDescription;
 import org.apache.uima.fit.pipeline.SimplePipeline;
-import org.apache.uima.util.FileUtils;
-import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.rules.TestName;
 
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import de.tudarmstadt.ukp.dkpro.core.frequency.tfidf.model.DfModel;
 import de.tudarmstadt.ukp.dkpro.core.frequency.tfidf.util.TfidfUtils;
 import de.tudarmstadt.ukp.dkpro.core.io.text.TextReader;
@@ -40,45 +43,48 @@ import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
 
 /**
  * Unit test of {@link TfidfConsumer} and {@link TfidfAnnotator}.
- *
+ * 
  * @author zesch, parzonka
- *
+ * 
  */
-public class TfidfConsumerTest {
-
-    private final static String CONSUMER_TEST_DATA_PATH = "src/test/resources/consumer/";
-    private final static String OUTPUT_PATH = CONSUMER_TEST_DATA_PATH + "output/df.model";
+public class TfidfConsumerTest
+{
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
-    public void RawScoresTest()
-	    throws Exception {
-
-        CollectionReader reader = createReader(
-                TextReader.class,
-                PARAM_SOURCE_LOCATION, CONSUMER_TEST_DATA_PATH,
-                PARAM_PATTERNS, new String[] { INCLUDE_PREFIX+"*.txt" });
+    public void rawScoresTest()
+        throws Exception
+    {
+        File target = folder.newFile(name.getMethodName());
+        
+        CollectionReaderDescription reader = createReaderDescription(TextReader.class, 
+                PARAM_SOURCE_LOCATION, "src/test/resources/consumer/", 
+                PARAM_PATTERNS, INCLUDE_PREFIX + "*.txt");
 
         AnalysisEngineDescription aggregate = createEngineDescription(
                 createEngineDescription(BreakIteratorSegmenter.class),
-                createEngineDescription(TfidfConsumer.class,
-                    TfidfConsumer.PARAM_FEATURE_PATH, de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token.class.getName(),
-                    TfidfConsumer.PARAM_OUTPUT_PATH,  OUTPUT_PATH
-                )
-        );
+                createEngineDescription(TfidfConsumer.class, 
+                        TfidfConsumer.PARAM_FEATURE_PATH, Token.class.getName(),
+                        TfidfConsumer.PARAM_TARGET_LOCATION, target));
 
         // now create the tf and df files
         SimplePipeline.runPipeline(reader, aggregate);
 
         // check whether they were really created and contain the correct values
-	DfModel dfModel = TfidfUtils.getDfModel(OUTPUT_PATH);
+        DfModel dfModel = TfidfUtils.getDfModel(target.getPath());
 
         assertEquals(2, dfModel.getDf("example"));
-	assertEquals(2, dfModel.getDf("sentence"));
-	assertEquals(1, dfModel.getDf("funny"));
+        assertEquals(2, dfModel.getDf("sentence"));
+        assertEquals(1, dfModel.getDf("funny"));
     }
 
-    @AfterClass
-    public static void cleanUp() {
-		FileUtils.deleteRecursive(new File(OUTPUT_PATH));
+    @Rule
+    public TestName name = new TestName();
+
+    @Before
+    public void printSeparator()
+    {
+        System.out.println("\n=== " + name.getMethodName() + " =====================");
     }
 }
