ba7856dfb4898e9413fea2a4158fef8efb34f667
Richard Eckart de Castilho <richard.eckart@gmail.com>
2013-09-17 08:57:44 +0000

    Update issue 169. Tag text with information from Uby - Downgrade from commons-lang3 to commons-lang

--- de.tudarmstadt.ukp.dkpro.core.dictionaryannotator-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/dictionaryannotator/semantictagging/SemanticTagResource.java
+++ de.tudarmstadt.ukp.dkpro.core.dictionaryannotator-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/dictionaryannotator/semantictagging/SemanticTagResource.java
@@ -25,8 +25,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.apache.commons.lang3.StringUtils;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.uima.fit.component.Resource_ImplBase;
 import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.resource.ResourceAccessException;
