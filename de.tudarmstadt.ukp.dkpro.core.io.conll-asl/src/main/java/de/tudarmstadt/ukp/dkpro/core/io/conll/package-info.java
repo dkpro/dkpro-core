b295ad69acecaec01b0512f54c420a79dcf060d6
Torsten Zesch <torsten.zesch@gmail.com>
2013-12-20 10:27:45 +0000

    Update issue 291. Add reader for Conll 2000 format (chunking) - CoNLL 2000 chunking reader.

--- de.tudarmstadt.ukp.dkpro.core.io.conll-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/conll/package-info.java
+++ de.tudarmstadt.ukp.dkpro.core.io.conll-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/conll/package-info.java
@@ -16,6 +16,7 @@
  * limitations under the License.
  ******************************************************************************/
 /**
- * Provides classes for the conversion of conll file to and from Brat response formats
+ * Provides classes for the conversion of conll file formats.
+ * Especially to and from Brat response formats
  */
 package de.tudarmstadt.ukp.dkpro.core.io.conll;
---
+++ de.tudarmstadt.ukp.dkpro.core.io.conll-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/io/conll/Conll2000ReaderTest.java
@@ -0,0 +1,61 @@
+package de.tudarmstadt.ukp.dkpro.core.io.conll;
+
+
+import static org.apache.uima.fit.factory.CollectionReaderFactory.createReaderDescription;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Collection;
+
+import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.pipeline.JCasIterable;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.junit.Test;
+
+import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+import de.tudarmstadt.ukp.dkpro.core.api.syntax.type.IOB;
+
+public class Conll2000ReaderTest
+{
+
+    @Test
+    public void conll2000test()
+        throws Exception
+    {
+        CollectionReaderDescription reader = createReaderDescription(
+                Conll2000Reader.class, 
+                Conll2000Reader.PARAM_SOURCE_LOCATION, "src/test/resources/conll/2000/", 
+                Conll2000Reader.PARAM_PATTERNS, "chunk2000_test.txt"
+        );
+        
+        int i=0;
+        int nrOfSentences = 0;
+        for (JCas jcas : new JCasIterable(reader)) {
+            for (Sentence sentence : JCasUtil.select(jcas, Sentence.class)) {                
+                if (nrOfSentences == 0) {
+                    assertEquals("Confidence in the pound is widely expected to take " +
+                    		"another sharp dive if trade figures for September , due for" +
+                    		" release tomorrow , fail to show a substantial improvement " +
+                    		"from July and August 's near-record deficits .", sentence.getCoveredText());
+                    for (Token token : JCasUtil.select(jcas, Token.class)) {
+                        POS pos = token.getPos();
+                        Collection<IOB> iobs = JCasUtil.selectCovered(jcas, IOB.class, token);
+                        assertEquals(1, iobs.size());
+                        IOB iob = iobs.iterator().next();
+                        
+                        System.out.println(token.getCoveredText() + "\t" + pos.getClass().getSimpleName() + "\t" + iob.getValue());
+                    }
+                }
+                nrOfSentences++;
+            }
+            i++;
+        }
+        
+        // 1 big document
+        assertEquals(1, i);
+        
+        assertEquals(3, nrOfSentences);
+    }
+}
