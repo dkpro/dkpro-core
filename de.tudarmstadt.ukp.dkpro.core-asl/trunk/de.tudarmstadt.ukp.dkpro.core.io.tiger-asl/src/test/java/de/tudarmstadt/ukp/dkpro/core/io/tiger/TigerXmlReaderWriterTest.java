d23f0f953b4d11154f960e5a5184508d0a2a3a9d
richard.eckart@gmail.com <richard.eckart@gmail.com@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2013-12-22 03:27:17 +0000

    Update issue 292. Add TigerXmlWriter - Added an initial version of the writer

---
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.io.tiger-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/io/tiger/TigerXmlReaderWriterTest.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright 2013
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.tudarmstadt.ukp.dkpro.core.io.tiger;
+
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+import static org.apache.uima.fit.factory.CollectionReaderFactory.createReaderDescription;
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class TigerXmlReaderWriterTest
+{
+    @Rule
+    public TemporaryFolder folder= new TemporaryFolder();
+    
+    @Test
+    public void test() throws Exception
+    {
+        File targetFolder = folder.newFolder();
+
+        CollectionReaderDescription reader = createReaderDescription(
+                TigerXmlReader.class,
+                TigerXmlReader.PARAM_SOURCE_LOCATION, "src/test/resources",
+                TigerXmlReader.PARAM_PATTERNS, "simple-sentence.xml");
+                
+        AnalysisEngineDescription writer = createEngineDescription(TigerXmlWriter.class,
+                TigerXmlWriter.PARAM_STRIP_EXTENSION, true,
+                TigerXmlWriter.PARAM_TARGET_LOCATION, targetFolder);
+        
+        SimplePipeline.runPipeline(reader, writer);
+        
+        assertEquals(
+                FileUtils.readFileToString(
+                        new File("src/test/resources/simple-sentence.xml"), 
+                        "UTF-8").trim(),
+                FileUtils.readFileToString(
+                        new File(targetFolder, "simple-sentence.xml"), 
+                        "UTF-8").trim());
+    }
+}
---
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.io.tiger-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/io/tiger/TigerXmlWriterTest.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * Copyright 2013
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.tudarmstadt.ukp.dkpro.core.io.tiger;
+
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngine;
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.jcas.JCas;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
+import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpParser;
+import de.tudarmstadt.ukp.dkpro.core.testing.TestRunner;
+
+public class TigerXmlWriterTest
+{
+    @Rule
+    public TemporaryFolder folder= new TemporaryFolder();
+    
+    @Test
+    public void test() throws Exception
+    {
+        File targetFolder = folder.newFolder();
+        
+        AnalysisEngine parser = createEngine(OpenNlpParser.class);
+        JCas jcas = TestRunner.runTest(parser, "en", "This is a test .");
+
+        DocumentMetaData meta = DocumentMetaData.create(jcas);
+        meta.setCollectionId("nocollection");
+        meta.setDocumentId("dummy");
+        
+        AnalysisEngine writer = createEngine(TigerXmlWriter.class,
+                TigerXmlWriter.PARAM_TARGET_LOCATION, targetFolder);
+        writer.process(jcas);
+        
+        assertEquals(
+                FileUtils.readFileToString(
+                        new File("src/test/resources/simple-sentence.xml"), 
+                        "UTF-8").trim(),
+                FileUtils.readFileToString(
+                        new File(targetFolder, "dummy.xml"), 
+                        "UTF-8").trim());
+    }
+}
