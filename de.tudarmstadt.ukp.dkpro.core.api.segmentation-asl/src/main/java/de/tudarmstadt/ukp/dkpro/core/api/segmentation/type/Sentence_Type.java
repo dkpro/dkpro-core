f8d3aa5d70018f4fadb472de84ddd8c7acb60a21
Richard Eckart de Castilho <richard.eckart@gmail.com>
2014-01-10 13:17:58 +0000

    No issue. Revert last change. See https://groups.google.com/d/msg/dkpro-core-developers/uuZllhSPY6A/ePS9gvcsf9cJ

--- de.tudarmstadt.ukp.dkpro.core.api.segmentation-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/api/segmentation/type/Sentence_Type.java
+++ de.tudarmstadt.ukp.dkpro.core.api.segmentation-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/api/segmentation/type/Sentence_Type.java
@@ -9,12 +9,10 @@ import org.apache.uima.cas.impl.FSGenerator;
 import org.apache.uima.cas.FeatureStructure;
 import org.apache.uima.cas.impl.TypeImpl;
 import org.apache.uima.cas.Type;
-import org.apache.uima.cas.impl.FeatureImpl;
-import org.apache.uima.cas.Feature;
 import org.apache.uima.jcas.tcas.Annotation_Type;
 
 /** 
- * Updated by JCasGen Fri Jan 10 09:54:06 CET 2014
+ * Updated by JCasGen Sat Aug 04 18:48:32 CEST 2012
  * @generated */
 public class Sentence_Type extends Annotation_Type {
   /** @generated */
@@ -43,35 +41,11 @@ public class Sentence_Type extends Annotation_Type {
 
 
 
-  /** @generated */
-  final Feature casFeat_comment;
-  /** @generated */
-  final int     casFeatCode_comment;
-  /** @generated */ 
-  public String getComment(int addr) {
-        if (featOkTst && casFeat_comment == null)
-      jcas.throwFeatMissing("comment", "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence");
-    return ll_cas.ll_getStringValue(addr, casFeatCode_comment);
-  }
-  /** @generated */    
-  public void setComment(int addr, String v) {
-        if (featOkTst && casFeat_comment == null)
-      jcas.throwFeatMissing("comment", "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence");
-    ll_cas.ll_setStringValue(addr, casFeatCode_comment, v);}
-    
-  
-
-
-
   /** initialize variables to correspond with Cas Type and Features
 	* @generated */
   public Sentence_Type(JCas jcas, Type casType) {
     super(jcas, casType);
     casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl)this.casType, getFSGenerator());
-
- 
-    casFeat_comment = jcas.getRequiredFeatureDE(casType, "comment", "uima.cas.String", featOkTst);
-    casFeatCode_comment  = (null == casFeat_comment) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_comment).getCode();
 
   }
 }
