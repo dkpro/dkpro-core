1c3ca3e4e52694908fca77e9c880c5d059093232
richard.eckart@gmail.com <richard.eckart@gmail.com@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2014-02-21 12:54:17 +0000

    Fixes issue 340. Better logging for Ivy - Moved adapter to internal package. - Added missing license header - Use ResourceObjectProviderBase as class to fetch logger

--- de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.api.resources-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/api/resources/internal/ApacheCommonsLoggingAdapter.java
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.api.resources-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/api/resources/internal/ApacheCommonsLoggingAdapter.java
@@ -1,16 +1,36 @@
-package de.tudarmstadt.ukp.dkpro.core.api.resources;
+/*******************************************************************************
+ * Copyright 2014
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.tudarmstadt.ukp.dkpro.core.api.resources.internal;
 
 import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.ivy.util.AbstractMessageLogger;
 import org.apache.ivy.util.Message;
 
-public class ApacheCommonsMessageLogger
+public class ApacheCommonsLoggingAdapter
     extends AbstractMessageLogger
 {
+    private final Log logger;
 
-    private final Log logger = LogFactory.getLog(getClass());
-
+    public ApacheCommonsLoggingAdapter(Log aLogger)
+    {
+        logger = aLogger;
+    }
+    
     @Override
     public void log(String msg, int level)
     {
