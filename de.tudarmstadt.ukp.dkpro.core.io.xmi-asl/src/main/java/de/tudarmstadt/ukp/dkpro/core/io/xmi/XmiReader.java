8d927a6f2aced1d905897d93807e77e4a40e664b
Richard Eckart de Castilho <richard.eckart@gmail.com>
2014-02-28 11:42:21 +0000

    Fixes issue 353. Support lenient mode in XMI reader

--- de.tudarmstadt.ukp.dkpro.core.io.xmi-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/xmi/XmiReader.java
+++ de.tudarmstadt.ukp.dkpro.core.io.xmi-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/xmi/XmiReader.java
@@ -25,6 +25,7 @@ import java.io.InputStream;
 import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.impl.XmiCasDeserializer;
 import org.apache.uima.collection.CollectionException;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.fit.descriptor.TypeCapability;
 import org.xml.sax.SAXException;
 
@@ -32,7 +33,7 @@ import de.tudarmstadt.ukp.dkpro.core.api.io.ResourceCollectionReaderBase;
 import de.tudarmstadt.ukp.dkpro.core.api.resources.CompressionUtils;
 
 /**
- * @author Richard Eckart de Castilho
+ * Reader for UIMA XMI files.
  */
 @TypeCapability(
         outputs={
@@ -40,6 +41,13 @@ import de.tudarmstadt.ukp.dkpro.core.api.resources.CompressionUtils;
 public class XmiReader
 	extends ResourceCollectionReaderBase
 {
+    /**
+     * In lenient mode, unknown types are ignored and do not cause an exception to be thrown.
+     */
+    public static final String PARAM_LENIENT = "lenient";
+    @ConfigurationParameter(name=PARAM_LENIENT, mandatory=true, defaultValue="false")
+    private boolean lenient;
+    
 	@Override
 	public void getNext(CAS aCAS)
 		throws IOException, CollectionException
@@ -51,7 +59,7 @@ public class XmiReader
 		try {
 			InputStream resolvedIS = CompressionUtils.getInputStream(res.getLocation(),
 					res.getInputStream());
-			XmiCasDeserializer.deserialize(resolvedIS, aCAS);
+			XmiCasDeserializer.deserialize(resolvedIS, aCAS, lenient);
 		}
 		catch (SAXException e) {
 			throw new IOException(e);
