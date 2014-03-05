e009f7ae9d7c7e041065db53cc5bc6c62279a27c
Torsten Zesch <torsten.zesch@gmail.com>
2014-03-05 17:26:04 +0000

    License headers and reorganizing.

--- de.tudarmstadt.ukp.dkpro.core.auebtools-gpl/src/main/java/gr/aueb/cs/nlp/postagger/BigSetFindAmbitags.java
+++ de.tudarmstadt.ukp.dkpro.core.auebtools-gpl/src/main/java/gr/aueb/cs/nlp/postagger/BigSetFindAmbitags.java
@@ -1,4 +1,8 @@
 /**
+ * Copyright 2011
+ * Athens University of Economics and Business
+ * Department of Informatics
+ * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
