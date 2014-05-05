2614faf2d8c89c792a0bda3388ff5288bb72d6d8
Richard Eckart de Castilho <richard.eckart@gmail.com>
2014-05-05 22:08:49 +0000

    Issue 228. Rewrite build.xml for TreeTagger module
    Update issue 228

--- de.tudarmstadt.ukp.dkpro.core.treetagger-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/treetagger/TreeTaggerChunkerTT4JTest.java
+++ de.tudarmstadt.ukp.dkpro.core.treetagger-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/treetagger/TreeTaggerChunkerTT4JTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2010
+ * Copyright 2014
  * Ubiquitous Knowledge Processing (UKP) Lab
  * Technische Universität Darmstadt
  *
@@ -17,119 +17,146 @@
  ******************************************************************************/
 package de.tudarmstadt.ukp.dkpro.core.treetagger;
 
+import static de.tudarmstadt.ukp.dkpro.core.testing.AssertAnnotations.assertChunks;
+import static de.tudarmstadt.ukp.dkpro.core.testing.AssertAnnotations.assertTagset;
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngine;
-import static org.apache.uima.fit.factory.TypeSystemDescriptionFactory.createTypeSystemDescription;
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
 import static org.apache.uima.fit.util.JCasUtil.select;
 import static org.junit.Assert.assertEquals;
 
 import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.fit.factory.JCasFactory;
 import org.apache.uima.fit.testing.factory.TokenBuilder;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.tcas.Annotation;
-import org.apache.uima.util.CasCreationUtils;
-import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
 
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import de.tudarmstadt.ukp.dkpro.core.api.syntax.type.chunk.Chunk;
+import de.tudarmstadt.ukp.dkpro.core.testing.TestRunner;
 
-public
-class TreeTaggerChunkerTT4JTest
+public class TreeTaggerChunkerTT4JTest
 {
+    @Before
+    public void setupLogging()
+    {
+        System.setProperty("org.apache.uima.logger.class", "org.apache.uima.util.impl.Log4jLogger_impl");
+    }
+    
     @Test
-    public
-    void treeTaggerAnnotatorEnglishTest()
-    throws Exception
+    public void testEnglish()
+        throws Exception
     {
-        runEnglishTest();
-        runEnglishTest2();
+        JCas jcas = runTest("en", null, "We need a very complicated example sentence, which " +
+                "contains as many constituents and dependencies as possible .");
+
+        String[] chunks = new String[] { 
+                "[  0,  2]NC(NC) (We)", 
+                "[  3,  7]VC(VC) (need)",
+                "[  8, 44]NC(NC) (a very complicated example sentence,)",
+                "[ 45, 50]NC(NC) (which)", 
+                "[ 51, 59]VC(VC) (contains)", 
+                "[ 60, 62]PC(PC) (as)",
+                "[ 63, 97]NC(NC) (many constituents and dependencies)", 
+                "[ 98,100]PC(PC) (as)",
+                "[101,109]ADJC(ADJC) (possible)", 
+                "[110,111]O(O) (.)" };
+
+        String[] chunkTags = new String[] { "ADJC", "ADVC", "CONJC", "INTJ", "LST", "NC", "O",
+                "PC", "PRT", "SBAR", "VC", "that" };
+
+        // String[] unmappedChunk = new String[] { "#", "$", "''", "-LRB-", "-RRB-", "``" };
+
+        assertChunks(chunks, select(jcas, Chunk.class));
+        assertTagset(Chunk.class, "tt", chunkTags, jcas);
+        // FIXME assertTagsetMapping(Chunk.class, "conll2000", unmappedChunk, jcas);
     }
 
     @Test
-    public
-    void treeTaggerAnnotatorGermanTest()
-    throws Exception
+    public void testGerman()
+        throws Exception
     {
-        runGermanTest();
+        JCas jcas = runTest("de", null, "Wir brauchen ein sehr kompliziertes Beispiel , welches "
+                + "möglichst viele Konstituenten und Dependenzen beinhaltet .");
+
+        String[] chunks = new String[] { 
+                "[  0,  3]NC(NC) (Wir)",
+                "[  4, 12]VC(VC) (brauchen)",
+                "[ 13, 44]NC(NC) (ein sehr kompliziertes Beispiel)",
+                "[ 45, 46]O(0) (,)",
+                "[ 47, 54]NC(NC) (welches)",
+                "[ 55, 64]O(0) (möglichst)",
+                "[ 65, 84]NC(NC) (viele Konstituenten)",
+                "[ 85, 88]O(0) (und)",
+                "[ 89,100]NC(NC) (Dependenzen)",
+                "[101,111]VC(VC) (beinhaltet)",
+                "[112,113]O(0) (.)" };
+
+        String[] chunkTags = new String[] { "0", "NC", "PC", "VC" };
+
+        // String[] unmappedChunk = new String[] { "#", "$", "''", "-LRB-", "-RRB-", "``" };
+
+        assertChunks(chunks, select(jcas, Chunk.class));
+        assertTagset(Chunk.class, "tt", chunkTags, jcas);
+        // FIXME assertTagsetMapping(Chunk.class, "conll2000", unmappedChunk, jcas);
     }
 
-    private
-    void runEnglishTest()
-    throws Exception
+    private JCas runTest(String aLanguage, String aVariant, String aText)
+        throws Exception
     {
-        runTest(
-        		"This is a test .",
-        		new String[] { "this", "be",  "a",   "test", "."    },
-        		new String[] { "NC",   "VC",  "NC",          "O" },
-        		new String[] { "NC",   "VC",  "NC",          "O" },
-        		"en");
+        AnalysisEngineDescription tagger = createEngineDescription(TreeTaggerPosLemmaTT4J.class);
+
+        AnalysisEngineDescription chunker = createEngineDescription(TreeTaggerChunkerTT4J.class,
+                TreeTaggerChunkerTT4J.PARAM_VARIANT, aVariant, 
+                TreeTaggerChunkerTT4J.PARAM_PRINT_TAGSET, true);
+        
+        AnalysisEngineDescription aggregate = createEngineDescription(tagger, chunker);
+        
+        return TestRunner.runTest(aggregate, aLanguage, aText);
     }
-
-    private
-    void runEnglishTest2()
-    throws Exception
-    {
-        runTest(
-        		"A neural-net .",
-        		new String[] { "a",   "neural", "-", "net", "." },
-        		new String[] { "NC",                        "O" },
-        		new String[] { "NC",                        "O" },
-        		"en");
-    }
-
-    private
-    void runGermanTest()
-    throws Exception
-    {
-        runTest(
-        		"Das ist ein Test .",
-        		new String[] { "d",   "sein",  "ein", "Test", "." },
-        		new String[] { "NC",  "VC",    "NC",          "0" },
-        		new String[] { "NC",  "VC",    "NC",          "O" },
-        		"de");
-    }
-
-    private
-    void checkModelsAndBinary(String lang)
-    {
-		Assume.assumeTrue(getClass().getResource(
-				"/de/tudarmstadt/ukp/dkpro/core/treetagger/lib/chunker-" + lang + "-little-endian.par") != null);
-
-		Assume.assumeTrue(getClass().getResource(
-				"/de/tudarmstadt/ukp/dkpro/core/treetagger/bin/LICENSE.txt") != null);
-    }
-
-	private JCas runTest(String testDocument, String[] lemmas, String[] tags, String[] tagClasses,
-			String language)
+    
+	private JCas runTest(String aLanguage, String aText, String[] aLemmas, String[] aTags,
+			String[] aTagClasses)
 		throws Exception
 	{
-    	checkModelsAndBinary(language);
-
 		AnalysisEngine tagger = createEngine(TreeTaggerPosLemmaTT4J.class);
         AnalysisEngine chunker = createEngine(TreeTaggerChunkerTT4J.class,
         		TreeTaggerPosLemmaTT4J.PARAM_PRINT_TAGSET, true);
 
-        JCas aJCas = CasCreationUtils.createCas(createTypeSystemDescription(), null, null).getJCas();
-        aJCas.setDocumentLanguage(language);
+        JCas aJCas = JCasFactory.createJCas();
+        aJCas.setDocumentLanguage(aLanguage);
 
         TokenBuilder<Token, Annotation> tb = TokenBuilder.create(Token.class, Annotation.class);
-        tb.buildTokens(aJCas, testDocument);
+        tb.buildTokens(aJCas, aText);
 
         tagger.process(aJCas);
         chunker.process(aJCas);
 
         // test Chunk annotations
-        if (tagClasses != null && tags != null) {
+        if (aTagClasses != null && aTags != null) {
 	        int i = 0;
 	        for (Chunk posAnnotation : select(aJCas, Chunk.class)) {
 	        	System.out.println(posAnnotation.getChunkValue()+": ["+posAnnotation.getCoveredText()+"]");
-	            assertEquals("In position "+i, tagClasses[i], posAnnotation.getType().getShortName());
-	            assertEquals("In position "+i, tags[i], posAnnotation.getChunkValue());
+	            assertEquals("In position "+i, aTagClasses[i], posAnnotation.getType().getShortName());
+	            assertEquals("In position "+i, aTags[i], posAnnotation.getChunkValue());
 	            i++;
 	        }
-	        assertEquals(tags.length, i);
+	        assertEquals(aTags.length, i);
         }
 
         return aJCas;
+    }
+
+    @Rule
+    public TestName name = new TestName();
+
+    @Before
+    public void printSeparator()
+    {
+        System.out.println("\n=== " + name.getMethodName() + " =====================");
     }
 }
