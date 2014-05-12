d7e21b1cb25031ca4c8203e4c8ef80f938b205f5
torsten.zesch <torsten.zesch@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2014-05-12 19:30:38 +0000

    Adding corrections contextualizer.

--- de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.jazzy-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/jazzy/JazzyCheckerTest.java
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.jazzy-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/jazzy/JazzyCheckerTest.java
@@ -18,6 +18,7 @@
 package de.tudarmstadt.ukp.dkpro.core.jazzy;
 
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngine;
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
 import static org.apache.uima.fit.util.JCasUtil.select;
 import static org.junit.Assert.assertEquals;
 
@@ -25,16 +26,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.fit.factory.ExternalResourceFactory;
 import org.apache.uima.fit.testing.factory.TokenBuilder;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ExternalResourceDescription;
 import org.junit.Test;
 
 import de.tudarmstadt.ukp.dkpro.core.api.anomaly.type.SpellingAnomaly;
+import de.tudarmstadt.ukp.dkpro.core.api.frequency.TestFrequencyCountResource;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 
 public class JazzyCheckerTest
 {
+
     @Test
     public void spellCheckerTest()
         throws Exception
@@ -48,27 +53,67 @@ public class JazzyCheckerTest
         errorsEnglish.add("morre");
         errorsEnglish.add("tink");
 
-        runEnglishSpellChecker(testDocumentEnglish, errorsEnglish);
-    }
-
-    private void runEnglishSpellChecker(String testDocument, List<String> errors)
-        throws Exception
-    {
-		AnalysisEngine engine = createEngine(JazzyChecker.class,
-				JazzyChecker.PARAM_MODEL_LOCATION, "src/test/resources/testdict.txt");
+        AnalysisEngine engine = createEngine(JazzyChecker.class,
+                JazzyChecker.PARAM_MODEL_LOCATION, "src/test/resources/testdict.txt");
+        
         JCas aJCas = engine.newJCas();
 
         TokenBuilder<Token, Sentence> tb = TokenBuilder.create(Token.class, Sentence.class);
-        tb.buildTokens(aJCas, testDocument);
+        tb.buildTokens(aJCas, testDocumentEnglish);
         engine.process(aJCas);
 
         int i = 0;
         for (SpellingAnomaly errorAnnotation : select(aJCas, SpellingAnomaly.class)) {
-            System.out.println(errorAnnotation.getCoveredText() + " - "
-                    + errorAnnotation.getSuggestions(0).getReplacement());
-            assertEquals(errors.get(i), errorAnnotation.getCoveredText());
+//            System.out.println(errorAnnotation.getCoveredText() + " - "
+//                    + errorAnnotation.getSuggestions(0).getReplacement());
+            assertEquals(errorsEnglish.get(i), errorAnnotation.getCoveredText());
             i++;
         }
-        System.out.println("Found " + i + " errors");
+        assertEquals(4, i);
+    }
+    
+    @Test
+    public void contextualizedSpellCheckerTest()
+        throws Exception
+    {
+        String testDocumentEnglish = "The cat sta on the mat .";
+
+        ExternalResourceDescription resource = ExternalResourceFactory.createExternalResourceDescription(TestFrequencyCountResource.class);
+
+//        String context = DkproContext.getContext().getWorkspace("web1t").getAbsolutePath();
+//        String workspace = "en";
+//        ExternalResourceDescription resource = ExternalResourceFactory.createExternalResourceDescription(
+//                Web1TFrequencyCountResource.class,
+//                Web1TFrequencyCountResource.PARAM_MIN_NGRAM_LEVEL, "1",
+//                Web1TFrequencyCountResource.PARAM_MAX_NGRAM_LEVEL, "3",
+//                Web1TFrequencyCountResource.PARAM_INDEX_PATH, new File(context, workspace).getAbsolutePath()
+//        );
+        
+        AnalysisEngine engine = createEngine(
+                createEngineDescription(
+                    createEngineDescription(
+                        JazzyChecker.class,
+                        JazzyChecker.PARAM_SCORE_THRESHOLD, 3,
+                        JazzyChecker.PARAM_MODEL_LOCATION, "src/test/resources/testdict_variants.txt"
+                    ),
+                    createEngineDescription(
+                        CorrectionsContextualizer.class,
+                        CorrectionsContextualizer.FREQUENCY_PROVIDER_RESOURCE, resource
+                    )
+                )
+        );
+        
+        JCas aJCas = engine.newJCas();
+
+        TokenBuilder<Token, Sentence> tb = TokenBuilder.create(Token.class, Sentence.class);
+        tb.buildTokens(aJCas, testDocumentEnglish);
+        engine.process(aJCas);
+
+        int i = 0;
+        for (SpellingAnomaly errorAnnotation : select(aJCas, SpellingAnomaly.class)) {
+            assertEquals("sat", errorAnnotation.getSuggestions(0).getReplacement());
+            i++;
+        }
+        assertEquals(1, i);
     }
 }
