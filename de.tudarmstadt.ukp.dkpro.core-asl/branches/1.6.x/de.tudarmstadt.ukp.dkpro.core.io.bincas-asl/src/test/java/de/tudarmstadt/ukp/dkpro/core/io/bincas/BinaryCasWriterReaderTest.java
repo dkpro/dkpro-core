cc87d20c8247a11906fe2d5c2289a16dec12119f
richard.eckart@gmail.com <richard.eckart@gmail.com@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2014-05-19 08:58:46 +0000

    Fixes issue 390. Improve BinaryCasReader/Writer

--- de.tudarmstadt.ukp.dkpro.core-asl/branches/1.6.x/de.tudarmstadt.ukp.dkpro.core.io.bincas-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/io/bincas/BinaryCasWriterReaderTest.java
+++ de.tudarmstadt.ukp.dkpro.core-asl/branches/1.6.x/de.tudarmstadt.ukp.dkpro.core.io.bincas-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/io/bincas/BinaryCasWriterReaderTest.java
@@ -17,25 +17,46 @@
  ******************************************************************************/
 package de.tudarmstadt.ukp.dkpro.core.io.bincas;
 
-import static org.apache.uima.fit.pipeline.SimplePipeline.*;
+import static de.tudarmstadt.ukp.dkpro.core.performance.PerformanceTestUtil.initRandomCas;
+import static de.tudarmstadt.ukp.dkpro.core.performance.PerformanceTestUtil.measurePerformance;
+import static de.tudarmstadt.ukp.dkpro.core.performance.PerformanceTestUtil.repeat;
 import static org.apache.commons.io.FileUtils.readFileToString;
-import static org.apache.uima.fit.factory.TypeSystemDescriptionFactory.*;
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngine;
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+import static org.apache.uima.fit.factory.TypeSystemDescriptionFactory.createTypeSystemDescription;
+import static org.apache.uima.fit.pipeline.SimplePipeline.runPipeline;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.PrefixFileFilter;
+import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
 import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.cas.CAS;
 import org.apache.uima.collection.CollectionReader;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.metadata.TypeSystemDescription;
 import org.apache.uima.util.CasCreationUtils;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import org.junit.rules.TestName;
 
 import de.tudarmstadt.ukp.dkpro.core.api.io.ResourceCollectionReaderBase;
+import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
+import de.tudarmstadt.ukp.dkpro.core.api.resources.CompressionMethod;
 import de.tudarmstadt.ukp.dkpro.core.io.text.TextReader;
 
 public class BinaryCasWriterReaderTest
@@ -43,112 +64,268 @@ public class BinaryCasWriterReaderTest
     @Rule
     public TemporaryFolder testFolder = new TemporaryFolder();
 
+    private static final int NONE = 1;
+    private static final int METADATA = 2;
+    private static final int ALL = 3;
+    
     @Test
-    public void test0()
+    public void testSReinitialize()
         throws Exception
     {
-        write("0");
-        read(createTypeSystemDescription());
+        write("S", true);
+        read(NONE, true); // Type system is reinitialized from the persisted type system
     }
 
     @Test
-    public void test4()
+    public void testSPreinitialized()
         throws Exception
     {
-        write("4");
-        read(createTypeSystemDescription());
+        write("S", false);
+        read(ALL, false);
     }
 
     @Test
-    public void test6()
+    public void testSplusReinitialize()
         throws Exception
     {
-        write("6");
-        read(createTypeSystemDescription());
+        write("S+", false);
+        read(NONE, false); // Type system is reinitialized from the persisted CAS
     }
 
     @Test
-    public void test6plus0()
+    public void test0Preinitialized()
         throws Exception
     {
-        write("6+");
-        read(createTypeSystemDescription());
+        write("0", false);
+        read(ALL, false);
     }
 
     @Test
-    public void test6plus1()
+    public void test4Preinitialized()
         throws Exception
     {
-        write("6+");
-        read(createTypeSystemDescription("desc.type.metadata"));
+        write("4", false);
+        read(ALL, false);
+    }
+    
+    /**
+     * The type system in the CAS is different from the one in the file. To do lenient loading with
+     * format 6, we need to know the type system that was used to originally store the CAS.
+     */
+    @Test
+    public void test6Lenient()
+        throws Exception
+    {
+        write("6", true);
+        read(METADATA, true);
     }
 
     @Test
-    public void testSerialized()
+    public void test6Preinitialized()
         throws Exception
     {
-        writeSerialized();
-        read(createTypeSystemDescription());
+        write("6", false);
+        read(ALL, false);
     }
 
-    public void write(String aFormat)
+    @Test
+    public void test6plusPreinitialized()
+        throws Exception
+    {
+        write("6+", false);
+        read(ALL, false);
+    }
+
+    @Test
+    public void test6plusLenient()
+        throws Exception
+    {
+        write("6+", false);
+        read(METADATA, false);
+    }
+
+    @Test
+    public void testSerializedEmbeddedTypeSystem()
+        throws Exception
+    {
+        writeSerialized(true);
+        read(NONE, false); // Type system is reinitialized from the persisted CAS
+    }
+
+    @Test
+    public void testSerializedSeparateTypeSystem()
+        throws Exception
+    {
+        writeSerialized(false);
+        read(NONE, true); // Type system is reinitialized from the persisted CAS
+    }
+
+    public void write(String aFormat, boolean aWriteTypeSystem)
         throws Exception
     {
         System.out.println("--- WRITING ---");
         CollectionReader textReader = CollectionReaderFactory.createReader(TextReader.class,
                 ResourceCollectionReaderBase.PARAM_SOURCE_LOCATION, "src/test/resources/texts",
-                ResourceCollectionReaderBase.PARAM_PATTERNS, "[+]*.txt",
+                ResourceCollectionReaderBase.PARAM_PATTERNS, "*.txt",
                 ResourceCollectionReaderBase.PARAM_LANGUAGE, "latin");
 
-        AnalysisEngine xmiWriter = AnalysisEngineFactory.createEngine(BinaryCasWriter.class,
+        AnalysisEngine writer = createEngine(
+                BinaryCasWriter.class, 
                 BinaryCasWriter.PARAM_FORMAT, aFormat, 
-                BinaryCasWriter.PARAM_TARGET_LOCATION, testFolder.getRoot().getPath());
+                BinaryCasWriter.PARAM_TARGET_LOCATION, testFolder.getRoot(),
+                BinaryCasWriter.PARAM_TYPE_SYSTEM_FILE, 
+                        aWriteTypeSystem ? testFolder.newFile("typesystem.bin") : null);
 
-//        AnalysisEngine dumper = createEngine(CASDumpWriter.class);
+        // AnalysisEngine dumper = createEngine(CASDumpWriter.class);
 
-        runPipeline(textReader, /*dumper,*/ xmiWriter);
+        runPipeline(textReader, /* dumper, */writer);
+
+        assertTrue(new File(testFolder.getRoot(), "example1.txt.bin").exists());
+        assertTrue(new File(testFolder.getRoot(), "example2.txt.bin").exists());
+    }
+
+    public void writeSerialized(boolean aIncludeTypeSystem)
+        throws Exception
+    {
+        System.out.println("--- WRITING ---");
+        CollectionReader reader = CollectionReaderFactory.createReader(
+                TextReader.class,
+                TextReader.PARAM_SOURCE_LOCATION, "src/test/resources/texts",
+                TextReader.PARAM_PATTERNS, "*.txt",
+                TextReader.PARAM_LANGUAGE, "latin");
+
+        AnalysisEngine writer = AnalysisEngineFactory.createEngine(
+                SerializedCasWriter.class,
+                SerializedCasWriter.PARAM_TARGET_LOCATION, testFolder.getRoot(),
+                SerializedCasWriter.PARAM_FILENAME_SUFFIX, ".bin",
+                SerializedCasWriter.PARAM_TYPE_SYSTEM_FILE, 
+                        aIncludeTypeSystem ?  null : testFolder.newFile("typesystem.bin"));
+
+        runPipeline(reader, writer);
 
         assertTrue(new File(testFolder.getRoot(), "example1.txt.bin").exists());
     }
 
-    public void writeSerialized()
-            throws Exception
-        {
-            System.out.println("--- WRITING ---");
-            CollectionReader textReader = CollectionReaderFactory.createReader(TextReader.class,
-                    ResourceCollectionReaderBase.PARAM_SOURCE_LOCATION, "src/test/resources/texts",
-                    ResourceCollectionReaderBase.PARAM_PATTERNS, "[+]*.txt",
-                    ResourceCollectionReaderBase.PARAM_LANGUAGE, "latin");
-
-            AnalysisEngine xmiWriter = AnalysisEngineFactory.createEngine(SerializedCasWriter.class,
-                    SerializedCasWriter.PARAM_FILENAME_SUFFIX, ".bin",
-                    SerializedCasWriter.PARAM_TARGET_LOCATION, testFolder.getRoot().getPath());
-
-//            AnalysisEngine dumper = createEngine(CASDumpWriter.class);
-
-            runPipeline(textReader, /*dumper,*/ xmiWriter);
-
-            assertTrue(new File(testFolder.getRoot(), "example1.txt.bin").exists());
-        }
-
-
-    public void read(TypeSystemDescription aTSD)
+    public void read(int aMode, boolean aLoadExternal)
         throws Exception
     {
-        System.out.println("--- READING ---");
-        CollectionReader reader = CollectionReaderFactory.createReader(BinaryCasReader.class,
-                ResourceCollectionReaderBase.PARAM_SOURCE_LOCATION, testFolder.getRoot().getPath(),
-                ResourceCollectionReaderBase.PARAM_PATTERNS,
-                new String[] { ResourceCollectionReaderBase.INCLUDE_PREFIX + "*.bin" });
-
-        CAS cas = CasCreationUtils.createCas(aTSD, null, null);
-        reader.getNext(cas);
-
-//        createEngine(CASDumpWriter.class).process(cas);
-
-        String refText = readFileToString(new File("src/test/resources/texts/example1.txt"));
+        TypeSystemDescription tsd;
+        switch (aMode) {
+        case NONE:
+            tsd = null;
+            break;
+        case METADATA:
+            tsd = createTypeSystemDescription("desc.type.metadata");
+            break;
+        case ALL:
+            tsd = createTypeSystemDescription();
+            break;
+        default:
+            throw new IllegalArgumentException("Unknown mode");
+        }
         
-        assertEquals(refText, cas.getDocumentText());
+        System.out.println("--- READING ---");
+        CollectionReader reader = CollectionReaderFactory.createReader(
+                BinaryCasReader.class,
+                BinaryCasReader.PARAM_SOURCE_LOCATION, testFolder.getRoot(),
+                BinaryCasReader.PARAM_PATTERNS, "*.bin",
+                // Allow loading only if TSD is not specified
+                BinaryCasReader.PARAM_TYPE_SYSTEM_FILE, 
+                aLoadExternal ? new File(testFolder.getRoot(), "typesystem.bin") : null);
+
+        CAS cas = CasCreationUtils.createCas(tsd, null, null);
+        
+        reader.getNext(cas);
+        String refText1 = readFileToString(new File("src/test/resources/texts/example1.txt"));
+        assertEquals(refText1, cas.getDocumentText());
         assertEquals("latin", cas.getDocumentLanguage());
+        
+        reader.getNext(cas);
+        String refText2 = readFileToString(new File("src/test/resources/texts/example2.txt"));
+        assertEquals(refText2, cas.getDocumentText());
+        assertEquals("latin", cas.getDocumentLanguage());
+        
+        assertFalse(reader.hasNext());
+    }
+
+    @Ignore("Run this only when you want to compare performance")
+    @Test
+    public void performanceTest()
+        throws Exception
+    {
+        // Generate test data
+        System.out.printf("Generating test data... ");
+        JCas jcas = JCasFactory.createJCas();
+        DocumentMetaData.create(jcas).setDocumentId("dummy");
+        initRandomCas(jcas, 10000, 30000, 0);
+        System.out.printf("done%n");
+        
+        System.out.printf("Data serialized to %s %n", testFolder.getRoot());
+        
+        // Set up configurations
+        Map<String, AnalysisEngineDescription> configs = new LinkedHashMap<String, AnalysisEngineDescription>();
+        configs.put(
+                "Format 6+ - no compression",
+                createEngineDescription(
+                    BinaryCasWriter.class, 
+                    BinaryCasWriter.PARAM_FORMAT, "6+", 
+                    BinaryCasWriter.PARAM_COMPRESSION, CompressionMethod.NONE,
+                    BinaryCasWriter.PARAM_TARGET_LOCATION, testFolder.getRoot()));
+        configs.put(
+                "Format 6+ - GZip compression",
+                createEngineDescription(
+                    BinaryCasWriter.class, 
+                    BinaryCasWriter.PARAM_FORMAT, "6+", 
+                    BinaryCasWriter.PARAM_COMPRESSION, CompressionMethod.GZIP,
+                    BinaryCasWriter.PARAM_TARGET_LOCATION, testFolder.getRoot()));
+        configs.put(
+                "Format 6+ - BZIP2 compression",
+                createEngineDescription(
+                    BinaryCasWriter.class, 
+                    BinaryCasWriter.PARAM_FORMAT, "6+", 
+                    BinaryCasWriter.PARAM_COMPRESSION, CompressionMethod.BZIP2,
+                    BinaryCasWriter.PARAM_TARGET_LOCATION, testFolder.getRoot()));
+        configs.put(
+                "Format 6+ - XZ compression",
+                createEngineDescription(
+                    BinaryCasWriter.class, 
+                    BinaryCasWriter.PARAM_FORMAT, "6+", 
+                    BinaryCasWriter.PARAM_COMPRESSION, CompressionMethod.XZ,
+                    BinaryCasWriter.PARAM_TARGET_LOCATION, testFolder.getRoot()));
+
+        // Run tests
+        for (Entry<String, AnalysisEngineDescription> cfg : configs.entrySet()) {
+            System.out.printf("%s%n", cfg.getKey());
+            
+            for (File f : FileUtils.listFiles(testFolder.getRoot(), new PrefixFileFilter("dummy.bin"), null)) {
+                f.delete();
+            }
+            
+            SummaryStatistics stats = measurePerformance(cfg.getValue(), repeat(jcas, 1000));
+            System.out.printf("  Repeat  %10d times%n", stats.getN());
+            System.out.printf("  Total   %10.0f ms %n", stats.getSum());
+            System.out.printf("  Mean    %10.0f ms %n", stats.getMean());
+            System.out.printf("  Min     %10.0f ms %n", stats.getMin());
+            System.out.printf("  Max     %10.0f ms %n", stats.getMax());
+            
+            for (File f : FileUtils.listFiles(testFolder.getRoot(), new PrefixFileFilter("dummy.bin"), null)) {
+                System.out.printf("  Size    %10d bytes%n", f.length());
+            }
+        }
+    }
+    
+    @Before
+    public void setupLogging()
+    {
+        System.setProperty("org.apache.uima.logger.class", "org.apache.uima.util.impl.Log4jLogger_impl");
+    }
+
+    @Rule
+    public TestName name = new TestName();
+
+    @Before
+    public void printSeparator()
+    {
+        System.out.println("\n=== " + name.getMethodName() + " =====================");
     }
 }
--- de.tudarmstadt.ukp.dkpro.core-asl/branches/1.6.x/de.tudarmstadt.ukp.dkpro.core.io.bincas-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/io/bincas/SerializedCasWriterReaderTest.java
+++ de.tudarmstadt.ukp.dkpro.core-asl/branches/1.6.x/de.tudarmstadt.ukp.dkpro.core.io.bincas-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/io/bincas/SerializedCasWriterReaderTest.java
@@ -31,11 +31,12 @@ import org.apache.uima.collection.CollectionReader;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
 import org.apache.uima.util.CasCreationUtils;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import org.junit.rules.TestName;
 
-import de.tudarmstadt.ukp.dkpro.core.api.io.ResourceCollectionReaderBase;
 import de.tudarmstadt.ukp.dkpro.core.io.text.TextReader;
 
 public class SerializedCasWriterReaderTest
@@ -44,45 +45,150 @@ public class SerializedCasWriterReaderTest
 	public TemporaryFolder testFolder = new TemporaryFolder();
 
 	@Test
-	public void test() throws Exception
+	public void testCasWithTypeSystemEmbedded() throws Exception
 	{
-		write();
+		write(true);
 		read();
 	}
 
-	public void write() throws Exception
+    @Test
+    public void testCasWithTypeSystemSeparate() throws Exception
+    {
+        write(false);
+        read();
+    }
+
+	public void write(boolean aIncludeTypeSystem) throws Exception
 	{
-		CollectionReader textReader = CollectionReaderFactory.createReader(
+		CollectionReader reader = CollectionReaderFactory.createReader(
 				TextReader.class,
-				ResourceCollectionReaderBase.PARAM_SOURCE_LOCATION, "src/test/resources/texts",
-				ResourceCollectionReaderBase.PARAM_PATTERNS, new String [] {
-					ResourceCollectionReaderBase.INCLUDE_PREFIX+"*.txt"
-				},
-				ResourceCollectionReaderBase.PARAM_LANGUAGE, "latin");
+				TextReader.PARAM_SOURCE_LOCATION, "src/test/resources/texts",
+				TextReader.PARAM_PATTERNS, "*.txt",
+				TextReader.PARAM_LANGUAGE, "latin");
 
-		AnalysisEngine xmiWriter = AnalysisEngineFactory.createEngine(
+		AnalysisEngine writer = AnalysisEngineFactory.createEngine(
 				SerializedCasWriter.class,
-				SerializedCasWriter.PARAM_TARGET_LOCATION, testFolder.getRoot().getPath());
+				SerializedCasWriter.PARAM_TARGET_LOCATION, testFolder.getRoot(),
+				SerializedCasWriter.PARAM_TYPE_SYSTEM_FILE, 
+				        aIncludeTypeSystem ? null : testFolder.newFile("typesystem.ser"));
 
-		runPipeline(textReader, xmiWriter);
+		runPipeline(reader, writer);
 
 		assertTrue(new File(testFolder.getRoot(), "example1.txt.ser").exists());
 	}
 
 	public void read() throws Exception
 	{
-		CollectionReader xmiReader = CollectionReaderFactory.createReader(
+		CollectionReader reader = CollectionReaderFactory.createReader(
 				SerializedCasReader.class,
-				ResourceCollectionReaderBase.PARAM_SOURCE_LOCATION, testFolder.getRoot().getPath(),
-				ResourceCollectionReaderBase.PARAM_PATTERNS, new String [] {
-					ResourceCollectionReaderBase.INCLUDE_PREFIX+"*.ser"
-				});
+				SerializedCasReader.PARAM_SOURCE_LOCATION, testFolder.getRoot(),
+				SerializedCasReader.PARAM_PATTERNS, "*.ser",
+				SerializedCasReader.PARAM_TYPE_SYSTEM_FILE, 
+				        new File(testFolder.getRoot(), "typesystem.ser"));
 
 		CAS cas = CasCreationUtils.createCas(createTypeSystemDescription(), null, null);
-		xmiReader.getNext(cas);
+		reader.getNext(cas);
 
 		String refText = readFileToString(new File("src/test/resources/texts/example1.txt"));
 		assertEquals(refText, cas.getDocumentText());
 		assertEquals("latin", cas.getDocumentLanguage());
 	}
+	
+//	@Test
+//	public void lenientTest() throws Exception
+//	{
+//        TypeSystemDescription tsdMeta = TypeSystemDescriptionFactory
+//                .createTypeSystemDescription("desc.type.metadata");
+//        
+//        
+//        // Create a CAS initialized with that type system and set the text
+//        CAS casOut = createCas(tsdMeta, null, null);
+//        casOut.setDocumentText("This is a test.");
+//        DocumentMetaData meta = DocumentMetaData.create(casOut);
+//        meta.setDocumentId("document");
+//        
+//        // Write out
+//        AnalysisEngine writer = AnalysisEngineFactory.createEngine(
+//                SerializedCasWriter.class, tsdMeta,
+//                SerializedCasWriter.PARAM_TARGET_LOCATION, testFolder.getRoot().getPath());
+//        writer.process(casOut);
+//        
+//        // Create a new type system from scratch
+//        TypeSystemDescription tsd = new TypeSystemDescription_impl();
+//        TypeDescription tokenTypeDesc = tsd.addType("Token", "", CAS.TYPE_NAME_ANNOTATION);
+//        tokenTypeDesc.addFeature("length", "", CAS.TYPE_NAME_INTEGER);
+//        tsd = CasCreationUtils.mergeTypeSystems(asList(tsd, tsdMeta));
+//        
+//        // Now read in to CAS with different type system
+//        CollectionReader reader = CollectionReaderFactory.createReader(
+//                SerializedCasReader.class,
+//                SerializedCasReader.PARAM_SOURCE_LOCATION, testFolder.getRoot().getPath(),
+//                SerializedCasReader.PARAM_PATTERNS, new String [] {
+//                    SerializedCasReader.INCLUDE_PREFIX+"*.ser"
+//                });
+//
+//        CAS casIn = CasCreationUtils.createCas(tsd, null, null);
+//        reader.getNext(casIn);
+//        
+//        upgrade(casIn, tsd);
+//        
+//        // Try to create an annotation with the extra type
+//        AnnotationFS fs = casOut.createAnnotation(casIn.getTypeSystem().getType("Token"), 0, 1);
+//        casOut.addFsToIndexes(fs);
+//	}
+//	
+//	private void upgrade(CAS aCas, TypeSystemDescription aTsd) throws Exception
+//	{
+//	    // Prepare template for new CAS
+//	    CAS newCas = CasCreationUtils.createCas(aTsd, null, null);
+//        CASCompleteSerializer serializer = Serialization.serializeCASComplete((CASImpl) newCas);
+//	    
+//        // Save old type system
+//	    TypeSystem oldTypeSystem = aCas.getTypeSystem();
+//	    
+//	    // Save old CAS contents
+//	    ByteArrayOutputStream os2 = new ByteArrayOutputStream();
+//	    Serialization.serializeWithCompression(aCas, os2, oldTypeSystem);
+//        
+//        // Prepare CAS with new type system
+//	    Serialization.deserializeCASComplete(serializer, (CASImpl) aCas);
+//        
+//        // Restore CAS data to new type system
+//	    Serialization.deserializeCAS(aCas, new ByteArrayInputStream(os2.toByteArray()), oldTypeSystem, null);
+//	}
+//	
+//    private void upgrade(CAS aCas) throws Exception
+//    {
+//        // Prepare template for new CAS
+//        CAS newCas = JCasFactory.createJCas().getCas();
+//        CASCompleteSerializer serializer = Serialization.serializeCASComplete((CASImpl) newCas);
+//        
+//        // Save old type system
+//        TypeSystem oldTypeSystem = aCas.getTypeSystem();
+//        
+//        // Save old CAS contents
+//        ByteArrayOutputStream os2 = new ByteArrayOutputStream();
+//        Serialization.serializeWithCompression(aCas, os2, oldTypeSystem);
+//        
+//        // Prepare CAS with new type system
+//        Serialization.deserializeCASComplete(serializer, (CASImpl) aCas);
+//        
+//        // Restore CAS data to new type system
+//        Serialization.deserializeCAS(aCas, new ByteArrayInputStream(os2.toByteArray()), oldTypeSystem, null);
+//    }
+
+    @Before
+    public void setupLogging()
+    {
+        System.setProperty("org.apache.uima.logger.class", "org.apache.uima.util.impl.Log4jLogger_impl");
+    }
+
+    @Rule
+    public TestName name = new TestName();
+
+    @Before
+    public void printSeparator()
+    {
+        System.out.println("\n=== " + name.getMethodName() + " =====================");
+    }
 }
