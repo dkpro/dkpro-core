9846e726d19c0d60875e222144043174c3e591e6
richard.eckart@gmail.com <richard.eckart@gmail.com@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2014-05-26 22:24:03 +0000

    Issue 381:	Add support for reading TüPP-D/Z format - Added missing license headers
    Update issue 381

--- de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.io.tuepp-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/tuepp/package-info.java
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.io.tuepp-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/tuepp/package-info.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2013
+ * Copyright 2014
  * Ubiquitous Knowledge Processing (UKP) Lab
  * Technische Universität Darmstadt
  *
