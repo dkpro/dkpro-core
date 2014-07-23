847629ea574906b974a9da9fd2148b1285cfda23
Pedro Santos <pedrobssantos@gmail.com>
2014-07-23 12:20:05 +0000

    Issue 417:	CompoundAnnotator does not auto-load required resources
    Update issue 417

--- de.tudarmstadt.ukp.dkpro.core.decompounding-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/decompounding/uima/resource/SharedLinkingMorphemes.java
+++ de.tudarmstadt.ukp.dkpro.core.decompounding-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/decompounding/uima/resource/SharedLinkingMorphemes.java
@@ -18,6 +18,7 @@
 package de.tudarmstadt.ukp.dkpro.core.decompounding.uima.resource;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.Map;
 
@@ -26,18 +27,42 @@ import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.resource.ResourceSpecifier;
 
+import de.tudarmstadt.ukp.dkpro.core.api.parameter.ComponentParameters;
+import de.tudarmstadt.ukp.dkpro.core.api.resources.CasConfigurableProviderBase;
+import de.tudarmstadt.ukp.dkpro.core.api.resources.ModelProviderBase;
 import de.tudarmstadt.ukp.dkpro.core.api.resources.ResourceUtils;
+import de.tudarmstadt.ukp.dkpro.core.decompounding.dictionary.Dictionary;
 import de.tudarmstadt.ukp.dkpro.core.decompounding.dictionary.LinkingMorphemes;
+import de.tudarmstadt.ukp.dkpro.core.decompounding.dictionary.SimpleDictionary;
 
 public class SharedLinkingMorphemes
     extends Resource_ImplBase
 {
 
-    public static final String PARAM_MORPHEMES_PATH = "morphemesPath";
-    @ConfigurationParameter(name = PARAM_MORPHEMES_PATH, mandatory = false, defaultValue = "classpath:de/tudarmstadt/ukp/dkpro/core/decompounding/lib/spelling/de/igerman98/de_DE.linking")
-    private String morphemesPath;
+    /**
+     * Use this language instead of the default language.
+     */
+    public static final String PARAM_LANGUAGE = ComponentParameters.PARAM_LANGUAGE;
+    @ConfigurationParameter(name = PARAM_LANGUAGE, mandatory = false)
+    protected String language;
 
+    /**
+     * Override the default variant used to locate the model.
+     */
+    public static final String PARAM_VARIANT = ComponentParameters.PARAM_VARIANT;
+    @ConfigurationParameter(name = PARAM_VARIANT, mandatory = false)
+    protected String variant;
+
+    /**
+     * Load the model from this location instead of locating the model automatically.
+     */
+    public static final String PARAM_MODEL_LOCATION = ComponentParameters.PARAM_MODEL_LOCATION;
+    @ConfigurationParameter(name = PARAM_MODEL_LOCATION, mandatory = false)
+    protected String modelLocation;
+
+    private CasConfigurableProviderBase<LinkingMorphemes> modelProvider;
     private LinkingMorphemes morphemes;
+
 
     @Override
     public boolean initialize(ResourceSpecifier aSpecifier,
@@ -48,21 +73,41 @@ public class SharedLinkingMorphemes
         if (!super.initialize(aSpecifier, aAdditionalParams)) {
             return false;
         }
-        try {
-            URL url = ResourceUtils.resolveLocation(morphemesPath, this, null);
 
-            morphemes = new LinkingMorphemes(url.openStream());
-        }
-        catch (IOException e) {
-            throw new ResourceInitializationException(e);
-        }
+        modelProvider = new ModelProviderBase<LinkingMorphemes>() {
+            {
+                setContextObject(SharedLinkingMorphemes.this);
+
+                setDefault(ARTIFACT_ID, "${groupId}.decompounding-model-spelling-${language}-"
+                        + "${variant}");
+                setDefault(LOCATION, "classpath:de/tudarmstadt/ukp/dkpro/core/decompounding/lib/"
+                        + "spelling-${language}-${variant}.properties");
+                setDefault(VARIANT, "linking");
+                setDefault(LANGUAGE, "de");
+
+                setOverride(LOCATION, modelLocation);
+                setOverride(LANGUAGE, language);
+                setOverride(VARIANT, variant);
+            }
+
+            @Override
+            protected LinkingMorphemes produceResource(InputStream aStream)
+                throws Exception
+            {
+                return new LinkingMorphemes(aStream);
+            }
+        };
 
         return true;
 
     }
 
-    public LinkingMorphemes getLinkingMorphemes()
+    public LinkingMorphemes getLinkingMorphemes() throws IOException
     {
+        if(morphemes == null){
+            modelProvider.configure();
+            morphemes = modelProvider.getResource();
+        }
         return morphemes;
     }
 
