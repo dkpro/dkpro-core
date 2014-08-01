6f57139f043b989a186b0bdf4a94aca19e36aa38
Tobias Horsmann <tobias.horsmann@gmail.com>
2014-08-01 16:55:11 +0000

    Issue 439. PennTreeBank Reader for tagged corpora
    Update issue 439
    - added license header to created java files.

--- de.tudarmstadt.ukp.dkpro.core.io.penntree-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/io/penntree/PTChunkReaderTestSpecialCasesInCorpusData.java
+++ de.tudarmstadt.ukp.dkpro.core.io.penntree-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/io/penntree/PTChunkReaderTestSpecialCasesInCorpusData.java
@@ -1,5 +1,23 @@
 package de.tudarmstadt.ukp.dkpro.core.io.penntree;
 
+/*******************************************************************************
+ * Copyright 2014
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
 import static org.junit.Assert.*;
 
 import java.io.IOException;
@@ -68,13 +86,17 @@ public class PTChunkReaderTestSpecialCasesInCorpusData {
 
 	JCas readTestFile(String file) throws ResourceInitializationException,
 			CollectionException, IOException, CASException {
-		CollectionReader reader = CollectionReaderFactory.createReader(
-				PennTreebankChunkedReader.class,
-				PennTreebankChunkedReader.PARAM_LANGUAGE, "en",
-				PennTreebankChunkedReader.PARAM_SOURCE_LOCATION,
-				"src/test/resources/pennTreebankChunkedReaderTestFiles/",
-				PennTreebankChunkedReader.PARAM_POS_TAGSET, "en-ptb-pos.map",
-				PennTreebankChunkedReader.PARAM_PATTERNS, new String[] { file });
+		CollectionReader reader = CollectionReaderFactory
+				.createReader(
+						PennTreebankChunkedReader.class,
+						PennTreebankChunkedReader.PARAM_LANGUAGE,
+						"en",
+						PennTreebankChunkedReader.PARAM_SOURCE_LOCATION,
+						"src/test/resources/pennTreebankChunkedReaderTestFiles/",
+						PennTreebankChunkedReader.PARAM_POS_TAGSET,
+						"en-ptb-pos.map",
+						PennTreebankChunkedReader.PARAM_PATTERNS,
+						new String[] { file });
 
 		TypeSystemDescription typeSystem = TypeSystemDescriptionFactory
 				.createTypeSystemDescription();
