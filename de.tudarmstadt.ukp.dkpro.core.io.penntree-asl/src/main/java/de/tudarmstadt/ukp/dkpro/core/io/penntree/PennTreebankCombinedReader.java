e2ab7c2b47c773a269e244c79bc8ce9b23b46670
Richard Eckart de Castilho <richard.eckart@gmail.com>
2014-09-24 20:34:51 +0000

    Issue 157. Conversion between penntree <-> Constituent annotations
    Update issue 157
    - Allow converting from bracketed representation to UIMA annotations

--- de.tudarmstadt.ukp.dkpro.core.io.penntree-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/penntree/PennTreebankCombinedReader.java
+++ de.tudarmstadt.ukp.dkpro.core.io.penntree-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/penntree/PennTreebankCombinedReader.java
@@ -17,38 +17,25 @@
  ******************************************************************************/
 package de.tudarmstadt.ukp.dkpro.core.io.penntree;
 
-import static de.tudarmstadt.ukp.dkpro.core.io.penntree.PennTreeUtils.trim;
-import static de.tudarmstadt.ukp.dkpro.core.io.penntree.PennTreeUtils.unescapeToken;
-import static org.apache.commons.lang.StringUtils.isBlank;
-import static org.apache.uima.fit.util.FSCollectionFactory.createFSArray;
-
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.LineIterator;
 import org.apache.commons.lang.StringUtils;
 import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
-import org.apache.uima.cas.Type;
 import org.apache.uima.collection.CollectionException;
 import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.fit.descriptor.TypeCapability;
 import org.apache.uima.jcas.JCas;
-import org.apache.uima.jcas.tcas.Annotation;
 import org.apache.uima.resource.ResourceInitializationException;
 
 import de.tudarmstadt.ukp.dkpro.core.api.io.JCasResourceCollectionReader_ImplBase;
-import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS;
 import de.tudarmstadt.ukp.dkpro.core.api.parameter.ComponentParameters;
 import de.tudarmstadt.ukp.dkpro.core.api.resources.MappingProvider;
 import de.tudarmstadt.ukp.dkpro.core.api.resources.MappingProviderFactory;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
-import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import de.tudarmstadt.ukp.dkpro.core.api.syntax.type.constituent.Constituent;
-import de.tudarmstadt.ukp.dkpro.core.api.syntax.type.constituent.ROOT;
 
 @TypeCapability(outputs = { 
         "de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData",
@@ -128,11 +115,12 @@ public class PennTreebankCombinedReader
     @ConfigurationParameter(name = PARAM_WRITE_TRACES_TO_TEXT, mandatory = false, defaultValue = "false")
     private boolean writeTracesToText;
 
-    private static final String ROOT = "ROOT";
     private static final String NONE = "-NONE-";
     
     private MappingProvider posMappingProvider;
     private MappingProvider constituentMappingProvider;
+    
+    private PennTreeToJCasConverter converter;
 
     @Override
     public void initialize(UimaContext aContext)
@@ -145,6 +133,11 @@ public class PennTreebankCombinedReader
         
         constituentMappingProvider = MappingProviderFactory.createConstituentMappingProvider(
                 constituentMappingLocation, constituentTagset, getLanguage());
+        
+        converter = new PennTreeToJCasConverter(posMappingProvider, constituentMappingProvider);
+        converter.setInternTags(internTags);
+        converter.setWriteTracesToText(writeTracesToText);
+        converter.setCreatePosTags(createPosTags);
     }
     
     @Override
@@ -172,7 +165,7 @@ public class PennTreebankCombinedReader
                 if (removeTraces) {
                     doRemoveTraces(tree);
                 }
-                Constituent root = convertPennTree(aJCas, text, tree, null, true);
+                Constituent root = converter.convertPennTree(aJCas, text, tree);
                 Sentence sentence = new Sentence(aJCas, root.getBegin(), root.getEnd());
                 sentence.addToIndexes();
                 text.append('\n');
@@ -235,119 +228,5 @@ public class PennTreebankCombinedReader
         }
         
         return PennTreeUtils.parsePennTree(tree.toString());
-    }
-    
-    private Constituent convertPennTree(JCas aJCas, StringBuilder aText, PennTreeNode aNode,
-            Constituent aParent, boolean aBOS)
-    {
-        boolean bos = aBOS;
-        Constituent constituent = null;
-        Constituent parent = aParent;
-        boolean generatedParent = false;
-        
-        // Do we need to insert an artificial ROOT node?
-        if (aParent == null) {
-            // Case 2: no root node:        (S
-            if (!ROOT.equals(aNode.getLabel()) && !isBlank(aNode.getLabel())) {
-                constituent = createConstituent(aJCas, aNode.getLabel());
-                
-                parent = new ROOT(aJCas);
-                parent.setConstituentType(ROOT);
-                parent.setChildren(createFSArray(aJCas, new Constituent[] { constituent }));
-                generatedParent = true;
-                
-                constituent.setParent(parent);
-            }
-            // Case 1: unlabeled root node: ( (S...
-            // Case 3: labeled root node:   (ROOT (S...
-            else {
-                constituent = new ROOT(aJCas);
-                constituent.setConstituentType(ROOT);
-            }
-        }
-        else {
-            constituent = createConstituent(aJCas, aNode.getLabel());
-        }
-        
-        constituent.setBegin(aText.length());
-        
-        List<Annotation> children = new ArrayList<Annotation>();
-        for (PennTreeNode c : aNode.getChildren()) {
-            if (c.isPreTerminal()) {
-                // Do not read traces into the CAS, at least not as tokens
-                if (!writeTracesToText && NONE.equals(c.getLabel())) {
-                    continue;
-                }
-                
-                // Add space between tokens with inside sentence. Do not add token at the beginning
-                // of the sentence, even if we append into a larger document.
-                if (!bos) {
-                    aText.append(' ');
-                }
-                
-                // Add to the document test
-                int begin = aText.length();
-                aText.append(unescapeToken(c.getChildren().get(0).getLabel()));
-                int end = aText.length();
-
-                // only add POS to index if we want POS-tagging
-                POS posAnno = null;
-                if (createPosTags) {
-                    Type posTag = posMappingProvider.getTagType(c.getLabel());
-                    posAnno = (POS) aJCas.getCas().createAnnotation(posTag, begin, end);
-                    posAnno.setPosValue(internTags ? c.getLabel().intern() : c.getLabel());
-                    posAnno.addToIndexes();
-                }
-                
-                Token token = new Token(aJCas, begin, end);
-                token.setPos(posAnno);
-                token.addToIndexes();
-                
-                children.add(token);
-            }
-            else {
-                children.add(convertPennTree(aJCas, aText, c, constituent, bos));
-            }
-            bos = false;
-        }
-
-        constituent.setEnd(aText.length());
-        
-        int[] offsets = {constituent.getBegin(), constituent.getEnd()};
-        trim(aText, offsets);
-        constituent.setBegin(offsets[0]);
-        constituent.setEnd(offsets[1]);
-        constituent.setChildren(createFSArray(aJCas, children));
-        constituent.addToIndexes();
-        
-        // We we created an additional ROOT node, then we need to set its offsets as well
-        if (generatedParent) {
-            parent.setBegin(constituent.getBegin());
-            parent.setEnd(constituent.getEnd());
-            parent.addToIndexes();
-        }
-        
-        return constituent;        
-    }
-    
-    private Constituent createConstituent(JCas aJCas, String aLabel)
-    {
-        if (NONE.equals(aLabel)) {
-            return new Constituent(aJCas);
-        }
-        
-        String[] label = aLabel.split("-");
-        Type constituentTag = constituentMappingProvider.getTagType(label[0]);
-        // We just set a dummy value for the offsets here. These need to be fixed when we know the
-        // children and before addToIndexes() is called.
-        Constituent constituentAnno = (Constituent) aJCas.getCas().createAnnotation(constituentTag,
-                0, 0);
-        constituentAnno.setConstituentType(label[0]);
-        
-        if (label.length >= 2) {
-            constituentAnno.setSyntacticFunction(label[1]);
-        }
-        
-        return constituentAnno;
     }
 }
