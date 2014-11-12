f91a05769a499f8cc10247004cef19ec0931504f
Carsten Schnober <carschno@users.noreply.github.com>
2014-11-12 09:26:37 +0000

    Issue 507: 	Integrate Mallet LDA - Test cases for Inferencer extended - Unnecessary/unhandled catch clauses in test cases removed

--- de.tudarmstadt.ukp.dkpro.core.mallet/src/test/java/de/tudarmstadt/ukp/dkpro/core/mallet/topicmodel/MalletTopicModelEstimatorTest.java
+++ de.tudarmstadt.ukp.dkpro.core.mallet/src/test/java/de/tudarmstadt/ukp/dkpro/core/mallet/topicmodel/MalletTopicModelEstimatorTest.java
@@ -20,15 +20,14 @@ package de.tudarmstadt.ukp.dkpro.core.mallet.topicmodel;
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
 import static org.apache.uima.fit.factory.CollectionReaderFactory.createReaderDescription;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
-import java.io.IOException;
 
-import org.apache.uima.UIMAException;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.collection.CollectionReaderDescription;
 import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.junit.Before;
 import org.junit.Test;
 
 import cc.mallet.topics.ParallelTopicModel;
@@ -38,45 +37,42 @@ import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
 public class MalletTopicModelEstimatorTest
 {
     private static final int N_THREADS = 4;
-    private static final String MODEL_FILE = "target/mallet/model";
+    private static final File MODEL_FILE = new File("target/mallet/model");
     private static final String CAS_DIR = "src/test/resources/txt";
     private static final String CAS_FILE_PATTERN = "[+]*.txt";
 
+    @Before
+    public void setUp()
+    {
+        MODEL_FILE.deleteOnExit();
+    }
+
     @Test
-    public void testParallel()
+    public void testEstimator()
+        throws Exception
     {
         int nTopics = 10;
         int nIterations = 50;
         boolean useLemmas = false;
         String language = "en";
 
-        try {
-            CollectionReaderDescription reader = createReaderDescription(TextReader.class,
-                    TextReader.PARAM_SOURCE_LOCATION, CAS_DIR,
-                    TextReader.PARAM_PATTERNS, CAS_FILE_PATTERN,
-                    TextReader.PARAM_LANGUAGE, language);
-            AnalysisEngineDescription segmenter = createEngineDescription(BreakIteratorSegmenter.class);
+        CollectionReaderDescription reader = createReaderDescription(TextReader.class,
+                TextReader.PARAM_SOURCE_LOCATION, CAS_DIR,
+                TextReader.PARAM_PATTERNS, CAS_FILE_PATTERN,
+                TextReader.PARAM_LANGUAGE, language);
+        AnalysisEngineDescription segmenter = createEngineDescription(BreakIteratorSegmenter.class);
 
-            AnalysisEngineDescription estimator = createEngineDescription(
-                    MalletTopicModelEstimator.class,
-                    MalletTopicModelEstimator.PARAM_N_THREADS, N_THREADS,
-                    MalletTopicModelEstimator.PARAM_TARGET_LOCATION, MODEL_FILE,
-                    MalletTopicModelEstimator.PARAM_N_ITERATIONS, nIterations,
-                    MalletTopicModelEstimator.PARAM_N_TOPICS, nTopics,
-                    MalletTopicModelEstimator.PARAM_USE_LEMMA, useLemmas);
-            SimplePipeline.runPipeline(reader, segmenter, estimator);
-        }
-        catch (UIMAException | IOException e) {
-            e.printStackTrace();
-            fail(e.getLocalizedMessage());
-        }
-        try {
-            ParallelTopicModel model = ParallelTopicModel.read(new File(MODEL_FILE));
-            assertEquals(nTopics, model.getNumTopics());
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-            fail(e.getLocalizedMessage());
-        }
+        AnalysisEngineDescription estimator = createEngineDescription(
+                MalletTopicModelEstimator.class,
+                MalletTopicModelEstimator.PARAM_N_THREADS, N_THREADS,
+                MalletTopicModelEstimator.PARAM_TARGET_LOCATION, MODEL_FILE,
+                MalletTopicModelEstimator.PARAM_N_ITERATIONS, nIterations,
+                MalletTopicModelEstimator.PARAM_N_TOPICS, nTopics,
+                MalletTopicModelEstimator.PARAM_USE_LEMMA, useLemmas);
+        SimplePipeline.runPipeline(reader, segmenter, estimator);
+
+        assertTrue(MODEL_FILE.exists());
+        ParallelTopicModel model = ParallelTopicModel.read(MODEL_FILE);
+        assertEquals(nTopics, model.getNumTopics());
     }
 }
