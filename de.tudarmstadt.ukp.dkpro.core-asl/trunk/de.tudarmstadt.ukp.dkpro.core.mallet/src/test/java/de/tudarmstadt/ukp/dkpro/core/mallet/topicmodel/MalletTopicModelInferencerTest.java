facaf71c8ec2b9a50d3ceb808b5f773d9f51c51b
schnober@ukp.informatik.tu-darmstadt.de <schnober@ukp.informatik.tu-darmstadt.de@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2014-11-12 09:26:37 +0000

    Issue 507: 	Integrate Mallet LDA - Test cases for Inferencer extended - Unnecessary/unhandled catch clauses in test cases removed

--- de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.mallet/src/test/java/de/tudarmstadt/ukp/dkpro/core/mallet/topicmodel/MalletTopicModelInferencerTest.java
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.mallet/src/test/java/de/tudarmstadt/ukp/dkpro/core/mallet/topicmodel/MalletTopicModelInferencerTest.java
@@ -22,15 +22,20 @@ import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDesc
 import static org.apache.uima.fit.factory.CollectionReaderFactory.createReaderDescription;
 import static org.apache.uima.fit.util.JCasUtil.select;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertTrue;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import org.apache.uima.UIMAException;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.collection.CollectionReaderDescription;
 import org.apache.uima.fit.pipeline.SimplePipeline;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -46,7 +51,7 @@ import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
 public class MalletTopicModelInferencerTest
 {
     private static final int N_THREADS = 4;
-    private static final String MODEL_FILE = "target/mallet/model";
+    private static final File MODEL_FILE = new File("target/mallet/model");
     private static final String CAS_DIR = "src/test/resources/txt";
     private static final String CAS_FILE_PATTERN = "[+]*.txt";
 
@@ -57,63 +62,69 @@ public class MalletTopicModelInferencerTest
 
     @Before
     public void setUp()
+        throws UIMAException, IOException
     {
         /* Generate model */
-        try {
-            CollectionReaderDescription reader = createReaderDescription(TextReader.class,
-                    TextReader.PARAM_SOURCE_LOCATION, CAS_DIR,
-                    TextReader.PARAM_PATTERNS, CAS_FILE_PATTERN,
-                    TextReader.PARAM_LANGUAGE, LANGUAGE);
-            AnalysisEngineDescription segmenter = createEngineDescription(BreakIteratorSegmenter.class);
+        CollectionReaderDescription reader = createReaderDescription(TextReader.class,
+                TextReader.PARAM_SOURCE_LOCATION, CAS_DIR,
+                TextReader.PARAM_PATTERNS, CAS_FILE_PATTERN,
+                TextReader.PARAM_LANGUAGE, LANGUAGE);
+        AnalysisEngineDescription segmenter = createEngineDescription(BreakIteratorSegmenter.class);
 
-            AnalysisEngineDescription estimator = createEngineDescription(
-                    MalletTopicModelEstimator.class,
-                    MalletTopicModelEstimator.PARAM_N_THREADS, N_THREADS,
-                    MalletTopicModelEstimator.PARAM_TARGET_LOCATION, MODEL_FILE,
-                    MalletTopicModelEstimator.PARAM_N_ITERATIONS, N_ITERATIONS,
-                    MalletTopicModelEstimator.PARAM_N_TOPICS, N_TOPICS,
-                    MalletTopicModelEstimator.PARAM_USE_LEMMA, USE_LEMMAS);
-            SimplePipeline.runPipeline(reader, segmenter, estimator);
-        }
-        catch (UIMAException | IOException e) {
-            e.printStackTrace();
-            fail(e.getLocalizedMessage());
-        }
+        AnalysisEngineDescription estimator = createEngineDescription(
+                MalletTopicModelEstimator.class,
+                MalletTopicModelEstimator.PARAM_N_THREADS, N_THREADS,
+                MalletTopicModelEstimator.PARAM_TARGET_LOCATION, MODEL_FILE,
+                MalletTopicModelEstimator.PARAM_N_ITERATIONS, N_ITERATIONS,
+                MalletTopicModelEstimator.PARAM_N_TOPICS, N_TOPICS,
+                MalletTopicModelEstimator.PARAM_USE_LEMMA, USE_LEMMAS);
+        SimplePipeline.runPipeline(reader, segmenter, estimator);
 
+        MODEL_FILE.deleteOnExit();
     }
 
     @Test
     public void testInferencer()
+        throws ResourceInitializationException
     {
         int expectedAssignments = 0;
+        double delta = 0.5; // topic proportions are almost random here, so don't expect a match
 
-        try {
-            CollectionReaderDescription reader = createReaderDescription(TextReader.class,
-                    TextReader.PARAM_SOURCE_LOCATION, CAS_DIR,
-                    TextReader.PARAM_PATTERNS, CAS_FILE_PATTERN,
-                    TextReader.PARAM_LANGUAGE, LANGUAGE);
-            AnalysisEngineDescription segmenter = createEngineDescription(BreakIteratorSegmenter.class);
+        List<Double[]> allExpectedTopicProportions = new ArrayList<>(2);
+        allExpectedTopicProportions.add(new Double[] { 0.0518783542039356, 0.13059033989266547,
+                0.11449016100178891, 0.06618962432915922, 0.0518783542039356, 0.1037567084078712,
+                0.11449016100178891, 0.148479427549195, 0.08944543828264759, 0.12880143112701253 });
+        allExpectedTopicProportions
+                .add(new Double[] { 0.13009708737864079, 0.09320388349514563, 0.05631067961165048,
+                        0.08932038834951456, 0.11650485436893204, 0.1669902912621359,
+                        0.06601941747572816, 0.08155339805825243, 0.1650485436893204,
+                        0.03495145631067961 });
 
-            AnalysisEngineDescription inferencer = createEngineDescription(
-                    MalletTopicModelInferencer.class,
-                    MalletTopicModelInferencer.PARAM_MODEL_LOCATION, MODEL_FILE,
-                    MalletTopicModelInferencer.PARAM_USE_LEMMA, USE_LEMMAS);
-            // TODO: extend test for PARAM_MIN_TOPIC_PROB; adapt expectedAssignments
+        CollectionReaderDescription reader = createReaderDescription(TextReader.class,
+                TextReader.PARAM_SOURCE_LOCATION, CAS_DIR,
+                TextReader.PARAM_PATTERNS, CAS_FILE_PATTERN,
+                TextReader.PARAM_LANGUAGE, LANGUAGE);
+        AnalysisEngineDescription segmenter = createEngineDescription(BreakIteratorSegmenter.class);
 
-            for (JCas jcas : SimplePipeline.iteratePipeline(reader, segmenter, inferencer)) {
-                for (TopicDistribution td : select(jcas, TopicDistribution.class)) {
-//                    System.err.println("Topic Proportions: ");
-//                    System.err.println(td.getTopicProportions());
-//                    System.err.println("Topic Assignment: ");
-//                    System.err.println(td.getTopicAssignment());
+        AnalysisEngineDescription inferencer = createEngineDescription(
+                MalletTopicModelInferencer.class,
+                MalletTopicModelInferencer.PARAM_MODEL_LOCATION, MODEL_FILE,
+                MalletTopicModelInferencer.PARAM_USE_LEMMA, USE_LEMMAS);
 
-                    assertEquals(expectedAssignments, td.getTopicAssignment().size());
+        Iterator<Double[]> expectedTopicProportionsIterator =
+                allExpectedTopicProportions.iterator();
+
+        for (JCas jcas : SimplePipeline.iteratePipeline(reader, segmenter, inferencer)) {
+            for (TopicDistribution td : select(jcas, TopicDistribution.class)) {
+                Double[] expectedTopicProportions = expectedTopicProportionsIterator.next();
+                for (int i = 0; i < td.getTopicProportions().toArray().length; i++) {
+                    double proportion = td.getTopicProportions().toArray()[i];
+                    assertTrue(proportion > 0.0);
+                    assertTrue(proportion < 1.0);
+                    assertEquals(expectedTopicProportions[i], proportion, delta);
                 }
+                assertEquals(expectedAssignments, td.getTopicAssignment().size());
             }
-        }
-        catch (UIMAException e) {
-            e.printStackTrace();
-            fail(e.getLocalizedMessage());
         }
     }
 }
