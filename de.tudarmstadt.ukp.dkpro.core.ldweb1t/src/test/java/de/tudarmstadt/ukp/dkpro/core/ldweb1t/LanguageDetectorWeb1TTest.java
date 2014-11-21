f8f07c1795307e0536d7e0cd696e7f0a30117fbe
Richard Eckart de Castilho <richard.eckart@gmail.com>
2014-11-21 21:45:11 +0000

    Issue 501. Add "language identification" to core.langdect
    Update issue 501
    - Fixed test setup - but assert still fails

--- de.tudarmstadt.ukp.dkpro.core.ldweb1t/src/test/java/de/tudarmstadt/ukp/dkpro/core/ldweb1t/LanguageDetectorWeb1TTest.java
+++ de.tudarmstadt.ukp.dkpro.core.ldweb1t/src/test/java/de/tudarmstadt/ukp/dkpro/core/ldweb1t/LanguageDetectorWeb1TTest.java
@@ -1,54 +1,74 @@
+/*******************************************************************************
+ * Copyright 2014
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package de.tudarmstadt.ukp.dkpro.core.ldweb1t;
 
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+import static org.apache.uima.fit.factory.ExternalResourceFactory.createExternalResourceDescription;
+import static org.apache.uima.fit.pipeline.SimplePipeline.runPipeline;
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.uima.analysis_engine.AnalysisEngine;
-import org.apache.uima.fit.factory.AnalysisEngineFactory;
-import org.apache.uima.fit.factory.ExternalResourceFactory;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.fit.factory.JCasFactory;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ExternalResourceDescription;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import de.tudarmstadt.ukp.dkpro.core.frequency.resources.Web1TInMemoryFrequencyCountResource;
-import de.tudarmstadt.ukp.dkpro.core.ldweb1t.LanguageDetectorWeb1T;
 import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
 
-public class LanguageDetectorWeb1TTest {
+public class LanguageDetectorWeb1TTest
+{
+    @Ignore
+    @Test
+    public void web1tLanguageDetectorTest()
+        throws Exception
+    {
+        ExternalResourceDescription en = createExternalResourceDescription(
+                Web1TInMemoryFrequencyCountResource.class,
+                Web1TInMemoryFrequencyCountResource.PARAM_MODEL_LOCATION,
+                "src/test/resources/web1t/en/",
+                Web1TInMemoryFrequencyCountResource.PARAM_MAX_NGRAM_LEVEL, "2");
 
-	@Ignore
-	@Test
-	public void web1tLanguageDetectorTeset() throws Exception {
-		
-		ExternalResourceDescription en = ExternalResourceFactory.createExternalResourceDescription(
-				Web1TInMemoryFrequencyCountResource.class,
-                Web1TInMemoryFrequencyCountResource.PARAM_MODEL_LOCATION, "src/test/resources/web1t/en/",
+        ExternalResourceDescription de = createExternalResourceDescription(
+                Web1TInMemoryFrequencyCountResource.class,
+                Web1TInMemoryFrequencyCountResource.PARAM_MODEL_LOCATION,
+                "src/test/resources/web1t/de/",
                 Web1TInMemoryFrequencyCountResource.PARAM_MAX_NGRAM_LEVEL, "2");
-		
-		ExternalResourceDescription de = ExternalResourceFactory.createExternalResourceDescription(
-				Web1TInMemoryFrequencyCountResource.class,
-                Web1TInMemoryFrequencyCountResource.PARAM_MODEL_LOCATION, "src/test/resources/web1t/de/",
-                Web1TInMemoryFrequencyCountResource.PARAM_MAX_NGRAM_LEVEL, "2");
-		
-		List<ExternalResourceDescription> resources = new ArrayList<ExternalResourceDescription>();
-		resources.add(en);
-		resources.add(de);
-		
-		AnalysisEngine engine = AnalysisEngineFactory.createEngine(
-				AnalysisEngineFactory.createEngineDescription(BreakIteratorSegmenter.class),
-				AnalysisEngineFactory.createEngineDescription(
-					LanguageDetectorWeb1T.class,
-					LanguageDetectorWeb1T.PARAM_MAX_NGRAM_SIZE, 2,
-					LanguageDetectorWeb1T.PARAM_FREQUENCY_PROVIDER_RESOURCES, resources.toArray()
-		));
-		
-		JCas jcas = engine.newJCas();
-		jcas.setDocumentText("This is an English example.");
-		engine.process(jcas);
-		
-		assertEquals("en", jcas.getDocumentLanguage());
-	}
+
+        List<ExternalResourceDescription> resources = new ArrayList<ExternalResourceDescription>();
+        resources.add(en);
+        resources.add(de);
+
+        AnalysisEngineDescription engine = createEngineDescription(
+                createEngineDescription(BreakIteratorSegmenter.class), 
+                createEngineDescription(LanguageDetectorWeb1T.class,
+                        LanguageDetectorWeb1T.PARAM_MAX_NGRAM_SIZE, 2,
+                        LanguageDetectorWeb1T.PARAM_FREQUENCY_PROVIDER_RESOURCES, resources));
+
+        JCas jcas = JCasFactory.createJCas();
+        jcas.setDocumentText("This is an English example.");
+        
+        runPipeline(jcas, engine);
+
+        assertEquals("en", jcas.getDocumentLanguage());
+    }
 }
