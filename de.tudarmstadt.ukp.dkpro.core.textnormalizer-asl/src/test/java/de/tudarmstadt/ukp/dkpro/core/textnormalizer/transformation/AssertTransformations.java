4c384e8528a793923716533cef5c6935c3b03a22
Carsten Schnober <carschno@users.noreply.github.com>
2014-11-25 14:30:17 +0000

    Issue 537. Introduce new normalization approach changing annotations and/or document text instead of operating on views.
    New normalization approach changing annotations and/or document text instead of operating on views.
    
    New issue
    Summary: Introduce new normalization approach changing annotations and/or document text instead of operating on views.
    Owner: schnober@ukp.informatik.tu-darmstadt.de
    Labels: Type-Enhancement, Milestone-1.8.0, DKPro-ASL, Usability, Module-textnormalizer, OpSys-All, Priority-Medium
    New normalization components added.

---
+++ de.tudarmstadt.ukp.dkpro.core.textnormalizer-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/textnormalizer/transformation/AssertTransformations.java
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * Copyright 2014
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package de.tudarmstadt.ukp.dkpro.core.textnormalizer.transformation;
+
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+import static org.apache.uima.fit.factory.CollectionReaderFactory.createReaderDescription;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+
+import de.tudarmstadt.ukp.dkpro.core.io.text.StringReader;
+
+public class AssertTransformations
+{
+    public static void assertTransformedText(String normalizedText, String inputText,
+            String language, AnalysisEngineDescription... aEngines)
+            throws ResourceInitializationException
+    {
+        CollectionReaderDescription reader = createReaderDescription(StringReader.class,
+                StringReader.PARAM_DOCUMENT_TEXT, inputText, StringReader.PARAM_LANGUAGE, language);
+
+
+        List<AnalysisEngineDescription> engines = new ArrayList<AnalysisEngineDescription>();
+        for (AnalysisEngineDescription e : aEngines) {
+            engines.add(e);
+        }
+
+        engines.add(createEngineDescription(JCasHolder.class));
+
+
+        for (JCas jcas : SimplePipeline.iteratePipeline(reader,
+                engines.toArray(new AnalysisEngineDescription[engines.size()]))) {
+            // iteratePipeline does not support CAS multipliers. jcas is not updated after the
+            // multiplier. In order to access the new CAS, we use the JCasHolder (not thread-safe!)
+            assertEquals(normalizedText, JCasHolder.get().getDocumentText());
+        }
+    }
+}
---
+++ de.tudarmstadt.ukp.dkpro.core.textnormalizer-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/textnormalizer/transformation/DictionaryBasedTokenTransformerTest.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright 2014
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package de.tudarmstadt.ukp.dkpro.core.textnormalizer.transformation;
+
+import static de.tudarmstadt.ukp.dkpro.core.textnormalizer.transformation.AssertTransformations.assertTransformedText;
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+
+import org.junit.Test;
+
+import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
+
+public class DictionaryBasedTokenTransformerTest
+{
+    private static final String MAPPINGS_FILE = "src/test/resources/mappings.txt";
+
+    @Test
+    public void test()
+        throws Exception
+    {
+        String expected = "Ich lebe in Braunschweig.";
+        String input = "Ich lebe in Brannfchweig.";
+
+        assertTransformedText(expected, input, "de",
+                createEngineDescription(BreakIteratorSegmenter.class),
+                createEngineDescription(DictionaryBasedTokenTransformer.class,
+                        DictionaryBasedTokenTransformer.PARAM_MODEL_LOCATION, MAPPINGS_FILE));
+    }
+}
