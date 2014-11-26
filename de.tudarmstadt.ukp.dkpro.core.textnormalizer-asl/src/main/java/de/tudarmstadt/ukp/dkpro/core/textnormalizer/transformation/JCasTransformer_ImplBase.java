b8125efe4fda19cd4c933adab71fd198e1a6fdc5
Richard Eckart de Castilho <richard.eckart@gmail.com>
2014-11-26 20:21:46 +0000

    Fixes issue 539. The copying of annotations in JCasTransformer_ImplBase.beforeProcess() does not work

--- de.tudarmstadt.ukp.dkpro.core.textnormalizer-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/textnormalizer/transformation/JCasTransformer_ImplBase.java
+++ de.tudarmstadt.ukp.dkpro.core.textnormalizer-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/textnormalizer/transformation/JCasTransformer_ImplBase.java
@@ -18,11 +18,16 @@
 
 package de.tudarmstadt.ukp.dkpro.core.textnormalizer.transformation;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.cas.AbstractCas;
 import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.FSIterator;
+import org.apache.uima.cas.Feature;
 import org.apache.uima.cas.FeatureStructure;
+import org.apache.uima.cas.text.AnnotationFS;
 import org.apache.uima.fit.component.JCasMultiplier_ImplBase;
 import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.jcas.JCas;
@@ -62,27 +67,47 @@ public abstract class JCasTransformer_ImplBase
     public void beforeProcess(JCas aInput, JCas aOutput)
         throws AnalysisEngineProcessException
     {
-        CAS inputCas = aInput.getCas();
-        CasCopier copier = new CasCopier(inputCas, aOutput.getCas());
-
-        for (String typeName : typesToCopy) {
-            FSIterator<FeatureStructure> fsIterator = inputCas.getIndexRepository()
-                    .getAllIndexedFS(inputCas.getTypeSystem().getType(typeName));
-            while (fsIterator.hasNext()) {
-                copier.copyFs(fsIterator.next());
-            }
-        }
-
         try {
-        DocumentMetaData.copy(aInput, aOutput);
-        } catch (IllegalArgumentException e) {
+            DocumentMetaData.copy(aInput, aOutput);
+        }
+        catch (IllegalArgumentException e) {
             getLogger().warn("Document does not have a DocumentMetaData annotation.");
         }
     }
 
     public void afterProcess(JCas aInput, JCas aOutput)
     {
-        // Nothing by default
+        // Copy the annotation types mentioned in PARAM_TYPES_TO_COPY
+        // We have do do this in the afterProcess() phase, because otherwise the SofA in the
+        // target CAS does not exist yet. 
+        CAS inputCas = aInput.getCas();
+        CasCopier copier = new CasCopier(inputCas, aOutput.getCas());
+
+        Feature mDestSofaFeature = aOutput.getTypeSystem()
+                .getFeatureByFullName(CAS.FEATURE_FULL_NAME_SOFA);
+
+        // Avoid indexing twice, even if we see an feature structure multiple time because it
+        // appears in multiple indexes e.g. via inheritance
+        Set<FeatureStructure> indexedFs = new HashSet<FeatureStructure>();
+        for (String typeName : typesToCopy) {
+            FSIterator<FeatureStructure> i = inputCas.getIndexRepository().getAllIndexedFS(
+                    inputCas.getTypeSystem().getType(typeName));
+            while (i.hasNext()) {
+                FeatureStructure fs = i.next();
+                if (!indexedFs.contains(fs)) {
+                    FeatureStructure fsCopy = copier.copyFs(fs);
+                    // Make sure that the sofa annotation in the copy is set
+                    if (fs instanceof AnnotationFS) {
+                        FeatureStructure sofa = fsCopy.getFeatureValue(mDestSofaFeature);
+                        if (sofa == null) {
+                            fsCopy.setFeatureValue(mDestSofaFeature, aOutput.getSofa());
+                        }
+                    }
+                    aOutput.addFsToIndexes(fsCopy);
+                    indexedFs.add(fs);
+                }
+            }
+        }
     }
 
     @Override
---
+++ de.tudarmstadt.ukp.dkpro.core.textnormalizer-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/textnormalizer/transformation/JCasTransformer_ImplBaseTest.java
@@ -0,0 +1,122 @@
+/*******************************************************************************
+ * Copyright 2014
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package de.tudarmstadt.ukp.dkpro.core.textnormalizer.transformation;
+
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+import static org.apache.uima.fit.factory.CollectionReaderFactory.createReaderDescription;
+import static org.apache.uima.fit.util.JCasUtil.select;
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.apache.uima.jcas.JCas;
+import org.junit.Test;
+
+import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+import de.tudarmstadt.ukp.dkpro.core.io.text.StringReader;
+
+public class JCasTransformer_ImplBaseTest
+{
+    @Test
+    public void testCopyNoAnnotations()
+        throws UIMAException, IOException
+    {
+        String inputText = "test";
+        int exptectedTokens = 0;
+        int expectedDocumentMetadata = 1;
+        int expectedSentences = 0;
+
+        CollectionReaderDescription reader = createReaderDescription(StringReader.class,
+                StringReader.PARAM_DOCUMENT_TEXT, inputText, 
+                StringReader.PARAM_LANGUAGE, "en");
+        AnalysisEngineDescription annotator = createEngineDescription(TestAnnotator.class);
+        AnalysisEngineDescription transformer = createEngineDescription(TestTransformer.class);
+        AnalysisEngineDescription holder = createEngineDescription(JCasHolder.class);
+
+        SimplePipeline.runPipeline(reader, annotator, transformer, holder);
+        JCas jcas = JCasHolder.get();
+        assertEquals(exptectedTokens, select(jcas, Token.class).size());
+        assertEquals(expectedSentences, select(jcas, Sentence.class).size());
+        assertEquals(expectedDocumentMetadata, select(jcas, DocumentMetaData.class).size());
+    }
+
+    @Test
+    public void testTypesToCopy()
+        throws UIMAException, IOException
+    {
+        String inputText = "test";
+        int exptectedTokens = 2;
+        int expectedDocumentMetadata = 1;
+        int expectedSentences = 0;
+
+        CollectionReaderDescription reader = createReaderDescription(StringReader.class,
+                StringReader.PARAM_DOCUMENT_TEXT, inputText, 
+                StringReader.PARAM_LANGUAGE, "en");
+        AnalysisEngineDescription annotator = createEngineDescription(TestAnnotator.class);
+        AnalysisEngineDescription transformer = createEngineDescription(TestTransformer.class,
+                TestTransformer.PARAM_TYPES_TO_COPY, Token.class.getName());
+        AnalysisEngineDescription holder = createEngineDescription(JCasHolder.class);
+
+        SimplePipeline.runPipeline(reader, annotator, transformer, holder);
+        JCas jcas = JCasHolder.get();
+        assertEquals(exptectedTokens, select(jcas, Token.class).size());
+        assertEquals(expectedSentences, select(jcas, Sentence.class).size());
+        assertEquals(expectedDocumentMetadata, select(jcas, DocumentMetaData.class).size());
+    }
+
+    public static class TestAnnotator
+        extends JCasAnnotator_ImplBase
+    {
+        @Override
+        public void process(JCas aJCas)
+            throws AnalysisEngineProcessException
+        {
+            Token token1 = new Token(aJCas);
+            token1.setBegin(0);
+            token1.setEnd(1);
+            token1.addToIndexes(aJCas);
+
+            Token token2 = new Token(aJCas);
+            token2.setBegin(2);
+            token2.setEnd(3);
+            token2.addToIndexes(aJCas);
+        }
+    }
+
+    public static class TestTransformer
+        extends JCasTransformer_ImplBase
+    {
+        @Override
+        public void process(JCas aInput, JCas aOutput)
+            throws AnalysisEngineProcessException
+        {
+            // Just copy the text. DocumentMetaData has already been copied and the TYPES_TO_COPY
+            // will be copied when this method returns.
+            aOutput.setDocumentText(aInput.getDocumentText());
+        }
+    }
+}
