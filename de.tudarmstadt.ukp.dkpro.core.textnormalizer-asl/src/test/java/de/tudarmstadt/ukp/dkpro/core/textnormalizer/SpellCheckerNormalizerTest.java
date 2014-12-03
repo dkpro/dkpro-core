8a79c6fa868b7df9e47072c20f9f86f55a470349
Richard Eckart de Castilho <richard.eckart@gmail.com>
2014-12-03 19:48:01 +0000

    Fixes issue 550. Convert SpellCheckerNormalizer to new normalization framework - Previously had a separate class that contained the converted code, now replaced the original normalizer with the updated code.

--- de.tudarmstadt.ukp.dkpro.core.textnormalizer-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/textnormalizer/SpellCheckerNormalizerTest.java
+++ de.tudarmstadt.ukp.dkpro.core.textnormalizer-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/textnormalizer/SpellCheckerNormalizerTest.java
@@ -17,24 +17,20 @@
  ******************************************************************************/
 package de.tudarmstadt.ukp.dkpro.core.textnormalizer;
 
+import static de.tudarmstadt.ukp.dkpro.core.textnormalizer.transformation.AssertTransformations.assertTransformedText;
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
-import static org.junit.Assert.assertEquals;
 
-import org.apache.uima.analysis_engine.AnalysisEngine;
-import org.apache.uima.cas.CAS;
-import org.apache.uima.fit.factory.AggregateBuilder;
-import org.apache.uima.jcas.JCas;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.junit.Test;
 
-import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
-import de.tudarmstadt.ukp.dkpro.core.castransformation.ApplyChangesAnnotator;
 import de.tudarmstadt.ukp.dkpro.core.jazzy.JazzyChecker;
 import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
 
 public class SpellCheckerNormalizerTest
 {
     @Test
-    public void testSpellCheckerNormalizer() throws Exception
+    public void testSpellCheckerNormalizer()
+        throws Exception
     {
         test("ich tesde meiine neue Rechtschreibbeprüfung.",
                 "ich teste meine neue Rechtschreibprüfung.");
@@ -42,33 +38,17 @@ public class SpellCheckerNormalizerTest
         // Doesn't work
         // test("LauFEN SCHIEßEN BaföG ÜBERlegen","laufen schießen BAföG überlegen");
     }
-    
-    public void test(String input, String output) throws Exception
+
+    public void test(String inputText, String normalizedText)
+        throws Exception
     {
-    	AggregateBuilder builder = new AggregateBuilder();
-    	builder.add(createEngineDescription(BreakIteratorSegmenter.class));
-    	builder.add(createEngineDescription(JazzyChecker.class, 
-    	        JazzyChecker.PARAM_MODEL_LOCATION, "src/test/resources/dictionary/ngerman"));
-    	builder.add(createEngineDescription(SpellCheckerNormalizer.class));
-    	builder.add(createEngineDescription(ApplyChangesAnnotator.class), 
-    	        ApplyChangesAnnotator.VIEW_SOURCE, CAS.NAME_DEFAULT_SOFA, 
-    	        ApplyChangesAnnotator.VIEW_TARGET, "spell_checked");
-    
-    	AnalysisEngine engine = builder.createAggregate();
-    
-    	String text = input;
-    	JCas jcas = engine.newJCas();
-    	jcas.setDocumentText(text);
-    	DocumentMetaData.create(jcas);
-    
-    	engine.process(jcas);
-    
-    	JCas view0 = jcas.getView(CAS.NAME_DEFAULT_SOFA);
-    	JCas view1 = jcas.getView("spell_checked");	
-    	
-    	System.out.println("View0: " + view0.getDocumentText());
-    	System.out.println("View1: " + view1.getDocumentText());
-    	
-    	assertEquals(output, view1.getDocumentText());
+        AnalysisEngineDescription segmenter = createEngineDescription(BreakIteratorSegmenter.class);
+
+        AnalysisEngineDescription spellchecker = createEngineDescription(JazzyChecker.class,
+                JazzyChecker.PARAM_MODEL_LOCATION, "src/test/resources/dictionary/ngerman");
+
+        AnalysisEngineDescription normalizer = createEngineDescription(SpellCheckerNormalizer.class);
+
+        assertTransformedText(normalizedText, inputText, "de", segmenter, spellchecker, normalizer);
     }
 }
