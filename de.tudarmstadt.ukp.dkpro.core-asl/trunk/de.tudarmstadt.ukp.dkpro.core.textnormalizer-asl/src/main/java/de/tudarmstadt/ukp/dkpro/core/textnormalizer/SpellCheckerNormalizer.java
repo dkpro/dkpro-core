3e235f7daae0f0329d88259f1901f6f8489ada1d
richard.eckart <richard.eckart@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2014-12-03 19:48:01 +0000

    Fixes issue 550. Convert SpellCheckerNormalizer to new normalization framework - Previously had a separate class that contained the converted code, now replaced the original normalizer with the updated code.

--- de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.textnormalizer-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/textnormalizer/SpellCheckerNormalizer.java
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.textnormalizer-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/textnormalizer/SpellCheckerNormalizer.java
@@ -17,92 +17,39 @@
  ******************************************************************************/
 package de.tudarmstadt.ukp.dkpro.core.textnormalizer;
 
-import static de.tudarmstadt.ukp.dkpro.core.castransformation.ApplyChangesAnnotator.OP_REPLACE;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import static org.apache.uima.fit.util.JCasUtil.select;
 
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.descriptor.TypeCapability;
-import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 
 import de.tudarmstadt.ukp.dkpro.core.api.anomaly.type.SpellingAnomaly;
-import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
-import de.tudarmstadt.ukp.dkpro.core.api.transform.type.SofaChangeAnnotation;
-import de.tudarmstadt.ukp.dkpro.core.castransformation.alignment.AlignedString;
+import de.tudarmstadt.ukp.dkpro.core.textnormalizer.transformation.JCasTransformerChangeBased_ImplBase;
 
 /**
  * Converts annotations of the type SpellingAnomaly into a SofaChangeAnnoatation.
- * 
- * @author Sebastian Kneise
  */
-@TypeCapability(
-        inputs = { "de.tudarmstadt.ukp.dkpro.core.api.anomaly.type.SpellingAnomaly" }, 
-        outputs = { "de.tudarmstadt.ukp.dkpro.core.api.transform.type.SofaChangeAnnotation" })
-@Deprecated
+@TypeCapability(inputs = { "de.tudarmstadt.ukp.dkpro.core.api.anomaly.type.SpellingAnomaly" })
 public class SpellCheckerNormalizer
-    extends Normalizer_ImplBase
+    extends JCasTransformerChangeBased_ImplBase
 {
     @Override
-    protected Map<Integer, List<SofaChangeAnnotation>> createSofaChangesMap(JCas jcas)
-    {
-        Map<Integer, List<SofaChangeAnnotation>> changesMap = new TreeMap<Integer, List<SofaChangeAnnotation>>();
-        int changesMapIndex = 1;
-
-        for (Token token : JCasUtil.select(jcas, Token.class)) {
-            List<SofaChangeAnnotation> tokenChangeList = new ArrayList<SofaChangeAnnotation>();
-
-            List<SpellingAnomaly> anomalyList = JCasUtil.selectCovered(jcas, SpellingAnomaly.class,
-                    token.getBegin(), token.getEnd());
-
-            if (!anomalyList.isEmpty()) {
-                SpellingAnomaly anomaly = anomalyList.get(0);
-                SofaChangeAnnotation sca = new SofaChangeAnnotation(jcas);
-                sca.setBegin(anomaly.getBegin());
-                sca.setEnd(anomaly.getEnd());
-                sca.setOperation(OP_REPLACE);
-                sca.setValue(getBestSuggestion(anomaly));
-                tokenChangeList.add(sca);
-            }
-
-            changesMap.put(changesMapIndex, tokenChangeList);
-            changesMapIndex++;
-        }
-
-        return changesMap;
-    }
-
-    @Override
-    protected Map<Integer, Boolean> createTokenReplaceMap(JCas jcas, AlignedString as)
+    public void process(JCas aInput, JCas aOutput)
         throws AnalysisEngineProcessException
     {
-        Map<Integer, Boolean> tokenReplaceMap = new TreeMap<Integer, Boolean>();
-
-        int replaceMapIndex = 1;
-
-        for (Token token : JCasUtil.select(jcas, Token.class)) {
-            List<SpellingAnomaly> anomalyList = JCasUtil.selectCovered(jcas, SpellingAnomaly.class,
-                    token.getBegin(), token.getEnd());
-
-            if (!anomalyList.isEmpty()) {
-                tokenReplaceMap.put(replaceMapIndex, true);
-            }
-            else {
-                tokenReplaceMap.put(replaceMapIndex, false);
-            }
-
-            replaceMapIndex++;
+        for (SpellingAnomaly anomaly : select(aInput, SpellingAnomaly.class)) {
+            replace(anomaly.getBegin(), anomaly.getEnd(), getBestSuggestion(anomaly));
         }
-
-        return tokenReplaceMap;
     }
 
-    // Just gets the Suggestion with the highest Certainty.
-    // In case there are more suggestions with certainty 100, it passes the
-    // first highest one.
+    /**
+     * Just gets the Suggestion with the highest Certainty. In case there are more suggestions with
+     * certainty 100, it passes the first highest one.
+     * 
+     * @param anomaly
+     *            the anomaly.
+     * @return the best suggestion.
+     */
     private String getBestSuggestion(SpellingAnomaly anomaly)
     {
         Float bestCertainty = 0.0f;
