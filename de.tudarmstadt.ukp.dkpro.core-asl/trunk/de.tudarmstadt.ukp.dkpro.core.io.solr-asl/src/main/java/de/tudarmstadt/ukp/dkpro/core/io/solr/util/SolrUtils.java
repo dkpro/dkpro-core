d8e38dc472b993024179a4ae44414a1001a23ead
schnober@ukp.informatik.tu-darmstadt.de <schnober@ukp.informatik.tu-darmstadt.de@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2015-01-23 09:48:25 +0000

    Issue 576. Add IO Module for Solr
    Update issue 576
    - SolrUtils#addDocument() now uses SolrInputDocument#setField() instead of SolrInputDocument#addField().
    - SolrUtils.java: javadoc updated
    - SolrutilsTest.java added with simple test case.

--- de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.io.solr-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/solr/util/SolrUtils.java
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.io.solr-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/solr/util/SolrUtils.java
@@ -65,24 +65,23 @@ public class SolrUtils
      * @param update
      *            if true, use Solr atomic update mechanism; otherwise overwrite
      * @param modifier
-     *            The {@link Modifier} to use for updating a document.
+     *            The {@link Modifier} to use for updating a document (only used if {@code update}
+     *            is true).
      * @see #addField(SolrInputDocument, String, Object, boolean)
      */
     public static void addField(SolrInputDocument document, String fieldname, Object value,
             boolean update, Modifier modifier)
     {
         if (update) {
-            document.addField(fieldname, new SingletonMap<>(modifierMap.get(modifier), value));
+            document.setField(fieldname, new SingletonMap<>(modifierMap.get(modifier), value));
         }
         else {
-            document.addField(fieldname, value);
+            document.setField(fieldname, value);
         }
     }
 
     /**
-     * Add a field and optionally update if applicable, calling
-     * {@link #addField(SolrInputDocument, String, Object, boolean, Modifier)} with the
-     * default operation ("set").
+     * Add a field and optionally update, using the default atomic update operation ("set").
      *
      * @param document
      *            the {@link SolrInputDocument} to add/update
@@ -92,6 +91,7 @@ public class SolrUtils
      *            the value to insert for the field.
      * @param update
      *            if true, use Solr atomic update mechanism; otherwise overwrite
+     * @see #addField(SolrInputDocument, String, Object, boolean, Modifier)
      */
     public static void addField(SolrInputDocument document, String fieldname, Object value,
             boolean update)
