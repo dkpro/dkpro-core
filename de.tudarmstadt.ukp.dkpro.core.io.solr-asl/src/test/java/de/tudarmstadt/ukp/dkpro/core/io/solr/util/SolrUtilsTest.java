436679485fe7d2894bbda4416adf96250d9b0948
Carsten Schnober <carschno@users.noreply.github.com>
2015-01-23 09:48:25 +0000

    Issue 576. Add IO Module for Solr
    Update issue 576
    - SolrUtils#addDocument() now uses SolrInputDocument#setField() instead of SolrInputDocument#addField().
    - SolrUtils.java: javadoc updated
    - SolrutilsTest.java added with simple test case.

---
+++ de.tudarmstadt.ukp.dkpro.core.io.solr-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/io/solr/util/SolrUtilsTest.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright 2015
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.tudarmstadt.ukp.dkpro.core.io.solr.util;
+
+import static org.junit.Assert.assertEquals;
+
+import org.apache.solr.common.SolrInputDocument;
+import org.junit.Test;
+
+public class SolrUtilsTest
+{
+    @Test
+    public void testAddField()
+    {
+        String fieldname = "field";
+        String value = "value";
+        String idValue = "id";
+        String idFieldname = idValue;
+        boolean update = false;
+
+        SolrInputDocument document = new SolrInputDocument();
+        document.addField(idFieldname, idFieldname);
+        SolrUtils.addField(document, fieldname, value, update);
+
+        assertEquals(idValue, document.getFieldValue(idValue));
+        assertEquals(value, document.getFieldValue(fieldname));
+    }
+}
