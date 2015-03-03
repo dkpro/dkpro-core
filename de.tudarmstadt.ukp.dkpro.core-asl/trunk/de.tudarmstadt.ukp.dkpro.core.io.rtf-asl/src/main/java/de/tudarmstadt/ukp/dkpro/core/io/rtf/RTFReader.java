6d994679fe6b6f494705ab70a0dead8d9c8e1fac
schnober@ukp.informatik.tu-darmstadt.de <schnober@ukp.informatik.tu-darmstadt.de@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2015-03-03 09:14:24 +0000

    Issue 588. Add RTF Reader Update issue 588 - Initial import.

---
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.io.rtf-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/rtf/RTFReader.java
@@ -0,0 +1,71 @@
+/*******************************************************************************
+ * Copyright 2015
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.tudarmstadt.ukp.dkpro.core.io.rtf;
+
+import java.io.IOException;
+
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.rtf.RTFEditorKit;
+
+import org.apache.uima.UimaContext;
+import org.apache.uima.cas.CAS;
+import org.apache.uima.collection.CollectionException;
+import org.apache.uima.resource.ResourceInitializationException;
+
+import de.tudarmstadt.ukp.dkpro.core.api.io.ResourceCollectionReaderBase;
+
+/**
+ * Read RTF (Rich Test Format) files. Uses {@link RTFEditorKit} for parsing RTF..
+ *
+ * @author Carsten Schnober
+ *
+ */
+public class RTFReader
+    extends ResourceCollectionReaderBase
+{
+    private RTFEditorKit rtfParser;
+
+    @Override
+    public void initialize(UimaContext aContext)
+        throws ResourceInitializationException
+    {
+        super.initialize(aContext);
+
+        rtfParser = new RTFEditorKit();
+    }
+
+    @Override
+    public void getNext(CAS aCas)
+        throws IOException, CollectionException
+    {
+        Resource resource = nextFile();
+        initCas(aCas, resource, null);
+        Document document = rtfParser.createDefaultDocument();
+
+        try {
+            rtfParser.read(resource.getInputStream(), document, 0);
+            aCas.setDocumentText(document.getText(
+                    document.getStartPosition().getOffset(),
+                    document.getEndPosition().getOffset()));
+        }
+        catch (BadLocationException e) {
+            throw new IOException(e);
+        }
+    }
+}
