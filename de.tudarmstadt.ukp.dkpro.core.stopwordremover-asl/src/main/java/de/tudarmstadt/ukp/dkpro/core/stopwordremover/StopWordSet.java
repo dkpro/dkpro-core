a8a3150d4cacaf1991a4306e3b50e4ec42831d9c
Carsten Schnober <carschno@users.noreply.github.com>
2015-03-12 11:17:46 +0000

    Issue 600. For multiple files with same language code, only one is applied
    Fixes issue 600
    - StopWordSet#load(): now initializing data set only when run for the first time.
    - StopwordRemoverTest#testFilesSameLanguage(): expected number of tokens fixed.

--- de.tudarmstadt.ukp.dkpro.core.stopwordremover-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/stopwordremover/StopWordSet.java
+++ de.tudarmstadt.ukp.dkpro.core.stopwordremover-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/stopwordremover/StopWordSet.java
@@ -35,64 +35,63 @@ import org.apache.uima.resource.SharedResourceObject;
  * word files are converted to lower case.
  */
 public class StopWordSet
-	implements SharedResourceObject
+    implements SharedResourceObject
 {
-	private HashSet<String> data;
+    private HashSet<String> data;
 
-	public StopWordSet()
-	{
-		super();
-	}
+    public StopWordSet()
+    {
+        super();
+    }
 
-	public StopWordSet(String[] fileNames)
-		throws IOException
-	{
-		super();
-		for (String fileName : fileNames) {
-			addStopWordListFile(fileName);
-		}
-	}
+    public StopWordSet(String[] fileNames)
+        throws IOException
+    {
+        super();
+        for (String fileName : fileNames) {
+            addStopWordListFile(fileName);
+        }
+    }
 
-	/**
-	 * Loads a text file (UTF-8 encoding!) containing stop words. Only first
-	 * word in each line will be taken into account. Everything after "|" will
-	 * be treated as comment.
-	 */
-	public void addStopWordListFile(String fileName)
-		throws IOException
-	{
-		Reader reader = null;
-		try {
-			reader = new FileReader(fileName);
-			load(new FileReader(fileName));
-		}
-		finally {
-			IOUtils.closeQuietly(reader);
-		}
-	}
+    /**
+     * Loads a text file (UTF-8 encoding!) containing stop words. Only first word in each line will
+     * be taken into account. Everything after "|" will be treated as comment.
+     */
+    public void addStopWordListFile(String fileName)
+        throws IOException
+    {
+        Reader reader = null;
+        try {
+            reader = new FileReader(fileName);
+            load(new FileReader(fileName));
+        }
+        finally {
+            IOUtils.closeQuietly(reader);
+        }
+    }
 
-	@Override
-	public void load(DataResource dataRes)
-		throws ResourceInitializationException
-	{
-		InputStream is = null;
-		try {
-			is = dataRes.getInputStream();
-			load(is);
-		}
-		catch (IOException e) {
-			throw new ResourceInitializationException(e);
-		}
-		finally {
-			IOUtils.closeQuietly(is);
-		}
-	}
+    @Override
+    public void load(DataResource dataRes)
+        throws ResourceInitializationException
+    {
+        InputStream is = null;
+        try {
+            is = dataRes.getInputStream();
+            load(is);
+        }
+        catch (IOException e) {
+            throw new ResourceInitializationException(e);
+        }
+        finally {
+            IOUtils.closeQuietly(is);
+        }
+    }
 
-	public void load(InputStream aIs)
-		throws IOException
-	{
-		load(aIs, "UTF-8");
-	}
+    public void load(InputStream aIs)
+        throws IOException
+    {
+        load(aIs, "UTF-8");
+    }
 
     public void load(InputStream aIs, String aEncoding)
         throws IOException
@@ -100,22 +99,31 @@ public class StopWordSet
         load(new InputStreamReader(aIs, aEncoding));
     }
 
-	public void load(Reader aReader)
-		throws IOException
-	{
-		data = new HashSet<String>();
-		String line = null;
-		BufferedReader br = new BufferedReader(aReader);
-		while ((line = br.readLine()) != null) {
-			String[] words = line.trim().split("\\s|\\|");
-			if (words.length > 0 && words[0].trim().length() > 0) {
-				data.add(words[0].toLowerCase());
-			}
-		}
-	}
+    /**
+     * Load the stopwords from the given reader. If the set already contains data, new stopwords are
+     * added.
+     *
+     * @param aReader
+     * @throws IOException
+     */
+    public void load(Reader aReader)
+        throws IOException
+    {
+        if (data == null) {
+            data = new HashSet<String>();
+        }
+        String line = null;
+        BufferedReader br = new BufferedReader(aReader);
+        while ((line = br.readLine()) != null) {
+            String[] words = line.trim().split("\\s|\\|");
+            if (words.length > 0 && words[0].trim().length() > 0) {
+                data.add(words[0].toLowerCase());
+            }
+        }
+    }
 
-	public boolean contains(String aWord)
-	{
-		return data.contains(aWord);
-	}
+    public boolean contains(String aWord)
+    {
+        return data.contains(aWord);
+    }
 }
