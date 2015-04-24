af146d51a2b4eb212b4911562c6ceadb0f4d69bc
tobias.horsmann <tobias.horsmann@2de02dc9-a36c-33c0-148d-ca8b1223715f>
2015-04-24 11:50:42 +0000

    Issue 611:	Add a dedicated DKPro POS-Tagging component based on DKPro-TC - removed log messages - added time measurement for token-index class

--- de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.flextag-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/flextag/features/token/TokenLookUpTable.java
+++ de.tudarmstadt.ukp.dkpro.core-asl/trunk/de.tudarmstadt.ukp.dkpro.core.flextag-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/flextag/features/token/TokenLookUpTable.java
@@ -21,9 +21,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import org.apache.commons.logging.LogFactory;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
-import org.apache.uima.util.Level;
 
 import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
@@ -34,63 +34,64 @@ import de.tudarmstadt.ukp.dkpro.tc.api.features.Feature;
 import de.tudarmstadt.ukp.dkpro.tc.api.features.FeatureExtractorResource_ImplBase;
 import de.tudarmstadt.ukp.dkpro.tc.api.type.TextClassificationUnit;
 
-public class TokenLookUpTable
-    extends FeatureExtractorResource_ImplBase
-    implements ClassificationUnitFeatureExtractor
-{
-    private static String lastSeenDocumentId = "";
+public class TokenLookUpTable extends FeatureExtractorResource_ImplBase
+		implements ClassificationUnitFeatureExtractor {
+	private static String lastSeenDocumentId = "";
 
-    protected static HashMap<Integer, Boolean> idx2SentenceBegin = new HashMap<Integer, Boolean>();
-    protected static HashMap<Integer, Boolean> idx2SentenceEnd = new HashMap<Integer, Boolean>();
+	protected static HashMap<Integer, Boolean> idx2SentenceBegin = new HashMap<Integer, Boolean>();
+	protected static HashMap<Integer, Boolean> idx2SentenceEnd = new HashMap<Integer, Boolean>();
 
-    protected static HashMap<Integer, Token> begin2Token = new HashMap<Integer, Token>();
-    protected static HashMap<Integer, Integer> tokenBegin2Idx = new HashMap<Integer, Integer>();
-    protected static HashMap<Integer, Integer> tokenEnd2Idx = new HashMap<Integer, Integer>();
-    protected static List<String> tokens = new ArrayList<String>();
-    
-    public List<Feature> extract(JCas aView, TextClassificationUnit aClassificationUnit)
-        throws TextClassificationException
-    {
-        if (isTheSameDocument(aView)) {
-            return null;
-        }
-        getLogger().log(Level.FINE, "START: (Re-)Build Token look up table");
+	protected static HashMap<Integer, Token> begin2Token = new HashMap<Integer, Token>();
+	protected static HashMap<Integer, Integer> tokenBegin2Idx = new HashMap<Integer, Integer>();
+	protected static HashMap<Integer, Integer> tokenEnd2Idx = new HashMap<Integer, Integer>();
+	protected static List<String> tokens = new ArrayList<String>();
 
-        begin2Token = new HashMap<Integer, Token>();
-        tokenBegin2Idx = new HashMap<Integer, Integer>();
-        idx2SentenceBegin = new HashMap<Integer, Boolean>();
-        idx2SentenceEnd = new HashMap<Integer, Boolean>();
-        tokens = new ArrayList<String>();
+	public List<Feature> extract(JCas aView,
+			TextClassificationUnit aClassificationUnit)
+			throws TextClassificationException {
+		if (isTheSameDocument(aView)) {
+			return null;
+		}
+		long s = System.currentTimeMillis();
 
-        int i = 0;
-        for (Token t : JCasUtil.select(aView, Token.class)) {
-            Integer begin = t.getBegin();
-            Integer end = t.getEnd();
-            begin2Token.put(begin, t);
-            tokenBegin2Idx.put(begin, i);
-            tokenEnd2Idx.put(end, i);
-            tokens.add(t.getCoveredText());
-            i++;
-        }
-        for (Sentence s : JCasUtil.select(aView, Sentence.class)) {
-            Integer begin = s.getBegin();
-            Integer end = s.getEnd();
-            Integer idxStartToken = tokenBegin2Idx.get(begin);
-            Integer idxEndtoken = tokenEnd2Idx.get(end);
-            idx2SentenceBegin.put(idxStartToken, true);
-            idx2SentenceEnd.put(idxEndtoken, true);
-        }
-        getLogger().log(Level.FINE, "FINISH: (Re-)Build Token look up table");
-        return null;
-    }
+		begin2Token = new HashMap<Integer, Token>();
+		tokenBegin2Idx = new HashMap<Integer, Integer>();
+		idx2SentenceBegin = new HashMap<Integer, Boolean>();
+		idx2SentenceEnd = new HashMap<Integer, Boolean>();
+		tokens = new ArrayList<String>();
 
-    private boolean isTheSameDocument(JCas aView)
-    {
-        DocumentMetaData meta = JCasUtil.selectSingle(aView, DocumentMetaData.class);
-        String currentId = meta.getDocumentId();
-        boolean isSame = currentId.equals(lastSeenDocumentId);
-        lastSeenDocumentId = currentId;
-        return isSame;
-    }
+		int i = 0;
+		for (Token t : JCasUtil.select(aView, Token.class)) {
+			Integer begin = t.getBegin();
+			Integer end = t.getEnd();
+			begin2Token.put(begin, t);
+			tokenBegin2Idx.put(begin, i);
+			tokenEnd2Idx.put(end, i);
+			tokens.add(t.getCoveredText());
+			i++;
+		}
+		for (Sentence sentence : JCasUtil.select(aView, Sentence.class)) {
+			Integer begin = sentence.getBegin();
+			Integer end = sentence.getEnd();
+			Integer idxStartToken = tokenBegin2Idx.get(begin);
+			Integer idxEndtoken = tokenEnd2Idx.get(end);
+			idx2SentenceBegin.put(idxStartToken, true);
+			idx2SentenceEnd.put(idxEndtoken, true);
+		}
+		long e = System.currentTimeMillis();
+		LogFactory.getLog(getClass()).debug(
+				"Seconds spent for (re-)creation of token index[" + ((double) e - s)
+						/ 1000 + "]");
+		return null;
+	}
+
+	private boolean isTheSameDocument(JCas aView) {
+		DocumentMetaData meta = JCasUtil.selectSingle(aView,
+				DocumentMetaData.class);
+		String currentId = meta.getDocumentId();
+		boolean isSame = currentId.equals(lastSeenDocumentId);
+		lastSeenDocumentId = currentId;
+		return isSame;
+	}
 
 }
