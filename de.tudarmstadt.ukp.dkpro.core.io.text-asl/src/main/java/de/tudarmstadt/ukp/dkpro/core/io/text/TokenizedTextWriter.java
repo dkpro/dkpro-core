37a2c99ad7bbe4af62eef8833af72ba23bdcb57d
Carsten Schnober <schnober@ukp.informatik.tu-darmstadt.de>
2015-12-04 16:34:45 +0100

    Fixes #703
    - TokenzedTextWriter.java: Javadoc updated
    - TokenizedTextWriterTest.java: testLemmas() implemented.

--- de.tudarmstadt.ukp.dkpro.core.io.text-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/text/TokenizedTextWriter.java
+++ de.tudarmstadt.ukp.dkpro.core.io.text-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/text/TokenizedTextWriter.java
@@ -17,19 +17,12 @@
  ******************************************************************************/
 package de.tudarmstadt.ukp.dkpro.core.io.text;
 
-import static org.apache.uima.fit.util.CasUtil.selectCovered;
-import static org.apache.uima.fit.util.JCasUtil.select;
-
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.nio.file.Files;
-import java.util.Collections;
-import java.util.Set;
-import java.util.stream.Collectors;
-
+import de.tudarmstadt.ukp.dkpro.core.api.featurepath.FeaturePathException;
+import de.tudarmstadt.ukp.dkpro.core.api.featurepath.FeaturePathFactory.FeaturePathIterator;
+import de.tudarmstadt.ukp.dkpro.core.api.featurepath.FeaturePathInfo;
+import de.tudarmstadt.ukp.dkpro.core.api.io.JCasFileWriter_ImplBase;
+import de.tudarmstadt.ukp.dkpro.core.api.parameter.ComponentParameters;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
 import org.apache.commons.io.IOUtils;
 import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
@@ -39,12 +32,14 @@ import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
 
-import de.tudarmstadt.ukp.dkpro.core.api.featurepath.FeaturePathException;
-import de.tudarmstadt.ukp.dkpro.core.api.featurepath.FeaturePathFactory.FeaturePathIterator;
-import de.tudarmstadt.ukp.dkpro.core.api.featurepath.FeaturePathInfo;
-import de.tudarmstadt.ukp.dkpro.core.api.io.JCasFileWriter_ImplBase;
-import de.tudarmstadt.ukp.dkpro.core.api.parameter.ComponentParameters;
-import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
+import java.io.*;
+import java.nio.file.Files;
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.apache.uima.fit.util.CasUtil.selectCovered;
+import static org.apache.uima.fit.util.JCasUtil.select;
 
 /**
  * This class writes a set of pre-processed documents into a large text file containing one sentence
@@ -54,31 +49,16 @@ import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
 public class TokenizedTextWriter
     extends JCasFileWriter_ImplBase
 {
-    private static final String TOKEN_SEPARATOR = " ";
-    private static final String NUMBER_REPLACEMENT = "NUM";
-    private static final String STOPWORD_REPLACEMENT = "STOP";
-
     /**
      * Encoding for the target file. Default is UTF-8.
      */
     public static final String PARAM_TARGET_ENCODING = ComponentParameters.PARAM_TARGET_ENCODING;
-    @ConfigurationParameter(name = PARAM_TARGET_ENCODING, mandatory = true, defaultValue = "UTF-8")
-    private String targetEncoding;
-
     /**
      * The feature path, e.g.
-     * {@code de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Lemma/value} for lemmas. Default:
+     * {@code de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token/lemma/value} for lemmas. Default:
      * {@code de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token} (i.e. token texts).
-     * <p>
-     * In order to specify a different annotation use the annotation class' type name (e.g.
-     * {@code Token.class.getTypeName()}) and optionally append a field, e.g. {@code /value} to
-     * specify the feature path. If you do not specify a field, the covered text is used.
-     * 
      */
     public static final String PARAM_FEATURE_PATH = "featurePath";
-    @ConfigurationParameter(name = PARAM_FEATURE_PATH, mandatory = true, defaultValue = "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token")
-    private String featurePath;
-
     /**
      * All tokens that match this regex are replaced by {@code NUM}. Examples:
      * <ul>
@@ -91,19 +71,45 @@ public class TokenizedTextWriter
      * tokens, your tokenizer might split prefixes such as + and - from the rest of the number.
      */
     public static final String PARAM_NUMBER_REGEX = "numberRegex";
-    @ConfigurationParameter(name = PARAM_NUMBER_REGEX, mandatory = false)
-    private String numberRegex;
-
     /**
      * All the tokens listed in this file (one token per line) are replaced by {@code STOP}. Empty
      * lines and lines starting with {@code #} are ignored. Casing is ignored.
      */
     public static final String PARAM_STOPWORDS_FILE = "stopwordsFile";
+    private static final String TOKEN_SEPARATOR = " ";
+    private static final String NUMBER_REPLACEMENT = "NUM";
+    private static final String STOPWORD_REPLACEMENT = "STOP";
+    @ConfigurationParameter(name = PARAM_TARGET_ENCODING, mandatory = true, defaultValue = "UTF-8")
+    private String targetEncoding;
+    @ConfigurationParameter(name = PARAM_FEATURE_PATH, mandatory = true, defaultValue = "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token")
+    private String featurePath;
+    @ConfigurationParameter(name = PARAM_NUMBER_REGEX, mandatory = false)
+    private String numberRegex;
     @ConfigurationParameter(name = PARAM_STOPWORDS_FILE, mandatory = false)
     private File stopwordsFile;
     private Set<String> stopwords;
 
     private BufferedWriter targetWriter;
+
+    /**
+     * Read a file containing stopwords (one per line).
+     * <p>
+     * Empty lines and lines starting with ("#") are filtered out.
+     *
+     * @param f
+     * @return
+     * @throws IOException
+     */
+    private static Set<String> readStopwordsFile(File f)
+            throws IOException
+    {
+        return Files.readAllLines(f.toPath()).stream()
+                .map(String::trim)
+                .filter(l -> !l.isEmpty())
+                .filter(l -> !l.startsWith("#"))
+                .map(w -> w.toLowerCase())
+                .collect(Collectors.toSet());
+    }
 
     @Override
     public void initialize(UimaContext context)
@@ -122,26 +128,6 @@ public class TokenizedTextWriter
         catch (IOException e) {
             throw new ResourceInitializationException(e);
         }
-    };
-
-    /**
-     * Read a file containing stopwords (one per line).
-     * <p>
-     * Empty lines and lines starting with ("#") are filtered out.
-     * 
-     * @param f
-     * @return
-     * @throws IOException
-     */
-    private static Set<String> readStopwordsFile(File f)
-        throws IOException
-    {
-        return Files.readAllLines(f.toPath()).stream()
-                .map(String::trim)
-                .filter(l -> !l.isEmpty())
-                .filter(l -> !l.startsWith("#"))
-                .map(w -> w.toLowerCase())
-                .collect(Collectors.toSet());
     }
 
     /*
