69ee7a272b696a212f1eaf6d5421896799279172
Richard Eckart de Castilho <richard.eckart@gmail.com>
2015-12-28 13:27:13 -0200

    #728 - Add CAS validation framework and checks

---
+++ de.tudarmstadt.ukp.dkpro.core.testing-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/testing/validation/CasValidator.java
@@ -0,0 +1,102 @@
+/*******************************************************************************
+ * Copyright 2015
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.tudarmstadt.ukp.dkpro.core.testing.validation;
+
+import static java.util.Arrays.asList;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.apache.uima.jcas.JCas;
+import org.reflections.Reflections;
+
+import de.tudarmstadt.ukp.dkpro.core.testing.validation.checks.Check;
+
+public class CasValidator
+{
+    private Set<Class<? extends Check>> checkClasses = new LinkedHashSet<>();
+
+    public CasValidator()
+    {
+        // Nothing to do
+    }
+    
+    @SafeVarargs
+    public CasValidator(Class<? extends Check>... aChecks)
+    {
+        setChecks(aChecks);
+    }
+    
+    public void setChecks(Collection<Class<? extends Check>> aChecks)
+    {
+        checkClasses = new LinkedHashSet<>();
+        if (aChecks != null) {
+            checkClasses.addAll(aChecks);
+        }
+    }
+
+    public void setChecks(Class<? extends Check>... aChecks)
+    {
+        checkClasses = new LinkedHashSet<>();
+        if (aChecks != null) {
+            checkClasses.addAll(asList(aChecks));
+        }
+    }
+
+    public List<Message> analyze(JCas aCas)
+    {
+        List<Message> messages = new ArrayList<>();
+        analyze(aCas, messages);
+        return messages;
+    }
+
+    private boolean analyze(JCas aCas, List<Message> aMessages)
+    {
+        boolean ok = true;
+        for (Class<? extends Check> checkClass : checkClasses) {
+            try {
+                Check check = checkClass.newInstance();
+                ok &= check.check(aCas, aMessages);
+            }
+            catch (InstantiationException | IllegalAccessException e) {
+                aMessages.add(new Message(this, Message.Level.ERROR,
+                        "Cannot instantiate [%s]: %s", checkClass.getSimpleName(), ExceptionUtils
+                                .getRootCauseMessage(e)));
+            }
+        }
+
+        return ok;
+    }
+    
+    public static CasValidator createWithAllChecks()
+    {
+        Reflections reflections = new Reflections(Check.class.getPackage().getName());
+        CasValidator validator = new CasValidator();
+        validator.setChecks(reflections.getSubTypesOf(Check.class));
+        return validator;
+    }
+
+    public void addCheck(Class<? extends Check> aExtra)
+    {
+        checkClasses.add(aExtra);
+    }
+}
