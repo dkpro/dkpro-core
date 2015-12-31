ad2b79892c531f8f005703c15516b3bc09d7c6f8
Richard Eckart de Castilho <richard.eckart@gmail.com>
2015-12-31 21:20:04 -0200

    #736 - ClearNlpSemanticRoleLabelerTest gets caught in infinite loop - Ignoring test for the moment.

--- dkpro-core-clearnlp-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/clearnlp/ClearNlpSemanticRoleLabelerTest.java
+++ dkpro-core-clearnlp-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/clearnlp/ClearNlpSemanticRoleLabelerTest.java
@@ -24,6 +24,7 @@ import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.jcas.JCas;
 import org.junit.Assume;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
@@ -33,6 +34,7 @@ import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpPosTagger;
 import de.tudarmstadt.ukp.dkpro.core.testing.AssertAnnotations;
 import de.tudarmstadt.ukp.dkpro.core.testing.TestRunner;
 
+@Ignore("Test appears to get caught in an infinite loop - needs investigation")
 public class ClearNlpSemanticRoleLabelerTest
 {
 	static final String documentEnglish = "We need a very complicated example sentence , which "
