b829c288467c7b1144672f516e91b5e0718ee9af
Carsten Schnober <schnober@ukp.informatik.tu-darmstadt.de>
2016-04-26 11:54:29 +0200

    Closes #827 Fix LdaTopicProportionsWriter
    - LdaTopicProportionsWriter.java: deprecation annotation removed, parameters changed, output format fixed.

--- dkpro-core-mallet-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/mallet/lda/io/LdaTopicProportionsWriter.java
+++ dkpro-core-mallet-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/mallet/lda/io/LdaTopicProportionsWriter.java
@@ -2,13 +2,13 @@
  * Copyright 2015
  * Ubiquitous Knowledge Processing (UKP) Lab
  * Technische Universität Darmstadt
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,89 +22,70 @@ import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
 import de.tudarmstadt.ukp.dkpro.core.api.parameter.ComponentParameters;
 import de.tudarmstadt.ukp.dkpro.core.mallet.lda.LdaTopicModelInferencer;
 import de.tudarmstadt.ukp.dkpro.core.mallet.type.TopicDistribution;
-import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.jcas.JCas;
-import org.apache.uima.resource.ResourceInitializationException;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Locale;
 
 import static org.apache.uima.fit.util.JCasUtil.select;
 
 /**
- * Write topic proportions to a file in the shape {@code<docId>\t<topic1>,<topic2>,...}
+ * Write topic proportions to a file in the shape {@code [<docId>\t]<topic_1>\t<topic_2>\t...<topic_n>}
  * <p>
- * This depends on the {@link TopicDistribution} annotation which should have been created by
+ * This writer depends on the {@link TopicDistribution} annotation which needs to be created by
  * {@link LdaTopicModelInferencer} before.
  * </p>
- *
- * @deprecated use {@link LdaTopicsProportionsSortedWriter}
  */
-@Deprecated
 public class LdaTopicProportionsWriter
-    extends JCasFileWriter_ImplBase
+        extends JCasFileWriter_ImplBase
 {
     private static final Locale LOCALE = Locale.US;
     private static final String COLUMN_SEPARATOR = "\t";
-    private static final String TOPIC_SEPARATOR = ",";
 
-    public static final String PARAM_TARGET_LOCATION = ComponentParameters.PARAM_TARGET_LOCATION;
-    @ConfigurationParameter(name = PARAM_TARGET_LOCATION, mandatory = true)
-    private File targetLocation;
-    private BufferedWriter targetWriter;
+    /**
+     * If set to true (default), each output line is preceded by the document id.
+     */
+    public static final String PARAM_WRITE_DOCID = "writeDocid";
+    @ConfigurationParameter(name = PARAM_WRITE_DOCID, mandatory = true, defaultValue = "true")
+    private boolean writeDocid;
 
-    @Override
-    public void initialize(UimaContext context)
-        throws ResourceInitializationException
-    {
-        super.initialize(context);
-
-        try {
-            targetWriter = new BufferedWriter(new FileWriter(targetLocation));
-        }
-        catch (IOException e) {
-            throw new ResourceInitializationException(e);
-        }
-
-    }
+    /**
+     * If {@link #PARAM_SINGULAR_TARGET} is set to false (default), this extension will be appended to the output
+     * files. Default: {@code .topics}.
+     */
+    public static final String PARAM_FILENAME_EXTENSION = ComponentParameters.PARAM_FILENAME_EXTENSION;
+    @ConfigurationParameter(name = PARAM_FILENAME_EXTENSION, mandatory = true, defaultValue = ".topics")
+    private String filenameExtension;
 
     @Override
     public void process(JCas aJCas)
-        throws AnalysisEngineProcessException
+            throws AnalysisEngineProcessException
     {
         try {
-            targetWriter.write(DocumentMetaData.get(aJCas).getDocumentId() + COLUMN_SEPARATOR);
+            OutputStream outputStream = getOutputStream(aJCas, filenameExtension);
+
+            if (writeDocid) {
+                outputStream.write(DocumentMetaData.get(aJCas).getDocumentId().getBytes());
+                outputStream.write(COLUMN_SEPARATOR.getBytes());
+            }
+
             for (TopicDistribution td : select(aJCas, TopicDistribution.class)) {
                 int nTopics = td.getTopicProportions().size();
-                for (int i = 0; i < nTopics; i++) {
-                    double proportion = td.getTopicProportions(i);
-                    targetWriter.write(String.format(LOCALE, "%.4f", proportion));
 
-                    /* write comma unless for last entry */
+                for (int i = 0; i < nTopics; i++) {
+                    outputStream.write(
+                            String.format(LOCALE, "%.4f", td.getTopicProportions(i)).getBytes());
+
+                    /* write column separator except for last entry */
                     if (i < nTopics - 1) {
-                        targetWriter.write(TOPIC_SEPARATOR);
+                        outputStream.write(COLUMN_SEPARATOR.getBytes());
                     }
                 }
+                outputStream.write(System.getProperty("line.separator").getBytes());
             }
-            targetWriter.newLine();
-        }
-        catch (IOException e) {
-            throw new AnalysisEngineProcessException(e);
-        }
-
-    }
-
-    @Override
-    public void collectionProcessComplete()
-        throws AnalysisEngineProcessException
-    {
-        try {
-            targetWriter.close();
         }
         catch (IOException e) {
             throw new AnalysisEngineProcessException(e);
--- dkpro-core-mallet-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/mallet/lda/io/LdaTopicProportionsWriterTest.java
+++ dkpro-core-mallet-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/mallet/lda/io/LdaTopicProportionsWriterTest.java
@@ -2,13 +2,13 @@
  * Copyright 2015
  * Ubiquitous Knowledge Processing (UKP) Lab
  * Technische Universität Darmstadt
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,6 +31,7 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
@@ -40,9 +41,7 @@ import static org.junit.Assert.assertTrue;
 
 /**
  *
- * @deprecated {@link LdaTopicProportionsWriter} has been deprecated
  */
-@Deprecated
 public class LdaTopicProportionsWriterTest
 {
     private static final File MODEL_FILE = new File("target/mallet/model");
@@ -55,7 +54,7 @@ public class LdaTopicProportionsWriterTest
 
     @Before
     public void setUp()
-        throws UIMAException, IOException
+            throws UIMAException, IOException
     {
         /* Generate model */
         CollectionReaderDescription reader = createReaderDescription(TextReader.class,
@@ -73,15 +72,14 @@ public class LdaTopicProportionsWriterTest
     }
 
     @Test
-    public void test()
-        throws UIMAException, IOException
+    public void testSingularTarget()
+            throws UIMAException, IOException, URISyntaxException
     {
         File targetFile = new File("target/topics.txt");
-        targetFile.deleteOnExit();
 
         int expectedLines = 2;
-        String expectedLine0Regex = "dummy1.txt\t(0\\.[0-9]{4},){9}0\\.[0-9]{4}";
-        String expectedLine1Regex = "dummy2.txt\t(0\\.[0-9]{4},){9}0\\.[0-9]{4}";
+        String expectedLine0Regex = "dummy1.txt\t(0\\.[0-9]{4}\\t){9}0\\.[0-9]{4}";
+        String expectedLine1Regex = "dummy2.txt\t(0\\.[0-9]{4}\\t){9}0\\.[0-9]{4}";
 
         CollectionReaderDescription reader = createReaderDescription(TextReader.class,
                 TextReader.PARAM_SOURCE_LOCATION, CAS_DIR,
@@ -95,13 +93,105 @@ public class LdaTopicProportionsWriterTest
 
         AnalysisEngineDescription writer = createEngineDescription(
                 LdaTopicProportionsWriter.class,
-                LdaTopicProportionsWriter.PARAM_TARGET_LOCATION, targetFile);
+                LdaTopicProportionsWriter.PARAM_TARGET_LOCATION, targetFile,
+                LdaTopicProportionsWriter.PARAM_OVERWRITE, true,
+                LdaTopicProportionsWriter.PARAM_SINGULAR_TARGET, true);
 
         SimplePipeline.runPipeline(reader, segmenter, inferencer, writer);
         List<String> lines = FileUtils.readLines(targetFile);
         assertTrue(lines.get(0).matches(expectedLine0Regex));
         assertTrue(lines.get(1).matches(expectedLine1Regex));
         assertEquals(expectedLines, lines.size());
-        MODEL_FILE.delete();
+        targetFile.delete();
+    }
+
+    @Test
+    public void testMultipleTargets()
+            throws IOException, UIMAException
+    {
+        File targetDir = new File("target");
+        File expectedFile0 = new File(targetDir, "dummy1.txt.topics");
+        File expectedFile1 = new File(targetDir, "dummy2.txt.topics");
+
+        int expectedLines = 1;
+        String expectedLine0Regex = "dummy1.txt\t(0\\.[0-9]{4}\\t){9}0\\.[0-9]{4}";
+        String expectedLine1Regex = "dummy2.txt\t(0\\.[0-9]{4}\\t){9}0\\.[0-9]{4}";
+
+        CollectionReaderDescription reader = createReaderDescription(TextReader.class,
+                TextReader.PARAM_SOURCE_LOCATION, CAS_DIR,
+                TextReader.PARAM_PATTERNS, CAS_FILE_PATTERN,
+                TextReader.PARAM_LANGUAGE, LANGUAGE);
+        AnalysisEngineDescription segmenter = createEngineDescription(BreakIteratorSegmenter.class);
+
+        AnalysisEngineDescription inferencer = createEngineDescription(
+                LdaTopicModelInferencer.class,
+                LdaTopicModelInferencer.PARAM_MODEL_LOCATION, MODEL_FILE);
+
+        AnalysisEngineDescription writer = createEngineDescription(
+                LdaTopicProportionsWriter.class,
+                LdaTopicProportionsWriter.PARAM_TARGET_LOCATION, targetDir,
+                LdaTopicProportionsWriter.PARAM_OVERWRITE, true,
+                LdaTopicProportionsWriter.PARAM_SINGULAR_TARGET, false,
+                LdaTopicProportionsWriter.PARAM_WRITE_DOCID, true);
+
+        SimplePipeline.runPipeline(reader, segmenter, inferencer, writer);
+
+        assertTrue(expectedFile0.exists());
+        assertTrue(expectedFile1.exists());
+
+        List<String> lines = FileUtils.readLines(expectedFile0);
+        assertTrue(lines.get(0).matches(expectedLine0Regex));
+        assertEquals(expectedLines, lines.size());
+        lines = FileUtils.readLines(expectedFile1);
+        assertEquals(expectedLines, lines.size());
+        assertTrue(lines.get(0).matches(expectedLine1Regex));
+
+        expectedFile0.delete();
+        expectedFile1.delete();
+    }
+
+    @Test
+    public void testMultipleTargetsNoDocids()
+            throws IOException, UIMAException
+    {
+        File targetDir = new File("target");
+        File expectedFile0 = new File(targetDir, "dummy1.txt.topics");
+        File expectedFile1 = new File(targetDir, "dummy2.txt.topics");
+
+        int expectedLines = 1;
+        String expectedLine0Regex = "(0\\.[0-9]{4}\\t){9}0\\.[0-9]{4}";
+        String expectedLine1Regex = "(0\\.[0-9]{4}\\t){9}0\\.[0-9]{4}";
+
+        CollectionReaderDescription reader = createReaderDescription(TextReader.class,
+                TextReader.PARAM_SOURCE_LOCATION, CAS_DIR,
+                TextReader.PARAM_PATTERNS, CAS_FILE_PATTERN,
+                TextReader.PARAM_LANGUAGE, LANGUAGE);
+        AnalysisEngineDescription segmenter = createEngineDescription(BreakIteratorSegmenter.class);
+
+        AnalysisEngineDescription inferencer = createEngineDescription(
+                LdaTopicModelInferencer.class,
+                LdaTopicModelInferencer.PARAM_MODEL_LOCATION, MODEL_FILE);
+
+        AnalysisEngineDescription writer = createEngineDescription(
+                LdaTopicProportionsWriter.class,
+                LdaTopicProportionsWriter.PARAM_TARGET_LOCATION, targetDir,
+                LdaTopicProportionsWriter.PARAM_OVERWRITE, true,
+                LdaTopicProportionsWriter.PARAM_SINGULAR_TARGET, false,
+                LdaTopicProportionsWriter.PARAM_WRITE_DOCID, false);
+
+        SimplePipeline.runPipeline(reader, segmenter, inferencer, writer);
+
+        assertTrue(expectedFile0.exists());
+        assertTrue(expectedFile1.exists());
+
+        List<String> lines = FileUtils.readLines(expectedFile0);
+        assertTrue(lines.get(0).matches(expectedLine0Regex));
+        assertEquals(expectedLines, lines.size());
+        lines = FileUtils.readLines(expectedFile1);
+        assertEquals(expectedLines, lines.size());
+        assertTrue(lines.get(0).matches(expectedLine1Regex));
+
+        expectedFile0.delete();
+        expectedFile1.delete();
     }
 }
