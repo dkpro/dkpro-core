c368972d2d75a4f061f3bfcb5a82ea40bb279c2b
Carsten Schnober <schnober@ukp.informatik.tu-darmstadt.de>
2016-07-21 14:28:19 +0200

    JdbcReader ignores PARAM_USER #909
    - JdbcReader.java: Parameter PARAM_CONNECTION_PARAMS introduced, minor style issues

--- dkpro-core-io-jdbc-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/jdbc/JdbcReader.java
+++ dkpro-core-io-jdbc-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/jdbc/JdbcReader.java
@@ -17,18 +17,7 @@
  */
 package de.tudarmstadt.ukp.dkpro.core.io.jdbc;
 
-import static java.util.Arrays.asList;
-
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.HashSet;
-import java.util.Set;
-
+import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
 import org.apache.commons.dbutils.DbUtils;
 import org.apache.uima.UimaContext;
 import org.apache.uima.cas.CAS;
@@ -41,7 +30,12 @@ import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.util.Progress;
 import org.apache.uima.util.ProgressImpl;
 
-import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
+import java.io.IOException;
+import java.sql.*;
+import java.util.HashSet;
+import java.util.Set;
+
+import static java.util.Arrays.asList;
 
 /**
  * Collection reader for JDBC database.The obtained data will be written into CAS DocumentText as
@@ -52,18 +46,16 @@ import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
  * <p>
  * <code>SELECT text AS cas_text, title AS cas_metadata_title FROM test_table</code>
  * <p>
- * will create a CAS for each record, write the content of "text" column into CAS documen text and
+ * will create a CAS for each record, write the content of "text" column into CAS document text and
  * that of "title" column into the document title field of the {@link DocumentMetaData} annotation.
- *
- *
  */
 
 @TypeCapability(
-        outputs={
-                "de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData"})
+        outputs = {
+                "de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData" })
 
 public class JdbcReader
-    extends CasCollectionReader_ImplBase
+        extends CasCollectionReader_ImplBase
 {
     public static final String CAS_TEXT = "cas_text";
     public static final String CAS_METADATA_TITLE = "cas_metadata_title";
@@ -74,18 +66,18 @@ public class JdbcReader
     public static final String CAS_METADATA_DOCUMENT_URI = "cas_metadata_document_uri";
     public static final String CAS_METADATA_DOCUMENT_BASE_URI = "cas_metadata_document_base_uri";
 
-    private static final Set<String> CAS_COLUMNS = new HashSet<String>(asList(
-    		CAS_TEXT, CAS_METADATA_TITLE, CAS_METADATA_LANGUAGE, CAS_METADATA_DOCUMENT_ID,
-    		CAS_METADATA_COLLECTION_ID, CAS_METADATA_DOCUMENT_URI, CAS_METADATA_DOCUMENT_BASE_URI));
+    private static final Set<String> CAS_COLUMNS = new HashSet<>(asList(
+            CAS_TEXT, CAS_METADATA_TITLE, CAS_METADATA_LANGUAGE, CAS_METADATA_DOCUMENT_ID,
+            CAS_METADATA_COLLECTION_ID, CAS_METADATA_DOCUMENT_URI, CAS_METADATA_DOCUMENT_BASE_URI));
 
     /**
      * Specify the class name of the JDBC driver.
      * <p>
-     * If used with uimaFIT and the value is not given, <code>com.mysql.jdbc.Driver</code> will be
+     * If used with uimaFIT and the value is not given, <code>com.mysql.cj.jdbc.Driver</code> will be
      * taken.
      */
     public static final String PARAM_DRIVER = "driver";
-    @ConfigurationParameter(name = PARAM_DRIVER, mandatory = true, defaultValue = "com.mysql.jdbc.Driver")
+    @ConfigurationParameter(name = PARAM_DRIVER, mandatory = true, defaultValue = "com.mysql.cj.jdbc.Driver")
     private String driver;
 
     /**
@@ -93,10 +85,20 @@ public class JdbcReader
      * <p>
      * If used with uimaFIT and the value is not given, <code>jdbc:mysql://127.0.0.1/</code> will be
      * taken.
+     * <p>
+     * Do not use this parameter to add additional parameters, but use {@link #PARAM_CONNECTION_PARAMS}
+     * instead.
      */
     public static final String PARAM_CONNECTION = "connection";
     @ConfigurationParameter(name = PARAM_CONNECTION, mandatory = true, defaultValue = "jdbc:mysql://127.0.0.1/")
     private String connection;
+
+    /**
+     * Add additional parameters for the connection URL here in a single string: {@code [&propertyName1=propertyValue1[&propertyName2=propertyValue2]...]}.
+     */
+    public static final String PARAM_CONNECTION_PARAMS = "connectionParams";
+    @ConfigurationParameter(name = PARAM_CONNECTION_PARAMS, mandatory = true, defaultValue = "")
+    private String connectionParams;
 
     /**
      * Specifies name of the database to be accessed.
@@ -141,72 +143,71 @@ public class JdbcReader
 
     @Override
     public void initialize(UimaContext context)
-        throws ResourceInitializationException
+            throws ResourceInitializationException
     {
         super.initialize(context);
 
-        openDatabaseConnection();
-        query();
+        try {
+            openDatabaseConnection();
+            query();
+        }
+        catch (SQLException | ClassNotFoundException e) {
+            throw new ResourceInitializationException(e);
+        }
     }
 
     private void openDatabaseConnection()
-    	throws ResourceInitializationException
+            throws SQLException, ClassNotFoundException
     {
         // Open connection
         if (!connection.endsWith("/")) {
             connection = connection + "/";
         }
-        String url = connection + database + "?user=" + user + "&password=" + password;
+        String url = connection + database + "?user=" + user + "&password=" + password
+                + connectionParams;
 
-        try {
-            Class.forName(driver);
-            sqlConnection = DriverManager.getConnection(url);
-        }
-        catch (ClassNotFoundException e) {
-            throw new ResourceInitializationException(e);
-        }
-        catch (SQLException e) {
-            throw new ResourceInitializationException(e);
-        }
+        Class.forName(driver);
+        sqlConnection = DriverManager.getConnection(url);
+
     }
 
-    private void query() throws ResourceInitializationException
+    private void query()
+            throws SQLException
     {
-        try {
-            Statement statement = sqlConnection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-            resultSet = statement.executeQuery(query);
-            resultSet.last();
-            resultSetSize = resultSet.getRow();
-            resultSet.beforeFirst();
-            completed = 0;
+        getLogger().info("Executing query: '" + query + "'.");
 
-            // Store available column names
-            columnNames = new HashSet<String>();
-            ResultSetMetaData meta = resultSet.getMetaData();
-            for (int i = 1; i < meta.getColumnCount() + 1; i++) {
-            	String columnName = meta.getColumnLabel(i);
-                columnNames.add(columnName);
-                if (!CAS_COLUMNS.contains(columnName)) {
-                    getLogger().warn("Unknown column [" + columnName + "].");
-                }
+        Statement statement = sqlConnection
+                .createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        resultSet = statement.executeQuery(query);
+        resultSet.last();
+        resultSetSize = resultSet.getRow();
+        resultSet.beforeFirst();
+        completed = 0;
+
+        // Store available column names
+        columnNames = new HashSet<>();
+        ResultSetMetaData meta = resultSet.getMetaData();
+        for (int i = 1; i < meta.getColumnCount() + 1; i++) {
+            String columnName = meta.getColumnLabel(i);
+            columnNames.add(columnName);
+            if (!CAS_COLUMNS.contains(columnName)) {
+                getLogger().warn("Unknown column [" + columnName + "].");
             }
-        }
-        catch (SQLException e) {
-            throw new ResourceInitializationException(e);
         }
     }
 
     @Override
     public void getNext(CAS cas)
-        throws IOException, CollectionException
+            throws IOException, CollectionException
     {
-    	try {
-    		resultSet.next(); // advance to next item (used to be done in hasNext())
-    	} catch (SQLException e) {
-    		throw new CollectionException(e);
-    	}
+        try {
+            resultSet.next(); // advance to next item (used to be done in hasNext())
+        }
+        catch (SQLException e) {
+            throw new CollectionException(e);
+        }
         // Store data into CAS
-        DocumentMetaData metadata = null;
+        DocumentMetaData metadata;
         try {
             metadata = DocumentMetaData.create(cas);
         }
@@ -217,7 +218,7 @@ public class JdbcReader
         cas.setDocumentText(getStringQuietly(CAS_TEXT));
         metadata.setDocumentTitle(getStringQuietly(CAS_METADATA_TITLE));
         String language = getStringQuietly(CAS_METADATA_LANGUAGE);
-        if(language == null){
+        if (language == null) {
             language = this.language;
         }
         metadata.setLanguage(language);
@@ -248,21 +249,21 @@ public class JdbcReader
     {
         // REC: It should be possible to determine the current row and the total size of the result
         // set and use this here to return progress information.
-        return new Progress[] { new ProgressImpl( completed, resultSetSize, "row" ) };
+        return new Progress[] { new ProgressImpl(completed, resultSetSize, "row") };
     }
 
     @Override
     public boolean hasNext()
-        throws IOException, CollectionException
+            throws IOException, CollectionException
     {
-       	return (completed < resultSetSize);
+        return (completed < resultSetSize);
     }
 
     @Override
     public void close()
-        throws IOException
+            throws IOException
     {
-    	DbUtils.closeQuietly(resultSet);
-    	DbUtils.closeQuietly(sqlConnection);
+        DbUtils.closeQuietly(resultSet);
+        DbUtils.closeQuietly(sqlConnection);
     }
 }
