4e2ee6076f653ae3be2ac445c4cdb2a41c6ea39a
Milen Kouylekov <milen.kouylekov@usit.uio.no>
2016-10-12 14:10:09 +0200

    #954 Changes to LXF DKPRO that makes the prototype working.
    The major difference is in the DKPro2Lxf. The LXF needs that each layer
    is created by a tool with corresponding name. If the layer of annotation
    is new the DKPro2Lxf will use the id of the tool in this map to create
    the nodes corresponding to this layer. The tool id is need for the
    origin part of the node. The appropriate test are added that reflect
    this major change. Mainly the ref.lxf files are generated with the
    default dkpro tool as origin.

--- dkpro-core-io-lxf-asl/src/test/java/org/dkpro/core/io/lxf/LxfReaderWriterTest.java
+++ dkpro-core-io-lxf-asl/src/test/java/org/dkpro/core/io/lxf/LxfReaderWriterTest.java
@@ -17,76 +17,66 @@
  */
 package org.dkpro.core.io.lxf;
 
-import static de.tudarmstadt.ukp.dkpro.core.testing.IOTestRunner.*;
+import static de.tudarmstadt.ukp.dkpro.core.testing.IOTestRunner.testOneWay;
 
-import org.dkpro.core.io.lxf.LxfReader;
-import org.dkpro.core.io.lxf.LxfWriter;
 import org.junit.Test;
 
 public class LxfReaderWriterTest
 {
+
     @Test
     public void roundTripText()
         throws Exception
     {
-//        testRoundTrip(
-//                LxfReader.class, // the reader
-//                LxfWriter.class,  // the writer
-//                "lxf/text/orig.lxf"); // the input also used as output reference
-        
-        testOneWay(
-                LxfReader.class, // the reader
-                LxfWriter.class,  // the writer
-                "lxf/text/ref.lxf",
-                "lxf/text/orig.lxf"); 
+        // testRoundTrip(
+        // LxfReader.class, // the reader
+        // LxfWriter.class, // the writer
+        // "lxf/text/orig.lxf"); // the input also used as output reference
+
+        testOneWay(LxfReader.class, // the reader
+                LxfWriter.class, // the writer
+                "lxf/text/ref.lxf", "lxf/text/orig.lxf");
     }
-    
+
     @Test
     public void roundTripTokenizerRepp()
         throws Exception
     {
-//        testRoundTrip(
-//                LxfReader.class, // the reader
-//                LxfWriter.class,  // the writer
-//                "lxf/tokenizer-repp/orig.lxf"); // the input also used as output reference
-        
-        testOneWay(
-                LxfReader.class, // the reader
-                LxfWriter.class,  // the writer
-                "lxf/tokenizer-repp/ref.lxf",
-                "lxf/tokenizer-repp/orig.lxf"); 
+        // testRoundTrip(
+        // LxfReader.class, // the reader
+        // LxfWriter.class, // the writer
+        // "lxf/tokenizer-repp/orig.lxf"); // the input also used as output reference
+
+        testOneWay(LxfReader.class, // the reader
+                LxfWriter.class, // the writer
+                "lxf/tokenizer-repp/ref.lxf", "lxf/tokenizer-repp/orig.lxf");
     }
 
     @Test
     public void roundTripTokenizerReppHunpos()
         throws Exception
     {
-//        testRoundTrip(
-//                LxfReader.class, // the reader
-//                LxfWriter.class,  // the writer
-//                "lxf/tokenizer-repp-hunpos/orig.lxf"); // the input also used as output reference
-        
-        testOneWay(
-                LxfReader.class, // the reader
-                LxfWriter.class,  // the writer
-                "lxf/tokenizer-repp-hunpos/ref.lxf",
-                "lxf/tokenizer-repp-hunpos/orig.lxf"); 
-    }
+        // testRoundTrip(
+        // LxfReader.class, // the reader
+        // LxfWriter.class, // the writer
+        // "lxf/tokenizer-repp-hunpos/orig.lxf"); // the input also used as output reference
 
+        testOneWay(LxfReader.class, // the reader
+                LxfWriter.class, // the writer
+                "lxf/tokenizer-repp-hunpos/ref.lxf", "lxf/tokenizer-repp-hunpos/orig.lxf");
+    }
 
     @Test
     public void roundTripTokenizerReppHunposBn()
         throws Exception
     {
-//        testRoundTrip(
-//                LxfReader.class, // the reader
-//                LxfWriter.class,  // the writer
-//                "lxf/tokenizer-repp-hunpos-bn/orig.lxf"); // the input also used as output reference
-        
-        testOneWay(
-                LxfReader.class, // the reader
-                LxfWriter.class,  // the writer
-                "lxf/tokenizer-repp-hunpos-bn/ref.lxf",
-                "lxf/tokenizer-repp-hunpos-bn/orig.lxf"); 
+        // testRoundTrip(
+        // LxfReader.class, // the reader
+        // LxfWriter.class, // the writer
+        // "lxf/tokenizer-repp-hunpos-bn/orig.lxf"); // the input also used as output reference
+
+        testOneWay(LxfReader.class, // the reader
+                LxfWriter.class, // the writer
+                "lxf/tokenizer-repp-hunpos-bn/ref.lxf", "lxf/tokenizer-repp-hunpos-bn/orig.lxf");
     }
 }
