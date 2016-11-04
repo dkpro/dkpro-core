33feacc3a1b4bd76b982f76987b6dc5aaa38774b
maxxkia <masoud.kiaeeha@gmail.com>
2016-11-04 11:37:59 +0100

    #978 Added license header
    - Added license header
    - Fixed copy/paste bug in test units

--- dkpro-core-testing-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/testing/validation/checks/NoZeroSizeTokenAndSentenceCheckTest.java
+++ dkpro-core-testing-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/testing/validation/checks/NoZeroSizeTokenAndSentenceCheckTest.java
@@ -1,7 +1,25 @@
+/*
+ * Copyright 2015
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.tudarmstadt.ukp.dkpro.core.testing.validation.checks;
 
 import static de.tudarmstadt.ukp.dkpro.core.testing.validation.Message.Level.ERROR;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 import java.util.List;
 
@@ -24,7 +42,7 @@ public class NoZeroSizeTokenAndSentenceCheckTest
         jcas.setDocumentText("test");
         new Token(jcas, 1, 1).addToIndexes();
 
-        CasValidator validator = new CasValidator(PosAttachedToTokenCheck.class);
+        CasValidator validator = new CasValidator(NoZeroSizeTokensAndSentencesCheck.class);
         List<Message> messages = validator.analyze(jcas);
 
         messages.forEach(m -> System.out.println(m));
@@ -40,7 +58,7 @@ public class NoZeroSizeTokenAndSentenceCheckTest
         jcas.setDocumentText("test");
         new Sentence(jcas, 3,3).addToIndexes();
 
-        CasValidator validator = new CasValidator(PosAttachedToTokenCheck.class);
+        CasValidator validator = new CasValidator(NoZeroSizeTokensAndSentencesCheck.class);
         List<Message> messages = validator.analyze(jcas);
 
         messages.forEach(m -> System.out.println(m));
@@ -57,11 +75,11 @@ public class NoZeroSizeTokenAndSentenceCheckTest
         new Sentence(jcas, 0,4).addToIndexes();
         new Token(jcas, 1, 2).addToIndexes();
 
-        CasValidator validator = new CasValidator(PosAttachedToTokenCheck.class);
+        CasValidator validator = new CasValidator(NoZeroSizeTokensAndSentencesCheck.class);
         List<Message> messages = validator.analyze(jcas);
 
         messages.forEach(m -> System.out.println(m));
 
-        assertTrue(messages.stream().anyMatch(m -> m.level == ERROR));
+        assertFalse(messages.stream().anyMatch(m -> m.level == ERROR));
     }
 }
