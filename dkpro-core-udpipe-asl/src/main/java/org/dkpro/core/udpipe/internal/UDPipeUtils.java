ae118a802578d9fe45981f664213369a01f91459
Richard Eckart de Castilho <richard.eckart@gmail.com>
2016-11-05 23:19:11 +0100

    #979 - Integrate UDPipe - Better packaging of binaries - Make use of RuntimeProvider

--- dkpro-core-udpipe-asl/src/main/java/org/dkpro/core/udpipe/internal/UDPipeUtils.java
+++ dkpro-core-udpipe-asl/src/main/java/org/dkpro/core/udpipe/internal/UDPipeUtils.java
@@ -1,13 +1,11 @@
 package org.dkpro.core.udpipe.internal;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
-import de.tudarmstadt.ukp.dkpro.core.api.resources.PlatformDetector;
-import de.tudarmstadt.ukp.dkpro.core.api.resources.ResourceUtils;
+import de.tudarmstadt.ukp.dkpro.core.api.resources.RuntimeProvider;
 
 public class UDPipeUtils
 {
@@ -19,16 +17,12 @@ public class UDPipeUtils
             return;
         }
 
-        PlatformDetector pd = new PlatformDetector();
-        
-        String platform = pd.getPlatformId();
-
-        File binFolder = ResourceUtils.getClasspathAsFolder(
-                "classpath*:org/dkpro/core/udpipe/lib/lib/bin/bin-" + platform, true);
+        RuntimeProvider runtimeProvider = new RuntimeProvider(
+                "classpath:/org/dkpro/core/udpipe/bin/");
         
         // See https://github.com/ufal/udpipe/issues/10
         String libname = System.mapLibraryName("udpipe_java");
-        Files.copy(new File(binFolder, libname).toPath(),
+        Files.copy(runtimeProvider.getFile(libname).toPath(),
                 Paths.get(libname), StandardCopyOption.REPLACE_EXISTING);
     }
 }
