ce4db5a5a701e0e1e172b5509e77e7dd27c26b74
Richard Eckart de Castilho <richard.eckart@gmail.com>
2016-12-25 11:02:59 +0100

    #979 - Integrate UDPipe - Fix unit test

--- dkpro-core-udpipe-asl/src/test/java/org/dkpro/core/udpipe/UDPipeParserTest.java
+++ dkpro-core-udpipe-asl/src/test/java/org/dkpro/core/udpipe/UDPipeParserTest.java
@@ -28,33 +28,32 @@ import org.junit.Test;
 
 import de.tudarmstadt.ukp.dkpro.core.api.syntax.type.dependency.Dependency;
 import de.tudarmstadt.ukp.dkpro.core.testing.AssertAnnotations;
+import de.tudarmstadt.ukp.dkpro.core.testing.AssumeResource;
 import de.tudarmstadt.ukp.dkpro.core.testing.DkproTestContext;
 import de.tudarmstadt.ukp.dkpro.core.testing.TestRunner;
 
 public class UDPipeParserTest
 {
-    
     @Test
     public void testNorwegian()
         throws Exception
     {
         JCas jcas = runTest("no", null, "Nichlas Sjøstedt Halvorsen har ikke angret.");
 
-        String[] dependencies = {"[  0,  7]Dependency(nsubj) D[0,7](Nichlas) G[36,43](angret.)",
-                                 "[  8, 16]Dependency(name) D[8,16](Sjøstedt) G[0,7](Nichlas)",
-                                 "[ 17, 26]Dependency(name) D[17,26](Halvorsen) G[0,7](Nichlas)",
-                                 "[ 27, 30]Dependency(aux) D[27,30](har) G[36,43](angret.)",
-                                 "[ 31, 35]Dependency(neg) D[31,35](ikke) G[36,43](angret.)",
-                                 "[ 36, 43]ROOT(advmod) D[36,43](angret.) G[36,43](angret.)"};
+        String[] dependencies = {
+                "[  0,  7]NSUBJ(nsubj) D[0,7](Nichlas) G[36,43](angret.)",
+                "[  8, 16]Dependency(name) D[8,16](Sjøstedt) G[0,7](Nichlas)",
+                "[ 17, 26]Dependency(name) D[17,26](Halvorsen) G[0,7](Nichlas)",
+                "[ 27, 30]AUX0(aux) D[27,30](har) G[36,43](angret.)",
+                "[ 31, 35]NEG(neg) D[31,35](ikke) G[36,43](angret.)",
+                "[ 36, 43]ROOT(advmod) D[36,43](angret.) G[36,43](angret.)"};
 
-        //String[] unmappedDep = {};
+        // String[] unmappedDep = {};
 
         AssertAnnotations.assertDependencies(dependencies, select(jcas, Dependency.class));
-        //AssertAnnotations.assertTagsetMapping(POS.class, "ud", unmappedDep, jcas);
-        //AssertAnnotations.assertTagsetMapping(Dependency.class, "universal", unmappedDep, jcas);
+        // AssertAnnotations.assertTagsetMapping(Dependency.class, "universal", unmappedDep, jcas);
     }
 
-    
     @Test
     public void testEnglish()
         throws Exception
@@ -85,14 +84,13 @@ public class UDPipeParserTest
         //String[] unmappedDep = {};
 
         AssertAnnotations.assertDependencies(dependencies, select(jcas, Dependency.class));
-        //AssertAnnotations.assertTagsetMapping(POS.class, "ud", unmappedDep, jcas);
         //AssertAnnotations.assertTagsetMapping(Dependency.class, "universal", unmappedDep, jcas);
     }
 
     private JCas runTest(String aLanguage, String aVariant, String aText, Object... aExtraParams)
         throws Exception
     {
-        //AssumeResource.assumeResource(UDPipeParser.class, "depparser", aLanguage, "default");
+        AssumeResource.assumeResource(UDPipeParser.class, "multiple", aLanguage, "ud");
         
         AggregateBuilder aggregate = new AggregateBuilder();
         
--- dkpro-core-udpipe-asl/src/test/java/org/dkpro/core/udpipe/UDPosTaggerTest.java
+++ dkpro-core-udpipe-asl/src/test/java/org/dkpro/core/udpipe/UDPosTaggerTest.java
@@ -37,10 +37,12 @@ public class UDPosTaggerTest
     public void testNorwegian()
         throws Exception
     {
-        // N.B. This test is incomplete as I am not sure how exactly the POS should be.
-        runTest("no", null, "Magnus Carlsen trengte bare de fire partiene med lynsjakk for å slå utfordreren Sergej Karjakin.",
-                new String[] { "PROPN", "PROPN", "VERB", "ADV", "DET", "NUM", "NOUN", "ADP", "NOUN", "ADP", "PART", "VERB", "NOUN", "PROPN", "PROPN" },
-                new String[] { "POS", "POS", "POS", "POS", "POS", "POS", "POS", "POS", "POS", "POS", "POS", "POS", "POS", "POS", "POS" });
+        runTest("no", null,
+                "Magnus Carlsen trengte bare de fire partiene med lynsjakk for å slå utfordreren Sergej Karjakin.",
+                new String[] { "PROPN", "PROPN", "VERB", "ADV", "DET", "NUM", "NOUN", "ADP", "NOUN",
+                        "ADP", "PART", "VERB", "NOUN", "PROPN", "PROPN" },
+                new String[] { "PROPN", "PROPN", "VERB", "ADV", "DET", "NUM", "NOUN", "ADP", "NOUN",
+                        "ADP", "PART", "VERB", "NOUN", "PROPN", "PROPN" });
     }
     
     @Test
