142fdf42452a1a6f38d3dde949182f0004906b58
Richard Eckart de Castilho <richard.eckart@gmail.com>
2017-01-03 16:31:21 +0100

    #1008 - Add support for RDF - Fix dependency issues

--- dkpro-core-io-rdf-asl/src/main/java/org/dkpro/core/io/rdf/internal/Rdf2Uima.java
+++ dkpro-core-io-rdf-asl/src/main/java/org/dkpro/core/io/rdf/internal/Rdf2Uima.java
@@ -24,7 +24,7 @@ import java.util.Map;
 import java.util.Set;
 
 import org.apache.commons.collections4.iterators.IteratorIterable;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.jena.ontology.OntResource;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.Property;
