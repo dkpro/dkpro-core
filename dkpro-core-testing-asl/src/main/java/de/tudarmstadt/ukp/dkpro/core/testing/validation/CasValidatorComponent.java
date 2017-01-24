704edd33af56ce5ed962b6141d553eb4231543c2
Richard Eckart de Castilho <richard.eckart@gmail.com>
2017-01-24 15:43:16 +0100

    #989 - Implement CasValidatorComponent - Use UIMA logging instead of log4j - Fix parameter name - Fix formatting

--- dkpro-core-testing-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/testing/validation/CasValidatorComponent.java
+++ dkpro-core-testing-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/testing/validation/CasValidatorComponent.java
@@ -17,7 +17,6 @@
  */
 package de.tudarmstadt.ukp.dkpro.core.testing.validation;
 
-import org.apache.log4j.Logger;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.component.JCasConsumer_ImplBase;
 import org.apache.uima.fit.descriptor.ConfigurationParameter;
@@ -26,47 +25,47 @@ import org.apache.uima.jcas.JCas;
 import java.util.List;
 
 public class CasValidatorComponent
-        extends JCasConsumer_ImplBase
+    extends JCasConsumer_ImplBase
 {
-    private static Logger logger = Logger.getLogger(CasValidatorComponent.class);
     private static CasValidator validator = CasValidator.createWithAllChecks();
 
-    public static final String PARAM_STRICT_CHECK = "false";
-    @ConfigurationParameter(name = PARAM_STRICT_CHECK, mandatory = false, defaultValue = "false", description =
-            "if set to true component will throw an exception when cas contains errors.")
+    /**
+     * If set to true component will throw an exception when cas contains errors.
+     */
+    public static final String PARAM_STRICT_CHECK = "strictCheck";
+    @ConfigurationParameter(name = PARAM_STRICT_CHECK, mandatory = false, defaultValue = "false")
     protected boolean strictCheck;
 
     @Override
     public void process(JCas aJCas)
-            throws AnalysisEngineProcessException {
-        logger.info("CasValidation process started.");
+        throws AnalysisEngineProcessException
+    {
+        getLogger().info("CasValidation process started.");
         List<Message> messages = validator.analyze(aJCas);
-        logger.info("CasValidation process finished.");
-        if (messages.isEmpty())
-        {
-            logger.info("CasValidation was successful!");
-        } else
-        {
-            logger.warn("CasValidator found " + messages.size() + " issues.");
-            for (Message message : messages)
-            {
-                switch (message.level)
-                {
-                    case INFO:
-                        logger.info(message.message);
-                        break;
-                    case ERROR:
-                        logger.error(message.message);
-                        break;
-                    default:
-                        throw new IllegalArgumentException("Unexpected value of message.level [" + message.level + "] encountered!");
+        getLogger().info("CasValidation process finished.");
+        if (messages.isEmpty()) {
+            getLogger().info("CasValidation was successful!");
+        }
+        else {
+            getLogger().warn("CasValidator found " + messages.size() + " issues.");
+            for (Message message : messages) {
+                switch (message.level) {
+                case INFO:
+                    getLogger().info(message.message);
+                    break;
+                case ERROR:
+                    getLogger().error(message.message);
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unexpected value of message.level ["
+                            + message.level + "] encountered!");
                 }
             }
 
-            if (strictCheck)
-            {
-                throw new IllegalArgumentException(messages.size() + " errors were found in the jcas. You should fix them " +
-                        "to be able to run the pipeline.");
+            if (strictCheck) {
+                throw new IllegalArgumentException(
+                        messages.size() + " errors were found in the jcas. You should fix them "
+                                + "to be able to run the pipeline.");
             }
         }
     }
