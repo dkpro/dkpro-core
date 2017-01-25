4faa2a6dbe581dea4ac418d6fbc6451e15560067
Richard Eckart de Castilho <richard.eckart@gmail.com>
2017-01-26 00:05:12 +0100

    #1004 - Upgrade to OpenNLP 1.7.0 - Lemmatizer was not working correctly because lemma encoding and decoding steps had been missing
    Fixes #1004

--- dkpro-core-opennlp-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/opennlp/OpenNlpLemmatizerTrainerTest.java
+++ dkpro-core-opennlp-asl/src/test/java/de/tudarmstadt/ukp/dkpro/core/opennlp/OpenNlpLemmatizerTrainerTest.java
@@ -40,6 +40,7 @@ import de.tudarmstadt.ukp.dkpro.core.eval.EvalUtil;
 import de.tudarmstadt.ukp.dkpro.core.eval.model.Span;
 import de.tudarmstadt.ukp.dkpro.core.eval.report.Result;
 import de.tudarmstadt.ukp.dkpro.core.io.conll.Conll2006Reader;
+import de.tudarmstadt.ukp.dkpro.core.io.conll.Conll2006Writer;
 import de.tudarmstadt.ukp.dkpro.core.testing.DkproTestContext;
 
 public class OpenNlpLemmatizerTrainerTest
@@ -53,30 +54,40 @@ public class OpenNlpLemmatizerTrainerTest
         
         // Obtain dataset
         DatasetFactory loader = new DatasetFactory(cache);
-        Dataset ds = loader.load("gum-en-conll-2.3.2");
+        Dataset ds = loader.load("gum-en-conll-3.0.0");
         Split split = ds.getSplit(0.8);
+        
+        // Parameters
+        boolean useExtendedTTTagset = false;
+        int iterations = 10;
         
         // Train model
         System.out.println("Training model from training data");
         CollectionReaderDescription trainReader = createReaderDescription(
                 Conll2006Reader.class,
                 Conll2006Reader.PARAM_PATTERNS, split.getTrainingFiles(),
-                Conll2006Reader.PARAM_USE_CPOS_AS_POS, true,
+                Conll2006Reader.PARAM_USE_CPOS_AS_POS, useExtendedTTTagset,
                 Conll2006Reader.PARAM_LANGUAGE, ds.getLanguage());
         
         AnalysisEngineDescription trainer = createEngineDescription(
                 OpenNlpLemmatizerTrainer.class,
                 OpenNlpLemmatizerTrainer.PARAM_TARGET_LOCATION, new File(targetFolder, "model.bin"),
                 OpenNlpLemmatizerTrainer.PARAM_LANGUAGE, ds.getLanguage(),
-                OpenNlpLemmatizerTrainer.PARAM_ITERATIONS, 10);
-        
-        SimplePipeline.runPipeline(trainReader, trainer);
+                OpenNlpLemmatizerTrainer.PARAM_ITERATIONS, iterations);
+
+        AnalysisEngineDescription trainWriter = createEngineDescription(
+                Conll2006Writer.class,
+                Conll2006Writer.PARAM_SINGULAR_TARGET, true,
+                Conll2006Writer.PARAM_TARGET_LOCATION, new File(targetFolder, "in.conll"));
+
+        SimplePipeline.runPipeline(trainReader, trainer, trainWriter);
         
         // Apply model and collect labels
         System.out.println("Applying model to test data");
         CollectionReaderDescription testReader = createReaderDescription(
                 Conll2006Reader.class,
                 Conll2006Reader.PARAM_PATTERNS, split.getTestFiles(),
+                Conll2006Reader.PARAM_USE_CPOS_AS_POS, useExtendedTTTagset,
                 Conll2006Reader.PARAM_READ_LEMMA, false,
                 Conll2006Reader.PARAM_LANGUAGE, ds.getLanguage());
         
@@ -84,7 +95,12 @@ public class OpenNlpLemmatizerTrainerTest
                 OpenNlpLemmatizer.class,
                 OpenNlpLemmatizer.PARAM_MODEL_LOCATION, new File(targetFolder, "model.bin"));
 
-        List<Span<String>> actual = EvalUtil.loadSamples(iteratePipeline(testReader, lemmatizer),
+        AnalysisEngineDescription testWriter = createEngineDescription(
+                Conll2006Writer.class,
+                Conll2006Writer.PARAM_SINGULAR_TARGET, true,
+                Conll2006Writer.PARAM_TARGET_LOCATION, new File(targetFolder, "out.conll"));
+        
+        List<Span<String>> actual = EvalUtil.loadSamples(iteratePipeline(testReader, lemmatizer, testWriter),
                 Lemma.class, lemma -> {
                     return lemma.getValue();
                 });
@@ -100,11 +116,11 @@ public class OpenNlpLemmatizerTrainerTest
 
         Result results = EvalUtil.dumpResults(targetFolder, expected, actual);
         
-        assertEquals(0.809843, results.getFscore(), 0.0001);
-        assertEquals(0.809843, results.getPrecision(), 0.0001);
-        assertEquals(0.809843, results.getRecall(), 0.0001);
+        assertEquals(0.961978, results.getFscore(), 0.0001);
+        assertEquals(0.961978, results.getPrecision(), 0.0001);
+        assertEquals(0.961978, results.getRecall(), 0.0001);
     }
-    
+
     @Rule
     public DkproTestContext testContext = new DkproTestContext();
 }
