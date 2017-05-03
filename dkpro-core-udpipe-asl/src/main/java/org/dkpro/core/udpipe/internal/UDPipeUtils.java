54caeff0c8a2f96a752cf20a0f11c9c5dd93e4d7
Richard Eckart de Castilho <richard.eckart@gmail.com>
2017-05-03 13:48:45 +0200

    #1039 - Upgrade to UDPipe 1.1.0 - Use new 1.1.0 API for setting the native library path

--- dkpro-core-udpipe-asl/src/main/java/org/dkpro/core/udpipe/internal/UDPipeUtils.java
+++ dkpro-core-udpipe-asl/src/main/java/org/dkpro/core/udpipe/internal/UDPipeUtils.java
@@ -18,10 +18,7 @@
 package org.dkpro.core.udpipe.internal;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-
+import cz.cuni.mff.ufal.udpipe.udpipe_java;
 import de.tudarmstadt.ukp.dkpro.core.api.resources.RuntimeProvider;
 
 public class UDPipeUtils
@@ -36,10 +33,8 @@ public class UDPipeUtils
 
         RuntimeProvider runtimeProvider = new RuntimeProvider(
                 "classpath:/org/dkpro/core/udpipe/bin/");
-        
-        // See https://github.com/ufal/udpipe/issues/10
+
         String libname = System.mapLibraryName("udpipe_java");
-        Files.copy(runtimeProvider.getFile(libname).toPath(),
-                Paths.get(libname), StandardCopyOption.REPLACE_EXISTING);
+        udpipe_java.setLibraryPath(runtimeProvider.getFile(libname).toString());
     }
 }
