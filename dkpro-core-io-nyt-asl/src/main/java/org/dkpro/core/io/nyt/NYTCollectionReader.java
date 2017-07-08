c0c1c2c4298c669b7e68a6cec3753d777ebc2215
Richard Eckart de Castilho <richard.eckart@gmail.com>
2017-07-08 23:12:04 +0200

    #1049 - Add reader for the New York Times Annotated Corpus
    - Added missing license headers
    - Formatting
    - NYTArticleMetaData has to inherit from AnnotationBase because there can only be one DocumentAnnotation per CAS (the DKPro Core DocumentMetaData annotation)
    - Removed unused dependencies
    - Added missing dependencies

--- dkpro-core-io-nyt-asl/src/main/java/org/dkpro/core/io/nyt/NYTCollectionReader.java
+++ dkpro-core-io-nyt-asl/src/main/java/org/dkpro/core/io/nyt/NYTCollectionReader.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Technische Universität Darmstadt under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The Technische Universität Darmstadt 
+ * licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.
+ *  
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.dkpro.core.io.nyt;
 
 import java.io.IOException;
@@ -73,16 +90,17 @@ public class NYTCollectionReader extends JCasResourceCollectionReader_ImplBase {
 
 	private NYTArticleMetaData createNYTArticleMetaData(JCas aJCas, NYTCorpusDocument doc) {
 		NYTArticleMetaData articleMetaData = new NYTArticleMetaData(aJCas);
-		articleMetaData.setLanguage(java.util.Locale.US.toString());
 		articleMetaData.setGuid(doc.getGuid());
 
 		URL alternateUrl = doc.getAlternateURL();
-		if (alternateUrl != null)
-			articleMetaData.setAlternateUrl(alternateUrl.toString());
+		if (alternateUrl != null) {
+            articleMetaData.setAlternateUrl(alternateUrl.toString());
+        }
 
 		URL url = doc.getUrl();
-		if (url != null)
-			articleMetaData.setAlternateUrl(url.toString());
+		if (url != null) {
+            articleMetaData.setAlternateUrl(url.toString());
+        }
 
 		articleMetaData.setAuthor(doc.getNormalizedByline());
 		articleMetaData.setColumnName(doc.getColumnName());
--- dkpro-core-io-nyt-asl/src/test/java/org/dkpro/core/io/nyt/NYTCollectionReaderTest.java
+++ dkpro-core-io-nyt-asl/src/test/java/org/dkpro/core/io/nyt/NYTCollectionReaderTest.java
@@ -1,10 +1,22 @@
+/*
+ * Licensed to the Technische Universität Darmstadt under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The Technische Universität Darmstadt 
+ * licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.
+ *  
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.dkpro.core.io.nyt;
 
-import static org.junit.Assert.fail;
-
-import java.io.IOException;
-
-import org.apache.uima.UIMAException;
 import org.apache.uima.analysis_engine.AnalysisEngine;
 import org.apache.uima.collection.CollectionReader;
 import org.apache.uima.fit.component.CasDumpWriter;
@@ -12,44 +24,30 @@ import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
 import org.apache.uima.fit.factory.TypeSystemDescriptionFactory;
 import org.apache.uima.fit.pipeline.SimplePipeline;
-import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.resource.metadata.TypeSystemDescription;
 import org.dkpro.core.io.nyt.NYTCollectionReader;
 import org.junit.Test;
 
+public class NYTCollectionReaderTest
+{
+    @Test
+    public void test() throws Exception
+    {
+        final String DATA_PATH = "src/test/resources/data/";
 
-public class NYTCollectionReaderTest {
+        final TypeSystemDescription typeSystemDescription = TypeSystemDescriptionFactory
+                .createTypeSystemDescription("desc.type.NYTArticleMetaData");
 
-	@Test
-	public void test() {
-		
-		final String DATA_PATH = "src/test/resources/data/";
-		//final String DATA_PATH = "/home/matthias/Entwicklung/UKP/corpora/nyt_corpus/data/";
+        CollectionReader articleReader = CollectionReaderFactory.createReader(
+                NYTCollectionReader.class, typeSystemDescription,
+                NYTCollectionReader.PARAM_SOURCE_LOCATION, DATA_PATH,
+                NYTCollectionReader.PARAM_PATTERNS, "[+]/**/*.xml",
+                NYTCollectionReader.PARAM_LANGUAGE, java.util.Locale.US.toString(),
+                NYTCollectionReader.PARAM_OFFSET, 0);
 
-		try {
+        AnalysisEngine extractor = AnalysisEngineFactory.createEngine(CasDumpWriter.class,
+                CasDumpWriter.PARAM_OUTPUT_FILE, "-");
 
-			final TypeSystemDescription typeSystemDescription = TypeSystemDescriptionFactory
-					.createTypeSystemDescription("desc.type.NYTArticleMetaData");
-
-			CollectionReader articleReader = CollectionReaderFactory.createReader(NYTCollectionReader.class,
-					typeSystemDescription, 
-					NYTCollectionReader.PARAM_SOURCE_LOCATION, DATA_PATH,
-					NYTCollectionReader.PARAM_PATTERNS, "[+]/**/*.xml",
-					NYTCollectionReader.PARAM_LANGUAGE, java.util.Locale.US.toString(),
-					NYTCollectionReader.PARAM_OFFSET, 0);
-
-			AnalysisEngine extractor = AnalysisEngineFactory.createEngine(CasDumpWriter.class,
-					CasDumpWriter.PARAM_OUTPUT_FILE, "-");
-
-			SimplePipeline.runPipeline(articleReader, extractor);
-
-		} catch (IOException | ResourceInitializationException e) {
-			e.printStackTrace();
-			fail();
-		} catch (UIMAException e) {
-			e.printStackTrace();
-			fail();
-		}
-	}
-
+        SimplePipeline.runPipeline(articleReader, extractor);
+    }
 }
