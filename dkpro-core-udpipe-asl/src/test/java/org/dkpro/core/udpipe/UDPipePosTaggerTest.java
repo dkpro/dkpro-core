d3dcc44a6ef3e1229c60c5aa30c7dd316a71e206
Richard Eckart de Castilho <richard.eckart@gmail.com>
2017-08-09 16:33:34 +0200

    #414 - PRN and AUX are reserved names on Windows
    - Adjust test reference data of serveral tests

--- dkpro-core-udpipe-asl/src/test/java/org/dkpro/core/udpipe/UDPipePosTaggerTest.java
+++ dkpro-core-udpipe-asl/src/test/java/org/dkpro/core/udpipe/UDPipePosTaggerTest.java
@@ -43,8 +43,9 @@ public class UDPipePosTaggerTest
                 "Magnus Carlsen trengte bare de fire partiene med lynsjakk for å slå utfordreren Sergej Karjakin.",
                 new String[] { "PROPN", "PROPN", "VERB", "ADV", "DET", "NUM", "NOUN", "ADP", "NOUN",
                         "ADP", "PART", "VERB", "NOUN", "PROPN", "PROPN" },
-                new String[] { "PROPN", "PROPN", "VERB", "ADV", "DET", "NUM", "NOUN", "ADP", "NOUN",
-                        "ADP", "PART", "VERB", "NOUN", "PROPN", "PROPN" });
+                new String[] { "POS_PROPN", "POS_PROPN", "POS_VERB", "POS_ADV", "POS_DET",
+                        "POS_NUM", "POS_NOUN", "POS_ADP", "POS_NOUN", "POS_ADP", "POS_PART",
+                        "POS_VERB", "POS_NOUN", "POS_PROPN", "POS_PROPN" });
     }
     
     @Test
@@ -53,15 +54,15 @@ public class UDPipePosTaggerTest
     {
         runTest("en", null, "This is a test .",
                 new String[] { "DT",   "VBZ", "DT",  "NN",   "." },
-                new String[] { "DET",  "VERB",   "DET", "NOUN",   "PUNCT" });
+                new String[] { "POS_DET", "POS_VERB", "POS_DET", "POS_NOUN", "POS_PUNCT" });
 
         runTest("en", null, "A neural net .",
                 new String[] { "DT",  "JJ",     "NN",  "." },
-                new String[] { "DET", "ADJ",    "NOUN",  "PUNCT" });
+                new String[] { "POS_DET", "POS_ADJ", "POS_NOUN", "POS_PUNCT" });
 
         runTest("en", null, "John is purchasing oranges .",
                 new String[] { "NNP",  "VBZ", "VBG",      "NNS",    "." },
-                new String[] { "PROPN",   "VERB",   "VERB",        "NOUN",     "PUNCT" });
+                new String[] { "POS_PROPN", "POS_VERB", "POS_VERB", "POS_NOUN", "POS_PUNCT" });
     }
         
     private JCas runTest(String language, String aVariant, String testDocument, String[] tags,
