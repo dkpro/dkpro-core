6bedb10aaf9ed8fc06c3db73db5099c10ba4c546
Richard Eckart de Castilho <richard.eckart@gmail.com>
2018-03-30 20:13:46 -0300

    No issue. License header updates.
    - Update currentYear
    - Remove spurious space character in GPL headers causing problems when auto-formatting

--- dkpro-core-lingpipe-gpl/src/main/java/de/tudarmstadt/ukp/dkpro/core/lingpipe/LingPipePosTagger.java
+++ dkpro-core-lingpipe-gpl/src/main/java/de/tudarmstadt/ukp/dkpro/core/lingpipe/LingPipePosTagger.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007-2017
+ * Copyright 2007-2018
  * Ubiquitous Knowledge Processing (UKP) Lab
  * Technische Universität Darmstadt
  *
@@ -14,7 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see http://www.gnu.org/licenses/.
+ * along with this program. If not, see http://www.gnu.org/licenses/.
  */
 package de.tudarmstadt.ukp.dkpro.core.lingpipe;
 
