7c584a388518d9439ffd6c35a0c9708d79404a93
Richard Eckart de Castilho <richard.eckart@gmail.com>
2018-07-09 23:55:33 +0200

    #1261 - Upgrade PdfReader to a more recent PdfBox version
    - Moved PDF parsing code to "internal" package
    - Adjusted code to PDFBox 2.0.9

--- dkpro-core-io-pdf-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/pdf/internal/Trie.java
+++ dkpro-core-io-pdf-asl/src/main/java/de/tudarmstadt/ukp/dkpro/core/io/pdf/internal/Trie.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.tudarmstadt.ukp.dkpro.core.io.pdf;
+package de.tudarmstadt.ukp.dkpro.core.io.pdf.internal;
 
 import java.util.ArrayList;
 import java.util.Collection;
