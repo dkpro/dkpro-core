3128fb30e6819fc8b6d02408ccbd9007dc42aebb
Richard Eckart de Castilho <richard.eckart@gmail.com>
2019-04-26 17:53:42 +0200

    #186 - Change artifactId to "dkpro-core-XXX"
    - Changed artifactId and package names for dkpro-core-sfst-asl

--- dkpro-core-sfst-gpl/src/main/java/org/dkpro/core/sfst/SfstAnnotator.java
+++ dkpro-core-sfst-gpl/src/main/java/org/dkpro/core/sfst/SfstAnnotator.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see http://www.gnu.org/licenses/.
  */
-package de.tudarmstadt.ukp.dkpro.core.sfst;
+package org.dkpro.core.sfst;
 
 import static org.apache.uima.fit.util.JCasUtil.select;
 import static org.apache.uima.fit.util.JCasUtil.selectCovered;
--- dkpro-core-sfst-gpl/src/test/java/org/dkpro/core/sfst/SfstAnnotatorTest.java
+++ dkpro-core-sfst-gpl/src/test/java/org/dkpro/core/sfst/SfstAnnotatorTest.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see http://www.gnu.org/licenses/.
  */
-package de.tudarmstadt.ukp.dkpro.core.sfst;
+package org.dkpro.core.sfst;
 
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngine;
 import static org.apache.uima.fit.util.JCasUtil.select;
@@ -26,6 +26,7 @@ import static org.dkpro.core.testing.AssertAnnotations.assertTagsetParser;
 
 import org.apache.uima.analysis_engine.AnalysisEngine;
 import org.apache.uima.jcas.JCas;
+import org.dkpro.core.sfst.SfstAnnotator;
 import org.dkpro.core.testing.DkproTestContext;
 import org.dkpro.core.testing.TestRunner;
 import org.junit.Rule;
