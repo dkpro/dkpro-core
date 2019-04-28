8769f720577ed32f088a5ecc56c0d053f080791c
Richard Eckart de Castilho <richard.eckart@gmail.com>
2019-04-28 18:50:37 +0200

    #186 - Change artifactId to "dkpro-core-XXX"
    - Changed artifactId and package names for dkpro-core-doc-asl

--- dkpro-core-doc/src/main/groovy/org/dkpro/core/doc/DocumentationBuilder.groovy
+++ dkpro-core-doc/src/main/groovy/org/dkpro/core/doc/DocumentationBuilder.groovy
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.tudarmstadt.ukp.dkpro.core.doc;
+package org.dkpro.core.doc;
 
 import static groovy.io.FileType.FILES;
 import groovy.json.*;
