3c7e783d11793cbbb076280870cfd687d24aa4ab
Richard Eckart de Castilho <richard.eckart@gmail.com>
2022-12-31 15:30:56 +0100

    #1555 - Remove constraints parameter on token merger
    - Removed the parameter
    - Changed some tests such that they work without JXPath

--- dkpro-core-tokit-asl/src/test/java/org/dkpro/core/tokit/TokenMergerTest.java
+++ dkpro-core-tokit-asl/src/test/java/org/dkpro/core/tokit/TokenMergerTest.java
@@ -18,28 +18,14 @@
 package org.dkpro.core.tokit;
 
 import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngine;
-import static org.apache.uima.fit.util.JCasUtil.select;
-import static org.apache.uima.fit.util.JCasUtil.toText;
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-import org.apache.commons.jxpath.ClassFunctions;
-import org.apache.commons.jxpath.DynamicPropertyHandler;
-import org.apache.commons.jxpath.ExpressionContext;
-import org.apache.commons.jxpath.JXPathContext;
-import org.apache.commons.jxpath.JXPathIntrospector;
 import org.apache.uima.UIMAException;
 import org.apache.uima.analysis_engine.AnalysisEngine;
 import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.CASException;
-import org.apache.uima.cas.Feature;
-import org.apache.uima.cas.FeatureStructure;
-import org.apache.uima.cas.Type;
-import org.apache.uima.cas.text.AnnotationFS;
 import org.apache.uima.fit.factory.JCasBuilder;
 import org.apache.uima.fit.factory.JCasFactory;
 import org.apache.uima.fit.util.CasUtil;
@@ -71,46 +57,48 @@ public class TokenMergerTest
         filter.process(jcas);
 
         assertEquals(asList("I", "love", "New York", "."),
-                pick(select(jcas, Token.class), "cas:text()"));
+                jcas.select(Token.class).map(Token::getCoveredText).collect(toList()));
     }
 
-    @Test
-    public void testWithConstraintMatch() throws Exception
-    {
-        AnalysisEngine filter = createEngine(TokenMerger.class, 
-                TokenMerger.PARAM_ANNOTATION_TYPE, NamedEntity.class, 
-                TokenMerger.PARAM_CONSTRAINT, ".[value = 'LOCATION']");
-
-        JCas jcas = initCas();
-        filter.process(jcas);
-
-        assertEquals(asList("I", "love", "New York", "."), toText(select(jcas, Token.class)));
-    }
-
-    @Test
-    public void testWithConstraintNoMatch() throws Exception
-    {
-        AnalysisEngine filter = createEngine(TokenMerger.class, 
-                TokenMerger.PARAM_ANNOTATION_TYPE, NamedEntity.class, 
-                TokenMerger.PARAM_CONSTRAINT, ".[value = 'PERSON']");
-
-        JCas jcas = initCas();
-        filter.process(jcas);
-
-        assertEquals(asList("I", "love", "New", "York", "."), toText(select(jcas, Token.class)));
-    }
+//    @Test
+//    public void testWithConstraintMatch() throws Exception
+//    {
+//        AnalysisEngine filter = createEngine(TokenMerger.class, 
+//                TokenMerger.PARAM_ANNOTATION_TYPE, NamedEntity.class, 
+//                TokenMerger.PARAM_CONSTRAINT, ".[value = 'LOCATION']");
+//
+//        JCas jcas = initCas();
+//        filter.process(jcas);
+//
+//        assertEquals(asList("I", "love", "New York", "."), toText(select(jcas, Token.class)));
+//    }
+//
+//    @Test
+//    public void testWithConstraintNoMatch() throws Exception
+//    {
+//        AnalysisEngine filter = createEngine(TokenMerger.class, 
+//                TokenMerger.PARAM_ANNOTATION_TYPE, NamedEntity.class, 
+//                TokenMerger.PARAM_CONSTRAINT, ".[value = 'PERSON']");
+//
+//        JCas jcas = initCas();
+//        filter.process(jcas);
+//
+//        assertEquals(asList("I", "love", "New", "York", "."), toText(select(jcas, Token.class)));
+//    }
 
     @Test
     public void testSimpleMergeLemmaJoin() throws Exception
     {
-        AnalysisEngine filter = createEngine(TokenMerger.class, TokenMerger.PARAM_ANNOTATION_TYPE,
-                NamedEntity.class, TokenMerger.PARAM_LEMMA_MODE, LemmaMode.JOIN);
+        AnalysisEngine filter = createEngine(
+                TokenMerger.class, //
+                TokenMerger.PARAM_ANNOTATION_TYPE, NamedEntity.class,  //
+                TokenMerger.PARAM_LEMMA_MODE, LemmaMode.JOIN);
 
         JCas jcas = initCas();
         filter.process(jcas);
 
         assertEquals(asList("I", "love", "new york", "."),
-                pick(select(jcas, Token.class), "./lemma/value"));
+                jcas.select(Token.class).map(t -> t.getLemma().getValue()).collect(toList()));
     }
 
     private JCas initCas() throws UIMAException
@@ -151,101 +139,6 @@ public class TokenMergerTest
         aToken.setLemma(lemma);
 
         return aToken;
-    }
-
-    // =============================================================================================
-    // == JXPath helper methods
-    // =============================================================================================
-
-    {
-        JXPathIntrospector.registerDynamicClass(FeatureStructure.class,
-                FeatureStructureHandler.class);
-    }
-
-    public static class FeatureStructureHandler
-        implements DynamicPropertyHandler
-    {
-        @Override
-        public String[] getPropertyNames(Object aObject)
-        {
-            FeatureStructure fs = (FeatureStructure) aObject;
-            Type t = fs.getType();
-            List<Feature> features = t.getFeatures();
-            String[] featureNames = new String[features.size()];
-
-            int i = 0;
-            for (Feature f : features) {
-                featureNames[i] = f.getShortName();
-                i++;
-            }
-            return featureNames;
-        }
-
-        @Override
-        public Object getProperty(Object aObject, String aPropertyName)
-        {
-            FeatureStructure fs = (FeatureStructure) aObject;
-            Feature f = fs.getType().getFeatureByBaseName(aPropertyName);
-            if (CAS.TYPE_NAME_BOOLEAN.equals(f.getRange().getName())) {
-                return fs.getBooleanValue(f);
-            }
-            else if (CAS.TYPE_NAME_BYTE.equals(f.getRange().getName())) {
-                return fs.getByteValue(f);
-            }
-            else if (CAS.TYPE_NAME_DOUBLE.equals(f.getRange().getName())) {
-                return fs.getDoubleValue(f);
-            }
-            else if (CAS.TYPE_NAME_FLOAT.equals(f.getRange().getName())) {
-                return fs.getFloatValue(f);
-            }
-            else if (CAS.TYPE_NAME_INTEGER.equals(f.getRange().getName())) {
-                return fs.getIntValue(f);
-            }
-            else if (CAS.TYPE_NAME_LONG.equals(f.getRange().getName())) {
-                return fs.getLongValue(f);
-            }
-            else if (CAS.TYPE_NAME_SHORT.equals(f.getRange().getName())) {
-                return fs.getShortValue(f);
-            }
-            else if (CAS.TYPE_NAME_STRING.equals(f.getRange().getName())) {
-                return fs.getStringValue(f);
-            }
-            else {
-                return fs.getFeatureValue(f);
-            }
-        }
-
-        @Override
-        public void setProperty(Object aObject, String aPropertyName, Object aValue)
-        {
-            throw new UnsupportedOperationException();
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    public static List<Object> pick(Collection<?> aContext, String aPath)
-    {
-        List<Object> result = new ArrayList<Object>();
-        for (Object a : aContext) {
-            JXPathContext ctx = JXPathContext.newContext(a);
-            ctx.setFunctions(new ClassFunctions(JXPathCasFunctions.class, "cas"));
-            result.addAll(ctx.selectNodes(aPath));
-        }
-        return result;
-    }
-
-    public static class JXPathCasFunctions
-    {
-        public static String text(ExpressionContext aCtx)
-        {
-            Object value = aCtx.getContextNodePointer().getValue();
-            if (value instanceof AnnotationFS) {
-                return ((AnnotationFS) value).getCoveredText();
-            }
-            else {
-                return String.valueOf(value);
-            }
-        }
     }
 
     @Rule
