400caa45f66284cb42121a801ec70df911fda68f
Richard Eckart de Castilho <richard.eckart@gmail.com>
2022-12-31 21:54:32 +0100

    No issue: HunPos tests shoud not run on modern OS X as we have no binary for those.

--- dkpro-core-matetools-gpl/src/test/java/org/dkpro/core/matetools/MateParserTest.java
+++ dkpro-core-matetools-gpl/src/test/java/org/dkpro/core/matetools/MateParserTest.java
@@ -290,7 +290,7 @@ public class MateParserTest
                     System.getProperty("os.name").toLowerCase(Locale.US).contains("win"));
             Assume.assumeTrue("HunPos does not run on OS X Catalina or higher",
                     System.getProperty("os.name").toLowerCase(Locale.US).contains("mac") &&
-                    !System.getProperty("os.version").matches("10\\.([0-9]|1[0-4]).*"));
+                    !System.getProperty("os.version").matches("1[0-9]\\..*|(10\\.([0-9]|1[0-4]).*)"));
             engines.add(createEngineDescription(HunPosTagger.class));
         }
         else {
