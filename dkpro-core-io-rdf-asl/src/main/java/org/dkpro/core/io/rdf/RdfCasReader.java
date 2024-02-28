9383d1ef5aa9686086f4c8bdeb659c07b7e83fac
Richard Eckart de Castilho <richard.eckart@gmail.com>
2024-02-28 20:36:08 +0100

    #1579 - Allow defining features as IRI features so they are not rendered as literal strings
    - Moved rdf module from experimental to main so it gets included into releases
    - Renamed RdfReader/RdfWriter to RdfCasReader/Writer
    - Added PARAM_IRI_FEATURES

--- dkpro-core-io-rdf-asl/src/main/java/org/dkpro/core/io/rdf/RdfCasReader.java
+++ dkpro-core-io-rdf-asl/src/main/java/org/dkpro/core/io/rdf/RdfCasReader.java
@@ -17,32 +17,26 @@
  */
 package org.dkpro.core.io.rdf;
 
-import static org.dkpro.core.api.resources.MappingProviderFactory.createPosMappingProvider;
+import static org.dkpro.core.api.resources.CompressionUtils.getInputStream;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.rdf.model.StmtIterator;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.RDFLanguages;
 import org.apache.jena.vocabulary.RDF;
 import org.apache.uima.UimaContext;
-import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.cas.CASException;
 import org.apache.uima.collection.CollectionException;
-import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.fit.descriptor.MimeTypeCapability;
 import org.apache.uima.fit.descriptor.ResourceMetaData;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.dkpro.core.api.io.JCasResourceCollectionReader_ImplBase;
-import org.dkpro.core.api.parameter.ComponentParameters;
 import org.dkpro.core.api.parameter.MimeTypes;
 import org.dkpro.core.api.resources.CompressionUtils;
-import org.dkpro.core.api.resources.MappingProvider;
 import org.dkpro.core.io.rdf.internal.Rdf2Uima;
 import org.dkpro.core.io.rdf.internal.RdfCas;
 
@@ -53,52 +47,19 @@ import eu.openminted.share.annotations.api.DocumentationResource;
  */
 @ResourceMetaData(name = "UIMA CAS RDF Reader")
 @DocumentationResource("${docbase}/format-reference.html#format-${command}")
-@MimeTypeCapability({MimeTypes.APPLICATION_X_UIMA_RDF})
-public class RdfReader
+@MimeTypeCapability({ MimeTypes.APPLICATION_X_UIMA_RDF })
+public class RdfCasReader
     extends JCasResourceCollectionReader_ImplBase
 {
-    /**
-     * Use this part-of-speech tag set to use to resolve the tag set mapping instead of using the
-     * tag set defined as part of the model meta data. This can be useful if a custom model is
-     * specified which does not have such meta data, or it can be used in readers.
-     */
-    public static final String PARAM_POS_TAG_SET = ComponentParameters.PARAM_POS_TAG_SET;
-    @ConfigurationParameter(name = PARAM_POS_TAG_SET, mandatory = false)
-    private String posTagset;
-
-    /**
-     * Enable/disable type mapping.
-     */
-    public static final String PARAM_MAPPING_ENABLED = ComponentParameters.PARAM_MAPPING_ENABLED;
-    @ConfigurationParameter(name = PARAM_MAPPING_ENABLED, mandatory = true, defaultValue = 
-            ComponentParameters.DEFAULT_MAPPING_ENABLED)
-    protected boolean mappingEnabled;
-
-    /**
-     * Load the part-of-speech tag to UIMA type mapping from this location instead of locating
-     * the mapping automatically.
-     */
-    public static final String PARAM_POS_MAPPING_LOCATION = 
-            ComponentParameters.PARAM_POS_MAPPING_LOCATION;
-    @ConfigurationParameter(name = PARAM_POS_MAPPING_LOCATION, mandatory = false)
-    private String posMappingLocation;
-    
-    private MappingProvider posMappingProvider;
-    
     private Resource res;
     private Model model;
     private StmtIterator contextIterator;
-    //private int inFileCount;
-    
+
     @Override
-    public void initialize(UimaContext aContext)
-        throws ResourceInitializationException
+    public void initialize(UimaContext aContext) throws ResourceInitializationException
     {
         super.initialize(aContext);
-        
-        posMappingProvider = createPosMappingProvider(this, posMappingLocation, posTagset,
-                getLanguage());
-        
+
         // Seek first article
         try {
             step();
@@ -107,54 +68,45 @@ public class RdfReader
             throw new ResourceInitializationException(e);
         }
     }
-    
+
     @Override
-    public void getNext(JCas aJCas)
-        throws IOException, CollectionException
+    public void getNext(JCas aJCas) throws IOException, CollectionException
     {
-        try {
-            posMappingProvider.configure(aJCas.getCas());
-        }
-        catch (AnalysisEngineProcessException e) {
-            throw new IOException(e);
-        }
-        
         initCas(aJCas, res);
-        
-        Statement context = contextIterator.next();
+
+        var context = contextIterator.next();
         try {
             Rdf2Uima.convert(context, aJCas);
         }
         catch (CASException e) {
             throw new CollectionException(e);
         }
-        
+
         // inFileCount++;
         step();
     }
-    
+
     private void closeAll()
     {
         res = null;
         contextIterator = null;
     }
-    
+
     @Override
     public void destroy()
     {
         closeAll();
         super.destroy();
     }
-    
+
     @Override
-    public boolean hasNext()
-        throws IOException, CollectionException
+    public boolean hasNext() throws IOException, CollectionException
     {
         // If there is still an iterator, then there is still data. This requires that we call
         // step() already during initialization.
         return contextIterator != null;
     }
-    
+
     /**
      * Seek article in file. Stop once article element has been found without reading it.
      */
@@ -168,13 +120,12 @@ public class RdfReader
                     // There are still resources left to read
                     res = nextFile();
                     // inFileCount = 0;
-                    try (InputStream is = CompressionUtils.getInputStream(res.getLocation(),
-                            res.getInputStream())) {
+                    try (var is = getInputStream(res.getLocation(), res.getInputStream())) {
                         model = ModelFactory.createOntologyModel();
                         RDFDataMgr.read(model, is, RDFLanguages.filenameToLang(
                                 CompressionUtils.stripCompressionExtension(res.getLocation())));
                     }
-                    contextIterator = model.listStatements(null, RDF.type, 
+                    contextIterator = model.listStatements(null, RDF.type,
                             model.createResource(RdfCas.TYPE_VIEW));
                 }
                 else {
@@ -182,11 +133,11 @@ public class RdfReader
                     return;
                 }
             }
-            
+
             if (contextIterator.hasNext()) {
                 return;
             }
-            
+
             // End of file reached
             closeAll();
         }
